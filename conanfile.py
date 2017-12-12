2303d2057e2e1f40871db9127e8f80b6a31f5e04
SSE4 <tomskside@gmail.com>
2017-12-12 18:45:58 +0700

    - initial prototype

--- conanfile.py
+++ conanfile.py
@@ -5,44 +5,39 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
+class LibX265Conan(ConanFile):
+    name = "libx265"
+    version = "2.6"
     url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
+    description = "x265 is the leading H.265 / HEVC encoder software library"
     license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
     exports_sources = ["CMakeLists.txt", "LICENSE"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    #use static org/channel for libs in conan-center
-    #use dynamic org/channel for libs in bincrafters
-    requires = "OpenSSL/1.0.2l@conan/stable", \
-        "zlib/1.2.11@conan/stable", \
-        "websocketpp/0.7.0@%s/%s" % (self.user, self.channel)
+    generators = ['cmake']
+
+    def build_requirements(self):
+        self.build_requires("yasm_installer/[>=1.3.0]@bincrafters/stable")
+        self.build_requires("ninja_installer/[>=1.8.2]@bincrafters/stable")
 
     def source(self):
-        source_url = "https://github.com/Microsoft/cpprestsdk"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        source_url = "https://bitbucket.org/multicoreware/x265/downloads/x265_%s.tar.gz" % self.version
+        tools.get(source_url)
+        extracted_dir = 'x265_v%s' % self.version
         os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_dir="sources")
+        cmake = CMake(self, generator='Ninja')
+        cmake.definitions['ENABLE_SHARED'] = self.options.shared
+        cmake.configure()
         cmake.build()
+        cmake.install()
 
     def package(self):
-        with tools.chdir("sources"):
-            self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="include")
-            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="COPYING", src='sources')
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.extend(['dl', 'pthread'])
