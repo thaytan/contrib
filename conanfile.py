56fbc535506a60a3024b3bee7fd57baa70257859
Niclas Moeslund Overby <noverby@prozum.dk>
2018-12-20 14:32:33 +0100

    Use pkg-config environment variable to detect dependencies

--- conanfile.py
+++ conanfile.py
@@ -4,42 +4,27 @@ import platform
 
 class LibffiConan(ConanFile):
     name = 'libffi'
-    version = '3.2.1'
+    version = '3.3-rc0'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/prozum/conan-libffi'
     license = 'https://github.com/libffi/libffi/blob/master/LICENSE'
     description = 'A portable, high level programming interface to various calling conventions'
-    source_subfolder = 'source'
-    build_subfolder = source_subfolder
-    generators = "pkg_config"
 
     def source(self):
-        tools.get('https://sourceware.org/pub/libffi/libffi-%s.tar.gz' % self.version,
-                  sha256='d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37')
-        os.rename("libffi-" + self.version, self.source_subfolder)
+        tools.get("https://github.com/libffi/libffi/archive/v%s.tar.gz" % self.version)
 
     def build(self):
-        tools.mkdir(self.build_subfolder)
-        with tools.chdir(self.build_subfolder):
-            self.run("autoconf")
+        with tools.chdir("libffi-" + self.version):
+            self.run("autoreconf --install")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=['--quiet',
                                       '--disable-debug',
                                       '--disable-dependency-tracking',
+                                      '--disable-docs',
                                       '--disable-static',
-                                      '--enable-shared',
-                                      '--prefix=%s' % os.getcwd()])
-            autotools.make(args=['install'])
-
-    def package(self):
-        libext = 'so'
-
-        self.copy('*.h', src='%s/include' % self.build_subfolder, dst='include')
-        self.copy('*.h', src='%s/x86_64-unknown-linux-gnu/include/' % self.build_subfolder, dst='include')
-        self.copy('libffi.%s' % libext, src='%s/lib' % self.build_subfolder, dst='lib')
-        self.copy(pattern='*.pc', dst='', keep_path=False)
-
-        self.copy('%s.txt' % self.name, src=self.source_subfolder, dst='license')
+                                      '--enable-shared'])
+            autotools.make()
+            autotools.install()
 
     def package_info(self):
-        self.cpp_info.libs = ['ffi']
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
