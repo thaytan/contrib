820bc82c3c67b73e632dd62aca3a63c21ded4198
Niclas Moeslund Overby <noverby@prozum.dk>
2019-01-21 12:49:08 +0100

    Add 1.15.1 release

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class GStreamerConan(ConanFile):
     name = "gstreamer"
-    version = "master"
+    version = "1.15.1"
     url = "https://github.com/bincrafters/conan-" + name
     description = "A framework for streaming media"
     license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
