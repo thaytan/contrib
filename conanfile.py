78356b05b18f7ff19b613f8ecd633749bef9987d
Niclas Moeslund Overby <noverby@prozum.dk>
2019-01-21 09:22:56 +0100

    Add library package info

--- conanfile.py
+++ conanfile.py
@@ -3,12 +3,12 @@ import os
 import platform
 
 class LibffiConan(ConanFile):
-    name = 'libffi'
-    version = '3.3-rc0'
-    settings = 'os', 'compiler', 'build_type', 'arch'
-    url = 'https://github.com/prozum/conan-libffi'
-    license = 'https://github.com/libffi/libffi/blob/master/LICENSE'
-    description = 'A portable, high level programming interface to various calling conventions'
+    name = "libffi"
+    version = "3.3-rc0"
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/prozum/conan-libffi"
+    license = "https://github.com/libffi/libffi/blob/master/LICENSE"
+    description = "A portable, high level programming interface to various calling conventions"
 
     def source(self):
         tools.get("https://github.com/libffi/libffi/archive/v%s.tar.gz" % self.version)
@@ -17,14 +17,15 @@ class LibffiConan(ConanFile):
         with tools.chdir("libffi-" + self.version):
             self.run("autoreconf --install")
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=['--quiet',
-                                      '--disable-debug',
-                                      '--disable-dependency-tracking',
-                                      '--disable-docs',
-                                      '--disable-static',
-                                      '--enable-shared'])
+            autotools.configure(args=["--quiet",
+                                      "--disable-debug",
+                                      "--disable-dependency-tracking",
+                                      "--disable-docs",
+                                      "--disable-static",
+                                      "--enable-shared"])
             autotools.make()
             autotools.install()
 
     def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
         self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
