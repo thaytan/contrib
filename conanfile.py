e6fb64ebf87723c9f2cd4c3301ef2b990ae83029
Niclas Moeslund Overby <noverby@prozum.dk>
2019-01-21 17:46:30 +0100

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Conan receipt package for USB Library
+"""
+import os
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+
+
+class LibXorgUtilMacrosConan(ConanFile):
+    name = "xorg-util-macros"
+    version = "1.19.1"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False",
+    url = "https://github.com/freedesktop/xorg-macros"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    license = "custom"
+    description = "X.Org Autotools macros"
+    source_subfolder = "source_subfolder"
+    exports = ["LICENSE.md"]
+
+    def source(self):
+        tools.get("https://github.com/freedesktop/xorg-macros/archive/util-macros-%s.tar.gz" % self.version)
+        os.rename("xorg-macros-util-macros-%s" % self.version, self.source_subfolder)
+
+    def build(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        with tools.chdir(self.source_subfolder):
+            self.run("autoreconf -i")
+            env_build.configure()
+            env_build.make()
+            env_build.make(args=["install"])
+
+    def package(self):
+        self.copy("COPYING", src=self.source_subfolder, dst="licenses", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
