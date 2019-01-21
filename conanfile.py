59145bc44d7a25c4efecc02f4cd9d35f3bb6e5bb
Niclas Moeslund Overby <noverby@prozum.dk>
2019-01-21 16:21:30 +0100

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, Meson, tools
+import os
+
+
+class GLibConan(ConanFile):
+    name = "libva"
+    version = "2.3.0"
+    description = "Libva is an implementation for VA-API (VIdeo Acceleration API)"
+    url = "https://github.com/bincrafters/conan-" + name
+    author = "BinCrafters <bincrafters@gmail.com>"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"fPIC": [True, False]}
+    default_options = "fPIC=True"
+
+    def source(self):
+        tools.get("https://github.com/intel/libva/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--default-library=shared"]
+        meson = Meson(self)
+        meson.configure(source_folder="libva-" + self.version, args=args)
+        meson.build()
+        meson.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
