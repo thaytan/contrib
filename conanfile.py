92a0cc1d1934b846bf52b064808872bb89883918
Niclas Moeslund Overby <noverby@prozum.dk>
2019-01-24 14:30:18 +0100

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from conans import ConanFile, tools, Meson
+import os
+
+
+class IntelVaapiDriverConan(ConanFile):
+    name            = "intel-vaapi-driver"
+    version         = "2.3.0"
+    license         = "MIT"
+    url             = "https://github.com/prozum/conan-intel-vaapi-driver.git"
+    description     = "VA-API user mode driver for Intel GEN Graphics family"
+    settings = "os", "arch", "compiler", "build_type"
+    requires = "libdrm/2.4.96@bincrafters/stable", "libva/2.3.0@bincrafters/stable"
+
+    def source(self):
+        tools.get("https://github.com/intel/intel-vaapi-driver/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--libdir=lib", "-Ddriverdir=" + os.path.join(self.package_folder, "lib", "dri")]
+        meson = Meson(self)
+        meson.configure(source_folder="intel-vaapi-driver-" + self.version, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.build()
+        meson.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.LIBVA_DRIVERS_PATH.append(os.path.join(self.package_folder, "lib", "dri"))
