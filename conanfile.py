b9fc1a65ad8928fc5d66c31e3e6c9e23dce9fcfc
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-01 00:30:53 +0100

    conan: use default_user property

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, Meson, tools
+
+import os
+
+class GStreamerLibavConan(ConanFile):
+    name = "gstreamer-libav"
+    version = "1.15.1"
+    default_user = "bincrafters"
+    url = "https://github.com/bincrafters/conan-" + name
+    description = "GStreamer plugin for the libav* library (former FFmpeg)"
+    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {}
+    default_options = ()
+    requires = (
+        "glib/2.58.1@%s/stable" % self.user,
+        "ffmpeg/4.1@%s/stable" % self.user,
+        "gstreamer/%s@%s/stable" % (version, self.user),
+    )
+
+    def source(self):
+        tools.get("https://github.com/GStreamer/gst-libav/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--libdir=lib"]
+        meson = Meson(self)
+        meson.configure(source_folder="gst-libav-" + self.version, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.build()
+        meson.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
