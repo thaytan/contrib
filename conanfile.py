f1c5872466446857a1f256d63bda1064b5875a33
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-01 00:30:53 +0100

    conan: use default_user property

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools
 class BisonConan(ConanFile):
     name = "bison"
     version = "3.0.4"
+    default_user = "bincrafters"
     url = "https://github.com/bincrafters/conan-bison"
     homepage = "https://www.gnu.org/software/bison/"
     description = "Bison is a general-purpose parser generator"
