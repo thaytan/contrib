1156f389a9cfb9ccdfc0f9566c33aacafe69c5da
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-01 10:43:24 +0100

    Move requirements to method

--- conanfile.py
+++ conanfile.py
@@ -9,14 +9,14 @@ class GObjectIntrospectionConan(ConanFile):
     description = "A framework for streaming media"
     license = "https://github.com/GNOME/gobject-introspection/blob/master/COPYING"
     settings = "os", "arch", "compiler", "build_type"
-    requires = (
-        ("glib/2.58.1@%s/stable" % self.user),
-        ("libffi/3.3-rc0@%s/stable" % self.user, "private"),
-        ("bison/3.0.4@%s/stable" % self.user, "private"),
-        ("flex/2.6.4@%s/stable" % self.user, "private")
-    )
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
+
+    def requirements(self):
+        self.requires("glib/2.58.1@%s/stable" % self.user)
+        self.requires("libffi/3.3-rc0@%s/stable" % self.user, private=True)
+        self.requires("bison/3.0.4@%s/stable" % self.user, private=True)
+        self.requires("flex/2.6.4@%s/stable" % self.user, private=True)
 
     def source(self):
         tools.get("https://github.com/GNOME/gobject-introspection/archive/%s.tar.gz" % self.version)
