ca12c68426910d2f86f2f45ab488cfd047305497
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-07 14:21:58 +0100

    meson: disable features by default

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class LibdrmConan(ConanFile):
         tools.get("http://dri.freedesktop.org/libdrm/libdrm-%s.tar.gz" % self.version)
 
     def build(self):
-        args = ["--libdir=lib"]
+        args = ["--libdir=lib", "--auto-features=disabled"]
         meson = Meson(self)
         meson.configure(source_folder="libdrm-" + self.version, args=args, pkg_config_paths=os.environ['PKG_CONFIG_PATH'].split(":"))
         meson.build()
