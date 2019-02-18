737e76094c47f3f4705a2e177d5db8a45efe8998
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-18 14:10:30 +0100

    Use dynamic pkg-config prefix

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class LibRealsenseConan(ConanFile):
     url = "https://github.com/ulricheck/conan-librealsense"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=True", "libusb:shared=True"
+    default_options = "shared=False"
     exports = "libusb-fix.patch"
 
     def requirements(self):
@@ -23,20 +23,23 @@ class LibRealsenseConan(ConanFile):
         tools.patch(patch_file="libusb-fix.patch", base_path="librealsense-" + self.version)
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_EXAMPLES"] = "OFF"
-        cmake.definitions["BUILD_GRAPHICAL_EXAMPLES"] = "OFF"
-        cmake.definitions["BUILD_PCL_EXAMPLES"] = "OFF"
-        cmake.definitions["BUILD_NODEJS_BINDINGS"] = "OFF"
-        cmake.definitions["BUILD_PYTHON_BINDINGS"] = "OFF"
-        cmake.definitions["BUILD_UNIT_TESTS"] = "OFF"
-        if self.options.shared:
-            cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
-        else:
-            cmake.definitions["BUILD_SHARED_LIBS"] = "OFF"
-        cmake.configure(source_dir="librealsense-" + self.version)
-        cmake.build()
-        cmake.install()
+        vars = {'PKG_CONFIG_LIBUSB_1_0_PREFIX': self.deps_cpp_info["libusb"].rootpath}
+        with tools.environment_append(vars):
+            os.system("pkg-config --cflags libusb-1.0")
+            cmake = CMake(self)
+            cmake.definitions["BUILD_EXAMPLES"] = "OFF"
+            cmake.definitions["BUILD_GRAPHICAL_EXAMPLES"] = "OFF"
+            cmake.definitions["BUILD_PCL_EXAMPLES"] = "OFF"
+            cmake.definitions["BUILD_NODEJS_BINDINGS"] = "OFF"
+            cmake.definitions["BUILD_PYTHON_BINDINGS"] = "OFF"
+            cmake.definitions["BUILD_UNIT_TESTS"] = "OFF"
+            if self.options.shared:
+                cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
+            else:
+                cmake.definitions["BUILD_SHARED_LIBS"] = "OFF"
+            cmake.configure(source_dir="librealsense-" + self.version)
+            cmake.build()
+            cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
