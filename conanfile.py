50de86e494458c9cccdffa03f946101239e5e53e
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-19 14:36:11 +0100

    Simplify dynamic pkg-config prefix

--- conanfile.py
+++ conanfile.py
@@ -40,14 +40,8 @@ class GLibConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PKG_CONFIG_GIO_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GIO_UNIX_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GLIB_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GMODULE_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GMODULE_EXPORT_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GMODULE_NO_EXPORT_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GOBJECT_2_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_GTHREAD_2_0_PREFIX = self.package_folder
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
         self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         self.cpp_info.includedirs.append(os.path.join("include", "glib-2.0"))
