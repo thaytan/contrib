41843290050bda27ed8501aaa124c9b5851a7930
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-20 16:53:40 +0100

    Use dynamic pkg-config prefix

--- conanfile.py
+++ conanfile.py
@@ -43,3 +43,5 @@ class LibPciAccessConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
