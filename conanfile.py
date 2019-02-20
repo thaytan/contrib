33f2ed4b31868a0e2286f58a3440e2108abc7a38
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-20 16:19:28 +0100

    Use dynamic pkg-config prefix

--- conanfile.py
+++ conanfile.py
@@ -55,6 +55,6 @@ class GStreamerPluginsGoodConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
         self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
-
-
