7ac733dbcc0264859ebd2b54546c9b868a12e407
Niclas Moeslund Overby <noverby@prozum.dk>
2019-02-20 17:16:45 +0100

    Use dynamic pkg-config prefix

--- conanfile.py
+++ conanfile.py
@@ -33,4 +33,6 @@ class LibVaConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
