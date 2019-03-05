a331a90828d294b591a0621c8356192e1f79bbc0
Niclas Moeslund Overby <noverby@prozum.dk>
2019-03-05 14:19:34 +0100

    Properly handle disabled features

--- conanfile.py
+++ conanfile.py
@@ -51,9 +51,10 @@ class GStreamerPluginsBadConan(ConanFile):
         args = ["--libdir=lib", "--auto-features=disabled", "-Dgl_api=opengl"]
         args.append("-Dvideoparsersbad=" + ("enabled" if self.options.videoparsersbad else "disabled"))
         args.append("-Dgl=" + ("enabled" if self.options.gl else "disabled"))
-        args.append("-Dnvdec=" + ("enabled" if self.options.nvdec else "disabled"))
-        args.append("-Dnvenc=" + ("enabled" if self.options.nvenc else "disabled"))
         args.append("-Dpnm=" + ("enabled" if self.options.pnm else "disabled"))
+        if self.settings.arch == "x86_64":
+            args.append("-Dnvdec=" + ("enabled" if self.options.nvdec else "disabled"))
+            args.append("-Dnvenc=" + ("enabled" if self.options.nvenc else "disabled"))
         meson = Meson(self)
         meson.configure(source_folder=self.folder_name, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.build()
