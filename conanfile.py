e322994c429c6917f597b12c79d7ce06fda03656
Niclas Moeslund Overby <noverby@prozum.dk>
2019-03-07 12:51:19 +0100

    Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,52 @@
+from conans import ConanFile, Meson, tools
+
+import os
+
+class JsonGlibBaseConan(ConanFile):
+    name = "json-glib"
+    version = "1.4.4"
+    default_user = "bincrafters"
+    default_channel = "stable"
+    url = "https://github.com/bincrafters/conan-" + name
+    description = "A well-groomed and well-maintained collection of GStreamer plugins and elements"
+    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "introspection": [True, False],
+    }
+    default_options = (
+        "shared=False",
+        "introspection=True",
+    )
+    folder_name = "json-glib-" + version
+
+    def requirements(self):
+        self.requires("glib/2.58.1@%s/%s" % (self.user, self.channel))
+        if self.options.introspection:
+            self.requires("gobject-introspection/1.59.3@%s/%s" % (self.user, self.channel),)
+
+    def source(self):
+        tools.get("https://github.com/GNOME/json-glib/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--libdir=lib", "--auto-features=disabled"]
+        args.append("-Dintrospection=" + ("true" if self.options.introspection else "false"))
+
+        meson = Meson(self)
+        meson.configure(source_folder=self.folder_name, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.build()
+        meson.install()
+
+    def package(self):
+        if self.channel == "testing":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.srcdirs.append("src")
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
