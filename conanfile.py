bf56ce1fc7d1b93de5cb1810790c971031e35e10
Niclas Moeslund Overby <noverby@prozum.dk>
2019-03-18 14:29:17 +0100

    Build shared libraries

--- conanfile.py
+++ conanfile.py
@@ -6,13 +6,12 @@ class GStreamerPluginsGoodConan(ConanFile):
     name = "gstreamer-plugins-good"
     version = "1.15.1"
     default_user = "bincrafters"
-    default_channel = "bincrafters"
+    default_channel = "stable"
     url = "https://github.com/bincrafters/conan-" + name
     description = "Plug-ins is a set of plugins that we consider to have good quality code and correct functionality"
     license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
     settings = "os", "arch", "compiler", "build_type"
     options = {
-        "shared": [True, False],
         "autodetect": [True, False],
         "rtp": [True, False],
         "udp": [True, False],
@@ -21,7 +20,6 @@ class GStreamerPluginsGoodConan(ConanFile):
         "videofilter": [True, False],
     }
     default_options = (
-        "shared=False",
         "autodetect=True",
         "rtp=True",
         "udp=True",
@@ -29,7 +27,7 @@ class GStreamerPluginsGoodConan(ConanFile):
         "isomp4=True",
         "videofilter=True",
     )
-    folder_name = "gst-plugins-good" + version
+    folder_name = "gst-plugins-good-" + version
 
     def requirements(self):
         self.requires("glib/2.58.1@%s/%s" % (self.user, self.channel))
@@ -40,7 +38,7 @@ class GStreamerPluginsGoodConan(ConanFile):
         tools.get("https://github.com/GStreamer/gst-plugins-good/archive/%s.tar.gz" % self.version)
 
     def build(self):
-        args = ["--libdir=lib", "--auto-features=disabled"]
+        args = ["--auto-features=disabled"]
         args.append("-Dautodetect=" + ("enabled" if self.options.autodetect else "disabled"))
         args.append("-Drtp=" + ("enabled" if self.options.rtp else "disabled"))
         args.append("-Drtpmanager=" + ("enabled" if self.options.rtp else "disabled"))
