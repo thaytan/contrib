0d4ae2f076472f957c7d3b374270a56e6173133f
Niclas Moeslund Overby <noverby@prozum.dk>
2019-03-21 09:51:15 +0100

    Remove redundant libdir arg

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class GObjectIntrospectionConan(ConanFile):
         tools.get("https://github.com/GNOME/gobject-introspection/archive/%s.tar.gz" % self.version)
 
     def build(self):
-        args = ["--libdir=lib", "--auto-features=disabled"]
+        args = ["--auto-features=disabled"]
         meson = Meson(self)
         meson.configure(source_folder="gobject-introspection-" + self.version, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.build()
