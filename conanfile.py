924ba739e0668e78769c38b246cfc2e367c2995e
Niclas Moeslund Overby <noverby@prozum.dk>
2019-04-08 17:27:43 +0200

    Simplify

--- conanfile.py
+++ conanfile.py
@@ -6,10 +6,12 @@ import os
 import shutil
 
 
-class LibX265Conan(ConanFile):
-    name = "libx265"
+class X265Conan(ConanFile):
+    name = "x265"
     version = "2.7"
     homepage = "http://x265.org"
+    default_user = "bincrafters"
+    default_channel = "stable"
     url = "https://github.com/bincrafters/conan-libx265"
     description = "x265 is the leading H.265 / HEVC encoder software library"
     license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
@@ -18,58 +20,32 @@ class LibX265Conan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False], "bit_depth": [8, 10, 12], "HDR10": [True, False]}
     default_options = "shared=False", "fPIC=True", "bit_depth=8", "HDR10=False"
     generators = ['cmake']
-    build_requires = "nasm_installer/2.13.02@bincrafters/stable", "ninja_installer/1.8.2@bincrafters/stable"
-
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
 
     def source(self):
-        source_url = "https://bitbucket.org/multicoreware/x265/downloads/x265_%s.tar.gz" % self.version
-        tools.get(source_url)
-        extracted_dir = 'x265_%s' % self.version
-        os.rename(extracted_dir, "sources")
+        tools.get("https://github.com/videolan/x265/archive/%s.tar.gz" % self.version)
+
+    def build_requirements(self):
+        self.build_requires("yasm_installer/1.3.0@%s/%s" % (self.user, self.channel))
 
     def build(self):
-        if self.settings.compiler == 'Visual Studio':
-            with tools.vcvars(self.settings, filter_known_paths=False):
-                self.build_cmake()
-        else:
-            self.build_cmake()
-
-    def build_cmake(self):
-        if self.settings.os == 'Windows':
-            tools.replace_in_file(os.path.join('sources', 'source', 'CMakeLists.txt'),
-                                  '${PROJECT_BINARY_DIR}/Debug/x265.pdb',
-                                  '${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/x265.pdb')
-            tools.replace_in_file(os.path.join('sources', 'source', 'CMakeLists.txt'),
-                                  '${PROJECT_BINARY_DIR}/x265.pdb',
-                                  '${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/x265.pdb')
         cmake = CMake(self, generator='Ninja')
         cmake.definitions['ENABLE_SHARED'] = self.options.shared
-        if self.settings.os == "Macos":
-            cmake.definitions['CMAKE_SHARED_LINKER_FLAGS'] = '-Wl,-read_only_relocs,suppress'
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-            cmake.definitions['ENABLE_PIC'] = self.options.fPIC
         cmake.definitions['HIGH_BIT_DEPTH'] = self.options.bit_depth != 8
         cmake.definitions['MAIN12'] = self.options.bit_depth == 12
         cmake.definitions['ENABLE_HDR10_PLUS'] = self.options.HDR10
-        cmake.configure()
+        cmake.configure(source_folder=os.path.join("x265-" + self.version, "source"))
         cmake.build()
         cmake.install()
-        if self.settings.os == 'Linux':
-            tools.replace_in_file(os.path.join(self.package_folder, 'lib', 'pkgconfig', 'x265.pc'),
-                                  'Libs.private:', 'Libs.private: -lpthread')
 
     def package(self):
-        self.copy(pattern="COPYING", src='sources', dst='licenses')
-        if self.settings.compiler == 'Visual Studio':
-            name = 'libx265.lib' if self.options.shared else 'x265-static.lib'
-            shutil.move(os.path.join(self.package_folder, 'lib', name),
-                        os.path.join(self.package_folder, 'lib', 'x265.lib'))
+        if self.channel == "testing":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.libs = ['x265']
-        if self.settings.os == "Linux":
-            self.cpp_info.libs.extend(['dl', 'pthread'])
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.srcdirs.append("src")
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
