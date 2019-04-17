72dac9198aa16695088332bf119313bbb1a433f6
Niclas Moeslund Overby <noverby@prozum.dk>
2019-04-17 20:22:58 +0200

    Make generated pkg-config useable

--- conanfile.py
+++ conanfile.py
@@ -36,8 +36,21 @@ class CppzmqConan(ConanFile):
         if self.channel == "testing":
             self.copy("*.cpp", "src")
             self.copy("*.hpp", "src")
+        os.makedirs(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        with open(os.path.join(self.package_folder, "lib", "pkgconfig", "cppzmq.pc"), "w+") as pc_file:
+            pc_file.write("prefix=%s\n" % self.package_folder)
+            pc_file.write("includedir=${prefix}/include\n")
+            pc_file.write("Name: cppzmq\n")
+            pc_file.write("Description: ZeroMQ core engine in C++\n")
+            pc_file.write("Version: 4.3.0\n")
+            pc_file.write("Cflags: -I${includedir}\n")
+            pc_file.write("Requires: libzmq\n")
 
     def package_info(self):
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
+        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
+        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
         self.cpp_info.srcdirs.append("src")
+
