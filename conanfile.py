e19efa2a2d27ba8e6d07222755e07011e7f0742e
Raphael Dürscheid <raphael.duerscheid@aivero.com>
2019-05-16 16:55:22 +0200

    Remove dependency on a conan packaged gst-depth-meta-rs

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class RealsenseConan(ConanFile):
     def requirements(self):
         self.requires("gstreamer/1.16.0@%s/%s" % (self.user, self.channel))
         self.requires("gstreamer-plugins-base/1.16.0@%s/%s" % (self.user, self.channel))
-        self.requires("gstreamer-depth-meta-rs/0.1@%s/%s" % (self.user, self.channel))
+        self.requires("gstreamer-depth-meta/0.1@%s/%s" % (self.user, self.channel))
         self.requires("librealsense/2.20.0@%s/%s" % (self.user, self.channel))
 
     def build(self):
