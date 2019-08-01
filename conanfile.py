728063451774609a7d987d0af465998d25122c61
Andrej Orsula <andrej.orsula@aivero.com>
2019-08-01 12:04:54 +0200

    fix build_type in conanfily.py

--- conanfile.py
+++ conanfile.py
@@ -24,9 +24,9 @@ class RealsenseConan(ConanFile):
         self.requires("librealsense/2.20.0@%s/%s" % (self.user, self.channel))
 
     def build(self):
-        if self.build_type == 'Release':
+        if self.settings.build_type == 'Release':
             self.run("cargo build --release")
-        elif self.build_type == 'Debug':
+        elif self.settings.build_type == 'Debug':
             self.run("cargo build")
         else:
             print('Invalid build_type selected')
