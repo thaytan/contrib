6c2de682b36545dfc3b426a9e8c299b5991bf0df
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-05 14:55:08 +0200

    Fix various warnings

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 import os
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.util import files
 
 
 class OpusConan(ConanFile):
@@ -17,16 +16,15 @@ class OpusConan(ConanFile):
         tools.get("https://archive.mozilla.org/pub/opus/opus-%s.tar.gz" % self.version)
 
     def build(self):
-        vars = {
+        env_vars = {
             "CFLAGS": "-fdebug-prefix-map=%s=." % self.source_folder,
             "CXXFLAGS": "-fdebug-prefix-map=%s=." % self.source_folder,
         }
-        with tools.environment_append(vars):
-            with tools.chdir(os.path.join(self.source_folder, "opus-" + self.version)), tools.environment_append(vars):
-                autotools = AutoToolsBuildEnvironment(self)
-                autotools.configure()
-                autotools.make()
-                autotools.make(args=["install"])
+        with tools.chdir(os.path.join(self.source_folder, "opus-" + self.version)), tools.environment_append(env_vars):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.make(args=["install"])
 
 
     def package(self):
@@ -36,10 +34,10 @@ class OpusConan(ConanFile):
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = ["opus"]
         self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
+        for pc in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
+            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % pc[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
         self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
