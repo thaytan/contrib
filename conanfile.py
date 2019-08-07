03bb526817c04671939172f172614b5f5691ce39
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-07 16:04:26 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -9,22 +9,20 @@ class OrcConan(ConanFile):
     name = "orc"
     version = "0.4.29"
     settings = "os", "compiler", "build_type", "arch"
-    default_user = "bincrafters"
-    url = "http://github.com/bincrafters/conan-orc"
-    author = "Bincrafters <bincrafters@gmail.com>"
+    url = "https://gitlab.com/aivero/public/conan/conan-orc"
     license = "LGPL-2.1"
     description = "Optimized Inner Loop Runtime Compiler"
-    folder_name = name + "-" + version
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def source(self):
         tools.get("https://github.com/GStreamer/orc/archive/%s.tar.gz" % self.version)
 
     def build(self):
         args = ['--disable-gtk-doc']
-        vars = {
-            "CFLAGS": "-fdebug-prefix-map=%s=." % self.source_folder,
-        }
-        with tools.chdir(os.path.join(self.source_folder, self.folder_name)), tools.environment_append(vars):
+        with tools.chdir("orc-" + self.version):
                 self.run("./autogen.sh " + " ".join(args))
                 autotools = AutoToolsBuildEnvironment(self)
                 autotools.configure(args=args)
@@ -32,16 +30,10 @@ class OrcConan(ConanFile):
                 autotools.make(args=["install"])
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
