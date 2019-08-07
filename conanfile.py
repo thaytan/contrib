fd03eea39ed76deedaa495ad2c092571abccb4fa
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-07 15:25:48 +0200

    Make generator copy pc files and overwrite prefix

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,26 @@
 from conans.model import Generator
 from conans import ConanFile
-class env(Generator):
+from os import path, pathsep, listdir, mkdir, environ
+from shutil import copy
 
+
+def replace_prefix_in_pc_file(pc_file, prefix):
+    with open(pc_file) as f:
+        # Get old prefix
+        for l in f:
+            if "prefix=" in l:
+                old_prefix = l.split("=")[1][:-1]
+                break
+
+        f.seek(0)
+        content=f.read().replace(old_prefix, prefix)
+    with open(pc_file, "w") as f:
+        f.write(content)
+
+
+class env(Generator):
     def __init__(self, conanfile):
+        super().__init__(conanfile)
         self.env = conanfile.env
 
     @property
@@ -11,7 +29,23 @@ class env(Generator):
 
     @property
     def content(self):
-        content = ""
+        pc_output_path = self.output_path
+        for _, cpp_info in self.deps_build_info.dependencies:
+            pc_lib_path = path.join(cpp_info.rootpath, "lib", "pkgconfig")
+            pc_share_path = path.join(cpp_info.rootpath, "share", "pkgconfig")
+            if path.exists(pc_lib_path):
+                for pc in listdir(pc_lib_path):
+                    copy(path.join(pc_lib_path, pc), pc_output_path)
+                    replace_prefix_in_pc_file(path.join(pc_output_path, pc), cpp_info.rootpath)
+            if path.exists(pc_share_path):
+                for pc in listdir(pc_share_path):
+                    copy(path.join(pc_share_path, pc), pc_output_path)
+                    replace_prefix_in_pc_file(path.join(pc_output_path, pc), cpp_info.rootpath)
+        environ.update({
+            "PKG_CONFIG_PATH": pc_output_path,
+        })
+
+        content = "export PKG_CONFIG_PATH=\"$PKG_CONFIG_PATH\":\"%s\"\n" % pc_output_path
         for var, val in self.env.items():
             if type(val) is list: 
                 content += "export {0}=\"${0}\":{1}\n".format(var, ":".join(map(lambda x: "\"%s\"" % x, val)))
