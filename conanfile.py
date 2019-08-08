5f20379edd593daa4938c274d4a2fd85352070f9
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-08 13:02:30 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,9 @@
 from conans import ConanFile, Meson, tools
-
 import os
 
 class GStreamerConan(ConanFile):
     name = "gstreamer"
     version = "1.16.0"
-    default_user = "bincrafters"
-    default_channel = "stable"
     url = "https://github.com/bincrafters/conan-" + name
     description = "A framework for streaming media"
     license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
@@ -21,9 +18,10 @@ class GStreamerConan(ConanFile):
         "check=True",
         "tools=True",
     )
-    folder_name = name + "-" + version
+    generators = "env"
 
     def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
         self.requires("glib/2.58.1@%s/%s" % (self.user, self.channel))
         self.requires("bison/3.3@%s/%s" % (self.user, self.channel), private=True)
         self.requires("flex/2.6.4@%s/%s" % (self.user, self.channel), private=True)
@@ -39,12 +37,12 @@ class GStreamerConan(ConanFile):
         args.append("-Dcheck=" + ("enabled" if self.options.check else "disabled"))
         args.append("-Dtools=" + ("enabled" if self.options.tools else "disabled"))
         meson = Meson(self)
-        meson.configure(source_folder=self.folder_name, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
         meson.build()
         meson.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
@@ -54,8 +52,3 @@ class GStreamerConan(ConanFile):
         self.cpp_info.srcdirs.append("src")
         self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
         self.env_info.GST_PLUGIN_SCANNER = os.path.join(self.package_folder, "bin", "gstreamer-1.0", "gst-plugin-scanner")
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
