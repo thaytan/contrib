eaab5bc20b223dcc11e91df597cf3c3e1b276e48
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-08 15:43:35 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -9,30 +9,25 @@ class LibVaConan(ConanFile):
     name = "libva"
     version = "2.3.0"
     description = "Libva is an implementation for VA-API (VIdeo Acceleration API)"
-    default_user = "bincrafters"
-    default_channel = "stable"
-    url = "https://github.com/bincrafters/conan-" + name
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     author = "BinCrafters <bincrafters@gmail.com>"
     license = "MIT"
     exports = ["LICENSE.md"]
     settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
 
     def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
         self.requires("libdrm/2.4.96@%s/%s" % (self.user, self.channel))
 
     def source(self):
         tools.get("https://github.com/intel/libva/archive/%s.tar.gz" % self.version)
 
     def build(self):
-        args = []
         meson = Meson(self)
-        meson.configure(source_folder="libva-" + self.version, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.configure(source_folder="libva-" + self.version)
         meson.build()
         meson.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
