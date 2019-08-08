148a6d4e3821b000b54aa67182546d0acc4b145b
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-08 13:26:58 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -10,53 +10,41 @@ import shutil
 class FFMpegConan(ConanFile):
     name = "ffmpeg"
     version = "4.1"
-    default_user = "bincrafters"
-    default_channel = "stable"
-    url = "https://github.com/bincrafters/conan-ffmpeg"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "A complete, cross-platform solution to record, convert and stream audio and video"
     license = "https://github.com/FFmpeg/FFmpeg/blob/master/LICENSE.md"
-    exports_sources = ["LICENSE"]
     settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "fPIC": [True, False],
-    }
-    default_options = (
-        "fPIC=True",
-    )
+    generators = "env"
 
-    def source(self):
-        tools.get("http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % self.version)
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def build_requirements(self):
         self.build_requires("yasm_installer/1.3.0@%s/%s" % (self.user, self.channel))
 
+    def source(self):
+        tools.get("http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % self.version)
+
     def build(self):
         prefix = tools.unix_path(self.package_folder) if self.settings.os == 'Windows' else self.package_folder
-        args = ['--prefix=%s' % prefix,
-                '--disable-doc',
-                '--disable-programs']
+        args = [
+            '--disable-doc',
+            '--disable-programs'
+        ]
         if self.settings.build_type == 'Debug':
             args.extend(['--disable-optimizations', '--disable-mmx', '--disable-stripping', '--enable-debug'])
 
-        with tools.chdir(os.path.join(self.source_folder, "ffmpeg-" + self.version)):
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.configure(args=args, build=False, host=False, target=False)
-            env_build.make()
-            env_build.make(args=['install'])
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args, build=False, host=False, target=False)
+            autotools.make()
+            autotools.make(args=['install'])
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.srcdirs.append("src")
-        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            env_name = file[:-3].replace(".", "_").replace("-", "_").upper()
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % env_name, self.package_folder)
-            setattr(self.env_info, "PKG_CONFIG_%s_LIBDIR" % env_name, os.path.join(self.package_folder, "lib"))
-            setattr(self.env_info, "PKG_CONFIG_%s_INCLUDEDIR" % env_name, os.path.join(self.package_folder, "include"))
