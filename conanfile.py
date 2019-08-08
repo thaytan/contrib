8abec784ff32764965b940d38603cd791a01fec4
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-08 15:45:58 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,16 @@ import os
 class JsonGlibBaseConan(ConanFile):
     name = "json-glib"
     version = "1.4.4"
-    default_user = "bincrafters"
-    default_channel = "stable"
     url = "https://github.com/bincrafters/conan-" + name
     description = "A well-groomed and well-maintained collection of GStreamer plugins and elements"
     license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
     settings = "os", "arch", "compiler", "build_type"
-    options = { "introspection": [True, False] }
-    default_options = "introspection=True",
+    options = {
+        "introspection": [True, False],
+    }
+    default_options = (
+        "introspection=True",
+    )
 
     def requirements(self):
         self.requires("glib/2.58.1@%s/%s" % (self.user, self.channel))
@@ -27,19 +29,14 @@ class JsonGlibBaseConan(ConanFile):
         args.append("-Dintrospection=" + ("true" if self.options.introspection else "false"))
 
         meson = Meson(self)
-        meson.configure(source_folder="json-glib-" + self.version, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
-        meson.build()
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
         meson.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.srcdirs.append("src")
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
