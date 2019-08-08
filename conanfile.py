79d2f921494fcaf4aa68aad3efb13652251a8975
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-08 15:52:53 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 
 from conans import ConanFile, tools, Meson
-import os
-
 
 class LibdrmConan(ConanFile):
-    name            = "libdrm"
-    version         = "2.4.96"
-    license         = "MIT"
-    default_user    = "bincrafters"
-    default_channel = "stable"
-    url             = "https://github.com/prozum/conan-libdrm.git"
-    description     = "Direct Rendering Manager headers and kernel modules"
+    name = "libdrm"
+    version = "2.4.96"
+    license = "MIT"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    description = "Direct Rendering Manager headers and kernel modules"
     settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
 
     def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
         self.requires("libpciaccess/0.14@%s/%s" % (self.user, self.channel))
 
     def source(self):
@@ -23,12 +21,9 @@ class LibdrmConan(ConanFile):
     def build(self):
         args = ["--auto-features=disabled"]
         meson = Meson(self)
-        meson.configure(source_folder="libdrm-" + self.version, args=args, pkg_config_paths=os.environ['PKG_CONFIG_PATH'].split(":"))
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
         meson.build()
         meson.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
