7aff8c318a2133a727c529b9a893118250f39907
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-09 17:05:11 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -6,41 +6,30 @@ from conans.util import files
 class LibzmqConan(ConanFile):
     name = "libzmq"
     version = "4.3.1"
-    license = "https://raw.githubusercontent.com/zeromq/libzmq/master/COPYING"
     description = "ZeroMQ core engine in C++, implements ZMTP/3.1"
-    default_user = "bincrafters"
-    default_channel = "stable"
-    url = "https://github.com/bincrafters/conan-" + name
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "https://raw.githubusercontent.com/zeromq/libzmq/master/COPYING"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def source(self):
         tools.get("https://github.com/zeromq/libzmq/archive/v%s.tar.gz" % self.version)
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_SHARED_LIBS"] = ("ON" if self.options.shared else "OFF")
         cmake.definitions['ZMQ_BUILD_TESTS'] = False
         cmake.definitions['WITH_PERF_TOOL'] = False
-        vars = {
-            "CFLAGS": "-fdebug-prefix-map=%s=." % self.source_folder,
-            "CXXFLAGS": "-fdebug-prefix-map=%s=." % self.source_folder,
-        }
-        with tools.environment_append(vars):
-            cmake.configure(source_folder=self.name + "-" + self.version)
-            cmake.build()
-            cmake.install()
+        cmake.configure(source_folder="%s-%s" % (self.name, self.version))
+        cmake.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.cpp", "src")
             self.copy("*.hpp", "src")
 
     def package_info(self):
         self.cpp_info.libs = ["zmq"]
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.PKG_CONFIG_LIBZMQ_LIBDIR = os.path.join(self.package_folder, "lib")
-        self.env_info.PKG_CONFIG_LIBZMQ_INCLUDEDIR = os.path.join(self.package_folder, "include")
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
