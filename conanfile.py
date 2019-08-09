b93b8c7265700a1b5b8957d1db20e471ececf872
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-09 11:35:04 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -1,70 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
 import os
 
 
-class YASMInstallerConan(ConanFile):
-    name = "yasm_installer"
+class YasmConan(ConanFile):
+    name = "yasm"
     version = "1.3.0"
-    url = "https://github.com/bincrafters/conan-yasm_installer"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Yasm is a complete rewrite of the NASM assembler under the “new” BSD License"
     license = "BSD"
-    exports_sources = ["LICENSE"]
     settings = "os_build", "arch_build", "compiler"
-    _source_subfolder = "sources"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def source(self):
-        source_url = "http://www.tortall.net/projects/yasm/releases/yasm-%s.tar.gz" % self.version
-        tools.get(source_url)
-        extracted_dir = 'yasm-%s' % self.version
-        os.rename(extracted_dir, self._source_subfolder)
-        tools.download('https://raw.githubusercontent.com/yasm/yasm/master/YASM-VERSION-GEN.bat',
-                       os.path.join('sources', 'YASM-VERSION-GEN.bat'))
+        tools.get("http://www.tortall.net/projects/yasm/releases/yasm-%s.tar.gz" % self.version)
 
     def build(self):
-        if self.settings.os_build == 'Windows':
-            self._build_vs()
-        else:
-            self._build_configure()
-
-    def _build_vs(self):
-        with tools.chdir(os.path.join(self._source_subfolder, 'Mkfiles', 'vc10')):
-            with tools.vcvars(self.settings, arch=str(self.settings.arch_build), force=True):
-                msbuild = MSBuild(self)
-                if self.settings.arch_build == "x86":
-                    msbuild.build_env.link_flags.append('/MACHINE:X86')
-                elif self.settings.arch_build == "x86_64":
-                    msbuild.build_env.link_flags.append('/SAFESEH:NO /MACHINE:X64')
-                msbuild.build(project_file="yasm.sln", arch=self.settings.arch_build, build_type="Release",
-                              targets=["yasm"], platforms={"x86": "Win32"}, force_vcvars=True)
-
-    def _build_configure(self):
-        with tools.chdir(self._source_subfolder):
-            cc = os.environ.get('CC', 'gcc')
-            cxx = os.environ.get('CXX', 'g++')
-            if self.settings.arch_build == 'x86':
-                cc += ' -m32'
-                cxx += ' -m32'
-            elif self.settings.arch_build == 'x86_64':
-                cc += ' -m64'
-                cxx += ' -m64'
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build_vars = env_build.vars
-            env_build_vars.update({'CC': cc, 'CXX': cxx})
-            env_build.configure(vars=env_build_vars)
-            env_build.make(vars=env_build_vars)
-            env_build.install(vars=env_build_vars)
-
-    def package(self):
-        self.copy(pattern="BSD.txt", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        if self.settings.os_build == 'Windows':
-            self.copy(pattern='*.exe', src=self._source_subfolder, dst='bin', keep_path=False)
-
-    def package_info(self):
-        self.env_info.PATH.append(os.path.join(self.package_folder, 'bin'))
-
-    def package_id(self):
-        del self.info.settings.compiler
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.install()
