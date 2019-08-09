ca25aa0bc043292f97eaf1d3a5195166ae9341b5
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-09 15:03:56 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Conan receipt package for USB Library
-"""
 import os
 from conans import ConanFile, AutoToolsBuildEnvironment, MSBuild, tools
 
 
 class LibUSBConan(ConanFile):
-    """Download libusb source, build and create package
-    """
     name = "libusb"
     version = "1.0.22"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"udev": [True, False], "fPIC": [True, False]}
-    default_options = "udev=False", "fPIC=True"
-    default_user = "bincrafters"
     url = "http://github.com/bincrafters/conan-libusb"
-    author = "Bincrafters <bincrafters@gmail.com>"
     license = "LGPL-2.1"
     description = "A cross-platform library to access USB devices"
-    exports = ["LICENSE.md"]
-    folder_name = name + "-" + version
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"udev": [True, False]}
+    default_options = "udev=False"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def source(self):
         tools.get("https://github.com/libusb/libusb/archive/v%s.tar.gz" % self.version)
@@ -29,24 +25,17 @@ class LibUSBConan(ConanFile):
     def build(self):
         args = ["--disable-static"]
         args.append("--enable-udev" if self.options.udev else "--disable-udev")
-        vars = {
-            "CFLAGS": "-fdebug-prefix-map=%s=." % self.source_folder,
-        }
-        with tools.chdir(os.path.join(self.source_folder, self.folder_name)), tools.environment_append(vars):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
                 self.run("./autogen.sh " + " ".join(args))
                 autotools = AutoToolsBuildEnvironment(self)
                 autotools.configure(args=args)
-                autotools.make()
-                autotools.make(args=["install"])
+                autotools.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PKG_CONFIG_LIBUSB_1_0_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
