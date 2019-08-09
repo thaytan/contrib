0b102e604df1c457f0a21758b1f4a9e7dc96f1da
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-09 11:18:35 +0200

    Minor cleanup

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,7 @@ from conans import ConanFile, AutoToolsBuildEnvironment, tools
 class FlexConan(ConanFile):
     name = "flex"
     version = "2.6.4"
-    url = "https://github.com/bincrafters/conan-flex"
-    homepage = "https://github.com/westes/flex"
+    url = "https://gitlab.com/aivero/public/conan/conan-flex"
     description = "Flex, the fast lexical analyzer generator"
     license = "BSD 2-Clause"
     settings = "os", "arch", "compiler", "build_type"
@@ -23,12 +22,11 @@ class FlexConan(ConanFile):
         tools.get("https://github.com/westes/flex/releases/download/v{0}/flex-{0}.tar.gz".format(self.version))
 
     def build(self):
-        args = ["--enable-shared", "--disable-nls", "ac_cv_func_reallocarray=no"]
+        args = ["--disable-nls", "ac_cv_func_reallocarray=no"]
         autotools = AutoToolsBuildEnvironment(self)
         with tools.chdir("%s-%s" % (self.name, self.version)):
             self.run("./autogen.sh")
             autotools.configure(args=args)
-            autotools.make()
             autotools.install()
 
     def package_info(self):
