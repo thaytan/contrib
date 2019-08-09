efca9d9a0eb7af722a921743fba73b8f0af1e099
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-09 17:30:57 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -6,11 +6,14 @@ import shutil
 class OpensslConan(ConanFile):
     name = "openssl"
     version = "1.1.1b"
-    default_user = "bincrafters"
-    settings = "os", "compiler", "build_type", "arch"
-    url = "https://github.com/prozum/conan-" + name
-    license = "https://raw.githubusercontent.com/openssl/openssl/master/LICENSE"
     description = "TLS/SSL and crypto library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "https://raw.githubusercontent.com/openssl/openssl/master/LICENSE"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def source(self):
         tools.get("https://github.com/openssl/openssl/archive/OpenSSL_%s.tar.gz" % self.version.replace(".", "_"))
@@ -21,22 +24,17 @@ class OpensslConan(ConanFile):
             args += ["linux-x86_64", "enable-ec_nistp_64_gcc_128"]
         elif self.settings.arch == "armv8":
             args += ["linux-aarch64", "no-afalgeng"]
-        with tools.chdir(os.path.join(self.source_folder, "openssl-OpenSSL_" + self.version.replace(".", "_"))):
+        with tools.chdir("openssl-OpenSSL_" + self.version.replace(".", "_")):
             shutil.copy("Configure", "configure")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
-            autotools.make()
             autotools.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
         self.cpp_info.libs = ["ssl", "crypto"]
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
