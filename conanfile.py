cd888dbf76f79b9a5209266b19d033f492c7f5b5
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-12 09:46:43 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -8,33 +8,25 @@ from conans import ConanFile, tools, AutoToolsBuildEnvironment
 class LibSrtpConan(ConanFile):
     name = "libsrtp"
     version = "2.2.0"
-    url = "https://github.com/cisco/libsrtp"
-    author = "Prozum"
+    url = "http://gitlab.com/aivero/public/conan/conan-" + name
     description = "Library for SRTP (Secure Realtime Transport Protocol)"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
 
     def source(self):
         tools.get("https://github.com/cisco/libsrtp/archive/v%s.tar.gz" % self.version)
 
     def build(self):
-        args = []
-        with tools.chdir("libsrtp-" + self.version):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.make()
+            autotools.configure()
             autotools.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
+        self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.srcdirs.append("src")
 
