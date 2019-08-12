3c56b41519e92018f4e8dd21c66fc726c03df124
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-12 09:32:10 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -6,14 +6,14 @@ from conans.util import files
 class GlfwcConan(ConanFile):
     version = "3.2.1"
     name = "glfw"
-    license = "https://github.com/prozum/openhevc/blob/master/LICENSE"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "GLFW is an Open Source, multi-platform library for OpenGL, OpenGL ES and Vulkan development on the desktop."
-    default_user = "bincrafters"
-    default_channel = "stable"
-    url = "https://github.com/glfw/glfw"
+    license = "https://github.com/prozum/openhevc/blob/master/LICENSE"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
 
     def source(self):
         tools.get("https://github.com/glfw/glfw/archive/%s.tar.gz" % self.version)
@@ -23,14 +23,10 @@ class GlfwcConan(ConanFile):
         cmake.definitions["GLFW_BUILD_EXAMPLES"] = False
         cmake.definitions["GLFW_BUILD_TESTS"] = False
         cmake.definitions["GLFW_BUILD_DOCS"] = False
-        cmake.configure(source_dir="glfw-" + self.version)
+        cmake.configure(source_dir="%s-%s" % (self.name, self.version))
         cmake.build()
         cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        for file in os.listdir(os.path.join(self.package_folder, "lib", "pkgconfig")):
-            setattr(self.env_info, "PKG_CONFIG_%s_PREFIX" % file[:-3].replace(".", "_").replace("-", "_").upper(), self.package_folder)
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
