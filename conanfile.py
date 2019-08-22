3c397e9c4fbc734c6c9ff497138f11f9a06b7c60
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-22 16:51:00 +0200

    Package the official deepstream sdk tarball

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ def get_version():
         if git.get_tag() and not git.get_branch():
             return git.get_tag()
         else:
-            return "32.2"
+            return "4.0"
     except:
         return None
 
@@ -24,23 +24,18 @@ class Deepstream(ConanFile):
 
     def requirements(self):
         self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
-        self.requires("nv-v4lconvert/%s@%s/%s" % (self.version, self.user, self.channel))
 
     def source(self):
         if self.options.jetson in ("TX2", "Xavier"):
-            tools.get("https://developer.download.nvidia.com/embedded/L4T/r%s_Release_v1.0/TX2-AGX/Jetson_Linux_R%s.0_aarch64.tbz2" % (self.version.replace(".", "-"), self.version))
-        elif self.options.jetson == "Nano":
-            tools.get("https://developer.download.nvidia.com/embedded/L4T/r%s_Release_v1.0/Nano-TX1/Jetson-210_Linux_R%s.0_aarch64.tbz2" % (self.version.replace(".", "-"), self.version))
+            tools.get("https://developer.download.nvidia.com/assets/Deepstream/Deepstream_{0}/deepstream_sdk_v{0}_jetson.tbz2".format(self.version))
         else:
             raise KeyError("Unknown option: " + self.options.jetson)
 
-        tools.untargz("Linux_for_Tegra/nv_tegra/nvidia_drivers.tbz2", self.source_folder)
-        tools.rmdir("Linux_for_Tegra")
-        for dl in ("nvbufsurface", "nvbuf_utils", "nvbuf_fdmap"):
-            symlink("lib%s.so.1.0.0" % dl, "lib%s.so" % dl)
+        tools.untargz("deepstream_sdk_v%s_jetson/binaries.tbz2" % self.version, self.source_folder)
+        tools.rmdir("deepstream_sdk_v%s_jetson" % self.version)
 
     def package(self):
-        self.copy("*libnvbuf*.so*", dst="lib", keep_path=False, links=True)
+        self.copy("*.so*", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
