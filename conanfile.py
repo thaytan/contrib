fca600ac8520f9155ac0197661afa1d0b697a8a7
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-23 13:38:44 +0200

    Port to env generator

--- conanfile.py
+++ conanfile.py
@@ -5,15 +5,10 @@ import platform
 class LibffiConan(ConanFile):
     name = "libffi"
     version = "3.3-rc0"
-    default_user = "bincrafters"
     settings = "os", "compiler", "build_type", "arch"
     url = "https://github.com/prozum/conan-libffi"
     license = "https://github.com/libffi/libffi/blob/master/LICENSE"
     description = "A portable, high level programming interface to various calling conventions"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-    folder_name = name + "-" + version
-    no_copy_source = True
 
     def source(self):
         tools.get("https://github.com/libffi/libffi/archive/v%s.tar.gz" % self.version)
@@ -23,13 +18,11 @@ class LibffiConan(ConanFile):
             "--quiet",
             "--disable-debug",
             "--disable-dependency-tracking",
-            "--disable-docs"
+            "--disable-docs",
+            "--disable-static",
+            "--enable-shared"
         ]
-        if self.options.shared:
-            args.extend(["--disable-static", "--enable-shared"])
-        else:
-            args.extend(["--disable-shared", "--enable-static"])
-        with tools.chdir(os.path.join(self.source_folder, self.folder_name)):
+        with tools.chdir("libffi-" + self.version):
             self.run("autoreconf -i")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
@@ -37,13 +30,10 @@ class LibffiConan(ConanFile):
             autotools.install()
 
     def package(self):
-        if self.channel == "testing":
+        if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.PKG_CONFIG_LIBFFI_PREFIX = self.package_folder
-        self.env_info.PKG_CONFIG_PATH.append(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self.env_info.SOURCE_PATH.append(os.path.join(self.package_folder, "src"))
         self.cpp_info.srcdirs.append("src")
