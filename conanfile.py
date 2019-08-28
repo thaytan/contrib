50a7c9573828c024f3ced237d17c4f63fbab4fe6
Niclas Moeslund Overby <noverby@prozum.dk>
2019-08-28 09:46:06 +0000

    Use stable channel for dependencies

--- conanfile.py
+++ conanfile.py
@@ -18,9 +18,9 @@ class RealsenseConan(ConanFile):
     generators = "env"
 
     def requirements(self):
-        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
-        self.requires("gstreamer-depth-meta/0.2.0@%s/%s" % (self.user, self.channel))
-        self.requires("librealsense/2.20.0@%s/%s" % (self.user, self.channel))
+        self.requires("env-generator/0.1@%s/stable" % self.user)
+        self.requires("gstreamer-depth-meta/0.2.0@%s/stable" % self.user)
+        self.requires("librealsense/2.20.0@%s/stable" % self.user)
 
     def build(self):
         if self.settings.build_type == 'Release':
