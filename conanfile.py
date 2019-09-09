af1a450058f9868fa7863783762adecee41a20be
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-09 16:38:53 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, Meson, tools
+import os
+
+class PixmanConan(ConanFile):
+    name = "pixman"
+    version = "0.38.4"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    description = "Image processing and manipulation library"
+    license = "https://gitlab.freedesktop.org/pixman/pixman/blob/master/COPYING"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
+
+    def source(self):
+        tools.get("https://gitlab.freedesktop.org/pixman/pixman/-/archive/pixman-{0}/pixman-pixman-{0}.tar.gz".format(self.version))
+
+    def build(self):
+        args = ["--auto-features=disabled"]
+        meson = Meson(self)
+        meson.configure(source_folder="pixman-pixman-%s" % self.version, args=args)
+        meson.install()
+
+    def package(self):
+        if self.settings.build_type == "Debug":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.srcdirs.append("src")
