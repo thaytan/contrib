953ebb1b2a52ab53fe00c6b0fae5133984874ddf
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-10 17:31:32 +0200

    Port to new versioning scheme

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,17 @@
 from conans import ConanFile, Meson, tools
 import os
 
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "0.38.4"
+    except:
+        return None
+
 class PixmanConan(ConanFile):
     name = "pixman"
-    version = "0.38.4"
+    version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Image processing and manipulation library"
     license = "https://gitlab.freedesktop.org/pixman/pixman/blob/master/COPYING"
@@ -11,7 +19,7 @@ class PixmanConan(ConanFile):
     generators = "env"
 
     def requirements(self):
-        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
+        self.requires("env-generator/0.1@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://gitlab.freedesktop.org/pixman/pixman/-/archive/pixman-{0}/pixman-pixman-{0}.tar.gz".format(self.version))
