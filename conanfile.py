42621c0d16327b8dcc8c65f2bc03e1c09e46ee60
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-13 15:55:41 +0200

    Port to new versioning scheme

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from conans import ConanFile, tools, CMake, AutoToolsBuildEnvironment
 import os
 import stat
-from conans import ConanFile, tools, CMake, AutoToolsBuildEnvironment
 
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.2.11"
+    except:
+        return None
 
 class ZlibConan(ConanFile):
     name = "zlib"
-    version = "1.2.11"
+    version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-zlib"
     license = "Zlib"
     description = ("A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
                    "(Also Free, Not to Mention Unencumbered by Patents)")
     settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/madler/zlib/archive/v%s.tar.gz" % self.version)
