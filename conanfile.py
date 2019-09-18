78b628e172b91ecad5250c33e0d51538cd2ea634
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-18 16:19:36 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans.util import files
+import os
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "2.13.1"
+    except:
+        return None
+
+class FontconfigConan(ConanFile):
+    name = "fontconfig"
+    version = get_version()
+    license = "Old MIT"
+    description = "A library for configuring and customizing font access"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/stable" % self.user)
+        self.requires("freetype/2.10.1@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/{0}/fontconfig-{0}.tar.gz".format(self.version))
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("%s-%s" % (self.name , self.version)):
+            self.run("./autogen.sh")
+            autotools.configure()
+            autotools.install()
+
+    def package(self):
+        if self.settings.build_type == "Debug":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.srcdirs.append("src")
