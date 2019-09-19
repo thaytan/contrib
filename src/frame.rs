30f980116c0993a14a6356d183c1507d399581dc
tmorell <tobiasmorell@hotmail.com>
2019-09-19 10:53:27 +0200

    Add metadata support

--- src/frame.rs
+++ src/frame.rs
@@ -1,7 +1,7 @@
 use crate::error::Error;
 use crate::stream::StreamProfile;
 use rs2;
-use crate::util::to_string;
+use crate::metadata::{Metadata, MetadataAttribute};
 use std::collections::HashMap;
 
 pub struct Frame {
@@ -105,9 +105,82 @@ impl Frame {
         }
     }
 
-    pub fn get_metadata(&self) -> Result<HashMap<String, i64>, Error> {
+    /// Check if the frame's metadata supports the given attribute. The function returns `true` if
+    /// it does, `false` if not. The error variant is used to propagate any errors encountered in
+    /// the librealsense code.
+    /// # Arguments
+    /// * `attribute` - The attribute to check support for.
+    /// # Example
+    /// ```
+    /// use librealsense2::pipeline::Pipeline;
+    /// use librealsense2::context::Context;
+    /// use librealsense2::metadata::MetadataAttribute;
+    /// let pipeline = Pipeline::new(&Context::new().unwrap()).unwrap();
+    ///
+    /// let frames = pipeline.wait_for_frames(2500).unwrap();
+    /// if frames[0].supports_frame_metadata(MetadataAttribute::Contrast).unwrap() {
+    ///     println!("frames[0] supports the 'Contrast' metadata.")
+    /// }
+    /// else { println!("frames[0] does not support the 'Contrast' metadata.") };
+    /// ```
+    pub fn supports_frame_metadata(&self, attribute: MetadataAttribute) -> Result<bool, Error> {
         let mut error = Error::default();
-        let mut meta_values : HashMap<String, i64> = HashMap::new();
+        let meta_supported = unsafe { rs2::rs2_supports_frame_metadata(self.raw, attribute as rs2::rs2_frame_metadata_value, error.inner()) };
+        if error.check() {
+            Err(error)
+        }
+        else if meta_supported == 1 {
+            Ok(true)
+        }
+        else {
+            Ok(false)
+        }
+    }
+
+    /// Read the given metadata attribute from the frame. Please use the `supports_frame_metadata`
+    /// function to check if the given metadata is supported before reading it, as librealsense may
+    /// fail with an exception when reading an un-supported metadata attribute.
+    /// # Arguments
+    /// * `attribute` - The attribute to read.
+    /// # Example
+    /// ```
+    /// use librealsense2::pipeline::Pipeline;
+    /// use librealsense2::context::Context;
+    /// use librealsense2::metadata::MetadataAttribute;
+    /// let pipeline = Pipeline::new(&Context::new().unwrap()).unwrap();
+    ///
+    /// let frames = pipeline.wait_for_frames(2500).unwrap();
+    /// let contrast =
+    ///     if frames[0].supports_frame_metadata(MetadataAttribute::Contrast).unwrap() {
+    ///         Some(frames[0].get_frame_metadata(MetadataAttribute::Contrast).unwrap())
+    ///     }
+    ///     else { None };
+    /// ```
+    pub fn get_frame_metadata(&self, attribute: MetadataAttribute) -> Result<i64, Error> {
+        let mut error = Error::default();
+        let value = unsafe { rs2::rs2_get_frame_metadata(self.raw, attribute as rs2::rs2_frame_metadata_value, error.inner()) };
+        if error.check() {
+            Err(error)
+        }
+        else {
+            Ok(value)
+        }
+    }
+
+    /// Get all the frame's supported metadata field represented as a `Metadata` struct.
+    /// # Example
+    /// ```
+    /// use librealsense2::pipeline::Pipeline;
+    /// use librealsense2::context::Context;
+    /// let pipeline = Pipeline::new(&Context::new().unwrap()).unwrap();
+    ///
+    /// let frames = pipeline.wait_for_frames(2500).unwrap();
+    /// let metadata = frames[0].get_metadata().unwrap();
+    /// println!("frames[0]'s contrast is {}", metadata.contrast.unwrap());
+    /// ```
+    pub fn get_metadata(&self) -> Result<Metadata, Error> {
+        let mut error = Error::default();
+        let mut meta_values : HashMap<u32, i64> = HashMap::new();
 
         for i in 0..rs2::rs2_frame_metadata_value_RS2_FRAME_METADATA_COUNT {
             // Cast the integer to a rs2_frame_metadata_value, which realsense uses to identify metadata fields
@@ -118,15 +191,14 @@ impl Frame {
                 continue;
             }
             // Attempt to get the meta's name and value
-            let meta_name = unsafe { to_string(rs2::rs2_frame_metadata_to_string(metadata_value)) };
             let mete_val = unsafe { rs2::rs2_get_frame_metadata(self.raw, metadata_value, error.inner()) };
             if error.check() {
                 return Err(error);
             }
 
             // Append that to the dictionary
-            meta_values.insert(meta_name, mete_val);
+            meta_values.insert(metadata_value, mete_val);
         }
-        Ok(meta_values)
+        Ok(Metadata::from(meta_values))
     }
 }
