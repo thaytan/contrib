5c919bcc3df6df70bb616dd5c27313ad056b266c
tmorell <tobiasmorell@hotmail.com>
2019-09-19 13:49:26 +0200

    (Attempt to) add support for per-frame meta data

---
+++ src/frame-meta.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <gst/gst.h>
+#include <gst/gstmeta.h>
+#include <gst/gsttaglist.h>
+#include <gst/video/video.h>
+
+G_BEGIN_DECLS
+
+typedef struct _FrameMeta FrameMeta;
+
+struct _FrameMeta {
+    GstMeta meta;
+    u_int8_t *bytes;
+};
+
+GType frame_meta_api_get_type(void);
+const GstMetaInfo *frame_meta_get_info(void);
+FrameMeta *frame_meta_get(GstBuffer *buffer);
+FrameMeta *frame_meta_add(GstBuffer *buffer, u_int8_t *bytes, size_t nbytes);
+
+G_END_DECLS
