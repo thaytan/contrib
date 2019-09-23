a8bb9e000b01a82ab56da548337af1f6ece39481
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-24 00:50:16 +0200

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 import glob
@@ -19,7 +16,7 @@ class FFMpegConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "A complete, cross-platform solution to record, convert and stream audio and video"
-    license = "https://github.com/FFmpeg/FFmpeg/blob/master/LICENSE.md"
+    license = "GPL3"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
@@ -33,17 +30,16 @@ class FFMpegConan(ConanFile):
         tools.get("http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % self.version)
 
     def build(self):
-        prefix = tools.unix_path(self.package_folder) if self.settings.os == 'Windows' else self.package_folder
         args = [
-            '--disable-doc',
-            '--disable-programs'
+            "--disable-doc",
+            "--disable-programs"
         ]
-        if self.settings.build_type == 'Debug':
-            args.extend(['--disable-optimizations', '--disable-mmx', '--disable-stripping', '--enable-debug'])
+        if self.settings.build_type == "Debug":
+            args.extend(["--disable-optimizations", "--disable-mmx", "--disable-stripping", "--enable-debug"])
 
         with tools.chdir("%s-%s" % (self.name, self.version)):
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args, build=False, host=False, target=False)
+            autotools.configure(args=args)
             autotools.make()
             autotools.install()
 
