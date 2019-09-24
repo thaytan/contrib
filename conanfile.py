5ef57f5695d3593d768fbe198125a4385b03f1d9
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-24 15:53:59 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,40 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "2.69"
+    except:
+        return None
+
+class AutoconfConan(ConanFile):
+    name = "autoconf"
+    version = get_version()
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "GPL3"
+    description = "A GNU tool for automatically configuring source code"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://ftp.gnu.org/gnu/autoconf/autoconf-%s.tar.gz" % self.version)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
+
+    def package(self):
+        if self.settings.build_type == "Debug":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.srcdirs.append("src")
