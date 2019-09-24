c0babe4d8a443df219490abb7aaff416fccb770c
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-24 17:33:41 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.16.1"
+    except:
+        return None
+
+class AutomakeConan(ConanFile):
+    name = "automake"
+    version = get_version()
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "GPL"
+    description = "A GNU tool for automatically creating Makefiles"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+        self.build_requires("autoconf/2.69@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("m4/1.4.18@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://ftp.gnu.org/gnu/automake/automake-%s.tar.gz" % self.version)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
+
+    def package(self):
+        if self.settings.build_type == "Debug":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.srcdirs.append("src")
