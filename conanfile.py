a14305111433a3427e18e5f61f9a4e0fe70d6a99
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-24 22:51:53 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "0.7.0"
+    except:
+        return None
+
+class CapNProtoConan(ConanFile):
+    name = "capnproto"
+    version = get_version()
+    url = "https://gitlab.com/aivero/public/conan/conan-zlib"
+    license = "MIT"
+    description = ("Cap'n Proto serialization/RPC system")
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/0.1@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://capnproto.org/capnproto-c++-%s.tar.gz" % self.version)
+
+    def build(self):
+        with tools.chdir("capnproto-c++-%s" % self.version):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
+
+    def package(self):
+        if self.settings.build_type == "Debug":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.srcdirs.append("src")
