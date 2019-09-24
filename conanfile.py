9c424cf0c553408079ecc8c600a348311b9121a6
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-24 22:39:53 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.47.11"
+    except:
+        return None
+
+class Help2ManConan(ConanFile):
+    name = "help2man"
+    version = get_version()
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "GPL"
+    description = "Conversion tool to create man files"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://ftp.gnu.org/gnu/help2man/help2man-%s.tar.xz" % self.version)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
+
+    def package(self):
+        if self.settings.build_type == "Debug":
+            self.copy("*.c", "src")
+            self.copy("*.h", "src")
+
+    def package_info(self):
+        self.cpp_info.srcdirs.append("src")
