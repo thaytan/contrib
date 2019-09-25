8ff0916278cb0f6c3ad5a4dc76d0bd672811de89
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-25 22:21:15 +0200

    Use conan cmake

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,4 @@
 from conans import ConanFile, CMake, tools
-import os
-import shutil
 
 def get_version():
     git = tools.Git()
@@ -15,26 +13,25 @@ class X265Conan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "x265 is the leading H.265 / HEVC encoder software library"
-    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
+    license = "GPL"
     settings = "os", "arch", "compiler", "build_type"
     options = {"bit_depth": [8, 10, 12], "HDR10": [True, False]}
     default_options = "bit_depth=8", "HDR10=False"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
-
     def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+        self.build_requires("cmake/3.15.3@%s/stable" % self.user)
         self.build_requires("yasm/1.3.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/videolan/x265/archive/%s.tar.gz" % self.version)
 
     def build(self):
-        cmake = CMake(self, generator='Ninja')
-        cmake.definitions['HIGH_BIT_DEPTH'] = self.options.bit_depth != 8
-        cmake.definitions['MAIN12'] = self.options.bit_depth == 12
-        cmake.definitions['ENABLE_HDR10_PLUS'] = self.options.HDR10
+        cmake = CMake(self, generator="Ninja")
+        cmake.definitions["HIGH_BIT_DEPTH"] = self.options.bit_depth != 8
+        cmake.definitions["MAIN12"] = self.options.bit_depth == 12
+        cmake.definitions["ENABLE_HDR10_PLUS"] = self.options.HDR10
         cmake.configure(source_folder=os.path.join("%s-%s" % (self.name, self.version), "source"))
         cmake.install()
 
