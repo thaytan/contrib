15bd74765f17f9e0e2edf7b1e36b54ec288e7c3b
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-25 22:22:53 +0200

    Use conan cmake

--- conanfile.py
+++ conanfile.py
@@ -17,14 +17,15 @@ class GTestConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+        self.build_requires("cmake/3.15.3@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/google/googletest/archive/release-%s.tar.gz" % self.version)
 
     def build(self):
-        cmake = CMake(self)
+        cmake = CMake(self, generator="Ninja")
         cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
         cmake.configure(source_folder="googletest-release-" + self.version)
         cmake.install()
