37ae9ed248f423778eb9e5894d75fb28f4290369
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-26 13:55:57 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class GStreamerPluginsBaseConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "A well-groomed and well-maintained collection of GStreamer plugins and elements"
-    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "introspection": [True, False],
@@ -28,6 +28,7 @@ class GStreamerPluginsBaseConan(ConanFile):
         "timeoverlay": [True, False],
         "orc": [True, False],
         "opus": [True, False],
+        "pango": [True, False],
     }
     default_options = (
         "introspection=True",
@@ -41,19 +42,24 @@ class GStreamerPluginsBaseConan(ConanFile):
         "timeoverlay=True",
         "orc=True",
         "opus=True",
+        "pango=True",
     )
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
-        self.requires("glib/2.58.1@%s/stable" % self.user)
-        self.requires("gstreamer/%s@%s/stable" % (self.version, self.user))
+    def build_requirements(self):
+        self.build_requires("env-generator/[>=0.1]@%s/stable" % self.user)
+        self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         if self.options.introspection:
-            self.requires("gobject-introspection/1.59.3@%s/stable" % self.user)
+            self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("gstreamer/[>=%s]@%s/stable" % (self.version, self.user))
         if self.options.orc:
-            self.requires("orc/0.4.29@%s/stable" % self.user)
+            self.requires("orc/[>=0.4.29]@%s/stable" % self.user)
         if self.options.opus:
-            self.requires("opus/1.3.1@%s/stable" % self.user)
+            self.requires("opus/[>=1.3.1]@%s/stable" % self.user)
+        if self.options.pango:
+            self.requires("pango/[>=1.43.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/GStreamer/gst-plugins-base/archive/%s.tar.gz" % self.version)
@@ -68,10 +74,9 @@ class GStreamerPluginsBaseConan(ConanFile):
         args.append("-Dapp=" + ("enabled" if self.options.app else "disabled"))
         args.append("-Dplayback=" + ("enabled" if self.options.playback else "disabled"))
         args.append("-Dtypefind=" + ("enabled" if self.options.typefind else "disabled"))
-        args.append("-Dtimeoverlay=" + ("enabled" if self.options.timeoverlay else "disabled"))
         args.append("-Dorc=" + ("enabled" if self.options.orc else "disabled"))
         args.append("-Dopus=" + ("enabled" if self.options.opus else "disabled"))
-        self.run("env")
+        args.append("-Dpango=" + ("enabled" if self.options.pango else "disabled"))
         meson = Meson(self)
         meson.configure(source_folder="gst-plugins-base-" + self.version, args=args)
         meson.install()
@@ -82,7 +87,4 @@ class GStreamerPluginsBaseConan(ConanFile):
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.includedirs = ["include/gstreamer-1.0"]
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
         self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
