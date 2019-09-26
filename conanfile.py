289d0de9cdfdd5cc295d286d26bc0d2f27839b3a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-26 08:46:39 +0200

    Add autotools dependency

--- conanfile.py
+++ conanfile.py
@@ -19,12 +19,15 @@ class FontconfigConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
+        self.build_requires("gettext/0.20.1@%s/stable" % self.user)
+        self.build_requires("gperf/3.1@%s/stable" % self.user)
+
     def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
         self.requires("freetype/2.10.1@%s/stable" % self.user)
-        self.requires("gperf/3.1@%s/stable" % self.user)
         self.requires("libuuid/1.0.3@%s/stable" % self.user)
-        self.requires("gettext/0.20.1@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/{0}/fontconfig-{0}.tar.gz".format(self.version))
@@ -33,9 +36,9 @@ class FontconfigConan(ConanFile):
         args = [
             "--disable-static"
         ]
-        autotools = AutoToolsBuildEnvironment(self)
         with tools.chdir("%s-%s" % (self.name , self.version)):
-            self.run("./autogen.sh")
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
             autotools.install()
         for f in glob(path.join(self.package_folder, "**", "*.la"), recursive=True):
