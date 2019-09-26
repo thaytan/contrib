8e848460b32df5d9eafcab9efb4662203ec1fcd1
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-26 08:43:57 +0200

    Add autotools dependency

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
-import os
 
 def get_version():
     git = tools.Git()
@@ -18,20 +17,19 @@ class FlexConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/0.1@%s/%s" % (self.user, self.channel))
-        self.requires("bison/3.3@%s/%s" % (self.user, self.channel), private=True)
+    def build_requirements(self):
+        self.build_requires("env-generator/0.1@%s/stable" % self.user)
+        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
+        self.build_requires("gettext/0.20.1@%s/stable" % self.user)
+        self.build_requires("bison/3.3@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/westes/flex/releases/download/v{0}/flex-{0}.tar.gz".format(self.version))
 
     def build(self):
         args = ["--disable-nls", "ac_cv_func_reallocarray=no"]
-        autotools = AutoToolsBuildEnvironment(self)
         with tools.chdir("%s-%s" % (self.name, self.version)):
-            self.run("./autogen.sh")
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
             autotools.install()
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
