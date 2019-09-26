c506bf05f4ee9530ff6d82e7f00cedcec24a63c7
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-26 13:41:19 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -20,22 +20,20 @@ class FontconfigConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/0.1@%s/stable" % self.user)
-        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
-        self.build_requires("gettext/0.20.1@%s/stable" % self.user)
-        self.build_requires("gperf/3.1@%s/stable" % self.user)
+        self.build_requires("env-generator/[>=0.1]@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("gettext/[>=0.20.1]@%s/stable" % self.user)
+        self.build_requires("gperf/[>=3.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("freetype/2.10.1@%s/stable" % self.user)
-        self.requires("libuuid/1.0.3@%s/stable" % self.user)
+        self.requires("freetype/[>=2.10.1]@%s/stable" % self.user)
+        self.requires("libuuid/[>=1.0.3]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/{0}/fontconfig-{0}.tar.gz".format(self.version))
 
     def build(self):
-        args = [
-            "--disable-static"
-        ]
+        args = ["--disable-static"]
         with tools.chdir("%s-%s" % (self.name , self.version)):
             self.run("sh autogen.sh")
             autotools = AutoToolsBuildEnvironment(self)
@@ -48,7 +46,3 @@ class FontconfigConan(ConanFile):
         if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
