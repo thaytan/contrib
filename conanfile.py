0814c38033320ef22f64988f30fbb9a30c228e85
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 23:20:33 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -14,16 +14,17 @@ class GStreamerDevtoolsConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Development and debugging tools for GStreamer"
-    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
+
     def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
-        self.requires("glib/2.58.1@%s/stable" % self.user)
-        self.requires("json-glib/1.4.4@%s/stable" % self.user)
-        self.requires("gstreamer/%s@%s/stable" % (self.version, self.user))
-        self.requires("gstreamer-plugins-base/%s@%s/stable" % (self.version, self.user))
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("gstreamer-plugins-base/[>=%s]@%s/stable" % (self.version, self.user))
+        self.requires("json-glib/[>=1.4.4]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/GStreamer/gst-devtools/archive/%s.tar.gz" % self.version)
@@ -38,7 +39,3 @@ class GStreamerDevtoolsConan(ConanFile):
         if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
