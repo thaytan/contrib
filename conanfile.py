eee3aab00ac27fde76785106c81a56b13cc5a16b
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 12:56:57 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class OrcConan(ConanFile):
     generators = "env"
 
     def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/GStreamer/orc/archive/%s.tar.gz" % self.version)
@@ -35,7 +35,3 @@ class OrcConan(ConanFile):
         if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
