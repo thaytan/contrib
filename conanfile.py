e684da4158f396ea26011fe2f5d32ad4ee595d29
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 14:28:31 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 from conans import ConanFile, Meson, tools
-import os
 
 def get_version():
     git = tools.Git()
@@ -14,12 +13,12 @@ class FribidiConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "The Free Implementation of the Unicode Bidirectional Algorithm"
-    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/fribidi/fribidi/archive/v%s.tar.gz" % self.version)
@@ -34,6 +33,3 @@ class FribidiConan(ConanFile):
         if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
-
-    def package_info(self):
-        self.cpp_info.srcdirs.append("src")
