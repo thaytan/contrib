e7a685f37d35e3e369075b2f50ca61ca3735d49c
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 23:12:45 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ def get_version():
     git = tools.Git()
     try:
         tag = git.get_tag()
-        return tag if tag else "2.40.1"
+        return tag if tag else "2.42.1"
     except:
         return None
 
@@ -13,25 +13,24 @@ class GraphvizConan(ConanFile):
     version = get_version()
     license = "EPL"
     description = "Graph Visualization Tools"
-    url = "https://gitlab.com/graphviz/graphviz"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/0.1@%s/stable" % self.user)
-        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
-        self.build_requires("flex/2.6.4@%s/stable" % self.user)
-        self.build_requires("bison/3.3@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("flex/[>=2.6.4]@%s/stable" % self.user)
+        self.build_requires("bison/[>=3.3]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
-        tools.get("https:https://gitlab.com/graphviz/graphviz/-/archive/stable_release_{0}/graphviz-stable_release_{0}.tar.gz".format(self.version))
+        tools.get("https://www2.graphviz.org/Packages/stable/portable_source/graphviz-%s.tar.gz" % self.version)
 
     def build(self):
-        with tools.chdir("graphviz-stable_release_" + self.version):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
             self.run("sh autogen.sh")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure()
             autotools.install()
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
