0e74c46a2f34c226ddd343fb2f4d301a0331f0fc
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 15:51:14 +0200

    Move env-generator to requirement

--- conanfile.py
+++ conanfile.py
@@ -21,12 +21,10 @@ class MesonConan(ConanFile):
     def source(self):
         tools.get("https://github.com/mesonbuild/meson/releases/download/{0}/meson-{0}.tar.gz".format(self.version))
 
-    def build_requirements(self):
-        self.build_requires("env-generator/0.1@%s/stable" % self.user)
-
     def requirements(self):
-        self.requires("ninja/1.9.0@%s/stable" % self.user)
-        self.requires("pkgconf/1.6.3@%s/stable" % self.user)
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("ninja/[>=1.9.0]@%s/stable" % self.user)
+        self.requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
 
     def build(self):
         py_path = os.path.join(self.package_folder, "lib", "python3.6", "site-packages")
