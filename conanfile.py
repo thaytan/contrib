93cfc4fa68c513ff4012bd74296c0acc761c9207
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 13:26:15 +0200

    Add autotools dependency

--- conanfile.py
+++ conanfile.py
@@ -17,8 +17,9 @@ class OrcConan(ConanFile):
     description = "Optimized Inner Loop Runtime Compiler"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/GStreamer/orc/archive/%s.tar.gz" % self.version)
@@ -26,10 +27,10 @@ class OrcConan(ConanFile):
     def build(self):
         args = ["--disable-gtk-doc"]
         with tools.chdir("%s-%s" % (self.name, self.version)):
-                self.run("./autogen.sh " + " ".join(args))
-                autotools = AutoToolsBuildEnvironment(self)
-                autotools.configure(args=args)
-                autotools.install()
+            self.run("./autogen.sh " + " ".join(args))
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.install()
 
     def package(self):
         if self.settings.build_type == "Debug":
