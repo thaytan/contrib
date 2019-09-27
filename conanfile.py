7065aabc3ae100bf5fd086449d3d4685fdfc1cfd
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 19:36:43 +0200

    Move env-generator to requirement

--- conanfile.py
+++ conanfile.py
@@ -18,10 +18,8 @@ class GStreamerLibavConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
-    def build_requirements(self):
-        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-
     def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.58.1]@%s/stable" % self.user)
         self.requires("gstreamer-plugins-base/[>=%s]@%s/stable" % (self.version, self.user))
         self.requires("ffmpeg/[>=4.1]@%s/stable" % self.user)
