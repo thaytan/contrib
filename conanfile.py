07bc16451cdc09e3f11c657fb726b6a74ff7e926
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 20:15:28 +0200

    Add license

--- conanfile.py
+++ conanfile.py
@@ -14,14 +14,17 @@ class PythonGobjectConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Python GObject bindings"
-    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
+
     def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
-        self.requires("gobject-introspection/1.59.3@%s/stable" % self.user)
-        self.requires("cairo/1.17.2@%s/stable" % self.user)
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
+        self.requires("cairo/[>=1.17.2]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://gitlab.gnome.org/GNOME/pygobject/-/archive/{0}/pygobject-{0}.tar.gz".format(self.version))
@@ -38,5 +41,4 @@ class PythonGobjectConan(ConanFile):
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.srcdirs.append("src")
         self.env_info.PYTHONPATH = os.path.join(self.package_folder, "lib", "python3.6", "site-packages")
