0adcfc430029f211c23e4566e6314f87b1900cc6
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 09:35:53 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -14,19 +14,20 @@ class LibNiceConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "An implementation of the IETF’s Interactive Connectivity Establishment (ICE) standard"
-    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     options = {"gstreamer": [True, False]}
     default_options = "gstreamer=True"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("env-generator/[>=0.1]@%s/stable" % self.user)
+
     def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
-        self.requires("glib/2.58.1@%s/stable" % self.user)
-        self.requires("openssl/1.1.1b@%s/stable" % self.user)
+        self.requires("glib/[>=2.58.1]@%s/stable" % self.user)
+        self.requires("openssl/[>=1.1.1b]@%s/stable" % self.user)
         if self.options.gstreamer:
-            self.requires("gstreamer/1.16.0@%s/stable" % self.user)
-            self.requires("gstreamer-plugins-base/1.16.0@%s/stable" % self.user)
+            self.requires("gstreamer-plugins-base/[>=1.16.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/libnice/libnice/archive/%s.tar.gz" % self.version)
@@ -44,6 +45,4 @@ class LibNiceConan(ConanFile):
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
         self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
