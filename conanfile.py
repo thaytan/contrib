45e4a11c35245a72b7da7e00ca326cc8749fef70
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 09:52:47 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -14,17 +14,18 @@ class GStreamerVaapiConan(ConanFile):
     version = get_version()
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Hardware-accelerated video decoding, encoding and processing on Intel graphics through VA-API"
-    license = "https://gitlab.freedesktop.org/gstreamer/gstreamer/raw/master/COPYING"
+    license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("env-generator/[>=0.1]@%s/stable" % self.user)
+
     def requirements(self):
-        self.requires("env-generator/0.1@%s/stable" % self.user)
-        self.requires("glib/2.58.1@%s/stable" % self.user)
-        self.requires("gstreamer/%s@%s/stable" % (self.version, self.user))
-        self.requires("gstreamer-plugins-base/%s@%s/stable" % (self.version, self.user))
-        self.requires("gstreamer-plugins-bad/%s@%s/stable" % (self.version, self.user))
-        self.requires("libva/2.3.0@%s/stable" % self.user)
+        self.requires("glib/[>=2.58.1]@%s/stable" % self.user)
+        self.requires("gstreamer-plugins-base/[>=%s]@%s/stable" % (self.version, self.user))
+        self.requires("gstreamer-plugins-bad/[>=%s]@%s/stable" % (self.version, self.user))
+        self.requires("libva/[>=2.3.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/GStreamer/gstreamer-vaapi/archive/%s.tar.gz" % self.version)
@@ -41,6 +42,4 @@ class GStreamerVaapiConan(ConanFile):
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
         self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
