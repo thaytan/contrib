aed736fea6b86a741416c61975d2a88b158030d0
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-27 13:07:10 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -20,11 +20,8 @@ class AutomakeConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/0.1@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("autoconf/2.69@%s/stable" % self.user)
-        self.requires("m4/1.4.18@%s/stable" % self.user)
+        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("autoconf/[>=2.69]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/automake/automake-%s.tar.gz" % self.version)
@@ -43,7 +40,6 @@ class AutomakeConan(ConanFile):
             self.copy("*.h", "src")
 
     def package_info(self):
-        self.cpp_info.srcdirs.append("src")
         self.env_info.AUTOMAKE = os.path.join(self.package_folder, "bin", "automake")
         self.env_info.AUTOMAKE_DIR = os.path.join(self.package_folder, "share")
         self.env_info.AUTOMAKE_LIBDIR = os.path.join(self.package_folder, "share", "automake-1.16")
