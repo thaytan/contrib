5c2bc6cacaaa9bfa8fc44c24859565cd8f202d2a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-28 00:18:49 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from glob import glob
 from os import path, remove
+from glob import glob
 
 def get_version():
     git = tools.Git()
@@ -20,16 +20,16 @@ class FreetypeNoHarfbuzzConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/0.1@%s/stable" % self.user)
-        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-%s.tar.gz" % self.version.replace(".", "-"))
 
     def build(self):
-        args = [
-            "--disable-static"
-        ]
+        args = ["--disable-static"]
         with tools.chdir("freetype2-VER-" + self.version.replace(".", "-")):
             self.run("sh autogen.sh")
             autotools = AutoToolsBuildEnvironment(self)
@@ -42,7 +42,3 @@ class FreetypeNoHarfbuzzConan(ConanFile):
         if self.settings.build_type == "Debug":
             self.copy("*.c", "src")
             self.copy("*.h", "src")
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
