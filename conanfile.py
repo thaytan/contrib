85ab79bebb5eead0385720f896a3573e60e15285
Niclas Moeslund Overby <noverby@prozum.dk>
2019-09-28 10:57:09 +0200

    Make dependency versions dynamic

--- conanfile.py
+++ conanfile.py
@@ -20,8 +20,10 @@ class GlfwcConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/0.1@%s/stable" % self.user)
-        self.build_requires("cmake/3.15.3@%s/stable" % self.user)
+        self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/glfw/glfw/archive/%s.tar.gz" % self.version)
@@ -34,7 +36,3 @@ class GlfwcConan(ConanFile):
         cmake.configure(source_dir="%s-%s" % (self.name, self.version))
         cmake.build()
         cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-        self.cpp_info.srcdirs.append("src")
