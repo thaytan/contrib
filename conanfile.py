beb24db5e7d402f9e65cce219b4ec60fff02bd2a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-02 15:05:04 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.4.0"
+    except:
+        return None
+
+class XtransConan(ConanFile):
+    name = "xtrans"
+    version = get_version()
+    description = "X transport library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/xtrans-%s.tar.gz" % self.version)
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools.configure()
+            autotools.install()
