1a5bd4083031cfca58dcf226540044278feac20a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-02 23:28:39 +0200

    Increase env-generator version

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, Meson, tools
 
+
 def get_version():
     git = tools.Git()
     try:
@@ -18,9 +19,11 @@ class XorgProtoConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/[>=0.1]@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/archive/individual/proto/xorgproto-%s.tar.bz2" % self.version)
@@ -30,8 +33,3 @@ class XorgProtoConan(ConanFile):
         meson = Meson(self)
         meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
         meson.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
