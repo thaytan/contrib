d85153d4f0070a93d63fad00b2a6b26712c75bfe
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-02 23:20:35 +0200

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,8 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from os import path, remove
 from glob import glob
+from os import path, remove
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
 
 def get_version():
     git = tools.Git()
@@ -20,25 +22,20 @@ class FreetypeConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/[>=0.1]@%s/stable" % self.user)
+        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("harfbuzz/2.6.1@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-%s.tar.gz" % self.version.replace(".", "-"))
 
     def build(self):
-        args = [
-            "--disable-static"
-        ]
+        args = ["--disable-static"]
         with tools.chdir("freetype2-VER-" + self.version.replace(".", "-")):
             self.run("sh autogen.sh")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
             autotools.install()
-        for f in glob(path.join(self.package_folder, "**", "*.la"), recursive=True):
-            remove(f)
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
