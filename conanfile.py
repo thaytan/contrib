62ef18e845c8f043006c93cdb9c21b65b644b2bd
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-02 22:23:39 +0200

    Move dependencies to requirement

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
 
 def get_version():
     git = tools.Git()
@@ -20,11 +21,11 @@ class LibxcbConan(ConanFile):
     def build_requirements(self):
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
         self.build_requires("xcb-proto/[>=1.13]@%s/stable" % self.user)
-        self.build_requires("libxau/[>=1.0.9]@%s/stable" % self.user)
-        self.build_requires("libpthread-stubs/[>=0.4]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("libxau/[>=1.0.9]@%s/stable" % self.user)
+        self.requires("libpthread-stubs/[>=0.4]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xcb.freedesktop.org/dist/libxcb-%s.tar.bz2" % self.version)
