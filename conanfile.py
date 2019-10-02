743af9eebd3e194b2edec937ff68831ceefa328f
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-02 16:24:35 +0200

    Move xorgproto to requirements

--- conanfile.py
+++ conanfile.py
@@ -19,10 +19,10 @@ class LibxauConan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
-        self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/lib/libXau-%s.tar.gz" % self.version)
