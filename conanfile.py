38533a0f7dd3388b0895f9c45cd647c3999b6d7e
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-03 09:02:18 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.6.8"
+    except:
+        return None
+
+class LibxrenderConan(ConanFile):
+    name = "libxrender"
+    version = get_version()
+    description = "X Rendering Extension client library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+        self.build_requires("xtrans/[>=1.4.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
+        self.requires("renderproto/[>=0.11.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libX11-%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--disable-static"]
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("libX11-" + self.version):
+            autotools.configure(args=args)
+            autotools.install()
