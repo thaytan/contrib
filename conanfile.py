ae3d2d46ce0ebfff1672e8146b8938d082f6aa68
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-03 10:56:22 +0200

    Overwrite old config.guest, config.sub

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,5 @@
+import os
+
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
@@ -24,8 +26,10 @@ class RenderprotoConan(ConanFile):
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/proto/renderproto-%s.tar.gz" % self.version)
         with tools.chdir("%s-%s" % (self.name, self.version)):
-            tools.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD", "config.guess", overwrite=True)
-            tools.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD", "config.sub", overwrite=True)
+            os.remove("config.guess")
+            os.remove("config.sub")
+            tools.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD", "config.guess")
+            tools.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD", "config.sub")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
