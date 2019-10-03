6d6dc80d4ab2beea74426d41c7963729dbdf3f77
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-03 14:36:17 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.12.16"
+    except:
+        return None
+
+class DbusConan(ConanFile):
+    name = "dbus"
+    version = get_version()
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "GPL"
+    description = "Freedesktop.org message bus system"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("autoconf-archive/[>=2019.01.06]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("expat/[>=2.2.7]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://gitlab.freedesktop.org/dbus/dbus/-/archive/dbus-{0}/dbus-dbus-{0}.tar.bz2".format(self.version))
+
+    def build(self):
+        args = ["--disable-static"]
+        with tools.chdir("dbus-dbus-" + self.version):
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
