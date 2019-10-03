ebadfb6db73387a6a35030ff48afdbdbf41f8bbf
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-03 08:43:37 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,31 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "0.11.1"
+    except:
+        return None
+
+class RenderprotoConan(ConanFile):
+    name = "renderproto"
+    version = get_version()
+    description = "X11 Render extension wire protocol"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/proto/renderproto-%s.tar.gz" % self.version)
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools.configure()
+            autotools.install()
