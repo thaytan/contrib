f0108d12b99d4902b39be0c88d9286d701b742e4
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-03 09:04:19 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.3.4"
+    except:
+        return None
+
+class LibxextConan(ConanFile):
+    name = "libxext"
+    version = get_version()
+    description = "X11 miscellaneous extensions library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libXext-%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--disable-static"]
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("libXext-" + self.version):
+            autotools.configure(args=args)
+            autotools.install()
