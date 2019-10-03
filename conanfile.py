2ab557dea29a64768d207f88d1294e44b989aabd
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-03 18:15:24 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "2.9.9"
+    except:
+        return None
+
+class Libxml2Conan(ConanFile):
+    name = "libxml2"
+    version = get_version()
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "MIT"
+    description = "XML parsing library, version 2"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://gitlab.gnome.org/GNOME/libxml2/-/archive/v{0}/libxml2-v{0}.tar.bz2".format(self.version))
+
+    def build(self):
+        args = ["--disable-static", "--with-python=no"]
+        with tools.chdir("%s-v%s" % (self.name, self.version)):
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
