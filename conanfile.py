726a5482169ea2e1bef1ca566859ae856531e83a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-04 22:21:58 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "2.0.6"
+    except:
+        return None
+
+class ItstoolConan(ConanFile):
+    name = "itstool"
+    version = get_version()
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "GPL3"
+    description = "XML to PO and back again"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("libxml2/[>=2.9.9]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/itstool/itstool/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
