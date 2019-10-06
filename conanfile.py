9f4e07c0d2d6a87e17e037926de062707e98b987
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-06 20:38:02 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.3"
+    except:
+        return None
+
+class LibxshmfenceConan(ConanFile):
+    name = "libxshmfence"
+    version = get_version()
+    description = "Library that exposes a event API on top of Linux futexes"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libxshmfence-%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--disable-static"]
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools.configure(args=args)
+            autotools.install()
