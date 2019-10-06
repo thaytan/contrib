17c605bbf5f1f5e168c0dba6cb48d6e2bfc0580f
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-06 20:52:50 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,38 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.1.4"
+    except:
+        return None
+
+class Libxxf86vmConan(ConanFile):
+    name = "libxxf86vm"
+    version = get_version()
+    description = "X11 XFree86 video mode extension library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+        self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
+        self.requires("libxfixes/[>=5.0.3]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libXdamage-%s.tar.gz" % self.version)
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("libXdamage-" + self.version):
+            autotools.configure()
+            autotools.install()
