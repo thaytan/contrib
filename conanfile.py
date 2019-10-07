ff1640f2fbea76080a2b2d093f777a2e3ff6907a
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-07 12:55:49 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+def get_version():
+    git = tools.Git()
+    try:
+        tag = git.get_tag()
+        return tag if tag else "1.2.0"
+    except:
+        return None
+
+class LibglvndConan(ConanFile):
+    name = "libglvnd"
+    version = get_version()
+    description = "The GL Vendor-Neutral Dispatch library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
+        self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/NVIDIA/libglvnd/archive/v%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--disable-static"]
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.install()
