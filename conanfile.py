a4b0a4900fe5ed0bd0176aad7edfd7f8e5506d11
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-07 14:18:32 +0200

    Make x11 support optional

--- conanfile.py
+++ conanfile.py
@@ -16,17 +16,25 @@ class LibglvndConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "x11": [True, False],
+    }
+    default_options = (
+        "x11=True",
+    )
     exports = "ignore-warnings.patch"
     generators = "env"
 
     def build_requirements(self):
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
-        self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
+        if self.options.x11:
+            self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
-        self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
+        if self.options.x11:
+            self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
+            self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/NVIDIA/libglvnd/archive/v%s.tar.gz" % self.version)
