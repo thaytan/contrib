d8fa7f5c1a4592c1bda2418fe1e46019019d2f13
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2019-10-08 13:50:14 +0000

    Point to 155-add-psnr fork of devtools

--- conanfile.py
+++ conanfile.py
@@ -17,6 +17,12 @@ class GStreamerDevtoolsConan(ConanFile):
     license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
+    scm = {
+        "type": "git",
+        "url": "https://gitlab.com/aivero/public/gstreamer/gst-devtools-mirror",
+        "revision": "155-add-psnr",
+        "recursive": True
+    }
 
     def build_requirements(self):
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
@@ -25,9 +31,6 @@ class GStreamerDevtoolsConan(ConanFile):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("gstreamer-plugins-base/[>=%s]@%s/stable" % (self.version, self.user))
         self.requires("json-glib/[>=1.4.4]@%s/stable" % self.user)
-
-    def source(self):
-        tools.get("https://github.com/GStreamer/gst-devtools/archive/%s.tar.gz" % self.version)
 
     def build(self):
         args = ["--auto-features=disabled"]
