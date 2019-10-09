56d8ea65c45a30cf081952e703d6147b4f0cec41
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-09 08:16:48 +0200

    Don't use dynamic version ofr libffi

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class RustConan(ConanFile):
     def build_requirements(self):
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
         self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
-        self.build_requires("libffi/[>=3.3-rc0]@%s/stable" % self.user)
+        self.build_requires("libffi/3.3-rc0@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
