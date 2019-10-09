5bbac276506352a4693e7da3e02d0977a460246b
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-09 11:26:32 +0200

    Fix git clone url

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
-import os
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
-from conans.util import files
+import os
 
 def get_version():
     git = tools.Git()
@@ -15,35 +14,29 @@ class GstreamerSharkConan(ConanFile):
     version = get_version()
     description = "GstShark is a front-end for GStreamer traces "
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
-    license = "https://raw.githubusercontent.com/strukturag/libde265/master/COPYING"
+    license = "LGPL"
     settings = "os", "compiler", "build_type", "arch"
-    scm = {
-        "type": "git",
-        "url": "https://github.com/RidgeRun/gst-shark/",
-        "revision": "master",
-        "recursive": True
-    }
     generators = "env"
-    
+
     def build_requirements(self):
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("gstreamer/[>=1.16.0]@%s/stable" % self.user)
-        self.requires("graphviz/[>=2.40.1]@%s/stable" % self.user)
+        self.requires("graphviz/[>=2.42.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/RidgeRun/gst-shark/archive/v%s.tar.gz" % self.version)
+        git = tools.Git(folder=os.path.join("gst-shark-" + self.version, "common"))
+        git.clone("git://anongit.freedesktop.org/gstreamer/common", "master")
 
     def build(self):
-        self.run("./autogen.sh --disable-gtk-doc")
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.configure()
-        autotools.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
+        with tools.chdir("gst-shark-" + self.version):
+            self.run("sh autogen.sh --disable-gtk-doc")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.install()
 
     def package_info(self):
         self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
-        self.cpp_info.srcdirs.append("src")
