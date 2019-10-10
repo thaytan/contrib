5911470cb035d9c2a8c1c19b12c7678c3c72b0a9
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 10:54:10 +0200

    Add gcc dependency + cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,11 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
 import os
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "2.69"
-    except:
-        return None
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
 
 class AutoconfConan(ConanFile):
     name = "autoconf"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "2.69")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL3"
@@ -19,13 +13,19 @@ class AutoconfConan(ConanFile):
     exports = "m4-include-fix.patch"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("m4/[>=1.4.18]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/autoconf/autoconf-%s.tar.gz" % self.version)
-        tools.patch(patch_file="m4-include-fix.patch", base_path="%s-%s" % (self.name, self.version))
+        tools.patch(
+            patch_file="m4-include-fix.patch",
+            base_path="%s-%s" % (self.name, self.version),
+        )
 
     def build(self):
         with tools.chdir("%s-%s" % (self.name, self.version)):
@@ -34,14 +34,15 @@ class AutoconfConan(ConanFile):
             autotools.make()
             autotools.install()
 
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
-
     def package_info(self):
         self.env_info.AUTOCONF = os.path.join(self.package_folder, "bin", "autoconf")
-        self.env_info.AUTOHEADER = os.path.join(self.package_folder, "bin", "autoheader")
+        self.env_info.AUTOHEADER = os.path.join(
+            self.package_folder, "bin", "autoheader"
+        )
         self.env_info.AUTOM4TE = os.path.join(self.package_folder, "bin", "autom4te")
-        self.env_info.AC_MACRODIR = os.path.join(self.package_folder, "share", "autoconf")
-        self.env_info.autom4te_perllibdir = os.path.join(self.package_folder, "share", "autoconf")
+        self.env_info.AC_MACRODIR = os.path.join(
+            self.package_folder, "share", "autoconf"
+        )
+        self.env_info.autom4te_perllibdir = os.path.join(
+            self.package_folder, "share", "autoconf"
+        )
