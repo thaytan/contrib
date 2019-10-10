93fd97c6daf5f8c5ed4c429dd8ec84c1669568f9
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 11:36:08 +0200

    Add gcc dependency + cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,7 @@
-from conans import ConanFile, tools
 import os
+
+from conans import ConanFile, tools
+
 
 def get_version():
     git = tools.Git()
@@ -8,6 +10,7 @@ def get_version():
         return tag if tag else "1.9.0"
     except:
         return None
+
 
 class NinjaConan(ConanFile):
     name = "ninja"
@@ -18,18 +21,25 @@ class NinjaConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+        self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
+
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
-        tools.get("https://github.com/ninja-build/ninja/archive/v%s.tar.gz" % self.version)
+        tools.get(
+            "https://github.com/ninja-build/ninja/archive/v%s.tar.gz" % self.version
+        )
 
     def build(self):
-        with tools.chdir("%s-%s" % (self.name , self.version)):
-            self.run("python3 configure.py --bootstrap")
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            self.run("python configure.py --bootstrap")
 
     def package(self):
-        self.copy(os.path.join("%s-%s" % (self.name, self.version), "ninja"), "bin", keep_path=False)
-        if self.settings.build_type == "Debug":
-            self.copy("*.cpp", "src")
-            self.copy("*.hpp", "src")
+        self.copy(
+            os.path.join("%s-%s" % (self.name, self.version), "ninja"),
+            "bin",
+            keep_path=False,
+        )
