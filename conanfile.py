2351aa965a4d757be76d6b9415c21575852323e9
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 10:43:28 +0200

    Add gcc dependency + cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,22 +1,19 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
 import os
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.4.18"
-    except:
-        return None
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
 
 class M4Conan(ConanFile):
     name = "m4"
-    version = get_version()
+    version = os.environ.get("GIT_TAG", "1.4.18")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL3"
     description = "The GNU macro processor"
     generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
@@ -30,11 +27,6 @@ class M4Conan(ConanFile):
             autotools.configure()
             autotools.make()
             autotools.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
 
     def package_info(self):
         self.env_info.M4 = os.path.join(self.package_folder, "bin", "m4")
