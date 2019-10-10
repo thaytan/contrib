cdeedeb26866f2b0b8923f0058581b5a2b1bafa5
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 11:27:23 +0200

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,20 @@
-from conans import ConanFile, tools, CMake
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.2.11"
-    except:
-        return None
 
 class ZlibConan(ConanFile):
     name = "zlib"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.2.11")
     url = "https://gitlab.com/aivero/public/conan/conan-zlib"
     license = "Zlib"
-    description = ("A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
-                   "(Also Free, Not to Mention Unencumbered by Patents)")
+    description = (
+        "A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
+        "(Also Free, Not to Mention Unencumbered by Patents)"
+    )
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
@@ -28,11 +23,9 @@ class ZlibConan(ConanFile):
         tools.get("https://github.com/madler/zlib/archive/v%s.tar.gz" % self.version)
 
     def build(self):
-        cmake = CMake(self, generator="Ninja")
-        cmake.configure(source_folder="%s-%s" % (self.name, self.version))
-        cmake.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
+        args = ["--enable-shared"]
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
