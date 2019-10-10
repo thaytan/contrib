33fe1d9e84ee96c97c4b6a9dbcf681bb8c5f174e
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 11:16:53 +0200

    Add gcc dependency + cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,19 @@
 from conans import ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.0.0"
-    except:
-        return None
 
 class AutotoolsConan(ConanFile):
     name = "autotools"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.0.0")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL"
-    description = "A suite of programming tools 'designed' to assist in making source code"
+    description = (
+        "A suite of programming tools 'designed' to assist in making source code"
+    )
     generators = "env"
 
     def requirements(self):
+        self.requires("gcc/[>=7.4.0]@%s/stable" % self.user)
         self.requires("autoconf/[>=2.69]@%s/stable" % self.user)
         self.requires("automake/[>=1.16.1]@%s/stable" % self.user)
         self.requires("libtool/[>=2.4.6]@%s/stable" % self.user)
