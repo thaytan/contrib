d6f04b23f7d2e07495f7373f4cf086218fa01212
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 11:30:51 +0200

    Add gcc dependency + cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,28 +1,27 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "2.2.7"
-    except:
-        return None
-
 class ExpatConan(ConanFile):
     name = "expat"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "2.2.7")
     description = "An XML parser library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
+    def build_requirements(self):
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
-        tools.get("https://github.com/libexpat/libexpat/releases/download/R_{0}/expat-{1}.tar.bz2".format(self.version.replace(".", "_"), self.version))
+        tools.get(
+            "https://github.com/libexpat/libexpat/releases/download/R_{0}/expat-{1}.tar.bz2".format(
+                self.version.replace(".", "_"), self.version
+            )
+        )
 
     def build(self):
         args = ["--disable-static"]
