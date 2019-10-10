36ad5daaf1356bbdafd502527add0dfbc7b79d9b
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-11 00:03:12 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+import os
+
+from conans import ConanFile, tools
+
+
+class PythonSetuptoolsConan(ConanFile):
+    name = "python-setuptools"
+    version = tools.get_env("GIT_TAG", "41.2.0")
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "Apache"
+    description = (
+        "Easily download, build, install, upgrade, and uninstall Python packages"
+    )
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "env"
+
+    def source(self):
+        tools.get(
+            "https://github.com/pypa/setuptools/archive/v%s.tar.gz" % self.version
+        )
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("python/[>=3.7.4]@%s/stable" % self.user)
+
+    def build(self):
+        py_path = os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
+        env = {"PYTHONPATH": py_path}
+        os.makedirs(py_path)
+        with tools.chdir("setuptools-" + self.version), tools.environment_append(env):
+            self.run("python bootstrap.py")
+            self.run('python setup.py install --prefix="%s"' % self.package_folder)
+
+    def package_info(self):
+        self.env_info.PYTHONPATH.append(
+            os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
+        )
