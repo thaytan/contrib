890e94a6dcfd3bf25f5c4a8816a807f2bca27715
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-10 08:57:50 +0200

    Use correct url for armv8

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
+import glob
 import os
 
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from conans import ConanFile, tools
 
 
 class BinutilsConan(ConanFile):
@@ -21,7 +22,7 @@ class BinutilsConan(ConanFile):
             ubuntu_binutils = "http://security.ubuntu.com/ubuntu/pool/main/b/binutils/binutils-x86-64-linux-gnu_2.30-21ubuntu1~18.04.2_amd64.deb"
         elif self.settings.arch == "armv8":
             arch_binutils = "http://mirror.archlinuxarm.org/aarch64/core/binutils-2.32-1-aarch64.pkg.tar.xz"
-            ubuntu_binutils = "http://ports.ubuntu.com/ubuntu-ports/pool/main/b/binutils/binutils_2.30-21ubuntu1~18.04.2_arm64.deb"
+            ubuntu_binutils = "http://ports.ubuntu.com/ubuntu-ports/pool/main/b/binutils/binutils-aarch64-linux-gnu_2.30-21ubuntu1~18.04.2_arm64.deb"
         tools.get(arch_binutils)
         tools.download(ubuntu_binutils, filename="binutils.deb")
 
@@ -30,10 +31,9 @@ class BinutilsConan(ConanFile):
         with tools.environment_append(env):
             self.run("ar x binutils.deb")
             tools.unzip("data.tar.xz", destination="data")
-        if self.settings.arch == "x86_64":
-            with tools.chdir("data/usr/bin"):
-                for lib in os.listdir():
-                    os.symlink(lib, lib.split("-")[-1])
+        with tools.chdir("data/usr/bin"):
+            for lib in glob.glob("*-linux-gnu-*"):
+                os.symlink(lib, lib.split("-")[-1])
         tools.rmdir("usr")
 
     def package(self):
