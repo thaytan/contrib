4e65f6c7ebc1d0510dbf840ed7757c971fcbd0bd
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-11 18:03:45 +0200

    Add binutils libraries

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BinutilsConan(ConanFile):
     name = "binutils"
-    version = tools.get_env("VERSION", "2.30")
+    version = tools.get_env("GIT_TAG", "2.30")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL"
@@ -19,18 +19,22 @@ class BinutilsConan(ConanFile):
     def source(self):
         if self.settings.arch == "x86_64":
             arch_binutils = "https://mex.mirror.pkgbuild.com/core/os/x86_64/binutils-2.32-3-x86_64.pkg.tar.xz"
-            ubuntu_binutils = "http://security.ubuntu.com/ubuntu/pool/main/b/binutils/binutils-x86-64-linux-gnu_2.30-21ubuntu1~18.04.2_amd64.deb"
+            binutils = "http://security.ubuntu.com/ubuntu/pool/main/b/binutils/binutils-x86-64-linux-gnu_2.30-21ubuntu1~18.04.2_amd64.deb"
+            libbinutils = "http://security.ubuntu.com/ubuntu/pool/main/b/binutils/libbinutils_2.30-21ubuntu1~18.04.2_amd64.deb"
         elif self.settings.arch == "armv8":
             arch_binutils = "http://mirror.archlinuxarm.org/aarch64/core/binutils-2.32-1-aarch64.pkg.tar.xz"
-            ubuntu_binutils = "http://ports.ubuntu.com/ubuntu-ports/pool/main/b/binutils/binutils-aarch64-linux-gnu_2.30-21ubuntu1~18.04.2_arm64.deb"
+            binutils = "http://ports.ubuntu.com/ubuntu-ports/pool/main/b/binutils/binutils-aarch64-linux-gnu_2.30-21ubuntu1~18.04.2_arm64.deb"
+            libbinutils = "http://ports.ubuntu.com/ubuntu-ports/pool/main/b/binutils/libbinutils_2.30-21ubuntu1~18.04.2_arm64.deb"
         tools.get(arch_binutils)
-        tools.download(ubuntu_binutils, filename="binutils.deb")
+        tools.download(binutils, filename="binutils.deb")
+        tools.download(libbinutils, filename="libbinutils.deb")
 
     def build(self):
         env = {"LD_LIBRARY_PATH": "usr/lib", "PATH": "usr/bin"}
         with tools.environment_append(env):
-            self.run("ar x binutils.deb")
-            tools.unzip("data.tar.xz", destination="data")
+            for pkg in ("binutils", "libbinutils"):
+                self.run("ar x %s.deb" % pkg)
+                tools.unzip("data.tar.xz", destination="data")
         with tools.chdir("data/usr/bin"):
             for lib in glob.glob("*-linux-gnu-*"):
                 os.symlink(lib, lib.split("-")[-1])
@@ -38,3 +42,4 @@ class BinutilsConan(ConanFile):
 
     def package(self):
         self.copy("*", dst="bin", src="data/usr/bin", symlinks=True)
+        self.copy("*.so", dst="lib", src="data/usr/lib", keep_path=False)
