1476bb9ee6cde4c05cb8d46766ac3906bd756be7
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-11 19:17:16 +0200

    Workaround hardcoded paths

--- conanfile.py
+++ conanfile.py
@@ -15,6 +15,7 @@ class GccConan(ConanFile):
         "gcc-7",
         "g++-7",
         "cpp-7",
+        "libc6-dev",
         "libgcc-7-dev",
         "libstdc++-7-dev",
         "libstdc++6",
@@ -38,7 +39,6 @@ class GccConan(ConanFile):
     def source(self):
         if self.settings.arch == "x86_64":
             self.deb_pkgs.extend(["libmpx2", "libquadmath0", "libcilkrts5"])
-        self.run("apt update")
         for pkg in self.deb_pkgs:
             self.run("apt download %s" % pkg)
 
@@ -50,6 +50,21 @@ class GccConan(ConanFile):
         self.copy("*", dst="bin", src="usr/bin", symlinks=True)
         self.copy("*", dst="lib", src="usr/lib", symlinks=True)
         self.copy("*", dst="include", src="usr/include", symlinks=True)
+
+        arch_conv = {"x86_64": "x86_64", "armv8": "aarch64"}
+
+        # Copy arch specific headers to include
+        self.copy(
+            "*",
+            dst="include",
+            src="usr/include/%s-linux-gnu/" % arch_conv[str(self.settings.arch)],
+            symlinks=True,
+        )
+        # Remove hardcoded path to libc_nonshared.a
+        self.run(
+            "sed %s/lib/%s-linux-gnu/libc.so -i -e 's/\/usr\/lib.*libc_nonshared.a/libc_nonshared.a/'"
+            % (self.package_folder, arch_conv[str(self.settings.arch)])
+        )
 
     def package_info(self):
         self.env_info.CC = os.path.join(self.package_folder, "bin", "gcc-7")
