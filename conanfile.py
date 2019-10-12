47812fe704f6a3479ad81f1d69a8a8d67e6696ed
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-12 11:54:28 +0000

    Use GIT_TAG env var for version

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class GccConan(ConanFile):
     name = "gcc"
-    version = tools.get_env("VERSION", "7.4.0")
+    version = tools.get_env("GIT_TAG", "7.4.0")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom", "FDL", "GPL", "LGPL"
