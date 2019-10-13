6bcd0dc3144439e1a7a5f671161bbc10c00347e3
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-14 01:44:49 +0200

    Add python as requirement + cleanup

--- conanfile.py
+++ conanfile.py
@@ -3,19 +3,13 @@ import os
 from conans import ConanFile, Meson, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.59.3"
-    except:
-        return None
-
 class GObjectIntrospectionConan(ConanFile):
     name = "gobject-introspection"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.59.3")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
-    description = "Middleware layer between C libraries (using GObject) and language bindings"
+    description = (
+        "Middleware layer between C libraries (using GObject) and language bindings"
+    )
     license = "GPL, LGPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
@@ -27,18 +21,26 @@ class GObjectIntrospectionConan(ConanFile):
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("python/[>=3.7.4]@%s/stable" % self.user)
         self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
 
     def source(self):
-        tools.get("https://github.com/GNOME/gobject-introspection/archive/%s.tar.gz" % self.version)
+        tools.get(
+            "https://github.com/GNOME/gobject-introspection/archive/%s.tar.gz"
+            % self.version
+        )
 
     def build(self):
         args = ["--auto-features=disabled"]
         meson = Meson(self)
-        meson.configure(source_folder="%s-%s" % (self.name,  self.version), args=args)
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
         meson.install()
 
     def package_info(self):
         self.env_info.XDG_DATA_DIRS.append(os.path.join(self.package_folder, "share"))
-        self.env_info.GI_TYPELIB_PATH.append(os.path.join(self.package_folder, "lib", "girepository-1.0"))
-        self.env_info.PYTHONPATH = os.path.join(self.package_folder, "lib", "gobject-introspection")
+        self.env_info.GI_TYPELIB_PATH.append(
+            os.path.join(self.package_folder, "lib", "girepository-1.0")
+        )
+        self.env_info.PYTHONPATH = os.path.join(
+            self.package_folder, "lib", "gobject-introspection"
+        )
