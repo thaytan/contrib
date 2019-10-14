66ef9a79face6581dae9f6dfe91c21d87846d829
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-14 02:03:51 +0200

    Fix version lookup + cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,11 @@
-from conans import ConanFile, Meson, tools
 import os
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.16.0"
-    except:
-        return None
+from conans import ConanFile, Meson, tools
+
 
 class GStreamerRtspServerConan(ConanFile):
-    name = "gst-rtsp-server"
-    version = get_version()
+    name = "gstreamer-rtsp-server"
+    version = tools.get_env("GIT_TAG", "1.16.0")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "A framework for streaming media"
     license = "LGPL"
@@ -21,12 +15,7 @@ class GStreamerRtspServerConan(ConanFile):
         "tests": [True, False],
         "introspection": [True, False],
     }
-    default_options = (
-        "examples=False",
-        "tests=False",
-        "introspection=True",
-    )
-
+    default_options = ("examples=False", "tests=False", "introspection=True")
     generators = "env"
 
     def build_requirements(self):
@@ -34,7 +23,9 @@ class GStreamerRtspServerConan(ConanFile):
         self.build_requires("bison/[>=3.3]@%s/stable" % self.user)
         self.build_requires("flex/[>=2.6.4]@%s/stable" % self.user)
         if self.options.introspection:
-            self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user,)
+            self.build_requires(
+                "gobject-introspection/[>=1.59.3]@%s/stable" % self.user
+            )
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
@@ -43,19 +34,30 @@ class GStreamerRtspServerConan(ConanFile):
         self.requires("gstreamer-plugins-base/%s@%s/stable" % (self.version, self.user))
 
     def source(self):
-        tools.get("https://gitlab.freedesktop.org/gstreamer/gst-rtsp-server/-/archive/%s/gst-rtsp-server-%s.tar.gz" % (self.version, self.version))
+        tools.get(
+            "https://gitlab.freedesktop.org/gstreamer/gst-rtsp-server/-/archive/%s/gst-rtsp-server-%s.tar.gz"
+            % (self.version, self.version)
+        )
 
     def build(self):
         args = ["--auto-features=disabled"]
         args.append("-Dcheck=" + ("enabled" if self.options.examples else "disabled"))
         args.append("-Dtools=" + ("enabled" if self.options.tests else "disabled"))
-        args.append("-Dintrospection=" + ("enabled" if self.options.introspection else "disabled"))
+        args.append(
+            "-Dintrospection="
+            + ("enabled" if self.options.introspection else "disabled")
+        )
         meson = Meson(self)
-        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
+        meson.configure(source_folder="gst-rtsp-server-%s" % self.version, args=args)
         meson.install()
 
-
     def package_info(self):
-        self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
-        self.env_info.GST_PLUGIN_SCANNER = os.path.join(self.package_folder, "bin", "gstreamer-1.0", "gst-plugin-scanner")
-        self.env_info.GI_TYPELIB_PATH.append(os.path.join(self.package_folder, "lib", "girepository-1.0"))
+        self.env_info.GST_PLUGIN_PATH.append(
+            os.path.join(self.package_folder, "lib", "gstreamer-1.0")
+        )
+        self.env_info.GST_PLUGIN_SCANNER = os.path.join(
+            self.package_folder, "bin", "gstreamer-1.0", "gst-plugin-scanner"
+        )
+        self.env_info.GI_TYPELIB_PATH.append(
+            os.path.join(self.package_folder, "lib", "girepository-1.0")
+        )
