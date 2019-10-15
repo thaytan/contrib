a410abd0f705f7ef99995508797f961c30335edb
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2019-10-15 08:38:44 +0200

    Add libx11

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, Meson, tools
 import os
 
+
 class GStreamerDevtoolsConan(ConanFile):
     name = "gstreamer-devtools"
     version = tools.get_env("GIT_TAG", "1.16.0")
@@ -16,8 +17,11 @@ class GStreamerDevtoolsConan(ConanFile):
         "recursive": True,
         "subfolder": ("gst-devtools-" + version)
     }
-    options = {"intel": [True, False], "ffmpeg": [True, False]}
-    default_options = "intel=True", "ffmpeg=False"
+    options = {"gtk_doc": [True, False],
+               "introspection": [True, False],
+               "tests": [True, False],
+               "nls": [True, False]}
+    default_options = "gtk_doc=False", "introspection=False", "tests=True", "nls=False"
 
     def build_requirements(self):
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
@@ -28,52 +32,13 @@ class GStreamerDevtoolsConan(ConanFile):
         self.requires("gstreamer/[>=%s]@%s/stable" % (self.version, self.user))
         self.requires("json-glib/[>=1.4.4]@%s/stable" % self.user)
         self.requires("cairo/[>=1.17.2]@%s/stable" % self.user)
-
-        # Temporarely in here to give easy access to encoders etc.
-        self.requires("depth-receiver/[>=0.7.1]@%s/stable" % self.user)
-        if self.settings.arch == "x86_64":
-            if self.options.intel:
-                self.requires("intel-vaapi-driver/[>=2.3.0]@%s/stable" % self.user)
-                self.requires("gstreamer-vaapi/%s@%s/stable" % (self.version, self.user))
-            if self.options.ffmpeg:
-                self.requires("gstreamer-libav/%s@%s/stable" % (self.version, self.user))
-        elif self.settings.arch == "armv8":
-            self.requires("gstreamer-omx-tx2/%s@%s/stable" % (self.version, self.user))
-            self.requires("jetson-drivers/[>=32.2.1]@%s/stable" % self.user)
+        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
 
     def build(self):
         meson = Meson(self)
-        meson.configure(source_folder="gst-devtools-" + self.version)
+        args = ["-Dgtk_doc=" + ("enabled" if self.options.gtk_doc else "disabled")]
+        args.append("-Dintrospection=" + ("enabled" if self.options.introspection else "disabled"))
+        args.append("-Dtests=" + ("enabled" if self.options.tests else "disabled"))
+        args.append("-Dnls=" + ("enabled" if self.options.nls else "disabled"))
+        meson.configure(source_folder="gst-devtools-" + self.version, args=args)
         meson.install()
-
-    def deploy(self):
-        install_path = os.getcwd()
-        # Binaries
-        self.copy("*gst-*", dst="bin", keep_path=False)
-
-        # Gstreamer binaries
-        self.copy_deps("*gst-inspect-1.0", dst="bin", keep_path=False)
-        self.copy_deps("*gst-launch-1.0", dst="bin", keep_path=False)
-        self.copy_deps("*gst-plugin-scanner", dst="bin", keep_path=False)
-
-        # Libraries
-        self.copy_deps("*.so*")
-        self.copy("*.so*", keep_path=True)
-
-        # Pkg - config files
-        deps = ["depth-*", "gstreamer*", "orc-0.4"]
-        for dep in deps:
-            self.copy_deps("*%s.pc" % dep, dst="lib/pkgconfig", keep_path=False)
-
-        for pc_file in os.listdir("lib/pkgconfig"):
-            tools.replace_prefix_in_pc_file(os.path.join("lib", "pkgconfig", pc_file), install_path)
-
-        # Environment script
-        with open(os.path.join(install_path, "dddq_environment.sh"), "w+") as env_file:
-            env_file.write("export PREFIX=" + install_path)
-            env_file.write("\nexport PATH=" + os.path.join("$PREFIX", "bin") + ":$PATH")
-            env_file.write("\nexport LD_LIBRARY_PATH=" + os.path.join("$PREFIX", "lib") + ":"  + os.path.join("$PREFIX", "lib/gstreamer-1.0") + ":$LD_LIBRARY_PATH")
-            env_file.write("\nexport PKG_CONFIG_PATH=" + os.path.join("$PREFIX", "lib", "pkgconfig"))
-            env_file.write("\nexport GST_PLUGIN_PATH=" + os.path.join("$PREFIX", "lib", "gstreamer-1.0") + ":" + os.path.join("$PREFIX", "lib", "depth_receiver"))
-            env_file.write("\nexport GST_PLUGIN_SCANNER=" + os.path.join("$PREFIX", "bin", "gst-plugin-scanner"))
-            env_file.write("\nexport GST_VALIDATE_PLUGIN_PATH=" + os.path.join("$PREFIX", "lib", "gstreamer-1.0", "validate"))
