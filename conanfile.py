0cc20d41fd2c3ee356c134af36509dd96ed9dd75
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-15 18:18:18 +0200

    Add JETSON_DRIVER_PATH env var

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,19 @@
-from conans import ConanFile, tools
-from os import symlink, path, listdir
 import re
+from os import listdir, path, symlink
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "32.2.1"
-    except:
-        return None
+from conans import ConanFile, tools
+
 
 class JetsonDrivers(ConanFile):
     name = "jetson-drivers"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "32.2.1")
     license = "LGPL"
     description = "NVIDIA built Accelerated GStreamer Plugins"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources= ["public_sources.tbz2"]
+    exports_sources = ["public_sources.tbz2"]
     options = {"jetson": ["Nano", "TX2", "Xavier"]}
-    default_options = ("jetson=TX2")
+    default_options = "jetson=TX2"
     generators = "env"
 
     def requirements(self):
@@ -27,12 +21,20 @@ class JetsonDrivers(ConanFile):
 
     def source(self):
         if self.options.jetson in ("TX2", "Xavier"):
-            tools.get("https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/TX2-AGX/Tegra186_Linux_R%s_aarch64.tbz2" % (self.version.replace(".", "-"), self.version))
+            tools.get(
+                "https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/TX2-AGX/Tegra186_Linux_R%s_aarch64.tbz2"
+                % (self.version.replace(".", "-"), self.version)
+            )
         elif self.options.jetson == "Nano":
-            tools.get("https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/Nano-TX1/Tegra210_Linux_R%s_aarch64.tbz2" % (self.version.replace(".", "-"), self.version))
+            tools.get(
+                "https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/Nano-TX1/Tegra210_Linux_R%s_aarch64.tbz2"
+                % (self.version.replace(".", "-"), self.version)
+            )
         else:
             raise KeyError("Unknown option: " + self.options.jetson)
-        tools.untargz("Linux_for_Tegra/nv_tegra/nvidia_drivers.tbz2", self.source_folder)
+        tools.untargz(
+            "Linux_for_Tegra/nv_tegra/nvidia_drivers.tbz2", self.source_folder
+        )
         tools.rmdir("Linux_for_Tegra")
 
     def package(self):
@@ -44,5 +46,10 @@ class JetsonDrivers(ConanFile):
                 old = re.search(r".*\.so((\.[0-9]+)+)$", dl)
                 new = re.search(r".*\.so", dl)
                 if old:
-                    symlink(old.group(0), new.group(0) )
-                    print("Created symlink from " + old.group(0) + " to " + new.group(0))
+                    symlink(old.group(0), new.group(0))
+                    print(
+                        "Created symlink from " + old.group(0) + " to " + new.group(0)
+                    )
+
+    def package_info(self):
+        self.env_info.JETSON_DRIVER_PATH = path.join(self.package_folder, "lib")
