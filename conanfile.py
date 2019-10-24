c404e2d8e959abd25338afdac56186292d007108
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-24 15:31:51 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class NcursesConan(ConanFile):
+    name = "ncurses"
+    version = tools.get_env("GIT_TAG", "6.1")
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "Zlib"
+    description = "System V Release 4.0 curses emulation library"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get(
+            "https://ftp.gnu.org/pub/gnu/ncurses/ncurses-%s.tar.gz" % self.version
+        )
+
+    def build(self):
+        args = [
+            "--with-shared",
+            "--with-cxx-shared",
+            "--with-cxx-binding",
+            "--enable-pc-files",
+            "--with-pkg-config-libdir=%s"
+            % os.path.join(self.package_folder, "lib", "pkgconfig"),
+        ]
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
