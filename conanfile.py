04a866af712b0943afb191ce7038bb3c40f6d47d
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-24 10:51:04 +0200

    Only build release + move python dep

--- conanfile.py
+++ conanfile.py
@@ -9,16 +9,16 @@ class GdbConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "The GNU Debugger"
     license = "GPL3"
-    settings = "os", "arch", "compiler", "build_type"
+    settings = "os", "arch", "compiler"
     generators = "env"
 
     def build_requirements(self):
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
-        self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
         self.build_requires("texinfo/[>=6.6]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("python/[>=3.7.4]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/gdb/gdb-%s.tar.gz" % self.version)
