b68c4a5fe9bdcbcbe0fd912e3b59250037041f72
Niclas Moeslund Overby <noverby@prozum.dk>
2019-10-30 09:21:42 +0100

    Move mesa to requirement

--- conanfile.py
+++ conanfile.py
@@ -17,11 +17,11 @@ class GstreamerNvJetsonEgl(ConanFile):
 
     def build_requirements(self):
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
-        self.build_requires("mesa/[>=19.2.0]@%s/stable" % self.user)
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("mesa/[>=19.2.0]@%s/stable" % self.user)
         self.requires("nv-jetson-drivers/[>=%s]@%s/stable" % (self.version, self.user))
         self.requires("nv-jetson-v4l2/[>=%s]@%s/stable" % (self.version, self.user))
         self.requires(
