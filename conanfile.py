23db9bcf35007c6c971fd3ba12985ceaa14ee0b5
Niclas Moeslund Overby <noverby@prozum.dk>
2019-11-01 16:10:15 +0100

    Bump version

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,9 @@
 from conans import ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "4.0"
-    except:
-        return None
 
 class Deepstream(ConanFile):
     name = "deepstream"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "4.0.1")
     license = "proprietary"
     description = "Complete streaming analytics toolkit for AI-based video"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
@@ -24,11 +17,17 @@ class Deepstream(ConanFile):
 
     def source(self):
         if self.options.jetson in ("TX2", "Xavier"):
-            tools.get("https://developer.download.nvidia.com/assets/Deepstream/Deepstream_{0}/deepstream_sdk_v{0}_jetson.tbz2".format(self.version))
+            tools.get(
+                "https://developer.download.nvidia.com/assets/Deepstream/Deepstream_{0}/deepstream_sdk_v{0}_jetson.tbz2".format(
+                    self.version
+                )
+            )
         else:
             raise KeyError("Unknown option: " + self.options.jetson)
 
-        tools.untargz("deepstream_sdk_v%s_jetson/binaries.tbz2" % self.version, self.source_folder)
+        tools.untargz(
+            "deepstream_sdk_v%s_jetson/binaries.tbz2" % self.version, self.source_folder
+        )
         tools.rmdir("deepstream_sdk_v%s_jetson" % self.version)
 
     def package(self):
