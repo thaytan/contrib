da26e4ef0f52c8e22b63b0a93048f72534a0fe75
Niclas Moeslund Overby <noverby@prozum.dk>
2019-11-11 11:27:59 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.14"
-    except:
-        return None
-
 class SharedMimeInfoConan(ConanFile):
     name = "shared-mime-info"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.14")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL2"
@@ -20,12 +12,12 @@ class SharedMimeInfoConan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("itstool/[>=2.0.6]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
         self.requires("libxml2/[>=2.9.9]@%s/stable" % self.user)
-        self.requires("itstool/[>=2.0.6]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/Release-{0}/shared-mime-info-Release-{0}.tar.bz2".format(self.version.replace(".", "-")))
