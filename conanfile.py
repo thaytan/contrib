3b8eeb8afd47646b548da4a8c3d55f88b8d9532f
Niclas Moeslund Overby <noverby@prozum.dk>
2019-11-12 11:28:30 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class ReadlineConan(ConanFile):
+    name = "readline"
+    version = tools.get_env("GIT_TAG", "8.0")
+    url = "https://gitlab.com/aivero/public/conan/conan" + name
+    description = "GNU readline library"
+    license = "GPL-3.0-or-later"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.requires("ncurses/[>=6.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://ftp.gnu.org/gnu/readline/readline-%s.tar.gz" % self.version)
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools.configure()
+            autotools.install()
