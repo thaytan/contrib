88209ee7814b10134357ef483d464cf45601f23c
Tobias Morell <tobias.morell@aivero.com>
2019-11-13 09:51:57 +0000

    Update conanfile.py: Add gcc and clang dependencies

--- conanfile.py
+++ conanfile.py
@@ -20,6 +20,8 @@ class RustConan(ConanFile):
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+        self.requires("clang/[>=9.0.0]@%s/stable" % self.user)
+        self.requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def source(self):
         tools.download("https://sh.rustup.rs", "rustup.sh")
