cb38ede1b6fc68665d2c8ab397a038e33529a749
Andrej Orsula <andrej.orsula@aivero.com>
2019-11-13 11:00:15 +0100

    Move `PROPERTIES` to a separate file

--- src/realsensesrc.rs
+++ src/realsensesrc.rs
@@ -14,180 +14,22 @@
 // Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
 // Boston, MA 02110-1301, USA.
 
+use std::sync::Mutex;
+
 use glib::subclass;
 use gst::subclass::prelude::*;
 use gst_base::prelude::*;
 use gst_base::subclass::prelude::*;
+
 use gst_depth_meta::buffer::BufferMeta;
 use gst_depth_meta::tags::TagsMeta;
-
-use std::sync::Mutex;
-
 use rs2;
 use rs2::high_level_utils::StreamInfo;
 
 use crate::errors::*;
+use crate::properties::*;
 use crate::rs_meta::rs_meta_serialization::*;
 use crate::settings::*;
-
-static PROPERTIES: [subclass::Property; 16] = [
-    subclass::Property("serial", |name| {
-        glib::ParamSpec::string(
-            name,
-            "Serial Number",
-            "Serial number of a realsense device. If unchanged or empty, `rosbag-location` is used to locate a file to play from.",
-            None,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("rosbag-location", |name| {
-        glib::ParamSpec::string(
-            name,
-            "Rosbag File Location",
-            "Location of a rosbag file to play from. If unchanged or empty, physical device specified by `serial` is used.",
-            None,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("json-location", |name| {
-        glib::ParamSpec::string(
-            name,
-            "JSON File Location",
-            "Location of a JSON file to load the RealSense device configuration from. This property applies only if `serial` is specified. If unchanged or empty, previous JSON configuration is used. If no previous configuration is present due to hardware reset, default configuration is used.",
-            None,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("enable-depth", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Enable Depth",
-            "Enables depth stream.",
-            DEFAULT_ENABLE_DEPTH,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("enable-infra1", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Enable Infra1",
-            "Enables infra1 stream.",
-            DEFAULT_ENABLE_INFRA1,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("enable-infra2", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Enable Infra2",
-            "Enables infra2 stream.",
-            DEFAULT_ENABLE_INFRA2,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("enable-color", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Enable Color",
-            "Enables color stream.",
-            DEFAULT_ENABLE_COLOR,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("depth-width", |name| {
-        glib::ParamSpec::int(
-            name,
-            "Depth Width",
-            "Width of the depth and infra1/infra2 frames.",
-            DEPTH_MIN_WIDTH,
-            DEPTH_MAX_WIDTH,
-            DEFAULT_DEPTH_WIDTH,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("depth-height", |name| {
-        glib::ParamSpec::int(
-            name,
-            "Depth Height",
-            "Height of the depth and infra1/infra2 frames.",
-            DEPTH_MIN_HEIGHT,
-            DEPTH_MAX_HEIGHT,
-            DEFAULT_DEPTH_HEIGHT,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("color-width", |name| {
-        glib::ParamSpec::int(
-            name,
-            "Color Width",
-            "Width of the color frame.",
-            COLOR_MIN_WIDTH,
-            COLOR_MAX_WIDTH,
-            DEFAULT_COLOR_WIDTH,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("color-height", |name| {
-        glib::ParamSpec::int(
-            name,
-            "Color Height",
-            "Height of the color frame.",
-            COLOR_MIN_HEIGHT,
-            COLOR_MAX_HEIGHT,
-            DEFAULT_COLOR_HEIGHT,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("framerate", |name| {
-        glib::ParamSpec::int(
-            name,
-            "Framerate",
-            "Common framerate of the selected streams.",
-            MIN_FRAMERATE,
-            MAX_FRAMERATE,
-            DEFAULT_FRAMERATE,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("loop-rosbag", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Loop Rosbag",
-            "Enables looping of playing from rosbag recording specified by `rosbag-location` property. This property applies only if `rosbag-location` and no `serial` are specified.",
-            DEFAULT_LOOP_ROSBAG,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("wait-for-frames-timeout", |name| {
-        glib::ParamSpec::uint(
-            name,
-            "Wait For Frames Timeout",
-            "Timeout used while waiting for frames from a RealSense device in milliseconds.",
-            std::u32::MIN,
-            std::u32::MAX,
-            DEFAULT_PIPELINE_WAIT_FOR_FRAMES_TIMEOUT,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("include-per-frame-metadata", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Include Per Frame Metadata",
-            "Adds librealsense2's per-frame metadata as an additional buffer on the video stream.",
-            DEFAULT_ENABLE_METADATA,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-    subclass::Property("do-custom-timestamp", |name| {
-        glib::ParamSpec::boolean(
-            name,
-            "Perform custom timestamp handling",
-            "Adds timestamps to all buffers based on the duration since the element was created. As oppose to `do-timestamp`, this property adds the timestamps to all meta Buffers.",
-            DEFAULT_DO_CUSTOM_TIMESTAMP,
-            glib::ParamFlags::READWRITE,
-        )
-    }),
-];
 
 /// A struct representation of the `realsensesrc` element
 struct RealsenseSrc {
