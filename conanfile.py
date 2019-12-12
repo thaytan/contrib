e5223704f8684071e951459cecd1c61725e22a77
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2019-12-12 15:02:42 +0100

    Build GLM, add .pc

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, tools
+import os
+
+class GlmConan(ConanFile):
+    name = "glm"
+    version = tools.get_env("GIT_TAG", "0.9.9.6")
+    settings = "os", "compiler", "build_type", "arch"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "MIT License"
+    description = "OpenGL Mathematics (GLM) is a header only C++ mathematics library for graphics software based on the OpenGL Shading Language (GLSL) specifications."
+    options = {}
+    default_options = ()
+    generators = "env"
+    exports_sources = ["glm.pc"]
+
+    def requirements(self):
+        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/g-truc/glm/releases/download/%s/glm-%s.zip" % (self.version, self.version))
+
+    def package(self):
+        self.copy(pattern="*",src="glm/glm", dst=os.path.join(self.package_folder, "include", "glm"), keep_path=True)
+        self.copy(pattern="glm.pc", dst=os.path.join(self.package_folder, "lib", "pkgconfig"), keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.name = "glm"
+        self.cpp_info.includedirs = ['include']
