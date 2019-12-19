13a333c13caf0bcb5ee02aad272f65b7d7b24225
Niclas Moeslund Overby <noverby@prozum.dk>
2019-12-19 15:32:32 +0100

    Add pkgconfig files

--- conanfile.py
+++ conanfile.py
@@ -3,33 +3,31 @@ import os
 from conans import ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "10.1.243"
-    except:
-        return None
-
 class CudaConan(ConanFile):
     name = "cuda"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "10.1.243")
     version_driver = "418.87.00"
     description = "NVIDIA's GPU programming toolkit"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     settings = "os", "compiler", "build_type", "arch"
+    exports_sources = ("cuda-10.1.pc", "cudart-10.1.pc")
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
-        tools.download("http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_%s_%s_linux.run" % (self.version, self.version_driver),
-                       filename="cuda_%s_%s_linux.run" % (self.version, self.version_driver))
+        tools.download(
+            "http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_%s_%s_linux.run"
+            % (self.version, self.version_driver),
+            filename="cuda_%s_%s_linux.run" %
+            (self.version, self.version_driver))
 
     def build(self):
-        self.run("sh cuda_%s_%s_linux.run --silent --override-driver-check --extract=\"%s\"" % (self.version, self.version_driver, self.build_folder))
+        self.run(
+            "sh cuda_%s_%s_linux.run --silent --override-driver-check --extract=\"%s\""
+            % (self.version, self.version_driver, self.build_folder))
         tools.rmdir("cublas")
         tools.rmdir("cuba-samples")
         os.remove("cuda_%s_%s_linux.run" % (self.version, self.version_driver))
@@ -41,3 +39,4 @@ class CudaConan(ConanFile):
             self.copy("*", dst="lib64", src="%s/lib64" % toolkit)
             self.copy("*", dst="include", src="%s/include" % toolkit)
         self.copy("*.bc", src="cuda-toolkit")
+        self.copy(pattern="*.pc", dst="lib/pkgconfig")
