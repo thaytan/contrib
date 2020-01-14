de80549db643b642b995eac8560b5d85ee0d732c
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2020-01-14 07:47:02 +0000

    Set upper gst version bound

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,27 @@
 import os
-
 from conans import ConanFile, Meson, tools
 
+def get_upper_version_bound(version, version_diff="0.1.0"):
+    try:
+        v = tools.Version(version)
+    except:
+        print("Input version is not a valid SemVer")
+    try:
+        v_diff = tools.Version(version_diff)
+        version_out = "%d.%d.%d" % ((int(v.major) + int(v_diff.major)),(int(v.minor) + int(v_diff.minor)), (int(v.patch) + int(v_diff.patch)))
+        if v.prerelease:
+            version_out = version_out + "-" + v.prerelease
+        elif v_diff.prerelease:
+            version_out = version_out + "-" + v_diff.prerelease
+        return version_out
+    except Exception as e:
+        print(e)
+        print("Version diff is not a valid SemVer")
 
 class GStreamerPluginsGoodConan(ConanFile):
     name = "gstreamer-plugins-good"
     version = tools.get_env("GIT_TAG", "1.16.0")
+    upper_version_bound = get_upper_version_bound(version)
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Plug-ins is a set of plugins that we consider to have good quality code and correct functionality"
     license = "LGPL"
@@ -44,7 +60,7 @@ class GStreamerPluginsGoodConan(ConanFile):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
         self.requires(
-            "gstreamer-plugins-base/[>=%s]@%s/stable" % (self.version, self.user)
+            "gstreamer-plugins-base/[>=%s <s]@%s/stable" % (self.version, self.upper_version_bound, self.user)
         )
         self.requires("libpng/[>=1.6.37]@%s/stable" % self.user)
         if self.options.vpx:
