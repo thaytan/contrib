70dbab749110270ca10bcfb42e832cb4c1ee6428
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2020-01-14 08:18:57 +0100

    Add get_upper_version_bound function

--- conanfile.py
+++ conanfile.py
@@ -1,30 +1,43 @@
 from conans import ConanFile, Meson, tools
 import os
 
-def get_version():
-    git = tools.Git()
+def get_upper_version_bound(version, version_diff):
     try:
-        tag = git.get_tag()
-        return tag if tag else "1.16.0"
+        v = tools.Version(version)
     except:
-        return None
+        print("Input version is not a valid SemVer")
+    try:
+        v_diff = tools.Version(version_diff)
+        version_out = "%d.%d.%d" % ((int(v.major) + int(v_diff.major)),(int(v.minor) + int(v_diff.minor)), (int(v.patch) + int(v_diff.patch)))
+        if v.prerelease:
+            version_out = version_out + "-" + v.prerelease
+        elif v_diff.prerelease:
+            version_out = version_out + "-" + v_diff.prerelease
+        return version_out
+    except Exception as e:
+        print(e)
+        print("Version diff is not a valid SemVer")
 
 class GStreamerLibavConan(ConanFile):
+
+
     name = "gstreamer-libav"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.16.0")
+    upper_version_bound = get_upper_version_bound(version, "0.1.0")
+
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "GStreamer plugin for the libav* library (former FFmpeg)"
     license = "GPL"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
-    
+
     def build_requirements(self):
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.58.1]@%s/stable" % self.user)
-        self.requires("gstreamer-plugins-base/[>=%s]@%s/stable" % (self.version, self.user))
+        self.requires("gstreamer-plugins-base/[>=%s <%s]@%s/stable" % (self.version, self.upper_version_bound, self.user))
         self.requires("ffmpeg/[>=4.1]@%s/stable" % self.user)
 
     def source(self):
