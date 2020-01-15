da86d4feb171cf2f69b9550bb52fa200d37f5ce1
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:12:17 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -3,25 +3,17 @@ import os
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "0.11.1"
-    except:
-        return None
-
 class RenderprotoConan(ConanFile):
     name = "renderproto"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "0.11.1")
     description = "X11 Render extension wire protocol"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/proto/renderproto-%s.tar.gz" % self.version)
