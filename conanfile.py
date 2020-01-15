27f2c89f410d767adbf339af251427f193002b5d
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:09:39 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,31 +1,18 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "3.3-rc0"
-    except:
-        return None
-
-
 class LibffiConan(ConanFile):
     name = "libffi"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "3.3-rc0")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
-    description = (
-        "A portable, high level programming interface to various calling conventions"
-    )
+    description = "A portable, high level programming interface to various calling conventions"
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/libffi/libffi/archive/v%s.tar.gz" % self.version)
