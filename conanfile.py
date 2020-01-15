a0b0aea8c48d5389fa41d571efd80d4c7e17fb77
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:18:10 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,9 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "2.6.4"
-    except:
-        return None
 
 class FlexConan(ConanFile):
     name = "flex"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "2.6.4")
     url = "https://gitlab.com/aivero/public/conan/conan-flex"
     description = "Flex, the fast lexical analyzer generator"
     license = "BSD 2-Clause"
@@ -18,11 +11,9 @@ class FlexConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
         self.build_requires("bison/[>=3.3]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/westes/flex/releases/download/v{0}/flex-{0}.tar.gz".format(self.version))
