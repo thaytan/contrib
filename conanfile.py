5d268a6141be47790176c4ed6c81b70fae024038
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 13:57:58 +0100

    Add support for building master branch on testing branch

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,7 @@ class GStreamerPluginsBaseConan(ConanFile):
     name = "gstreamer-plugins-base"
     version = tools.get_env("GIT_TAG", "master")
     gst_version = "master" if version == "master" else "[~%s]" % version
+    gst_channel = "testing" if version == "master" else "stable"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "A well-groomed and well-maintained collection of GStreamer plugins and elements"
     license = "LGPL"
@@ -45,53 +46,39 @@ class GStreamerPluginsBaseConan(ConanFile):
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         self.build_requires("mesa/[>=19.2.0]@%s/stable" % self.user)
         if self.options.introspection:
-            self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" %
-                                self.user)
+            self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-        self.requires("gstreamer/%s@%s/stable" % (self.gst_version, self.user))
+        self.requires("gstreamer/%s@%s/%s" % (self.gst_version, self.user, self.get_channel))
         if self.options.orc:
             self.requires("orc/[>=0.4.29]@%s/stable" % self.user)
         if self.options.opus:
             self.requires("opus/[>=1.3.1]@%s/stable" % self.user)
         if self.options.pango:
-            self.requires(
-                "pango/[>=1.43.0, include_prerelease=True]@%s/stable" %
-                self.user)
+            self.requires("pango/[>=1.43.0, include_prerelease=True]@%s/stable" % self.user)
         if self.options.x11:
             self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
 
     def source(self):
-        tools.get(
-            "https://github.com/GStreamer/gst-plugins-base/archive/%s.tar.gz" %
-            self.version)
+        tools.get("https://github.com/GStreamer/gst-plugins-base/archive/%s.tar.gz" % self.version)
 
     def build(self):
         args = ["--auto-features=disabled", "-Dgl_platform=egl"]
-        args.append("-Dintrospection=" +
-                    ("enabled" if self.options.introspection else "disabled"))
+        args.append("-Dintrospection=" + ("enabled" if self.options.introspection else "disabled"))
         args.append("-Dgl=" + ("enabled" if self.options.gl else "disabled"))
         args.append("-Dx11=" + ("enabled" if self.options.x11 else "disabled"))
-        args.append("-Dvideotestsrc=" +
-                    ("enabled" if self.options.videotestsrc else "disabled"))
-        args.append("-Dvideoconvert=" +
-                    ("enabled" if self.options.videoconvert else "disabled"))
+        args.append("-Dvideotestsrc=" + ("enabled" if self.options.videotestsrc else "disabled"))
+        args.append("-Dvideoconvert=" + ("enabled" if self.options.videoconvert else "disabled"))
         args.append("-Dapp=" + ("enabled" if self.options.app else "disabled"))
-        args.append("-Dplayback=" +
-                    ("enabled" if self.options.playback else "disabled"))
-        args.append("-Dtypefind=" +
-                    ("enabled" if self.options.typefind else "disabled"))
+        args.append("-Dplayback=" + ("enabled" if self.options.playback else "disabled"))
+        args.append("-Dtypefind=" + ("enabled" if self.options.typefind else "disabled"))
         args.append("-Dorc=" + ("enabled" if self.options.orc else "disabled"))
-        args.append("-Dopus=" +
-                    ("enabled" if self.options.opus else "disabled"))
-        args.append("-Dpango=" +
-                    ("enabled" if self.options.pango else "disabled"))
+        args.append("-Dopus=" + ("enabled" if self.options.opus else "disabled"))
+        args.append("-Dpango=" + ("enabled" if self.options.pango else "disabled"))
         meson = Meson(self)
-        meson.configure(source_folder="gst-plugins-base-" + self.version,
-                        args=args)
+        meson.configure(source_folder="gst-plugins-base-" + self.version, args=args)
         meson.install()
 
     def package_info(self):
-        self.env_info.GST_PLUGIN_PATH.append(
-            os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
+        self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
