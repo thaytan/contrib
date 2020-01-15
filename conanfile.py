4b58343ffe1f3bdb8916d892e842aa9fd367dabc
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:27:19 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.1.4"
-    except:
-        return None
-
 class Libxxf86vmConan(ConanFile):
     name = "libxxf86vm"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.1.4")
     description = "X11 XFree86 video mode extension library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
@@ -19,12 +11,12 @@ class Libxxf86vmConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
         self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
         self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
 
     def source(self):
