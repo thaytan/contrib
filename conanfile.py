2010c0fe24b922ac3c8b68fce86516adbdc113a5
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:26:36 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -3,31 +3,20 @@ import os
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "6.6"
-    except:
-        return None
-
-
 class TexinfoConan(ConanFile):
     name = "texinfo"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "6.6")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL3"
     description = "GNU documentation system for on-line information and printed output"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
-        tools.get(
-            "https://ftp.gnu.org/pub/gnu/texinfo/texinfo-%s.tar.xz" % self.version
-        )
+        tools.get("https://ftp.gnu.org/pub/gnu/texinfo/texinfo-%s.tar.xz" % self.version)
 
     def build(self):
         with tools.chdir("%s-%s" % (self.name, self.version)):
@@ -38,10 +27,6 @@ class TexinfoConan(ConanFile):
 
     def package_info(self):
         self.env_info.MAKEINFO = os.path.join(self.package_folder, "bin", "makeinfo")
-        self.env_info.PERL5LIB.append(
-            os.path.join(self.package_folder, "share", "texinfo")
-        )
+        self.env_info.PERL5LIB.append(os.path.join(self.package_folder, "share", "texinfo"))
         for mod in ["libintl-perl", "Text-Unidecode", "Unicode-EastAsianWidth"]:
-            self.env_info.PERL5LIB.append(
-                os.path.join(self.package_folder, "share", "texinfo", "lib", mod, "lib")
-            )
+            self.env_info.PERL5LIB.append(os.path.join(self.package_folder, "share", "texinfo", "lib", mod, "lib"))
