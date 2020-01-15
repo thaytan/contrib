3e544a6fe417a01eb61a0b0b7b774c07f6790ac7
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:22:17 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -3,17 +3,9 @@ import os
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "2019.01.06"
-    except:
-        return None
-
 class AutoconfArchiveConan(ConanFile):
     name = "autoconf-archive"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "2019.01.06")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL3"
     description = "A collection of freely re-usable Autoconf macros"
@@ -21,10 +13,8 @@ class AutoconfArchiveConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("autoconf/[>=2.69]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-%s.tar.xz" % self.version)
