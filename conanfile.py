121a3198b73e5915e6727b06d9e23e136e8325f5
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:27:00 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,17 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "0.4"
-    except:
-        return None
 
 class LibpthreadStubsConan(ConanFile):
     name = "libpthread-stubs"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "0.4")
     description = "X11 client-side library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xcb.freedesktop.org/dist/libpthread-stubs-%s.tar.bz2" % self.version)
