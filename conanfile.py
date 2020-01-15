b0f38c9d32d98859c42e1c82b1e082467990c138
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 16:37:30 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+import os
+
+from conans import ConanFile, Meson, tools
+
+
+class PythonCairoConan(ConanFile):
+    name = "python-cairo"
+    version = tools.get_env("GIT_TAG", "1.18.2")
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    description = "Python bindings for the cairo graphics library"
+    license = "LGPL2.1"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "env"
+
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("python/[>=3.7.4]@%s/stable" % self.user)
+        self.requires("cairo/[>=1.16.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/pygobject/pycairo/releases/download/v{0}/pycairo-{0}.tar.gz".format(self.version))
+
+    def build(self):
+        with tools.chdir("pycairo-%s" % self.version):
+            self.run('python setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
+
+    def package_info(self):
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.7", "site-packages"))
