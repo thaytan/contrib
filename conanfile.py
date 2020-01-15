c466014e57484e8504018f748568ee8e4317d8d3
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-15 14:18:37 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,11 @@
-from conans import ConanFile, Meson, tools
 import os
+
+from conans import ConanFile, Meson, tools
+
 
 class GStreamerLibavConan(ConanFile):
     name = "gstreamer-libav"
     version = tools.get_env("GIT_TAG", "1.16.0")
-
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "GStreamer plugin for the libav* library (former FFmpeg)"
     license = "GPL"
@@ -12,10 +13,10 @@ class GStreamerLibavConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.58.1]@%s/stable" % self.user)
         self.requires("gstreamer-plugins-base/[~%s]@%s/stable" % (self.version, self.user))
         self.requires("ffmpeg/[>=4.1]@%s/stable" % self.user)
