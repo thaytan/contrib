e6f4abe147280ee27886882d3034ae8e75c68406
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:34:06 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.5.2"
-    except:
-        return None
-
 class LibxrandrConan(ConanFile):
     name = "libxrandr"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.5.2")
     description = "X11 RandR extension library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
@@ -19,11 +11,11 @@ class LibxrandrConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
         self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
         self.requires("libxrender/[>=0.9.10]@%s/stable" % self.user)
         self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
