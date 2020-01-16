7d31c1afc755ee24fddb80022dec277c72543c40
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:43:40 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,9 @@
 from conans import ConanFile, Meson, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "0.38.4"
-    except:
-        return None
 
 class PixmanConan(ConanFile):
     name = "pixman"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "0.38.4")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Image processing and manipulation library"
     license = "custom"
@@ -18,10 +11,8 @@ class PixmanConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/lib/pixman-%s.tar.bz2" % self.version)
@@ -29,10 +20,5 @@ class PixmanConan(ConanFile):
     def build(self):
         args = ["--auto-features=disabled"]
         meson = Meson(self)
-        meson.configure(source_folder="pixman-%s" % self.version, args=args)
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
         meson.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
