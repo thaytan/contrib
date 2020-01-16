920e3c141c0f821f07ac7d3eebacc732915639ec
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:44:47 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,9 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.3.1"
-    except:
-        return None
 
 class OpusConan(ConanFile):
     name = "opus"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.3.1")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Modern audio compression for the internet"
     license = "BSD"
@@ -18,7 +11,7 @@ class OpusConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
-        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://archive.mozilla.org/pub/opus/opus-%s.tar.gz" % self.version)
@@ -29,9 +22,3 @@ class OpusConan(ConanFile):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
             autotools.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.cpp", "src")
-            self.copy("*.hpp", "src")
-            self.copy("*.h", "src")
