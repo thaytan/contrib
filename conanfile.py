096cc89dab2c291b3a77e2062fefbaa339d25105
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:51:05 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,9 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "4.1"
-    except:
-        return None
 
 class FFMpegConan(ConanFile):
     name = "ffmpeg"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "4.1")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "A complete, cross-platform solution to record, convert and stream audio and video"
     license = "GPL3"
@@ -18,21 +11,14 @@ class FFMpegConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("yasm/[>=1.3.0]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % self.version)
 
     def build(self):
-        args = [
-            "--disable-static",
-            "--enable-shared",
-            "--disable-doc",
-            "--disable-programs"
-        ]
+        args = ["--disable-static", "--enable-shared", "--disable-doc", "--disable-programs"]
         if self.settings.build_type == "Debug":
             args.extend(["--disable-optimizations", "--disable-mmx", "--disable-stripping", "--enable-debug"])
         with tools.chdir("%s-%s" % (self.name, self.version)):
@@ -40,8 +26,3 @@ class FFMpegConan(ConanFile):
             autotools.configure(args=args)
             autotools.make()
             autotools.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
