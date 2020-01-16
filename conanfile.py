00871c8358ae2fda4f8c8dc4df64f997bca08768
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:37:20 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,17 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "0.7.0"
-    except:
-        return None
 
 class CapNProtoConan(ConanFile):
     name = "capnproto"
-    version = get_version()
-    url = "https://gitlab.com/aivero/public/conan/conan-zlib"
+    version = tools.get_env("GIT_TAG", "0.7.0")
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
-    description = ("Cap'n Proto serialization/RPC system")
+    description = "Cap'n Proto serialization/RPC system"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://capnproto.org/capnproto-c++-%s.tar.gz" % self.version)
@@ -29,8 +22,3 @@ class CapNProtoConan(ConanFile):
             autotools.configure()
             autotools.make()
             autotools.install()
-
-    def package(self):
-        if self.settings.build_type == "Debug":
-            self.copy("*.c", "src")
-            self.copy("*.h", "src")
