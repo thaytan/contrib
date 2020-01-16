0e817d4748afba22119d369c5be1f611b5871e34
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:05:51 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,17 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.4.0"
-    except:
-        return None
 
 class XtransConan(ConanFile):
     name = "xtrans"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.4.0")
     description = "X transport library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/lib/xtrans-%s.tar.gz" % self.version)
