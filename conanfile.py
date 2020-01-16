fb1d410889475c82e10a35c943e8e818b31e7762
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:58:15 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import ConanFile, Meson, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "0.8.4"
-    except:
-        return None
-
 class LibxcbConan(ConanFile):
     name = "libxkbcommon"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "0.8.4")
     description = "Keymap handling library for toolkits and window systems"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
@@ -19,13 +11,13 @@ class LibxcbConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         self.build_requires("bison/[>=3.3]@%s/stable" % self.user)
         self.build_requires("flex/[>=2.6.4]@%s/stable" % self.user)
         #self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("libxcb/[>=1.13.1]@%s/stable" % self.user)
 
     def source(self):
