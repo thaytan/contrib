66a0035225838672fa71e3bce1466143687fba9c
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:31:39 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -13,19 +13,17 @@ class Libxml2Conan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
         self.build_requires("zlib/[>=1.2.11]@%s/stable" % self.user)
         self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://gitlab.gnome.org/GNOME/libxml2/-/archive/v{0}/libxml2-v{0}.tar.bz2".format(self.version))
 
     def build(self):
         args = ["--disable-static"]
-        env = {"with_python_install_dir": os.path.join(self.package_folder, "lib", "python3.6", "site-packages")}
+        env = {"with_python_install_dir": os.path.join(self.package_folder, "lib", "python3.7", "site-packages")}
         with tools.chdir("%s-v%s" % (self.name, self.version)), tools.environment_append(env):
             self.run("sh autogen.sh")
             autotools = AutoToolsBuildEnvironment(self)
@@ -34,4 +32,4 @@ class Libxml2Conan(ConanFile):
             autotools.install()
 
     def package_info(self):
-        self.env_info.PYTHONPATH = os.path.join(self.package_folder, "lib", "python3.6", "site-packages")
+        self.env_info.PYTHONPATH = os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
