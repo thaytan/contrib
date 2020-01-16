e00d82f3e66cb818e259edba76d5e6b297319b22
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:48:48 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,32 +1,26 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.19.1"
-    except:
-        return None
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
 
 class LibXorgUtilMacrosConan(ConanFile):
     name = "xorg-util-macros"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.19.1")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     description = "X.Org Autotools macros"
     settings = "os", "compiler", "build_type", "arch"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/util/util-macros-%s.tar.gz" % self.version)
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir("util-macros-" + self.version):
+        with tools.chdir("util-macros-%s" % self.version):
             autotools.configure()
             autotools.install()
 
