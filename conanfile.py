ef4428edc4168687e057422bd791d7526edfa5ad
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:58:51 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.13.1"
-    except:
-        return None
-
 class LibxcbConan(ConanFile):
     name = "libxcb"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.13.1")
     description = "X11 client-side library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
@@ -19,11 +11,11 @@ class LibxcbConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
         self.build_requires("xcb-proto/[>=1.13]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("libxau/[>=1.0.9]@%s/stable" % self.user)
         self.requires("libpthread-stubs/[>=0.4]@%s/stable" % self.user)
 
