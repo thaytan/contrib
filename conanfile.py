7a26c36844f38167176b92b6cd32654bf41ab7ac
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:24:52 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.6.8"
-    except:
-        return None
-
 class Libx11Conan(ConanFile):
     name = "libx11"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.6.8")
     description = "X11 client-side library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
@@ -19,21 +11,21 @@ class Libx11Conan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
         self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
         self.build_requires("xtrans/[>=1.4.0]@%s/stable" % self.user)
         self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("libxcb/[>=1.13.1]@%s/stable" % self.user)
 
     def source(self):
-        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libX11-%s.tar.gz" % self.version)
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libX11-{}.tar.gz".format(self.version), destination="{}-{}".format(self.name, self.version))
 
     def build(self):
         args = ["--disable-static"]
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir("libX11-" + self.version):
+        with tools.chdir("{}-{}".format(self.name, self.version)):
             autotools.configure(args=args)
             autotools.install()
