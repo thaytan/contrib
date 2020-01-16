a3d387a2592bfb2326b2ec658d2a257e632fbce4
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:28:00 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -3,18 +3,9 @@ import os
 from conans import ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.9.0"
-    except:
-        return None
-
-
 class NinjaConan(ConanFile):
     name = "ninja"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.9.0")
     license = "Apache"
     description = "Small build system with a focus on speed"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
@@ -22,16 +13,12 @@ class NinjaConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
         self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-
     def source(self):
-        tools.get(
-            "https://github.com/ninja-build/ninja/archive/v%s.tar.gz" % self.version
-        )
+        tools.get("https://github.com/ninja-build/ninja/archive/v%s.tar.gz" % self.version)
 
     def build(self):
         with tools.chdir("%s-%s" % (self.name, self.version)):
