2ab69bedda37e4c0991c42bb1d28a56b21084250
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-16 16:45:40 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import ConanFile, Meson, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.5.3"
-    except:
-        return None
-
 class LibepoxyConan(ConanFile):
     name = "libepoxy"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.5.3")
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
@@ -19,12 +11,10 @@ class LibepoxyConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         self.build_requires("libx11/[>=1.6.8]@%s/stable" % self.user)
         self.build_requires("mesa/[>=19.2.0]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/anholt/libepoxy/archive/%s.tar.gz" % self.version)
