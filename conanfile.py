26aff390923b6b1a22ab99987a1d6b3e05283d5e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:34:17 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -11,16 +11,14 @@ class ClangConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("llvm/[>=9.0.0]@%s/stable" % self.user)
 
     def source(self):
-        tools.get(
-            "https://releases.llvm.org/{0}/cfe-{0}.src.tar.xz".format(self.version)
-        )
+        tools.get("https://releases.llvm.org/{0}/cfe-{0}.src.tar.xz".format(self.version))
 
     def build(self):
         cmake = CMake(self, generator="Ninja", build_type="Release")
