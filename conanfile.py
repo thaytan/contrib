f207650e27a8f9006bf183dc8b3c2101f00a268e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:30:02 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -16,20 +16,15 @@ class PythonPipConan(ConanFile):
         tools.get("https://github.com/pypa/pip/archive/%s.tar.gz" % self.version)
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("python-setuptools/[>=41.2.0]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("python/[>=3.7.4]@%s/stable" % self.user)
 
     def build(self):
         with tools.chdir("pip-" + self.version):
-            self.run(
-                'python setup.py install --optimize=1 --prefix= --root="%s"'
-                % self.package_folder
-            )
+            self.run('python setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
 
     def package_info(self):
-        self.env_info.PYTHONPATH.append(
-            os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
-        )
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.7", "site-packages"))
