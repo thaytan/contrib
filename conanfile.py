becc3c6aa745d974a232f534fb632e2396c6ed39
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:13:51 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -13,15 +13,11 @@ class NcursesConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-
     def source(self):
-        tools.get(
-            "https://ftp.gnu.org/pub/gnu/ncurses/ncurses-%s.tar.gz" % self.version
-        )
+        tools.get("https://ftp.gnu.org/pub/gnu/ncurses/ncurses-%s.tar.gz" % self.version)
 
     def build(self):
         args = [
@@ -30,8 +26,7 @@ class NcursesConan(ConanFile):
             "--with-cxx-shared",
             "--with-cxx-binding",
             "--enable-pc-files",
-            "--with-pkg-config-libdir=%s"
-            % os.path.join(self.package_folder, "lib", "pkgconfig"),
+            "--with-pkg-config-libdir=%s" % os.path.join(self.package_folder, "lib", "pkgconfig"),
         ]
         with tools.chdir("%s-%s" % (self.name, self.version)):
             autotools = AutoToolsBuildEnvironment(self)
@@ -40,6 +35,4 @@ class NcursesConan(ConanFile):
             autotools.install()
 
     def package_info(self):
-        self.env_info.TERMINFO = os.path.join(
-            self.package_folder, "share", "terminfo"
-        )
+        self.env_info.TERMINFO = os.path.join(self.package_folder, "share", "terminfo")
