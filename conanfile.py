bfdb753ccde340f82c516756e6d38e418e44808f
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:19:44 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -14,17 +14,15 @@ class AutoconfConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("m4/[>=1.4.18]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/autoconf/autoconf-%s.tar.gz" % self.version)
-        tools.patch(
-            patch_file="m4-include.patch", base_path="%s-%s" % (self.name, self.version)
-        )
+        tools.patch(patch_file="m4-include.patch", base_path="%s-%s" % (self.name, self.version))
 
     def build(self):
         with tools.chdir("%s-%s" % (self.name, self.version)):
@@ -35,13 +33,7 @@ class AutoconfConan(ConanFile):
 
     def package_info(self):
         self.env_info.AUTOCONF = os.path.join(self.package_folder, "bin", "autoconf")
-        self.env_info.AUTOHEADER = os.path.join(
-            self.package_folder, "bin", "autoheader"
-        )
+        self.env_info.AUTOHEADER = os.path.join(self.package_folder, "bin", "autoheader")
         self.env_info.AUTOM4TE = os.path.join(self.package_folder, "bin", "autom4te")
-        self.env_info.AC_MACRODIR = os.path.join(
-            self.package_folder, "share", "autoconf"
-        )
-        self.env_info.autom4te_perllibdir = os.path.join(
-            self.package_folder, "share", "autoconf"
-        )
+        self.env_info.AC_MACRODIR = os.path.join(self.package_folder, "share", "autoconf")
+        self.env_info.autom4te_perllibdir = os.path.join(self.package_folder, "share", "autoconf")
