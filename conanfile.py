b9aa36f0f806b57d4f400a588da23d81c5046cbc
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:17:31 +0100

    Add gcc dependency

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,9 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "1.8.0"
-    except:
-        return None
-
 class LibVpxConan(ConanFile):
     name = "libvpx"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "1.8.0")
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "WebM VP8/VP9 Codec SDK"
     license = "BSD"
@@ -19,10 +11,9 @@ class LibVpxConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("gcc/7.4.0@%s/stable" % self.user)
         self.build_requires("yasm/[>=1.3.0]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/webmproject/libvpx/archive/v%s.tar.gz" % self.version)
