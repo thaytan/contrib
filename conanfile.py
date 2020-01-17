050b036e836d6926a649f6011035abdc1813d790
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:38:41 +0100

    Move env-generator to build requirement 4 real

--- conanfile.py
+++ conanfile.py
@@ -13,15 +13,11 @@ class RapidJsonConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-
     def source(self):
-        tools.get(
-            "https://github.com/Tencent/rapidjson/archive/%s.tar.gz" % self.version
-        )
+        tools.get("https://github.com/Tencent/rapidjson/archive/%s.tar.gz" % self.version)
 
     def build(self):
         cmake = CMake(self, generator="Ninja")
