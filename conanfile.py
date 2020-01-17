332e70f8e4eed37ea4757cfa3ccd8b3c1f327250
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 13:15:06 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -11,17 +11,15 @@ class GitConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
         self.build_requires("gettext/[>=0.20.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("zlib/[>=1.2.11]@%s/stable" % self.user)
 
     def source(self):
-        tools.get(
-            "https://www.kernel.org/pub/software/scm/git/git-%s.tar.xz" % self.version
-        )
+        tools.get("https://www.kernel.org/pub/software/scm/git/git-%s.tar.xz" % self.version)
 
     def build(self):
         with tools.chdir("%s-%s" % (self.name, self.version)):
