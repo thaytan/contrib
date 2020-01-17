5d82cb534a749fcb3b5c8010385009a1f660cbb1
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:48:59 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -14,12 +14,12 @@ class GdkPixbufConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
-        self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user,)
-        self.build_requires("gettext/[>=0.20.1]@%s/stable" % self.user,)
+        self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
+        self.build_requires("gettext/[>=0.20.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
         self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
         self.requires("libpng/[>=1.6.37]@%s/stable" % self.user)
