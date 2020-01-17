a58950dbb1ea88f74f7294f5269a792b9e9d340b
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:07:51 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -9,25 +9,19 @@ class Deepstream(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "build_type", "arch"
     options = {"jetson": ["Nano", "TX2", "Xavier"]}
-    default_options = ("jetson=TX2",)
+    default_options = ("jetson=TX2", )
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/%s" % (self.user, self.channel))
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         if self.options.jetson in ("TX2", "Xavier"):
-            tools.get(
-                "https://developer.download.nvidia.com/assets/Deepstream/Deepstream_{0}/deepstream_sdk_v{0}_jetson.tbz2".format(
-                    self.version
-                )
-            )
+            tools.get("https://developer.download.nvidia.com/assets/Deepstream/Deepstream_{0}/deepstream_sdk_v{0}_jetson.tbz2".format(self.version))
         else:
             raise KeyError("Unknown option: " + self.options.jetson)
 
-        tools.untargz(
-            "deepstream_sdk_v%s_jetson/binaries.tbz2" % self.version, self.source_folder
-        )
+        tools.untargz("deepstream_sdk_v%s_jetson/binaries.tbz2" % self.version, self.source_folder)
 
     def package(self):
         self.copy("*.so*", dst="lib", keep_path=False)
