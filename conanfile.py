206f674c04bd994169d9ebe9bc061412f6d22dd6
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:37:09 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -13,10 +13,8 @@ class M4Conan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/m4/m4-%s.tar.gz" % self.version)
