f2064e69ceeb4d8728ffd88ec7f0f72fc69c60aa
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:31:18 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -16,24 +16,16 @@ class NvJetsonMultimediaApi(ConanFile):
     default_options = "jetson=TX2"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         if self.options.jetson in ("TX2", "Xavier"):
-            tools.get(
-                "https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/TX2-AGX/Tegra_Multimedia_API_R%s_aarch64.tbz2"
-                % (self.version.replace(".", "-"), self.version)
-            )
+            tools.get("https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/TX2-AGX/Tegra_Multimedia_API_R%s_aarch64.tbz2" % (self.version.replace(".", "-"), self.version))
         elif self.options.jetson == "Nano":
-            tools.get(
-                "https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/Nano-TX1/Tegra_Multimedia_API_R%s_aarch64.tbz2"
-                % (self.version.replace(".", "-"), self.version)
-            )
+            tools.get("https://developer.nvidia.com/embedded/dlc/r%s_Release_v1.0/Nano-TX1/Tegra_Multimedia_API_R%s_aarch64.tbz2" % (self.version.replace(".", "-"), self.version))
         else:
             raise KeyError("Unknown option: " + self.options.jetson)
 
     def package(self):
-        self.copy(
-            "*", dst="include", src="tegra_multimedia_api/include", symlinks=False
-        )
+        self.copy("*", dst="include", src="tegra_multimedia_api/include", symlinks=False)
