2f310b1cbfaf95e0c09512de655db27d390b6afb
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 13:12:38 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -11,10 +11,10 @@ class CclsConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("clang/[>=9.0.0]@%s/stable" % self.user)
         self.requires("rapidjson/master@%s/stable" % self.user)
 
