30a3378d46511d000df21a26319daa0cda9a81c5
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:29:20 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -6,18 +6,13 @@ class ZlibConan(ConanFile):
     version = tools.get_env("GIT_TAG", "1.2.11")
     url = "https://gitlab.com/aivero/public/conan/conan-zlib"
     license = "Zlib"
-    description = (
-        "A Massively Spiffy Yet Delicately Unobtrusive Compression Library "
-        "(Also Free, Not to Mention Unencumbered by Patents)"
-    )
+    description = "A Massively Spiffy Yet Delicately Unobtrusive Compression Library " "(Also Free, Not to Mention Unencumbered by Patents)"
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/madler/zlib/archive/v%s.tar.gz" % self.version)
