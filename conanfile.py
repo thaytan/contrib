b94468e182b0fcccad646e85f594278fca55bd34
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:46:08 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -7,25 +7,21 @@ class LLVMConan(ConanFile):
     name = "llvm"
     version = tools.get_env("GIT_TAG", "9.0.0")
     license = "custom", "Apache"
-    description = (
-        "Collection of modular and reusable compiler and toolchain technologies"
-    )
+    description = "Collection of modular and reusable compiler and toolchain technologies"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "arch"
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("libffi/3.3-rc0@%s/stable" % self.user)
         self.requires("zlib/[>=1.2.11]@%s/stable" % self.user)
 
     def source(self):
-        tools.get(
-            "https://releases.llvm.org/{0}/llvm-{0}.src.tar.xz".format(self.version)
-        )
+        tools.get("https://releases.llvm.org/{0}/llvm-{0}.src.tar.xz".format(self.version))
 
     def build(self):
         cmake = CMake(self, generator="Ninja", build_type="Release")
