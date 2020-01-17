0da4c3ae1e201aae771c25cf0ea58d202d346172
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:18:23 +0100

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,17 @@
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 
-def get_version():
-    git = tools.Git()
-    try:
-        tag = git.get_tag()
-        return tag if tag else "0.14"
-    except:
-        return None
-
 class LibPciAccessConan(ConanFile):
     name = "libpciaccess"
-    version = get_version()
+    version = tools.get_env("GIT_TAG", "0.14")
     description = "Generic PCI access library"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://xorg.freedesktop.org/releases/individual/lib/libpciaccess-%s.tar.gz" % self.version)
