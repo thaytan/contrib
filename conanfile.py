1337a2f641c1edec39d4afd2184b893572bf74b8
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:01:56 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -13,26 +13,16 @@ class SwigConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
         self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
-
     def source(self):
-        tools.get(
-            "https://downloads.sourceforge.net/swig/swig-%s.tar.gz" % self.version
-        )
+        tools.get("https://downloads.sourceforge.net/swig/swig-%s.tar.gz" % self.version)
 
     def build(self):
-        env = {
-            "PATH": tools.get_env("PATH")
-            + os.path.pathsep
-            + os.path.join(self.package_folder, "bin")
-        }
-        with tools.chdir("%s-%s" % (self.name, self.version)), tools.environment_append(
-            env
-        ):
+        env = {"PATH": tools.get_env("PATH") + os.path.pathsep + os.path.join(self.package_folder, "bin")}
+        with tools.chdir("%s-%s" % (self.name, self.version)), tools.environment_append(env):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure()
             autotools.install()
