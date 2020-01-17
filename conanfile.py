1e916aed20943450222e2ba59a6df6868ae836f8
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:35:30 +0100

    Move env-generator to build requirement 4 real

--- conanfile.py
+++ conanfile.py
@@ -12,8 +12,8 @@ class XzConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "env"
 
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://tukaani.org/xz/xz-%s.tar.gz" % self.version)
