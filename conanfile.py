7be6279bb3631665f73b5a281f5e07793e0c9411
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:59:44 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -13,12 +13,10 @@ class CurlConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
         self.build_requires("zlib/[>=1.2.11]@%s/stable" % self.user)
         self.build_requires("openssl/[>=1.1.1b]@%s/stable" % self.user)
-
-    def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://curl.haxx.se/download/curl-%s.tar.gz" % self.version)
