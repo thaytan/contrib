d8cb19af1f81efae6dd23ae13fd533ffe1e43833
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 12:13:11 +0100

    Move env-generator to build requirement

--- conanfile.py
+++ conanfile.py
@@ -13,14 +13,12 @@ class MesonConan(ConanFile):
     generators = "env"
 
     def source(self):
-        tools.get(
-            "https://github.com/mesonbuild/meson/releases/download/{0}/meson-{0}.tar.gz".format(
-                self.version
-            )
-        )
+        tools.get("https://github.com/mesonbuild/meson/releases/download/{0}/meson-{0}.tar.gz".format(self.version))
+
+    def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("python/[>=3.7.4]@%s/stable" % self.user)
         self.requires("python-setuptools/[>=41.2.0]@%s/stable" % self.user)
         self.requires("ninja/[>=1.9.0]@%s/stable" % self.user)
@@ -31,15 +29,8 @@ class MesonConan(ConanFile):
         py_path = os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
         env = {"PYTHONPATH": os.environ["PYTHONPATH"] + os.pathsep + py_path}
         os.makedirs(py_path)
-        with tools.chdir("%s-%s" % (self.name, self.version)), tools.environment_append(
-            env
-        ):
-            self.run(
-                'python setup.py install --optimize=1 --prefix= --root="%s"'
-                % self.package_folder
-            )
+        with tools.chdir("%s-%s" % (self.name, self.version)), tools.environment_append(env):
+            self.run('python setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
 
     def package_info(self):
-        self.env_info.PYTHONPATH.append(
-            os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
-        )
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.7", "site-packages"))
