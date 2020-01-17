47b5a11a0cd3df418c1340c3d756eb83c0bc01c0
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-17 11:36:55 +0100

    Move env-generator to build requirement 4 real

--- conanfile.py
+++ conanfile.py
@@ -23,6 +23,7 @@ class GStreamerConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         self.build_requires("bison/[>=3.3]@%s/stable" % self.user)
         self.build_requires("flex/[>=2.6.4]@%s/stable" % self.user)
@@ -30,7 +31,6 @@ class GStreamerConan(ConanFile):
             self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user, )
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
 
     def source(self):
