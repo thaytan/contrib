3aa783bf364eabb0d98123f74b6eb28722644900
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-30 14:10:03 +0100

    Add make requirement

--- conanfile.py
+++ conanfile.py
@@ -7,13 +7,12 @@ class AutotoolsConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL"
-    description = (
-        "A suite of programming tools 'designed' to assist in making source code"
-    )
+    description = "A suite of programming tools 'designed' to assist in making source code"
     generators = "env"
 
     def requirements(self):
         self.requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+        self.requires("make/[>=3.4.0]@%s/stable" % self.user)
         self.requires("autoconf/[>=2.69]@%s/stable" % self.user)
         self.requires("automake/[>=1.16.1]@%s/stable" % self.user)
         self.requires("libtool/[>=2.4.6]@%s/stable" % self.user)
