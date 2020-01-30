f97b85586d6472ba88ff1307acf398613df1ce27
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-30 15:26:47 +0100

    Build in release mode + add binutils env vars

--- conanfile.py
+++ conanfile.py
@@ -33,8 +33,22 @@ class BinutilsConan(ConanFile):
             "--with-system-zlib",
         ]
         with tools.chdir("%s-%s" % (self.name, self.version)):
-            autotools = AutoToolsBuildEnvironment(self)
+            autotools = AutoToolsBuildEnvironment(self, build_type="Release")
             autotools.configure(args=args)
             autotools.make(target="configure-host")
             autotools.make(["tooldir=" + self.package_folder])
             autotools.install(["tooldir=" + self.package_folder])
+
+    def package_info(self):
+        self.env_info.LD = os.path.join(self.package_folder, "bin", "ld")
+        self.env_info.AS = os.path.join(self.package_folder, "bin", "as")
+        self.env_info.ADDR2LINE = os.path.join(self.package_folder, "bin", "addr2line")
+        self.env_info.AR = os.path.join(self.package_folder, "bin", "ar")
+        self.env_info.NM = os.path.join(self.package_folder, "bin", "nm")
+        self.env_info.OBJCOPY = os.path.join(self.package_folder, "bin", "objcopy")
+        self.env_info.OBJDUMP = os.path.join(self.package_folder, "bin", "objdump")
+        self.env_info.RANLIB = os.path.join(self.package_folder, "bin", "ranlib")
+        self.env_info.READELF = os.path.join(self.package_folder, "bin", "readelf")
+        self.env_info.SIZE = os.path.join(self.package_folder, "bin", "size")
+        self.env_info.STRINGS = os.path.join(self.package_folder, "bin", "strings")
+        self.env_info.STRIP = os.path.join(self.package_folder, "bin", "strip")
