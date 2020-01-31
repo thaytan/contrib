cf734ea3b2dae51167bd00c740f51634d7da89b9
Niclas Moeslund Overby <noverby@prozum.dk>
2020-01-31 16:33:07 +0100

    Build with bootstrap gcc

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import AutoToolsBuildEnvironment, ConanFile, tools
 class M4Conan(ConanFile):
     name = "m4"
     version = tools.get_env("GIT_TAG", "1.4.18")
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL3"
     description = "The GNU macro processor"
@@ -14,8 +14,7 @@ class M4Conan(ConanFile):
 
     def build_requirements(self):
         self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
-        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
-        self.build_requires("make/[>=4.3]@%s/stable" % self.user)
+        self.build_requires("bootstrap-gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/m4/m4-%s.tar.gz" % self.version)
@@ -25,7 +24,7 @@ class M4Conan(ConanFile):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure()
             autotools.make()
-            autotools.install()
+            autotools.make(target="install-strip")
 
     def package_info(self):
         self.env_info.M4 = os.path.join(self.package_folder, "bin", "m4")
