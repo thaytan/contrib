2e50761086098f243b3541bbc061dbfb96310fde
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-03 13:22:42 +0000

    Change name to gcc

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, tools
 
 
 class GccConan(ConanFile):
-    name = "bootstrap-gcc"
+    name = "gcc"
     version = tools.get_env("GIT_TAG", "7.4.0")
     settings = "os", "compiler", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
