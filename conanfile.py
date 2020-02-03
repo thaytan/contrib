ee39cfb0e6c83d26581b37ac6c7fd2ea7c36d573
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-03 19:36:23 +0100

    Move env-generator to build_requirement

--- conanfile.py
+++ conanfile.py
@@ -13,26 +13,18 @@ class PythonMakoConan(ConanFile):
     generators = "env"
 
     def source(self):
-        tools.get(
-            "https://github.com/sqlalchemy/mako/archive/rel_%s.tar.gz"
-            % self.version.replace(".", "_")
-        )
+        tools.get("https://github.com/sqlalchemy/mako/archive/rel_%s.tar.gz" % self.version.replace(".", "_"))
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("python-setuptools/[>=41.2.0]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("python/[>=3.7.4]@%s/stable" % self.user)
 
     def build(self):
         with tools.chdir("mako-rel_" + self.version.replace(".", "_")):
-            self.run(
-                'python setup.py install --optimize=1 --prefix= --root="%s"'
-                % self.package_folder
-            )
+            self.run('python setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
 
     def package_info(self):
-        self.env_info.PYTHONPATH.append(
-            os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
-        )
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.7", "site-packages"))
