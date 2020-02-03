b926f7ccc656c57e09fbab099bd611b5e10647fb
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-03 14:57:07 +0100

    Add mpc requirement

--- conanfile.py
+++ conanfile.py
@@ -15,6 +15,9 @@ class MpcConan(ConanFile):
         self.build_requires("bootstrap-gcc/[>=7.4.0]@%s/stable" % self.user)
         self.build_requires("make/[>=4.3]@%s/stable" % self.user)
 
+    def requirements(self):
+        self.requires("gmp/[>=6.1.2]@%s/stable" % self.user)
+
     def source(self):
         tools.get("https://ftp.gnu.org/gnu/mpc/mpc-%s.tar.gz" % self.version)
 
