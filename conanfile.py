f1cb45f33ce35a0db8655c9769e7d753268fef19
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-03 20:07:24 +0100

    Move env-generator to build_requirement

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
-from glob import glob
 import os
+from glob import glob
 
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
@@ -14,11 +14,11 @@ class FontconfigConan(ConanFile):
     generators = "env"
 
     def build_requirements(self):
+        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
         self.build_requires("gperf/[>=3.1]@%s/stable" % self.user)
 
     def requirements(self):
-        self.requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
         self.requires("freetype/[>=2.10.1]@%s/stable" % self.user)
         self.requires("libuuid/[>=1.0.3]@%s/stable" % self.user)
         self.requires("expat/[>=2.2.7]@%s/stable" % self.user)
@@ -28,7 +28,7 @@ class FontconfigConan(ConanFile):
 
     def build(self):
         args = ["--disable-static"]
-        with tools.chdir("%s-%s" % (self.name , self.version)):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
             self.run("sh autogen.sh")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
@@ -46,6 +46,4 @@ class FontconfigConan(ConanFile):
                     os.symlink("../../../share/fontconfig/conf.avail/" + filename, filename)
 
     def package_info(self):
-        self.env_info.FONTCONFIG_PATH.append(
-            os.path.join(self.package_folder, "etc", "fonts")
-        )
+        self.env_info.FONTCONFIG_PATH.append(os.path.join(self.package_folder, "etc", "fonts"))
