e0c62045a8a4d127afa2de097d1cd74e04043720
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-06 08:52:45 +0100

    Set CMAKE_INSTALL_INCLUDEDIR cmake def

--- conanfile.py
+++ conanfile.py
@@ -27,6 +27,7 @@ class LibjpegTurboConan(ConanFile):
         cmake = CMake(self, generator="Ninja")
         cmake.definitions["WITH_JPEG8"] = True
         cmake.definitions["CMAKE_INSTALL_LIBDIR"] = os.path.join(self.package_folder, "lib")
+        cmake.definitions["CMAKE_INSTALL_INCLUDEDIR"] = os.path.join(self.package_folder, "include")
         cmake.configure(source_folder="%s-%s" % (self.name, self.version))
         cmake.build()
         cmake.install()
