2d33ed9cda4af84173c1c1595cc1802d5aecb25f
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 15:36:20 +0100

    Bundle source and default tag to nightly

--- conanfile.py
+++ conanfile.py
@@ -5,8 +5,8 @@ from conans import ConanFile, tools
 
 class RustConan(ConanFile):
     name = "rust"
-    version = tools.get_env("GIT_TAG", "1.38.0")
-    settings = "os", "compiler", "build_type", "arch"
+    version = tools.get_env("GIT_TAG", "nightly")
+    settings = "os", "compiler", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT", "Apache"
     description = "Systems programming language focused on safety, speed and concurrency"
@@ -32,14 +32,15 @@ class RustConan(ConanFile):
         }
         with tools.environment_append(env):
             self.run("sh rustup.sh -y --default-toolchain " + self.version)
+            self.run("./bin/rustup component add rust-src")
 
     def package(self):
         arch = {"x86_64": "x86_64", "armv8": "aarch64"}[str(self.settings.arch)]
         src = os.path.join("toolchains", "%s-%s-unknown-linux-gnu" % (self.version, arch))
-        self.copy("*", src=os.path.join(src, "bin"), dst="bin", keep_path=False)
-        self.copy("*.so*", src=os.path.join(src, "lib"), dst="lib", keep_path=False)
-        self.copy(
-            "*",
-            src=os.path.join(src, "lib", "rustlib"),
-            dst=os.path.join("lib", "rustlib"),
-        )
+        self.copy("*", src=os.path.join(src, "bin"), dst="bin")
+        self.copy("*.so*", src=os.path.join(src, "lib"), dst="lib")
+        self.copy("*", src=os.path.join(src, "etc", "bash_completion.d"), dst=os.path.join("share", "bash-completion", "completions"))
+        self.copy("*", src=os.path.join(src, "lib", "rustlib"), dst=os.path.join("lib", "rustlib"))
+
+    def package_info(self):
+        self.env_info.RUST_SRC_PATH = os.path.join(self.package_folder, "lib", "rustlib", "src", "rust", "src")
