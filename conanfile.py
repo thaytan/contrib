ffe37d7244833c0cd061649533013a15135911b3
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:22 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class LuajitConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     description = "Just-in-time compiler and drop-in replacement for Lua 5.1"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def source(self):
