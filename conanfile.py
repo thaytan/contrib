7b8bf28bf86d8bdfdfa8021bb9abc174f675e52e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:24 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,13 +10,13 @@ class PythonSetuptoolsConan(ConanFile):
     license = "Apache"
     description = ("Easily download, build, install, upgrade, and uninstall Python packages")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "env"
+    generators ="pkgconf"
 
     def source(self):
         tools.get("https://github.com/pypa/setuptools/archive/v%s.tar.gz" % self.version)
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("python/[>=3.7.4]@%s/stable" % self.user)
