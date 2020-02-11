0f2a990aebd135f5dc134110ce0792b2bcb0a9ea
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:24 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class Bzip2Conan(ConanFile):
     description = "A high-quality data compression program"
     license = "custom"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def source(self):
