73b392bcb311c2542b8620baba02f7a28b39a61f
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:22 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class TclConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("generators/[>=1.0.0]@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
 
     def requirements(self):
