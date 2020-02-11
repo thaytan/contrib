a95c9586a2d7b19e0caca8c7da660e8ffb651e7e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:24 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class LibxineramaConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
         self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
 
