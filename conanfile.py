a4d804adeb123ce4e131c14e4a5d39809ffafec8
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:23 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class ClangConan(ConanFile):
     description = "C language family frontend for LLVM"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "arch"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def requirements(self):
