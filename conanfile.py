073df3c83c3c984b0568fa30cf15ae73abcefa53
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:23 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class NcursesConan(ConanFile):
     license = "Zlib"
     description = "System V Release 4.0 curses emulation library"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
 
     def source(self):
