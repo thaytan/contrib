7cdebddab43e4921d505f545ebe45622a9aad62e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:21 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class RapidJsonConan(ConanFile):
     description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "build_type", "arch"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def source(self):
