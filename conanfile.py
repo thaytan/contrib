f0fa70789768dd7dbf0977d3a7f821591a67b0a1
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:23 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class LibvncserverConan(ConanFile):
     description = "Cross-platform C libraries that allow you to easily implement VNC server or client functionality"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "arch"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def requirements(self):
