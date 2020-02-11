72829a082c937791f94954e047ab46bc780cc897
Tobias Morell <tobias.morell@aivero.com>
2020-02-11 12:49:53 +0100

    Add openssl dependency

--- conanfile.py
+++ conanfile.py
@@ -14,6 +14,9 @@ class LibeventConan(ConanFile):
         self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/3.15.3@%s/stable" % self.user)
 
+    def requirements(self):
+        self.requires("openssl/1.1.1b@%s/stable" % self.user)
+
     def source(self):
         tools.get("https://github.com/libevent/libevent/releases/download/release-%s-stable/libevent-%s-stable.tar.gz" % (self.version, self.version))
 
