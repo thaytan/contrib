3b9dffdc437f03dcbeff9c4c7e85917fface740e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:20 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class XzConan(ConanFile):
     description = "Library and command line tools for XZ and LZMA compressed files"
     license = "custom", "GPL", "LGPL"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://tukaani.org/xz/xz-%s.tar.gz" % self.version)
