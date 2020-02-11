0e5af5250c4ddc43a2c9eae9641a44f9377bfb20
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:22 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,10 +10,10 @@ class PythonGobjectConan(ConanFile):
     description = "Python GObject bindings"
     license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
         self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
 
