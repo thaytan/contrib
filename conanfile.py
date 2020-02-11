6698223bfe78f249736917c0b1addb76719e880e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:22 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class MinuzConan(ConanFile):
     description = "Single C source file zlib-replacement library"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
 
     def source(self):
