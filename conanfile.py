1881af07425190bae61e033ffcec0e7594123cac
Tobias Morell <tobias.morell@aivero.com>
2020-02-11 13:18:22 +0100

    Add patch that removes Unistall target

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import CMake, ConanFile, tools
+import os
 
 
 class LibeventConan(ConanFile):
@@ -7,8 +8,9 @@ class LibeventConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "BSD-3"
-    description = "C++ library for asynchronous non-blocking communication with RabbitMQ"
+    description = "Event notification library https://libevent.org"
     generators = "env"
+    exports = "uninstall.patch"
 
     def build_requirements(self):
         self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
@@ -16,9 +18,11 @@ class LibeventConan(ConanFile):
 
     def requirements(self):
         self.requires("openssl/1.1.1b@%s/stable" % self.user)
+        self.requires("zlib/[>=1.2.11]@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/libevent/libevent/releases/download/release-%s-stable/libevent-%s-stable.tar.gz" % (self.version, self.version))
+        tools.patch(patch_file="uninstall.patch", base_path=os.path.join(self.source_folder, "libevent-%s-stable" % self.version))
 
     def build(self):
         cmake = CMake(self, generator="Ninja")
