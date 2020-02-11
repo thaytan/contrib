ba0e4d003c9efbc7865c40454489ec87ef031a29
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-11 21:02:23 +0100

    Convert to use pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class LibSrtpConan(ConanFile):
     license = "BSD"
     description = "Library for SRTP (Secure Realtime Transport Protocol)"
     settings = "os", "arch", "compiler", "build_type"
-    generators = "env"
+    generators ="pkgconf"
 
     def build_requirements(self):
-        self.build_requires("env-generator/1.0.0@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
 
     def source(self):
         tools.get("https://github.com/cisco/libsrtp/archive/v%s.tar.gz" % self.version)
