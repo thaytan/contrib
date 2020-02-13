b28e173c8efedc9872b68d507f33504b4d55e11e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-13 10:19:14 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,9 @@
+class EnvPackage(ConanFile):
+    name = "env-generator"
+    version = "1.0.0"
+    url = "https://gitlab.com/aivero/public/conan-" + name
+    license = "MIT"
+    description = "Generate environment file for build and runtime"
+
+    def requirements(self):
+        self.requires("generators/1.0.0@%s/stable" % self.user)
