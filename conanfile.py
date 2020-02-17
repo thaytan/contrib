9c38ee21774ad829633ad6f841d5419ae853d74d
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:13 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class CppzmqConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
