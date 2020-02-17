e03d0d1a8259999a709510b3806cf24ff7ec5de7
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:12 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class FlexConan(ConanFile):
     description = "Flex, the fast lexical analyzer generator"
     license = "BSD 2-Clause"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
