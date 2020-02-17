0ac23fa121ac51c7fa6da7162d8c15fc60b6e7da
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:12 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class CurlConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     description = "An URL retrieval utility and library"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
