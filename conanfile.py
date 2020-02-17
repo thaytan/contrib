55432d9b5a22bf25add88aac2bd4dac45666a837
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:12 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class PythonCairoConan(ConanFile):
     description = "Python bindings for the cairo graphics library"
     license = "LGPL2.1"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
