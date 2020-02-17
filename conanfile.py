1b68a80a73fe6dbfa94987e6517d2066849cb687
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:14 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class SharedMimeInfoConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL2"
     description = "Freedesktop.org Shared MIME Info"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
