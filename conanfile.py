f55765b14cd08f218a1d349fc7ad005555f19914
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:15 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class GitConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "GPL2"
     description = "The fast distributed version control system"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
