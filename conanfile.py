99ddcdf77a0d1bfb94add74b0c86127dc2f587ed
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:12 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,6 @@ class LibuuidConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "BSD-3-Clause"
     description = "Portable uuid C library"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
