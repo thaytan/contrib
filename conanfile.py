fa0d047e5a8fa349f1a2b2e82725d16ec5d9b019
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:12 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class XzConan(ConanFile):
     description = "Library and command line tools for XZ and LZMA compressed files"
     license = "custom", "GPL", "LGPL"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
