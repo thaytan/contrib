61df42de68c3d42cb5a66af0e3b0a0ee280ce77b
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:14 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class LibvncserverConan(ConanFile):
     description = "Cross-platform C libraries that allow you to easily implement VNC server or client functionality"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "arch"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
