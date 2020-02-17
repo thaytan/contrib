aa9a87f10c82ac58e54033fb1fe4f6ecc2f0d8bd
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:14 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class PangoConan(ConanFile):
     description = "A library for layout and rendering of text"
     license = "GPL"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
