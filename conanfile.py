b041e9500567c596f9683983ec1f9c703ca7ed0c
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:13 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class CapNProtoConan(ConanFile):
     license = "MIT"
     description = "Cap'n Proto serialization/RPC system"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
