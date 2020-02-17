ae833948ef26c7de29c4266e58dc4d12ccc26725
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:12 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class JsonGlibBaseConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {"introspection": [True, False]}
     default_options = "introspection=True"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
