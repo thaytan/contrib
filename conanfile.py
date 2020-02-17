cd429c5d78595bd3e38955fd559cd75c6838a0a2
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:13 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class PythonGobjectConan(ConanFile):
     description = "Python GObject bindings"
     license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
