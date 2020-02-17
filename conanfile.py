c9de4168791858b382e2a40231d495bbdba6ca2d
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:11 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class GdbConan(ConanFile):
     description = "The GNU Debugger"
     license = "GPL3"
     settings = "os", "arch", "compiler"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
