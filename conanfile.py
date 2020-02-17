70cc2eb8970f888054db4a1ec1d782172f16346f
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:15 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class GraphvizConan(ConanFile):
     description = "Graph Visualization Tools"
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     settings = "os", "compiler", "build_type", "arch"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
