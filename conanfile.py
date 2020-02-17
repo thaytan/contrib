0fff0e3121a6b522bfadf8fcae15d06ea772e7cf
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:14 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class GstreamerSharkConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "LGPL"
     settings = "os", "compiler", "build_type", "arch"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
