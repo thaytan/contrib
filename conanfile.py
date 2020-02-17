64a34b91ac9091dbea049e79a49addb0b31c1f04
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:13 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class LibepoxyConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     description = "Library handling OpenGL function pointer management"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
