60787cd9f8989ec7167ebdf041c5c70493b30094
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:14 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class LibdrmConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Direct Rendering Manager headers and kernel modules"
     settings = "os", "arch", "compiler", "build_type"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
