0a1910cbf1dafb7368c742108658fa1418d45739
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:13 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,6 @@ class Libxml2Conan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     description = "XML parsing library, version 2"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
