0a265bb6706af5643a201382a4ee53374c9b5190
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-17 14:40:14 +0100

    Remove pkgconf generator

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class LibpngConan(ConanFile):
     url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     description = "A collection of routines used to create PNG format graphics files"
-    generators ="pkgconf"
 
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
