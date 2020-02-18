42af700ec128d7cdf7119ff5a738ffddcfe9f7d1
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-18 14:59:15 +0100

    Copy go/src and go/pkg folders

--- conanfile.py
+++ conanfile.py
@@ -18,8 +18,13 @@ class GoConan(ConanFile):
         arch = {"x86_64": "amd64", "armv8": "arm64"}[str(self.settings.arch)]
         filename = "go{}.linux-{}.tar.gz".format(self.version, arch)
         tools.download("https://dl.google.com/go/go{}.linux-{}.tar.gz".format(self.version, arch), filename)
-        # Workaround: Python3 in Ubuntu 18.04 does not support ascii encododed tarballs
+        # Workaround: Python3 in Ubuntu 18.04 does not support ascii encoded tarballs
         self.run("tar -xf " + filename)
 
     def package(self):
         self.copy("*", src="go/bin", dst="bin")
+        self.copy("*", src="go/src", dst="src")
+        self.copy("*", src="go/pkg", dst="pkg")
+
+    def package_info(self):
+        self.env_info.GOROOT = self.package_folder
