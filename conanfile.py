f7257a2ff394c0a907e63b1f653f4d5ec4bcf54e
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-18 09:41:17 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+import os
+
+from conans import ConanFile, tools
+
+
+class RustfmtConan(ConanFile):
+    name = "rustfmt"
+    version = tools.get_env("GIT_TAG", "master")
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    description = "A tool for formatting Rust code according to style guidelines"
+    license = "MIT"
+    settings = "os", "arch", "compiler"
+
+    def source(self):
+        tools.get("https://github.com/rust-lang/rustfmt/archive/{}.tar.gz".format(self.version))
+
+    def requirements(self):
+        self.requires("rust/nightly@%s/stable" % self.user)
+        self.requires("generators/[>=1.0.0]@%s/stable" % self.user)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            self.run("cargo build --release")
+
+    def package(self):
+        self.copy(pattern="*/rustfmt", dst="bin", keep_path=False)
