266cc86735feeaf28d69da04d6b7abf2ea91bab8
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-20 10:59:32 +0100

    Use PKG_CONFIG_PATH in Meson helper

--- conanfile.py
+++ conanfile.py
@@ -20,5 +20,5 @@ class XorgProtoConan(ConanFile):
     def build(self):
         args = ["--auto-features=disabled"]
         meson = Meson(self)
-        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
