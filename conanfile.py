b01bc2d1c81cce55fe2be042fefc63a2e7e3c66b
Niclas Moeslund Overby <noverby@prozum.dk>
2020-02-20 10:59:32 +0100

    Use PKG_CONFIG_PATH in Meson helper

--- conanfile.py
+++ conanfile.py
@@ -32,5 +32,5 @@ class GLibConan(ConanFile):
             "-Dinternal_pcre=False",
         ]
         meson = Meson(self)
-        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args)
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
