746835bdb707e53eba358f881ce4e059aa678173
Niclas Moeslund Overby <noverby@prozum.dk>
2020-03-26 15:14:39 +0100

    Remove url attribute

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BootstrapGccConan(ConanFile):
     name = "bootstrap-gcc"
     version = tools.get_env("GIT_TAG", "7.4.0")
     settings = "os", "compiler", "arch"
-    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom", "FDL", "GPL", "LGPL"
     description = "The GNU Compiler Collection - C and C++ frontends"
 
