b97f3a65daba6089fb5052a1330a041144f9abfd
Niclas Moeslund Overby <noverby@prozum.dk>
2020-03-26 15:14:46 +0100

    Remove url attribute

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class LibglvndConan(ConanFile):
     name = "libglvnd"
     version = tools.get_env("GIT_TAG", "1.2.0")
     description = "The GL Vendor-Neutral Dispatch library"
-    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "custom"
     settings = "os", "compiler", "build_type", "arch"
     options = {
