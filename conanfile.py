4c0401038ac551c82dada43e23736c8e443df501
Niclas Moeslund Overby <noverby@prozum.dk>
2020-03-26 15:14:39 +0100

    Remove url attribute

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,6 @@ from conans import CMake, ConanFile, tools
 class MinuzConan(ConanFile):
     name = "miniz"
     version = tools.get_env("GIT_TAG", "2.1.0")
-    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     description = "Single C source file zlib-replacement library"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
