43393b558de66e3af84fa3700b9d50cd50267910
Niclas Moeslund Overby <noverby@prozum.dk>
2020-03-26 14:49:04 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class LibxpmConan(ConanFile):
+    name = "libxpm"
+    version = tools.get_env("GIT_TAG", "3.5.13")
+    description = "X11 pixmap library"
+    url = "https://gitlab.com/aivero/public/conan/conan-" + name
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
+        self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libXpm-%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--disable-static"]
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("libXpm-" + self.version):
+            autotools.configure(args=args)
+            autotools.install()
