44cd6d28842c1fa4060c89b94d97bc03cfd69d4a
Niclas Moeslund Overby <noverby@prozum.dk>
2020-03-26 15:14:35 +0100

    Remove url attribute

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,6 @@ class RenderprotoConan(ConanFile):
     name = "renderproto"
     version = tools.get_env("GIT_TAG", "0.11.1")
     description = "X11 Render extension wire protocol"
-    url = "https://gitlab.com/aivero/public/conan/conan-" + name
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
 
