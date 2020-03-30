92aa4f22614f631d5c9bcf5ee9d65c8766f17272
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2020-03-30 11:08:20 +0200

    Fix conanfile

--- conanfile.py
+++ conanfile.py
@@ -6,37 +6,29 @@ from conans import ConanFile, Meson, tools, CMake, AutoToolsBuildEnvironment
 
 class GStreamerPerfConan(ConanFile):
     name = "gst-perf"
-    version = tools.get_env("GIT_TAG", "1.16.0")
+    version = tools.get_env("GIT_TAG", "0.2.3")
     description = "Performance Evaluation tool for Gstreamer"
     license = "LGPL"
     settings = "os", "arch", "compiler", "build_type"
-    scm = {
-        "type": "git",
-        "url": "https://github.com/RidgeRun/gst-perf",
-        "revision": "master",
-        "recursive": True,
-        "subfolder": ("gst-perf-" + version)
-    }
+
 
     def requirements(self):
-        self.requires("glib/[>=2.62.0]@%s/stable" % self.user) 
-        self.requires("gstreamer/[>=%s]@%s/stable" % (self.version,self.user)) 
+        self.requires("glib/[>=2.62.0]@%s/stable" % self.user)
+        self.requires("gstreamer/[>=%s]@%s/stable" % (self.version,self.user))
 
     def build_requirements(self):
-        self.build_requires("generators/[>=1.0.0]@%s/stable" % self.user) 
-        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user) 
-        self.build_requires("automake/[>=1.16.1]@%s/stable" % self.user) 
-        self.build_requires("autoconf/[>=2.69]@%s/stable" % self.user) 
+        self.build_requires("generators/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("automake/[>=1.16.1]@%s/stable" % self.user)
+        self.build_requires("autoconf/[>=2.69]@%s/stable" % self.user)
 
     def source(self):
-        git = tools.Git(folder="src/gst-perf-" + self.version)
-        git.clone("https://github.com/RidgeRun/gst-perf.git", "master")
-        os.rename("%s-%s" % (self.name, self.version), "sources")
+        git = tools.Git()
+        git.clone("https://github.com/RidgeRun/gst-perf.git", "v%s" % self.version)
 
     def build(self):
-        with tools.chdir("sources"):
-            self.run("sh autogen.sh")
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure()
-            autotools.make()
-            autotools.install()
+        self.run("sh autogen.sh")
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure()
+        autotools.make()
+        autotools.install()
