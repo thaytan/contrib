17d7b1bdcdae1213602f7da987404da8b2d7bb37
Niclas Overby <noverby@prozum.dk>
2020-05-15 12:15:21 +0000

    Remove duplicate requirement

--- conanfile.py
+++ conanfile.py
@@ -13,7 +13,6 @@ class PythonGobjectConan(ConanFile):
     def build_requirements(self):
         self.build_requires("generators/1.0.0@%s/stable" % self.user)
         self.build_requires("meson/[>=0.51.2]@%s/stable" % self.user)
-        self.build_requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
 
     def requirements(self):
         self.requires("gobject-introspection/[>=1.59.3]@%s/stable" % self.user)
