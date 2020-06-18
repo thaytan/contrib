a652609e5be4a9b741c247fc7451671cd0277dee
Niclas Overby <noverby@prozum.dk>
2020-06-18 09:01:46 +0000

    Bump version

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 class NodejsConan(ConanFile):
     name = "nodejs"
-    version = tools.get_env("GIT_TAG", "13.0.1")
+    version = tools.get_env("GIT_TAG", "14.4.0")
     description = "Evented I/O for V8 javascript"
     license = "MIT"
     settings = "os", "arch", "compiler"
