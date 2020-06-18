19b2bf4e99218db442082ea2cb133a6268a590e9
Niclas Moeslund Overby <noverby@prozum.dk>
2020-06-18 19:06:55 +0200

    Copy pngquant and cwebp binaries from Conan packages

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 
 from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
@@ -11,6 +11,7 @@ class NpmConan(ConanFile):
     settings = "os", "arch", "compiler"
 
     def build_requirements(self):
+        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
         self.build_requires("python/[>=3.7.4]@%s/stable" % self.user)
         self.build_requires("libpng/[>=1.6.37]@%s/stable" % self.user)
         self.build_requires("mozjpeg/[>=3.3.1]@%s/stable" % self.user)
@@ -25,7 +26,18 @@ class NpmConan(ConanFile):
         tools.get("https://github.com/npm/cli/archive/v%s.tar.gz" % self.version)
 
     def build(self):
+        pngquant_src = os.path.join(self.deps_cpp_info["pngquant"].rootpath, "bin", "pngquant")
+        pngquant_dir = os.path.join("cli-%s" % self.version, "docs", "node_modules", "pngquant-bin", "vendor")
+        os.makedirs(pngquant_dir)
+        pngquant_dst = os.path.join(pngquant_dir, "pngquant")
+        shutil.copy2(pngquant_src, pngquant_dst)
+        cwebp_src = os.path.join(self.deps_cpp_info["libwebp"].rootpath, "bin", "cwebp")
+        cwebp_dir = os.path.join("cli-%s" % self.version, "docs", "node_modules", "cwebp-bin", "vendor")
+        os.makedirs(cwebp_dir)
+        cwebp_dst = os.path.join(cwebp_dir, "cwebp")
+        shutil.copy2(cwebp_src, cwebp_dst)
         with tools.chdir("cli-%s" % self.version):
             autotools = AutoToolsBuildEnvironment(self)
+
             self.run("mkdir -p man/man1")
             autotools.install(['NPMOPTS=--prefix="%s"' % self.package_folder])
