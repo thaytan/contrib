11de4b72dfd21a517b3d0ed8549df84b26f97cdc
Andrej Orsula <orsula.andrej@gmail.com>
2020-06-18 10:46:45 +0200

    Add request for user to install wget if missing

--- scripts/setup_udev_rules_k4a.sh
+++ scripts/setup_udev_rules_k4a.sh
@@ -8,7 +8,7 @@ fi
 
 # Manual setup
 echo -e "Downloading latest udev rules for K4A devices...\n"
-wget --version &> /dev/null || apt-get install -y wget &> /dev/null
+wget --version &> /dev/null || (>&2 echo -e "\e[31mDownloading udev rules failed, please install `wget`\n\e[0m\t& sudo apt-get install wget"; exit 1)
 wget https://github.com/microsoft/Azure-Kinect-Sensor-SDK/blob/master/scripts/99-k4a.rules
 mv 99-k4a.rules /etc/udev/rules.d/ && udevadm control --reload-rules && udevadm trigger
 echo -e "\e[32mSuccessfuly deployed udev-rules for K4A cameras\n\e[33mPlease replug any attached K4A cameras before continuing\e[0m"
