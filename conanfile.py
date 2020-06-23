7389bdc107e1a9ae2380a0c7197844f8db54a668
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2020-06-23 12:27:44 +0200

    Set gst deps to [~1]. \n This will enable us to define our desired gst versions on the top level package

--- conanfile.py
+++ conanfile.py
@@ -8,21 +8,15 @@ class DepthMetaConan(ConanFile):
     description = "Library to stream depth video"
     url = "https://aivero.com"
     settings = "os", "arch", "compiler", "build_type"
+    version = tools.get_env("GIT_TAG", "1.16.2")
     exports_sources = ["CMakeLists.txt", "src/*"]
-
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        tag, branch = git.get_tag(), git.get_branch()
-        self.version = tag if tag and branch.startswith("HEAD") else branch
 
     def build_requirements(self):
         self.build_requires("cmake/[>=3.15.3]@%s/stable" % (self.user))
 
     def requirements(self):
-        gst_version = "master" if self.version == "master" else "[~%s]" % "1.17.1"
-        gst_channel = "testing" if self.version == "master" else "stable"
-
-        self.requires("gstreamer-plugins-base/%s@%s/%s" % (gst_version, self.user, gst_channel))
+        gst_version = "master" if self.version == "master" else "[~1]"
+        self.requires("gstreamer-plugins-base/%s@%s/stable" % (gst_version, self.user))
 
     def build(self):
         env = {
