93daf3bd1ffe6cd8fc70e4a8fc27e75a16283e5e
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:12 +0200

    Move conanfile.py to subdir

---
+++ mpc/all/conanfile.py
@@ -0,0 +1,31 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class MpcConan(ConanFile):
+    name = "mpc"
+    version = tools.get_env("GIT_TAG", "1.1.0")
+    settings = "os", "compiler", "arch"
+    license = "LGPL"
+    description = "Library for the arithmetic of complex numbers with arbitrarily high precision"
+
+    def build_requirements(self):
+        self.build_requires("bootstrap-gcc/[>=7.4.0]@%s/stable" % self.user)
+        self.build_requires("make/[>=4.3]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("mpfr/[>=4.0.2]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://ftp.gnu.org/gnu/mpc/mpc-%s.tar.gz" % self.version)
+
+    def build(self):
+        args = [
+            "--disable-static",
+        ]
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.make(target="install-strip")
