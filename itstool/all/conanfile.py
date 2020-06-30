7eb972beb068a60ec6b9d7673ec20ae6a5a27998
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ itstool/all/conanfile.py
@@ -0,0 +1,30 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class ItstoolConan(ConanFile):
+    name = "itstool"
+    version = tools.get_env("GIT_TAG", "2.0.6")
+    settings = "os", "compiler", "build_type", "arch"
+    license = "GPL3"
+    description = "XML to PO and back again"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("libxml2/[>=2.9.9]@%s/stable" % self.user)
+        self.requires("python/[>=3.7.4]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/itstool/itstool/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
