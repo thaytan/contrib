a89d08525673523aa3794d9592048c6ff45b2238
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:12 +0200

    Move conanfile.py to subdir

---
+++ libxtst/all/conanfile.py
@@ -0,0 +1,30 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class LibxtstConan(ConanFile):
+    name = "libxtst"
+    version = tools.get_env("GIT_TAG", "1.2.3")
+    description = "X11 Testing Resource extension library"
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("libx11/[>=1.6.8]@%s/stable" % self.user)
+        self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
+        self.requires("libxfixes/[>=5.0.3]@%s/stable" % self.user)
+        self.requires("libxi/[>=1.7.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libXtst-%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--disable-static"]
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("libXtst-" + self.version):
+            autotools.configure(args=args)
+            autotools.install()
