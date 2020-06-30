165dac2b1f18c4476503fd21cbc9feae368bc09d
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:12 +0200

    Move conanfile.py to subdir

---
+++ mozjpeg/all/conanfile.py
@@ -0,0 +1,32 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class MozjpegConan(ConanFile):
+    name = "mozjpeg"
+    version = tools.get_env("GIT_TAG", "3.3.1")
+    settings = "os", "compiler", "build_type", "arch"
+    license = "custom"
+    description = "JPEG image codec with accelerated baseline compression and decompression"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
+        self.build_requires("yasm/[>=1.3.0]@%s/stable" % self.user)
+        self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/mozilla/mozjpeg/archive/v{0}.tar.gz".format(self.version))
+
+    def build(self):
+        args = [
+            "--disable-static",
+        ]
+
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            self.run("autoreconf -i")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
