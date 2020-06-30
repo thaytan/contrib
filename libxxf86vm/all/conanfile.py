ba967343d913612792d7b3b404046b8a93fdcea0
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:12 +0200

    Move conanfile.py to subdir

---
+++ libxxf86vm/all/conanfile.py
@@ -0,0 +1,27 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class Libxxf86vmConan(ConanFile):
+    name = "libxxf86vm"
+    version = tools.get_env("GIT_TAG", "1.1.4")
+    description = "X11 XFree86 video mode extension library"
+    license = "custom"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+        self.build_requires("xorg-util-macros/[>=1.19.1]@%s/stable" % self.user)
+        self.build_requires("xorgproto/[>=2019.1]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("libxext/[>=1.3.4]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://xorg.freedesktop.org/releases/individual/lib/libXxf86vm-%s.tar.gz" % self.version)
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("libXxf86vm-" + self.version):
+            autotools.configure()
+            autotools.install()
