ebf5c6e226dcc6d62742ecb889bd29d607e0f59f
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ gstreamer-shark/all/conanfile.py
@@ -0,0 +1,34 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class GstreamerSharkConan(ConanFile):
+    name = "gstreamer-shark"
+    version = tools.get_env("GIT_TAG", "0.6.1")
+    description = "GstShark is a front-end for GStreamer traces "
+    license = "LGPL"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("gstreamer/[>=1.16.0]@%s/stable" % self.user)
+        self.requires("graphviz/[>=2.42.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/RidgeRun/gst-shark/archive/v%s.tar.gz" % self.version)
+        git = tools.Git(folder=os.path.join("gst-shark-" + self.version, "common"))
+        git.clone("git://anongit.freedesktop.org/gstreamer/common", "master")
+
+    def build(self):
+        with tools.chdir("gst-shark-" + self.version):
+            self.run("sh autogen.sh --disable-gtk-doc")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.install()
+
+    def package_info(self):
+        self.env_info.GST_PLUGIN_PATH.append(os.path.join(self.package_folder, "lib", "gstreamer-1.0"))
