024357ad1b66d020b9921961602a2a4e9bc13dd0
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ libedit/all/conanfile.py
@@ -0,0 +1,29 @@
+import os
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class LibeditConan(ConanFile):
+    name = "libedit"
+    version = tools.get_env("GIT_TAG", "20190324-3.1")
+    license = "Zlib"
+    description = "System V Release 4.0 curses emulation library"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+        self.build_requires("pkgconf/[>=1.6.3]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("ncurses/[>=6.1]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://thrysoee.dk/editline/libedit-%s.tar.gz" % self.version)
+
+    def build(self):
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure()
+            autotools.make()
+            autotools.install()
