879676aa556496fd673d8e47627999277f20e2c7
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ freetype/all/conanfile.py
@@ -0,0 +1,30 @@
+from glob import glob
+from os import path, remove
+
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class FreetypeConan(ConanFile):
+    name = "freetype"
+    version = tools.get_env("GIT_TAG", "2.10.1")
+    description = "FreeType is a software library to render fonts"
+    license = "GPL2"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("harfbuzz/2.6.1@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-%s.tar.gz" % self.version.replace(".", "-"))
+
+    def build(self):
+        args = ["--disable-static"]
+        with tools.chdir("freetype2-VER-" + self.version.replace(".", "-")):
+            self.run("sh autogen.sh")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.install()
