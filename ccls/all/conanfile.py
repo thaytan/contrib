473040f2d0cc1c816e4cbe7a03e37eb2277e0978
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ ccls/all/conanfile.py
@@ -0,0 +1,26 @@
+from conans import CMake, ConanFile, tools
+
+
+class CclsConan(ConanFile):
+    name = "ccls"
+    version = tools.get_env("GIT_TAG", "0.20190823.5")
+    license = "Apache"
+    description = "C/C++ language server supporting cross references, hierarchies, completion and semantic highlighting"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("clang/[>=9.0.0]@%s/stable" % self.user)
+        self.requires("rapidjson/master@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/MaskRay/ccls/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        cmake = CMake(self, generator="Ninja")
+        cmake.configure(source_folder="%s-%s" % (self.name, self.version))
+        cmake.build()
+        cmake.install()
