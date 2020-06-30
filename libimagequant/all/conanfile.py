54646a704a4e6d786e542c3727c6aa3ea134d996
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:12 +0200

    Move conanfile.py to subdir

---
+++ libimagequant/all/conanfile.py
@@ -0,0 +1,29 @@
+from conans import *
+
+class LibimagequantConan(ConanFile):
+    name = "libimagequant"
+    version = tools.get_env("GIT_TAG", "2.12.6")
+    description = "Library for high-quality conversion of RGBA images to 8-bit indexed-color (palette) images"
+    license = "BSD"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def build_requirements(self):
+        self.build_requires("autotools/1.0.0@%s/stable" % self.user)
+
+    def requirements(self):
+        self.requires("generators/1.0.0@%s/stable" % self.user)
+
+    def source(self):
+        tools.get(f"https://github.com/ImageOptim/libimagequant/archive/{self.version}/libimagequant-{self.version}.tar.gz")
+
+    def build(self):
+        args = [
+            "--with-openmp",
+        ]
+        env = {
+            "DESTDIR": self.package_folder,
+        }
+        with tools.chdir(f"{self.name}-{self.version}"), tools.environment_append(env):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.install()
