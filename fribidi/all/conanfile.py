cbe4df3812b35034270469c94bb2ec76e17cf9b6
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ fribidi/all/conanfile.py
@@ -0,0 +1,24 @@
+import os
+
+from conans import ConanFile, Meson, tools
+
+
+class FribidiConan(ConanFile):
+    name = "fribidi"
+    version = tools.get_env("GIT_TAG", "1.0.5")
+    description = "The Free Implementation of the Unicode Bidirectional Algorithm"
+    license = "LGPL"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("meson/[>=0.5.12]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/fribidi/fribidi/archive/v%s.tar.gz" % self.version)
+
+    def build(self):
+        args = ["--auto-features=disabled", "-Ddocs=false"]
+        meson = Meson(self)
+        meson.configure(source_folder="%s-%s" % (self.name, self.version), args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.install()
