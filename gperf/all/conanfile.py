05b58df3b99d619f24dafc055bdfc05b7507839c
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ gperf/all/conanfile.py
@@ -0,0 +1,21 @@
+from conans import ConanFile, Meson, tools
+
+
+class GperfConan(ConanFile):
+    name = "gperf"
+    version = tools.get_env("GIT_TAG", "3.1")
+    settings = "os", "compiler", "build_type", "arch"
+    license = "GPL3"
+    description = "A portable, high level programming interface to various calling conventions"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+
+    def source(self):
+        git = tools.Git("%s-%s" % (self.name, self.version))
+        git.clone("https://gitlab.com/aivero/public/gperf.git", "meson")
+
+    def build(self):
+        meson = Meson(self)
+        meson.configure(source_folder="%s-%s" % (self.name, self.version))
+        meson.install()
