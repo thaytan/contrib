2fbfd1e9d78f56f86c173abceb6b2d6f55a490de
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ ffmpeg/all/conanfile.py
@@ -0,0 +1,26 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class FFMpegConan(ConanFile):
+    name = "ffmpeg"
+    version = tools.get_env("GIT_TAG", "4.1")
+    description = "A complete, cross-platform solution to record, convert and stream audio and video"
+    license = "GPL3"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("yasm/[>=1.3.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % self.version)
+
+    def build(self):
+        args = ["--disable-static", "--enable-shared", "--disable-doc", "--disable-programs"]
+        if self.settings.build_type == "Debug":
+            args.extend(["--disable-optimizations", "--disable-mmx", "--disable-stripping", "--enable-debug"])
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
