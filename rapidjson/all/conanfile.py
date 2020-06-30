002830b6c209b199061fb7dbc21b367d7a31c737
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:12 +0200

    Move conanfile.py to subdir

---
+++ rapidjson/all/conanfile.py
@@ -0,0 +1,27 @@
+import os
+
+from conans import CMake, ConanFile, tools
+
+
+class RapidJsonConan(ConanFile):
+    name = "rapidjson"
+    version = tools.get_env("GIT_TAG", "master")
+    license = "MIT"
+    description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("cmake/[>=3.15.3]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/Tencent/rapidjson/archive/%s.tar.gz" % self.version)
+
+    def build(self):
+        cmake = CMake(self, generator="Ninja")
+        cmake.definitions["RAPIDJSON_HAS_STDSTRING"] = True
+        cmake.definitions["RAPIDJSON_BUILD_CXX11"] = True
+        cmake.definitions["RAPIDJSON_ENABLE_INSTRUMENTATION_OPT"] = True
+        cmake.configure(source_folder="%s-%s" % (self.name, self.version))
+        cmake.build()
+        cmake.install()
