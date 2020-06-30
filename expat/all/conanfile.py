6ac59a9f06d384a9436bfbf57b6b0f58fe1aafcc
Niclas Overby <noverby@prozum.dk>
2020-06-30 14:32:11 +0200

    Move conanfile.py to subdir

---
+++ expat/all/conanfile.py
@@ -0,0 +1,23 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+
+
+class ExpatConan(ConanFile):
+    name = "expat"
+    version = tools.get_env("GIT_TAG", "2.2.7")
+    description = "An XML parser library"
+    license = "MIT"
+    settings = "os", "compiler", "build_type", "arch"
+
+    def build_requirements(self):
+        self.build_requires("generators/1.0.0@%s/stable" % self.user)
+        self.build_requires("gcc/[>=7.4.0]@%s/stable" % self.user)
+
+    def source(self):
+        tools.get("https://github.com/libexpat/libexpat/releases/download/R_{0}/expat-{1}.tar.bz2".format(self.version.replace(".", "_"), self.version))
+
+    def build(self):
+        args = ["--disable-static"]
+        autotools = AutoToolsBuildEnvironment(self)
+        with tools.chdir("%s-%s" % (self.name, self.version)):
+            autotools.configure(args=args)
+            autotools.install()
