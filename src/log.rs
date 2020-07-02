e6a2a74f701f1ae759af3a118c66b6c3d2d3f0ab
Andrej Orsula <orsula.andrej@gmail.com>
2020-07-02 14:51:01 +0200

    Use correct type for string pointer based on arch

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,6 @@
 // License: MIT. See LICENSE file in root directory.
 // Copyright(c) 2019 Aivero. All Rights Reserved.
 use crate::error::Error;
-use rs2;
 
 // Expose `rs2_log_severity` for external use.
 pub use rs2::rs2_log_severity;
@@ -28,10 +27,31 @@ pub fn log_to_console(min_severity: rs2_log_severity) -> Result<(), Error> {
 /// # Returns
 /// * `Ok()` on success.
 /// * `Err(Error)` on failure.
+#[cfg(target_arch = "x86")]
+#[cfg(target_arch = "x86_64")]
 pub fn log_to_file(min_severity: rs2_log_severity, file_path: &str) -> Result<(), Error> {
     let mut error = Error::default();
     unsafe {
         rs2::rs2_log_to_file(min_severity, file_path.as_ptr() as *const i8, error.inner());
+    }
+    if error.check() {
+        Err(error)
+    } else {
+        Ok(())
+    }
+}
+
+/// Select a file for logging and determine the level of severity to be logged inside such file.
+///
+/// # Returns
+/// * `Ok()` on success.
+/// * `Err(Error)` on failure.
+#[cfg(target_arch = "arm")]
+#[cfg(target_arch = "aarch64")]
+pub fn log_to_file(min_severity: rs2_log_severity, file_path: &str) -> Result<(), Error> {
+    let mut error = Error::default();
+    unsafe {
+        rs2::rs2_log_to_file(min_severity, file_path.as_ptr() as *const u8, error.inner());
     }
     if error.check() {
         Err(error)
