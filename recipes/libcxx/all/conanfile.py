93c12ca9c5b556748c73422724c934edeaeaf1af
Niclas Overby <noverby@prozum.dk>
2020-07-06 15:18:00 +0200

    Add c++ std library packages

---
+++ recipes/libcxx/all/conanfile.py
@@ -0,0 +1,12 @@
+from conans import *
+
+
+class LibcxxConan(ConanFile):
+    name = "libcxx"
+    description = "Virtual libc++ package"
+    settings = {"os": ["Linux"], "arch": ["x86_64", "armv8"], "libcxx": ["system", "libc++"]}
+    license = "Public"
+
+    def requirements(self):
+        if self.settings.libcxx == "libc++":
+            self.requires("libc++/[^10.0.0]")
