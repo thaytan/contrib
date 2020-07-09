55e821e69ea338ce916689cfece435b8f0620692
Niclas Overby <noverby@prozum.dk>
2020-07-09 08:19:28 +0200

    Fix armv8 build

--- recipes/linux-headers/all/conanfile.py
+++ recipes/linux-headers/all/conanfile.py
@@ -13,7 +13,7 @@ class LinuxHeadersConan(ConanFile):
         tools.get(f"https://cdn.kernel.org/pub/linux/kernel/v{self.version.split('.')[0]}.x/linux-{self.version}.tar.xz")
 
     def build(self):
-        arch = {"x86_64": "x86_64", "armv8": "aarch64"}[str(self.settings.arch_build)]
+        arch = {"x86_64": "x86_64", "armv8": "arm64"}[str(self.settings.arch_build)]
         with tools.chdir(f"linux-{self.version}"):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.make(target="headers_install", args=[f"ARCH={arch}", f'INSTALL_HDR_PATH="{self.package_folder}"'])
