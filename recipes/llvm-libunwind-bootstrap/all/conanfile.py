fa95b8cf32afc217af5734f9a6d8f9fff8093e87
Niclas Overby <noverby@prozum.dk>
2020-07-10 16:23:44 +0200

    llvm-libunwind-bootstrap: add cmake and ninja requirement

--- recipes/llvm-libunwind-bootstrap/all/conanfile.py
+++ recipes/llvm-libunwind-bootstrap/all/conanfile.py
@@ -4,8 +4,12 @@ from conans import *
 class LlvmLibunwindBoostrapConan(ConanFile):
     name = "llvm-libunwind-bootstrap"
     description = "LLVM version of libunwind library"
-    license = "GPL"
+    license = "Apache-2.0"
     settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    build_requires = (
+        "cmake-bootstrap/[^3.17.3]",
+        "ninja-bootstrap/[^1.10.0]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{self.version}/libunwind-{self.version}.src.tar.xz")
