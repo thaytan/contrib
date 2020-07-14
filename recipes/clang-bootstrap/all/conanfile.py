f9c7c52d961675a61a2e4736819c1d62a71aa310
Niclas Overby <noverby@prozum.dk>
2020-07-14 21:21:33 +0200

    clang-bootstrap: actually build lld

--- recipes/clang-bootstrap/all/conanfile.py
+++ recipes/clang-bootstrap/all/conanfile.py
@@ -40,6 +40,8 @@ class ClangBootstrapConan(ConanFile):
         cmake.definitions["LLVM_INCLUDE_EXAMPLES"] = False
 
         cmake.configure(source_folder=f"llvm-{self.version}")
+        cmake.build(target="lld")
+        cmake.build(target="lld-install")
         cmake.build(target="clang")
         cmake.build(target="clang-install")
 
