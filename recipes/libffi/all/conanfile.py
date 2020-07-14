5cc31871420998885492c32d786178b83566c3ff
Niclas Overby <noverby@prozum.dk>
2020-07-14 17:06:25 +0200

    libffi: remove cmake requirement

--- recipes/libffi/all/conanfile.py
+++ recipes/libffi/all/conanfile.py
@@ -6,11 +6,7 @@ class LibffiConan(ConanFile):
     description = "A portable, high level programming interface to various calling conventions"
     license = "MIT"
     settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
-    build_requires = (
-        "clang-bootstrap/[^10.0.0]",
-        "cmake-bootstrap/[^3.17.3]",
-        "ninja-bootstrap/[^1.10.0]",
-    )
+    build_requires = ("clang-bootstrap/[^10.0.0]",)
 
     def source(self):
         tools.get(f"https://github.com/libffi/libffi/archive/v{self.version}.tar.gz")
