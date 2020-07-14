a0a16b85de7ef1d4f709088e9d840ce0731febcd
Niclas Overby <noverby@prozum.dk>
2020-07-14 17:21:24 +0200

    base: simplify

--- recipes/base/all/conanfile.py
+++ recipes/base/all/conanfile.py
@@ -4,9 +4,8 @@ from conans import *
 class BaseConan(ConanFile):
     name = "base"
     description = "Virtual base package"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc": ["glibc", "musl", "system"]}
-    license = "Public"
-
-    def requirements(self):
-        self.requires("generators/[^1.0.0]")
-        self.requires("base/[^1.0.0]")
+    license = "MIT"
+    requires = (
+        "generators/[^1.0.0]",
+        "libc/[^1.0.0]",
+    )
