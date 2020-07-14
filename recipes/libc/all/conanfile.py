bf13e189416e12c5aa7408412c42742deb65cedf
Niclas Overby <noverby@prozum.dk>
2020-07-14 17:16:32 +0200

    libc: remove glibc setting

--- recipes/libc/all/conanfile.py
+++ recipes/libc/all/conanfile.py
@@ -5,10 +5,8 @@ class LibcConan(ConanFile):
     name = "libc"
     description = "Virtual libc package"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc": ["glibc", "musl", "system"]}
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc": ["system", "musl"]}
 
     def requirements(self):
-        if self.settings.libc == "glibc":
-            self.requires("glibc/[^2.27]")
         if self.settings.libc == "musl":
             self.requires("musl/[^1.2.0]")
