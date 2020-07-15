30c280ca029318742974393a845ae6d8677b9aa2
Niclas Overby <noverby@prozum.dk>
2020-07-15 15:59:30 +0200

    zlib: use llvm-bootstrap

--- recipes/llvm/all/conanfile.py
+++ recipes/llvm/all/conanfile.py
@@ -4,26 +4,48 @@ from conans import *
 class LlvmConan(ConanFile):
     name = "llvm"
     description = "Collection of modular and reusable compiler and toolchain technologies"
-    license = "custom", "Apache"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    requires = ("base/[^1.0.0]", "libcxx/[^1.0.0]")
+    license = "Apache"
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
+    build_requires = (
+        "llvm-bootstrap/[^10.0.0]",
+        "cmake-bootstrap/[^3.17.3]",
+        "ninja-bootstrap/[^1.10.0]",
+    )
+    requires = (
+        "generators/[^1.0.0]",
+        "libcxx/[^10.0.0]",
+        "zlib/[^1.2.11]",
+        "ncurses/[^6.2]",
+        "libffi/[^3.3]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{self.version}/llvm-{self.version}.src.tar.xz")
 
     def build(self):
-        env = {
-            "CC": "gcc",
-        }
-        with tools.environment_append(env):
-            cmake = CMake(self, generator="Ninja")
-            cmake.definitions["LLVM_BUILD_LLVM_DYLIB"] = True
-            cmake.definitions["LLVM_LINK_LLVM_DYLIB"] = True
-            cmake.definitions["LLVM_INSTALL_UTILS"] = True
-            cmake.definitions["LLVM_ENABLE_FFI"] = True
-            cmake.definitions["LLVM_ENABLE_RTTI"] = True
-            cmake.definitions["CLANG_DEFAULT_CXX_STDLIB"] = "libc++"
-            cmake.definitions["CMAKE_CXX_FLAGS"] = "-U_FORTIFY_SOURCE"
-            cmake.configure(source_folder=f"{self.name}-{self.version}.src")
-            cmake.build()
-            cmake.install()
+        cmake = CMake(self, generator="Ninja")
+
+        # LLVM build options
+        cmake.definitions["LLVM_BUILD_DOCS"] = False
+        cmake.definitions["LLVM_BUILD_EXAMPLES"] = False
+        cmake.definitions["LLVM_BUILD_RUNTIME"] = True
+        cmake.definitions["LLVM_BUILD_TESTS"] = False
+        cmake.definitions["LLVM_BUILD_LLVM_DYLIB"] = True
+        cmake.definitions["LLVM_LINK_LLVM_DYLIB"] = True
+        cmake.definitions["LLVM_INSTALL_UTILS"] = True
+        cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
+
+        # LLVM enable options
+        cmake.definitions["LLVM_ENABLE_ASSERTIONS"] = False
+        cmake.definitions["LLVM_ENABLE_FFI"] = True
+        cmake.definitions["LLVM_ENABLE_LIBXML2"] = False
+        cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
+        cmake.definitions["LLVM_ENABLE_PIC"] = True
+        cmake.definitions["LLVM_ENABLE_RTTI"] = True
+        cmake.definitions["LLVM_ENABLE_SPHINX"] = False
+        cmake.definitions["LLVM_ENABLE_TERMINFO"] = True
+        cmake.definitions["LLVM_ENABLE_ZLIB"] = True
+
+        cmake.configure(source_folder=f"{self.name}-{self.version}.src")
+        cmake.build()
+        cmake.install()
--- recipes/zlib/all/conanfile.py
+++ recipes/zlib/all/conanfile.py
@@ -3,10 +3,11 @@ from conans import *
 
 class ZlibConan(ConanFile):
     name = "zlib"
-    description = "A Massively Spiffy Yet Delicately Unobtrusive Compression Library " "(Also Free, Not to Mention Unencumbered by Patents)"
+    description = "A Massively Spiffy Yet Delicately Unobtrusive Compression Library (Also Free, Not to Mention Unencumbered by Patents)"
     license = "Zlib"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    build_requires = ("clang-bootstrap/[^10.0.0]",)
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
+    build_requires = ("llvm-bootstrap/[^10.0.0]",)
+    requires = ("generators/[^1.0.0]",)
 
     def source(self):
         tools.get(f"https://github.com/madler/zlib/archive/v{self.version}.tar.gz")
