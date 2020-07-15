4f85a0332e7dd1cb67572aabeca5a2312b5c90c7
Niclas Overby <noverby@prozum.dk>
2020-07-15 16:03:48 +0200

    ncurses: use llvm-bootstrap

--- recipes/ncurses/all/conanfile.py
+++ recipes/ncurses/all/conanfile.py
@@ -6,9 +6,13 @@ from conans import *
 class NcursesConan(ConanFile):
     name = "ncurses"
     description = "System V Release 4.0 curses emulation library"
-    license = "Zlib"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    build_requires = ("cc/[^1.0.0]",)
+    license = "MIT"
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
+    build_requires = ("llvm-bootstrap/[^10.0.0]",)
+    requires = (
+        "generators/[^1.0.0]",
+        "libcxx/[^10.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://ftp.gnu.org/pub/gnu/ncurses/ncurses-{self.version}.tar.gz")
@@ -17,16 +21,17 @@ class NcursesConan(ConanFile):
         args = [
             "--enable-overwrite",
             "--with-shared",
+            "--without-normal",
+            "--without-debug",
             "--with-cxx-shared",
             "--with-cxx-binding",
             "--enable-pc-files",
             "--with-pkg-config-libdir=" + os.path.join(self.package_folder, "lib", "pkgconfig"),
         ]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.make()
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(args=args, configure_dir=f"{self.name}-{self.version}")
+        autotools.make()
+        autotools.install()
 
     def package_info(self):
         self.env_info.TERMINFO = os.path.join(self.package_folder, "share", "terminfo")
