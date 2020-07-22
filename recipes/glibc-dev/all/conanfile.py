709a758285ab70dcd166979c7d8edf0b9f5d1423
Niclas Overby <noverby@prozum.dk>
2020-07-22 22:49:20 +0200

    glibc-dev: add settings

--- recipes/glibc-dev/all/conanfile.py
+++ recipes/glibc-dev/all/conanfile.py
@@ -5,6 +5,7 @@ class GlibcDevConan(ConanFile):
     name = "glibc-dev"
     description = "glibc development files"
     license = "GPL"
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
 
     def source(self):
         tools.get(f"https://ftp.gnu.org/gnu/glibc/glibc-{self.version}.tar.xz")
