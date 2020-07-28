1c2dc37a601136e9dcb922b289316ba61c6c7ca9
Niclas Overby <noverby@prozum.dk>
2020-07-28 19:06:40 +0200

    libffi: depend on llvm-bootstrap-dev

--- recipes/libffi/all/conanfile.py
+++ recipes/libffi/all/conanfile.py
@@ -6,7 +6,7 @@ class LibffiConan(ConanFile):
     description = "A portable, high level programming interface to various calling conventions"
     license = "MIT"
     settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
-    build_requires = ("llvm-bootstrap/[^10.0.0]",)
+    build_requires = ("llvm-bootstrap-dev/[^10.0.0]",)
     requires = (("generators/[^1.0.0]", "private"),)
 
     def source(self):
