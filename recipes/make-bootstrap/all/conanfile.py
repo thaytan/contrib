d80a22802e0ff881ee793fbd2d6c971088415f2e
Niclas Overby <noverby@prozum.dk>
2020-07-30 09:01:02 +0200

    make-bootstrap: add package

---
+++ recipes/make-bootstrap/all/conanfile.py
@@ -0,0 +1,24 @@
+import os
+
+from conans import *
+
+
+class MakeBootstrapConan(ConanFile):
+    name = "make"
+    description = "GNU make utility to maintain groups of programs"
+    license = "GPL"
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
+    build_requires = "llvm-bootstrap/[^10.0.0]"
+
+    def source(self):
+        tools.get(f"https://ftp.gnu.org/gnu/make/make-{self.version}.tar.gz")
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(configure_dir=f"{self.name}-{self.version}")
+        autotools.make()
+        autotools.install()
+
+    def package_info(self):
+        self.env_info.MAKE = os.path.join(self.package_folder, "bin", "make")
