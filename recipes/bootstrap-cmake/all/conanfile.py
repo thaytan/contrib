acf02c5e696df1569bdbb0c2115e5f6845c77e39
Niclas Overby <noverby@prozum.dk>
2020-08-03 19:16:18 +0200

    bootstrap-cmake: don't build ninja

--- recipes/bootstrap-cmake/all/conanfile.py
+++ recipes/bootstrap-cmake/all/conanfile.py
@@ -4,26 +4,16 @@ from conans import *
 
 class BootstrapCMakeConan(ConanFile):
     name = "bootstrap-cmake"
-    ninja_version = "1.10.0"
     description = "A cross-platform open-source make system + ninja"
     license = "custom", "Apache"
     settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system", "musl"]}
 
     def source(self):
-        tools.get(f"https://github.com/ninja-build/ninja/archive/v{self.ninja_version}.tar.gz")
         tools.get(f"https://github.com/Kitware/CMake/releases/download/v{self.version}/cmake-{self.version}.tar.gz")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder=f"ninja-{self.ninja_version}")
-        cmake.build()
-        cmake.install()
-
         cmake = CMake(self)
         cmake.definitions["CMAKE_USE_OPENSSL"] = False
         cmake.configure(source_folder=f"cmake-{self.version}")
         cmake.build()
         cmake.install()
-
-    def package_info(self):
-        self.env_info.CONAN_CMAKE_GENERATOR = "Ninja"
