7a23e3a0298bbc77c09b52f1b9db544a46701b86
Niclas Overby <noverby@prozum.dk>
2020-08-16 08:48:26 +0200

    bootstrap-llvm: fix CFLAGS_AUTO argument for building musl 4 real

--- recipes/bootstrap-llvm/all/conanfile.py
+++ recipes/bootstrap-llvm/all/conanfile.py
@@ -142,7 +142,7 @@ class BootstrapLlvmConan(ConanFile):
             }
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(vars=vars, configure_dir=f"musl-{self.musl_version}")
-            autotools.make(target="install", args=["CFLAGS_AUTO= -O2 -pipe"])
+            autotools.make(target="install", args=['CFLAGS_AUTO=" -O2 -pipe"'])
             # GVN causes segmentation fault during recursion higher than 290
             ldflags += " -Wl,-Bstatic,-mllvm,-gvn-max-recurse-depth=250"
 
