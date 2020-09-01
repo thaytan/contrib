4b688bf00218f05446c72f3873364831aab14165
Niclas Overby <noverby@prozum.dk>
2020-09-01 14:11:12 +0200

    bootstrap-libc: fix libc setting

--- recipes/bootstrap-libc/all/conanfile.py
+++ recipes/bootstrap-libc/all/conanfile.py
@@ -5,7 +5,7 @@ class BootstrapLibcConan(ConanFile):
     name = "bootstrap-libc"
     description = "Virtual bootstrap libc package"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "libc": ["system", "musl"]}
+    settings = {"os_build": ["Linux"], "libc_build": ["system", "musl"]}
 
     def requirements(self):
         if self.settings.os_build == "Linux":
