30eb8dc1b08fb0ffe8931716144a92f0260d4589
Niclas Overby <noverby@prozum.dk>
2020-09-01 17:53:41 +0200

    bootstrap-musl: update settings

--- recipes/bootstrap-musl/all/conanfile.py
+++ recipes/bootstrap-musl/all/conanfile.py
@@ -7,7 +7,7 @@ class BootstrapMuslConan(ConanFile):
     name = "bootstrap-musl"
     description = "Lightweight implementation of C standard library"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["musl"]}
+    settings = {"build_type": ["RelWithDebInfo"], "os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["musl"]}
 
     def source(self):
         tools.get(f"https://www.musl-libc.org/releases/musl-{self.version}.tar.gz")
