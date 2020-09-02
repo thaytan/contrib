a708eec916c9efac48e0d0e2da3b8d24bd5d5f34
Niclas Overby <noverby@prozum.dk>
2020-09-02 11:35:45 +0200

    bootstrap-make: add build requirement on bootstrap-libc

--- recipes/bootstrap-make/all/conanfile.py
+++ recipes/bootstrap-make/all/conanfile.py
@@ -8,6 +8,7 @@ class BootstrapMakeConan(ConanFile):
     description = "GNU make utility to maintain groups of programs"
     license = "GPL"
     settings = {"build_type": ["RelWithDebInfo"], "os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system", "musl"]}
+    requires = (("generators/[^1.0.0]", "private"), "bootstrap-libc/[^1.0.0]")
 
     def source(self):
         tools.get(f"https://ftp.gnu.org/gnu/make/make-{self.version}.tar.gz")
