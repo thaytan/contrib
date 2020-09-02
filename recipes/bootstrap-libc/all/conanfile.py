e31c3253a02b6679d739c934cb5ff787cb10cfec
Niclas Overby <noverby@prozum.dk>
2020-09-02 14:00:56 +0200

    bootstrap-libc: use libc header packages

--- recipes/bootstrap-libc/all/conanfile.py
+++ recipes/bootstrap-libc/all/conanfile.py
@@ -5,11 +5,11 @@ class BootstrapLibcConan(ConanFile):
     name = "bootstrap-libc"
     description = "Virtual bootstrap libc package"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system", "musl"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
 
     def requirements(self):
         if self.settings.os_build == "Linux":
             if self.settings.libc_build == "system":
-                self.requires("bootstrap-glibc/[~2.27]")
+                self.requires("bootstrap-glibc-headers/[~2.27]")
             if self.settings.libc_build == "musl":
-                self.requires("bootstrap-musl/[~1.2.1]")
+                self.requires("bootstrap-musl-headers/[~1.2.1]")
