99c46afaecb87b2c8c584fbd6fbd1a12c2092605
Niclas Overby <noverby@prozum.dk>
2020-09-27 18:28:49 +0200

    bootstrap-ninja: cleanup

--- recipes/bootstrap-ninja/all/conanfile.py
+++ recipes/bootstrap-ninja/all/conanfile.py
@@ -4,12 +4,10 @@ from conans import *
 
 
 class BootstrapNinjaConan(ConanFile):
-    name = "bootstrap-ninja"
     description = "Small build system with a focus on speed"
     license = "Apache"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = ("bootstrap-libc-headers/[^1.0.0]", "bootstrap-cmake/[^3.18.0]")
-    requires = (("generators/[^1.0.0]", "private"),)
 
     def source(self):
         tools.get(f"https://github.com/ninja-build/ninja/archive/v{self.version}.tar.gz")
