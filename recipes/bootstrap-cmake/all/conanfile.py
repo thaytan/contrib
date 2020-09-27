0031def047254d81ba437bac37278f77d26f9e51
Niclas Overby <noverby@prozum.dk>
2020-09-27 18:13:02 +0200

    bootstrap-cmake: cleanup

--- recipes/bootstrap-cmake/all/conanfile.py
+++ recipes/bootstrap-cmake/all/conanfile.py
@@ -5,10 +5,9 @@ from conans import *
 class BootstrapCMakeConan(ConanFile):
     name = "bootstrap-cmake"
     description = "A cross-platform open-source make system + ninja"
-    license = "custom", "Apache"
+    license = "Apache"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = ("bootstrap-libc-headers/[^1.0.0]",)
-    requires = (("generators/[^1.0.0]", "private"),)
 
     def source(self):
         tools.get(f"https://github.com/Kitware/CMake/releases/download/v{self.version}/cmake-{self.version}.tar.gz")
