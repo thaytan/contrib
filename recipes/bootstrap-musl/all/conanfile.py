fd2ca51ca66f78673342df3fc2a6994411b2bc3c
Niclas Overby <noverby@prozum.dk>
2020-09-28 16:28:27 +0200

    bootstrap-musl: don't use development package

--- recipes/bootstrap-musl/all/conanfile.py
+++ recipes/bootstrap-musl/all/conanfile.py
@@ -5,8 +5,7 @@ class BootstrapMuslConan(ConanFile):
     description = "Lightweight implementation of C standard library"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    requires = ("bootstrap-linux-headers-dev/[^5.4.50]",)
-    no_dev_pkg = True
+    requires = ("bootstrap-linux-headers/[^5.4.50]",)
 
     def source(self):
         tools.get(f"https://www.musl-libc.org/releases/musl-{self.version}.tar.gz")
