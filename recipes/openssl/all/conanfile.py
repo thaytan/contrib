c40ccfde40dc233b1c09948c89fd21e3e4e8f748
Niclas Overby <noverby@prozum.dk>
2020-09-28 22:37:23 +0200

    openssl: build only static + cleanup

--- recipes/openssl/all/conanfile.py
+++ recipes/openssl/all/conanfile.py
@@ -1,27 +1,27 @@
-import os
-import platform
 import shutil
 
 from conans import *
 
 
 class OpensslConan(ConanFile):
-    name = "openssl"
     description = "TLS/SSL and crypto library"
-    license = "custom"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    build_requires = ("gcc/7.4.0",)
+    license = "BSD"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "bootstrap-llvm/[^10.0.1]",
+        "make/[^4.3]",
+    )
 
     def source(self):
-        tools.get(f"https://github.com/openssl/openssl/archive/OpenSSL_{self.version}.tar.gz".replace(".", "_"))
+        tools.get(f"https://github.com/openssl/openssl/archive/openssl-{self.version}.tar.gz")
 
     def build(self):
-        args = ["shared", "no-ssl3-method"]
+        args = ["no-shared", "no-ssl3-method"]
         if self.settings.arch_build == "x86_64":
             args += ["linux-x86_64", "enable-ec_nistp_64_gcc_128"]
         elif self.settings.arch_build == "armv8":
             args += ["linux-aarch64", "no-afalgeng"]
-        with tools.chdir("openssl-OpenSSL_" + self.version.replace(".", "_")):
+        with tools.chdir(f"openssl-openssl-{self.version}"):
             shutil.copy("Configure", "configure")
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
