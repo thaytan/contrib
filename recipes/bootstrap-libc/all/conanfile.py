533e08b81a6cb69e6fbfd10c65310d77e2c5c603
Niclas Overby <noverby@prozum.dk>
2020-09-28 14:55:20 +0200

    bootstrap-libc: rename package

--- recipes/bootstrap-libc/all/conanfile.py
+++ recipes/bootstrap-libc/all/conanfile.py
@@ -1,14 +1,14 @@
 from conans import *
 
 
-class BootstrapLibcHeadersConan(ConanFile):
-    description = "Virtual bootstrap libc headers"
+class BootstrapLibcConan(ConanFile):
+    description = "Virtual bootstrap libc"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
 
     def requirements(self):
         if self.settings.os_build == "Linux":
             if self.settings.libc_build == "system":
-                self.requires("bootstrap-glibc-headers/[~2.27]")
+                self.requires("bootstrap-glibc-headers-dev/[~2.27]")
             if self.settings.libc_build == "musl":
-                self.requires("bootstrap-musl-headers/[~1.2.1]")
+                self.requires("bootstrap-musl-dev/[~1.2.1]")
