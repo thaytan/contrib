30bcf35b7af05a99f9125586aa6ef535fe275f5e
Niclas Overby <noverby@prozum.dk>
2020-09-28 23:31:54 +0200

    bzip2: fix build

--- recipes/bzip2/all/conanfile.py
+++ recipes/bzip2/all/conanfile.py
@@ -18,15 +18,10 @@ class Bzip2Conan(ConanFile):
     def build(self):
         with tools.chdir(f"{self.name}-{self.version}"):
             autotools = AutoToolsBuildEnvironment(self)
-            self.run(f"make -f Makefile-libbz2_so CC={tools.get_env('CC')}")
-            autotools.make(target="bzip2 bzip2recover", args=[f"CC={tools.get_env('CC')}"])
-            autotools.install(args=["PREFIX=" + self.package_folder])
+            autotools.make([f"CC=cc"])
+            autotools.install([f"PREFIX={self.package_folder}"])
 
     def package(self):
-        self.copy(pattern="*libbz2.so*", dst="lib", symlinks=True, keep_path=False)
-        os.symlink(
-            f"libbz2.so.{self.version}", os.path.join(self.package_folder, "lib", "libbz2.so"),
-        )
         with tools.chdir(os.path.join(self.package_folder, "bin")):
             for source, link in (
                 ("bzdiff", "bzcmp"),
