105ec18211943d7f4438515f210a8f409602af35
Niclas Overby <noverby@prozum.dk>
2020-09-30 11:28:27 +0200

    python: port to github

--- recipes/python/all/conanfile.py
+++ recipes/python/all/conanfile.py
@@ -7,12 +7,12 @@ class PythonConan(ConanFile):
     name = "python"
     description = "Next generation of the python high-level scripting language"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    build_requires = ("cc/[^1.0.0]",)
-    requires = (
-        "base/[^1.0.0]",
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "bootstrap-llvm/[^10.0.1]",
+        "make/[^4.3]",
         "expat/[^2.2.7]",
-        "openssl/[^1.1.1b]",
+        "openssl/[^3.0.0-alpha6]",
         "libffi/[^3.3]",
         "zlib/[^1.2.11]",
         "bzip2/[^1.0.8]",
@@ -24,7 +24,6 @@ class PythonConan(ConanFile):
 
     def build(self):
         args = [
-            "--enable-shared",
             "--with-openssl=" + self.deps_cpp_info["openssl"].rootpath,
             "--with-computed-gotos",
             "--enable-optimizations",
@@ -40,12 +39,12 @@ class PythonConan(ConanFile):
             autotools.configure(args=args)
             autotools.make()
             autotools.install()
-        os.symlink("python3.7", os.path.join(self.package_folder, "bin", "python"))
+        os.symlink("python3.8", os.path.join(self.package_folder, "bin", "python"))
 
     def package_info(self):
         self.env_info.PYTHON = os.path.join(self.package_folder, "bin", "python")
         self.env_info.PYTHONHOME = self.package_folder
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.7"))
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.8"))
         if "CC" in os.environ:
             ldshared = os.environ["CC"] + " -pthread -shared "
             if self.settings.arch_build == "x86_64":
