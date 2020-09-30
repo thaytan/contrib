66fdc5842b9ba4df67df0f4237e0aad965e05ce9
Niclas Overby <noverby@prozum.dk>
2020-09-30 09:57:10 +0200

    sqlite: cleanup

--- recipes/sqlite/all/conanfile.py
+++ recipes/sqlite/all/conanfile.py
@@ -7,17 +7,13 @@ def conv_version(version):
 
 
 class SqliteConan(ConanFile):
-    name = "sqlite"
     description = "A C library that implements an SQL database engine"
-    license = "custom:Public Domain"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    license = "custom"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "base/[^1.0.0]",
-        "autotools/[^1.0.0]",
+        "bootstrap-llvm/[^10.0.1]",
+        "make/[^4.3]",
         "tcl/[^8.6.10]",
-    )
-    requires = (
-        "base/[^1.0.0]",
         "zlib/[^1.2.11]",
         "readline/[^8.0]",
     )
@@ -26,8 +22,11 @@ class SqliteConan(ConanFile):
         tools.get(f"https://www.sqlite.org/2019/sqlite-src-{conv_version(self.version)}.zip")
 
     def build(self):
+        args = [
+            "--disable-shared",
+        ]
         autotools = AutoToolsBuildEnvironment(self)
         with tools.chdir(f"{self.name}-src-{conv_version(self.version)}"):
             self.run("chmod +x configure")
-            autotools.configure()
+            autotools.configure(args=args)
             autotools.install()
