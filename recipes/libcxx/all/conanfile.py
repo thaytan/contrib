8376f635599da960d333aeadee63c7f4379d8a87
Niclas Overby <noverby@prozum.dk>
2020-09-30 13:10:42 +0200

    libcxx: port to github

--- recipes/libcxx/all/conanfile.py
+++ recipes/libcxx/all/conanfile.py
@@ -4,15 +4,15 @@ from conans import *
 
 
 class LibcxxConan(ConanFile):
-    name = "libcxx"
     description = "LLVM C++ Standard Library"
     license = "custom"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "llvm-bootstrap/[^10.0.0]",
-        "cmake-bootstrap/[^3.17.3]",
+        "bootstrap-llvm/[^10.0.1]",
+        "bootstrap-cmake/[^3.18.0]",
+        "bootstrap-ninja/[^1.10.0]",
+        "python/[^3.8.5]",
     )
-    requires = (("generators/[^1.0.0]", "private"),)
 
     def source(self):
         tools.get(f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{self.version}/llvm-{self.version}.src.tar.xz")
@@ -22,47 +22,29 @@ class LibcxxConan(ConanFile):
         shutil.move(f"llvm-{self.version}.src", f"llvm-{self.version}")
         shutil.move(f"libcxx-{self.version}.src", os.path.join(f"llvm-{self.version}", "projects", "libcxx"))
         shutil.move(f"libcxxabi-{self.version}.src", os.path.join(f"llvm-{self.version}", "projects", "libcxxabi"))
-        shutil.move(f"libunwind-{self.version}.src", os.path.join(f"llvm-{self.version}", "projects", "libunwind"))
 
     def build(self):
         cmake = CMake(self)
 
-        # LLVM build options
-        cmake.definitions["LLVM_BUILD_DOCS"] = False
-        cmake.definitions["LLVM_BUILD_EXAMPLES"] = False
-        cmake.definitions["LLVM_BUILD_RUNTIME"] = True
-        cmake.definitions["LLVM_BUILD_TESTS"] = False
-
         # LLVM enable options
+        cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
         cmake.definitions["LLVM_ENABLE_LTO"] = True
-        cmake.definitions["LLVM_ENABLE_ASSERTIONS"] = False
-        cmake.definitions["LLVM_ENABLE_FFI"] = False
-        cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
-        cmake.definitions["LLVM_ENABLE_PIC"] = True
-        cmake.definitions["LLVM_ENABLE_RTTI"] = True
-        cmake.definitions["LLVM_ENABLE_SPHINX"] = False
-        cmake.definitions["LLVM_ENABLE_TERMINFO"] = True
-        cmake.definitions["LLVM_ENABLE_ZLIB"] = True
-        cmake.definitions["LLVM_INCLUDE_EXAMPLES"] = False
-        cmake.definitions["LLVM_ENABLE_LIBCXX"] = True
 
         # libcxx options
         cmake.definitions["LIBCXX_ENABLE_SHARED"] = False
         cmake.definitions["LIBCXX_ENABLE_STATIC_ABI_LIBRARY"] = True
         cmake.definitions["LIBCXX_USE_COMPILER_RT"] = True
+        if self.settings.libc_build == "musl":
+            cmake.definitions["LIBCXX_HAS_MUSL_LIBC"] = True
 
         # libcxxabi options
         cmake.definitions["LIBCXXABI_ENABLE_SHARED"] = False
-        cmake.definitions["LIBCXXABI_ENABLE_STATIC_UNWINDER"] = True
         cmake.definitions["LIBCXXABI_USE_LLVM_UNWINDER"] = True
         cmake.definitions["LIBCXXABI_USE_COMPILER_RT"] = True
+        if self.settings.libc_build == "musl":
+            cmake.definitions["LIBCXXABI_ENABLE_STATIC_UNWINDER"] = True
 
-        # libunwind options
-        cmake.definitions["LIBUNWIND_ENABLE_SHARED"] = True
-
-        cmake.configure(source_folder=f"llvm-{self.version}")
-        cmake.build(target="install-libcxx")
-        cmake.build(target="install-unwind")
-
-    def package_info(self):
-        self.env_info.CPLUS_INCLUDE_PATH = os.path.join(self.package_folder, "include", "c++", "v1")
+        env = {"CXXFLAGS": ""}
+        with tools.environment_append(env):
+            cmake.configure(source_folder=f"llvm-{self.version}")
+            cmake.build(target="install-libcxx")
