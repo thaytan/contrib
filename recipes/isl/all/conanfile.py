86ef8181c367606c9358098a911450ed147b8deb
Niclas Overby <noverby@prozum.dk>
2020-09-30 17:04:18 +0200

    isl: port to github

--- recipes/isl/all/conanfile.py
+++ recipes/isl/all/conanfile.py
@@ -1,20 +1,14 @@
-import os
-
 from conans import *
 
 
 class IslConan(ConanFile):
-    name = "isl"
     description = "Library for manipulating sets and relations of integer points bounded by linear constraints"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "bootstrap-cc/[^1.0.0]",
+        "bootstrap-llvm/[^10.0.1]",
         "make/[^4.3]",
-    )
-    requires = (
-        "base/[^1.0.0]",
-        "gmp/[^6.1.2]",
+        "gmp/[^6.2.0]",
     )
 
     def source(self):
@@ -22,10 +16,9 @@ class IslConan(ConanFile):
 
     def build(self):
         args = [
-            "--disable-static",
+            "--disable-shared",
         ]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.make()
-            autotools.make(target="install-strip")
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"{self.name}-{self.version}", args)
+        autotools.make()
+        autotools.install()
