bd48d0c93a29209932393a9c6c27d08c4c155faa
Niclas Overby <noverby@prozum.dk>
2020-09-30 13:33:03 +0200

    libunwind: port to llvm libunwind

--- recipes/libunwind/all/conanfile.py
+++ recipes/libunwind/all/conanfile.py
@@ -1,22 +1,35 @@
+import os
+import shutil
 from conans import *
 
 
 class LibunwindConan(ConanFile):
-    name = "libunwind"
-    description = "Portable and efficient C programming interface (API) to determine the call-chain of a programs"
-    license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    build_requires = ("autotools/[^1.0.0]",)
+    description = "LLVM libunwind"
+    license = "custom"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "bootstrap-llvm/[^10.0.1]",
+        "bootstrap-cmake/[^3.18.0]",
+        "bootstrap-ninja/[^1.10.0]",
+        "python/[^3.8.5]",
+    )
 
     def source(self):
-        tools.get(f"https://download.savannah.gnu.org/releases/libunwind/libunwind-{self.version}.tar.gz")
+        tools.get(f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{self.version}/llvm-{self.version}.src.tar.xz")
+        tools.get(f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{self.version}/libunwind-{self.version}.src.tar.xz")
+        shutil.move(f"llvm-{self.version}.src", f"llvm-{self.version}")
+        shutil.move(f"libunwind-{self.version}.src", os.path.join(f"llvm-{self.version}", "projects", "libunwind"))
 
     def build(self):
-        args = [
-            "--disable-static",
-        ]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.make()
-            autotools.install()
+        cmake = CMake(self)
+
+        # LLVM enable options
+        cmake.definitions["LLVM_ENABLE_LTO"] = True
+
+        # libunwind options
+        cmake.definitions["LIBUNWIND_USE_COMPILER_RT"] = True
+        cmake.definitions["LIBUNWIND_ENABLE_SHARED"] = True
+        cmake.definitions["LIBUNWIND_ENABLE_STATIC"] = False
+
+        cmake.configure(source_folder=f"llvm-{self.version}")
+        cmake.build(target="install-unwind")
