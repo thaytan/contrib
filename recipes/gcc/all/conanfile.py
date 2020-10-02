e20772e2b08696be9ac80f4c384e7756c55aab71
Niclas Overby <noverby@prozum.dk>
2020-10-02 11:49:43 +0200

    gcc: use llvm headers

--- recipes/gcc/all/conanfile.py
+++ recipes/gcc/all/conanfile.py
@@ -24,7 +24,7 @@ class GccConan(ConanFile):
     def build(self):
         env = {
             "CFLAGS": os.environ["CFLAGS"].replace("-flto=thin", ""),
-            "CXXFLAGS": os.environ["CFLAGS"] + " -nostdinc++ ",  # Don't include libcxx headers
+            "CXXFLAGS": os.environ["CXXFLAGS"].replace("-flto=thin", ""),
         }
         args = [
             f"--libexecdir={os.path.join(self.package_folder, 'lib')}",
