d3615749c14a5b3f1056959d8e3f2f5e31bc6b90
Niclas Overby <noverby@prozum.dk>
2020-10-06 20:24:53 +0200

    curl: port to github

--- recipes/curl/all/conanfile.py
+++ recipes/curl/all/conanfile.py
@@ -1,15 +1,14 @@
-import os
-
 from conans import *
 
 
 class CurlConan(ConanFile):
-    name = "curl"
     description = "An URL retrieval utility and library"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
         "clang/[^10.0.1]",
+        "make/[^4.3]",
+        "pkgconf/[^1.7.3]",
         "zlib/[^1.2.11]",
         "openssl/[^3.0.0-alpha6]",
     )
