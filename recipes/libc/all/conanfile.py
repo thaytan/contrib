0e94fb786ae672988c9d444c94d495c789b24c01
Niclas Overby <noverby@prozum.dk>
2020-10-06 16:55:33 +0200

    libc: port to github

--- recipes/libc/all/conanfile.py
+++ recipes/libc/all/conanfile.py
@@ -2,11 +2,13 @@ from conans import *
 
 
 class LibcConan(ConanFile):
-    name = "libc"
     description = "Virtual libc package"
     license = "MIT"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc": ["system", "musl"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
 
     def requirements(self):
-        if self.settings.libc == "musl":
-            self.requires("musl/[^1.2.0]")
+        if self.settings.os_build == "Linux":
+            if self.settings.libc_build == "system":
+                self.requires("glibc-headers/[~2.27]")
+            if self.settings.libc_build == "musl":
+                self.requires("musl/[~1.2.1]")
