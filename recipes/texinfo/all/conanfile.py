60bc331768a11bb2d64ccd3222499f8940c8bdbc
Niclas Overby <noverby@prozum.dk>
2020-10-06 18:43:07 +0200

    texinfo: port to github

--- recipes/texinfo/all/conanfile.py
+++ recipes/texinfo/all/conanfile.py
@@ -4,20 +4,23 @@ from conans import *
 
 
 class TexinfoConan(ConanFile):
-    name = "texinfo"
     description = "GNU documentation system for on-line information and printed output"
     license = "GPL3"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "clang/[^10.0.1]",
+        "make/[^4.3]",
+        "perl/[^5.30.0]",
+    )
 
     def source(self):
         tools.get(f"https://ftp.gnu.org/pub/gnu/texinfo/texinfo-{self.version}.tar.xz")
 
     def build(self):
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure()
-            autotools.make()
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"texinfo-{self.version}")
+        autotools.make()
+        autotools.install()
 
     def package_info(self):
         self.env_info.MAKEINFO = os.path.join(self.package_folder, "bin", "makeinfo")
