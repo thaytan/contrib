3cd2c7df38bade19898f3cfce4b93aae654fb6d2
Niclas Overby <noverby@prozum.dk>
2020-10-06 08:56:34 +0200

    llvm: port to github

--- recipes/llvm/all/conanfile.py
+++ recipes/llvm/all/conanfile.py
@@ -1,32 +1,46 @@
+import shutil
 from conans import *
 
 
 class LlvmConan(ConanFile):
-    name = "llvm"
     description = "Collection of modular and reusable compiler and toolchain technologies"
     license = "Apache"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"], "libc_build": ["system"]}
-    build_requires = ("cmake-bootstrap/[^3.17.3]",)
-    requires = (
-        ("generators/[^1.0.0]", "private"),
-        "libcxx/[^10.0.0]",
-        ("llvm-bootstrap/[^10.0.0]", "private"),
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "bootstrap-llvm/[^10.0.1]",
+        "bootstrap-cmake/[^3.18.0]",
+        "bootstrap-ninja/[^1.10.0]",
+        "python/[^3.8.5]",
+        "libunwind/[^10.0.1]",
         "zlib/[^1.2.11]",
         "ncurses/[^6.2]",
         "libffi/[^3.3]",
     )
+    requires = (
+        "libcxx/[^10.0.1]",
+        "libunwind/[^10.0.1]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/llvm/llvm-project/releases/download/llvmorg-{self.version}/llvm-{self.version}.src.tar.xz")
+        shutil.move(f"llvm-{self.version}.src", f"llvm-{self.version}")
 
     def build(self):
         cmake = CMake(self)
 
         # LLVM build options
         if self.settings.arch_build == "x86_64":
-            cmake.definitions["LLVM_TARGETS_TO_BUILD"] = "X86;WebAssembly"
+            cmake.definitions["LLVM_TARGETS_TO_BUILD"] = "X86"
+            arch = "x86_64"
         elif self.settings.arch_build == "armv8":
             cmake.definitions["LLVM_TARGETS_TO_BUILD"] = "AArch64"
+            arch = "aarch64"
+        if self.settings.libc_build == "musl":
+            abi = "musl"
+        else:
+            abi = "gnu"
+        cmake.definitions["LLVM_HOST_TRIPLE"] = f"{arch}-aivero-linux-{abi}"
+
         cmake.definitions["LLVM_BUILD_RUNTIME"] = True
         cmake.definitions["LLVM_BUILD_DOCS"] = False
         cmake.definitions["LLVM_BUILD_EXAMPLES"] = False
@@ -46,17 +60,7 @@ class LlvmConan(ConanFile):
 
         # LLVM other options
         cmake.definitions["LLVM_INCLUDE_EXAMPLES"] = False
-        cmake.definitions["LLVM_INSTALL_BINUTILS_SYMLINKS"] = True
-        cmake.definitions["LLVM_INSTALL_UTILS"] = True
 
-        # Reduce memory usage
-        cmake.definitions["CMAKE_JOB_POOL_LINK"] = "link"
-        cmake.definitions["CMAKE_JOB_POOLS"] = "link=1"
-
-        env = {
-            "CPLUS_INCLUDE_PATH": "",  # Use only llvm-bootstrap header files to avoid header conflicts with libcxx
-        }
-        with tools.environment_append(env):
-            cmake.configure(source_folder=f"{self.name}-{self.version}.src")
-            cmake.build()
-            cmake.install()
+        cmake.configure(source_folder=f"llvm-{self.version}")
+        cmake.build()
+        cmake.install()
