59bf09286170c173df49674df7b7a2313b2339ba
Niclas Overby <noverby@prozum.dk>
2020-10-09 15:57:55 +0200

    bootstrap-llvm: install libraries

--- recipes/bootstrap-llvm/all/conanfile.py
+++ recipes/bootstrap-llvm/all/conanfile.py
@@ -190,6 +190,7 @@ class BootstrapLlvmConan(ConanFile):
             cmake.build(target="install-llvm-tblgen")
             cmake.build(target="install-llvm-profdata")
             cmake.build(target="install-FileCheck")
+            cmake.build(target="install-llvm-libraries")
             cmake.build(target="install-llvm-headers")
 
         # Make lld, clang, clang++ default
