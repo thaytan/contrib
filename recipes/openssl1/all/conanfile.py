dbd7cff8f2e23463d5c37ded61c98800faf412f2
Niclas Overby <noverby@prozum.dk>
2020-10-10 17:05:59 +0200

    openssl1: add package

---
+++ recipes/openssl1/all/conanfile.py
@@ -0,0 +1,34 @@
+import shutil
+import os
+from conans import *
+
+
+class Openssl1Conan(ConanFile):
+    description = "TLS/SSL and crypto library version 1"
+    license = "BSD"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "bootstrap-llvm/[^10.0.1]",
+        "make/[^4.3]",
+        "perl/[^5.30.0]",
+    )
+    requires = ("ca-certificates/[^20191127]",)
+
+    def source(self):
+        tools.get(f"https://github.com/openssl/openssl/archive/openssl-{self.version}.tar.gz")
+
+    def build(self):
+        args = ["no-shared", "no-ssl3-method"]
+        if self.settings.arch_build == "x86_64":
+            args += ["linux-x86_64", "enable-ec_nistp_64_gcc_128"]
+        elif self.settings.arch_build == "armv8":
+            args += ["linux-aarch64", "no-afalgeng"]
+        with tools.chdir(f"openssl-openssl-{self.version}"):
+            shutil.copy("Configure", "configure")
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.configure(args=args)
+            autotools.make()
+            autotools.install()
+
+    def package_info(self):
+        self.env_info.SSL_CERT_DIR = os.path.join(self.deps_cpp_info["ca-certificates"].rootpath, "etc", "ssl", "certs")
