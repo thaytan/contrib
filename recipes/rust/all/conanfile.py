637fe1968b452470072b4fcab70f899ab28de4a5
Niclas Overby <noverby@prozum.dk>
2020-10-10 10:30:42 +0200

    rust: fix pkgconf version

--- recipes/rust/all/conanfile.py
+++ recipes/rust/all/conanfile.py
@@ -13,7 +13,7 @@ class RustConan(ConanFile):
         "bootstrap-ninja/[^1.10.0]",
         "python/[^3.8.5]",
         "curl/[^7.72.0]",
-        "pkgconf/[^2.28.0]",
+        "pkgconf/[^1.7.3]",
         "zlib/[^1.2.11]",
         "openssl/[^3.0.0-alpha6]",
     )
