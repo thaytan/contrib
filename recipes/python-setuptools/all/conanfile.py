5daafd22b34303144bc03a4aff7100c03354f16b
Niclas Overby <noverby@prozum.dk>
2020-10-11 14:58:08 +0200

    python-setuptools: port to github

--- recipes/python-setuptools/all/conanfile.py
+++ recipes/python-setuptools/all/conanfile.py
@@ -1,23 +1,18 @@
 import os
-
 from conans import *
 
 
 class PythonSetuptoolsConan(ConanFile):
-    name = "python-setuptools"
     description = "Easily download, build, install, upgrade, and uninstall Python packages"
     license = "Apache"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    requires = (
-        "base/[^1.0.0]",
-        "python/[^3.7.4]",
-    )
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    requires = ("python/[^3.8.5]",)
 
     def source(self):
         tools.get(f"https://github.com/pypa/setuptools/archive/v{self.version}.tar.gz")
 
     def build(self):
-        py_path = os.path.join(self.package_folder, "lib", "python3.7", "site-packages")
+        py_path = os.path.join(self.package_folder, "lib", "python3.8", "site-packages")
         env = {"PYTHONPATH": py_path}
         os.makedirs(py_path)
         with tools.chdir("setuptools-" + self.version), tools.environment_append(env):
