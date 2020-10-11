fcd4da8dbffa50658df45a46d06d80a077c363e5
Niclas Overby <noverby@prozum.dk>
2020-10-11 14:35:17 +0200

    autotools: port to github

--- recipes/autotools/all/conanfile.py
+++ recipes/autotools/all/conanfile.py
@@ -5,15 +5,13 @@ class AutotoolsConan(ConanFile):
     name = "autotools"
     description = "A suite of programming tools 'designed' to assist in making source code"
     license = "GPL"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     requires = (
-        "base/[^1.0.0]",
-        "cc/[^1.0.0]",
-        "make/[^3.4.0]",
+        "make/[^4.3]",
         "autoconf/[^2.69]",
         "automake/[^1.16.1]",
         "libtool/[^2.4.6]",
-        "pkgconf/[^1.6.3]",
-        "gettext/[^0.20.1]",
+        "pkgconf/[^1.7.3]",
+        "gettext/[^0.21]",
         "texinfo/[^6.6]",
     )
