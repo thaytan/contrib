b5b336dac3629eade36a0b5f80d280715a4626fb
Niclas Overby <noverby@prozum.dk>
2020-10-12 16:11:22 +0200

    cmake: cleanup

--- recipes/cmake/all/conanfile.py
+++ recipes/cmake/all/conanfile.py
@@ -16,11 +16,9 @@ class CMakeConan(ConanFile):
         tools.get(f"https://github.com/Kitware/CMake/releases/download/v{self.version}/cmake-{self.version}.tar.gz")
 
     def build(self):
-        env = {"CXXFLAGS": os.environ["CXXFLAGS"] + ""}
-        with tools.environment_append(env):
-            cmake = CMake(self)
-            cmake.definitions["CMAKE_USE_OPENSSL"] = False
-            cmake.configure(source_folder=f"cmake-{self.version}")
-            cmake.build()
-            cmake.install()
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_USE_OPENSSL"] = False
+        cmake.configure(source_folder=f"cmake-{self.version}")
+        cmake.build()
+        cmake.install()
 
