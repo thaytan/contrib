9aba13a7cae8288d278fb044576d73dbd8b0c399
Niclas Overby <noverby@prozum.dk>
2020-10-12 19:12:19 +0200

    gobject-introspection: port to github

--- recipes/gobject-introspection/all/conanfile.py
+++ recipes/gobject-introspection/all/conanfile.py
@@ -1,22 +1,23 @@
 import os
-
 from conans import *
 
 
 class GObjectIntrospectionConan(ConanFile):
-    name = "gobject-introspection"
     description = "Middleware layer between C libraries (using GObject) and language bindings"
-    license = "GPL, LGPL"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    license = "LGPL"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "meson/[^0.51.2]",
-        "bison/[^3.3]",
+        "clang/[^10.0.1]",
+        "meson/[^0.55.3]",
+        "bison/[^3.7.2]",
         "flex/[^2.6.4]",
+        "git/[^2.28.0]",
+        "libffi/[^3.3]",
+        "zlib/[^1.2.11]",
     )
     requires = (
-        "base/[^1.0.0]",
-        "python/[^3.7.4]",
-        "glib/[^2.62.0]",
+        "python/[^3.8.5]",
+        "glib/[^2.66.1]",
     )
 
     def source(self):
@@ -25,7 +26,7 @@ class GObjectIntrospectionConan(ConanFile):
     def build(self):
         args = ["--auto-features=disabled"]
         meson = Meson(self)
-        meson.configure(source_folder=f"{self.name, self.version), args=args}-{pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"}")
+        meson.configure(args, source_folder=f"gobject-introspection-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
 
     def package_info(self):
