319a0a1ddba57f9a21c1d060b333f8dea8ef3fed
Niclas Overby <noverby@prozum.dk>
2020-10-13 09:11:44 +0200

    Add file and bootstrap-file packages

---
+++ recipes/bootstrap-file/all/conanfile.py
@@ -0,0 +1,20 @@
+from conans import *
+
+
+class BootstrapFileConan(ConanFile):
+    description = "File type identification utility"
+    license = "custom"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+
+    def source(self):
+        tools.get(f"https://astron.com/pub/file/file-{self.version}.tar.gz")
+
+    def build(self):
+        args = [
+            "--disable-shared",
+            "--disable-dependency-tracking",
+        ]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"file-{self.version}", args)
+        autotools.make()
+        autotools.install()
