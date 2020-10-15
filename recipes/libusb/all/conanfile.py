fdb446d487944b2dd0cafef4c1b7d1f6df7b64b0
Niclas Overby <noverby@prozum.dk>
2020-10-15 17:30:37 +0200

    libusb: port to github

--- recipes/libusb/all/conanfile.py
+++ recipes/libusb/all/conanfile.py
@@ -1,25 +1,24 @@
-from glob import glob
-from os import path, remove
-
 from conans import *
 
 
 class LibUSBConan(ConanFile):
-    name = "libusb"
     description = "A cross-platform library to access USB devices"
     license = "LGPL-2.1"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    options = {"udev": [True, False]}
-    default_options = "udev=False"
-    build_requires = ("autotools/[^1.0.0]",)
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "clang/[^10.0.1]",
+        "autotools/[^1.0.0]",
+    )
+    requires = ("eudev/[^3.2.9]",)
 
     def source(self):
         tools.get(f"https://github.com/libusb/libusb/releases/download/v{self.version}/libusb-{self.version}.tar.bz2")
 
     def build(self):
-        args = ["--disable-static"]
-        args.append("--enable-udev" if self.options.udev else "--disable-udev")
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        args = [
+            "--disable-shared",
+            "--enable-udev",
+        ]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"libusb-{self.version}", args)
+        autotools.install()
