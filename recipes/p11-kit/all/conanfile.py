125a3657ee579af0cc89984b415a6c65024f2f09
Niclas Overby <noverby@prozum.dk>
2020-10-15 15:34:12 +0200

    p11-kit: add package

---
+++ recipes/p11-kit/all/conanfile.py
@@ -0,0 +1,26 @@
+from conans import *
+
+
+class P11KitConan(ConanFile):
+    description = "Loads and enumerates PKCS#11 modules"
+    license = "BSD"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "clang/[^10.0.1]",
+        "make/[^4.3]",
+        "pkgconf/[^1.7.3]",
+        "libtasn1/[^4.16.0]",
+        "libffi/[^3.3]",
+    )
+
+    def source(self):
+        tools.get(f"https://github.com/p11-glue/p11-kit/releases/download/{self.version}/p11-kit-{self.version}.tar.xz")
+
+    def build(self):
+        args = [
+            "--without-trust-paths",
+        ]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"p11-kit-{self.version}", args)
+        autotools.make()
+        autotools.install()
