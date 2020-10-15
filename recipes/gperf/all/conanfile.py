7bd0dc374b92d54ccda6ad7530704c712082f789
Niclas Overby <noverby@prozum.dk>
2020-10-15 17:03:56 +0200

    gperf: use meson port

--- recipes/gperf/all/conanfile.py
+++ recipes/gperf/all/conanfile.py
@@ -2,16 +2,18 @@ from conans import *
 
 
 class GperfConan(ConanFile):
-    name = "gperf"
     description = "A portable, high level programming interface to various calling conventions"
     license = "GPL3"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "meson/[^0.55.3]",
+        "clang/[^10.0.1]",
+    )
 
     def source(self):
-        git = tools.Git(f"{self.name}-{self.version}")
-        git.clone("https://gitlab.com/aivero/public/gperf.git", "meson")
+        tools.get(f"https://gitlab.com/aivero/public/gperf/-/archive/meson/gperf-meson.tar.gz")
 
     def build(self):
         meson = Meson(self)
-        meson.configure(source_folder=f"{self.name}-{self.version}")
+        meson.configure(source_folder="gperf-meson")
         meson.install()
