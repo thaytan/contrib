423da53038ceb26ac6eecd6faa0a55d5f5e8c82a
Niclas Overby <noverby@prozum.dk>
2020-10-15 13:58:51 +0200

    libtasn1: add package

---
+++ recipes/libtasn1/all/conanfile.py
@@ -0,0 +1,23 @@
+from conans import *
+
+
+class Libtasn1Conan(ConanFile):
+    description = "The ASN.1 library used in GNUTLS"
+    license = "GPL3"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "clang/[^10.0.1]",
+        "make/[^4.3]",
+    )
+
+    def source(self):
+        tools.get(f"https://ftp.gnu.org/gnu/libtasn1/libtasn1-{self.version}.tar.gz")
+
+    def build(self):
+        args = [
+            "--disable-shared",
+        ]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"libtasn1-{self.version}", args)
+        autotools.make()
+        autotools.install()
