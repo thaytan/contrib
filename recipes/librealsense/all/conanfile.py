9a32e14760156be969affbe9b223475928989de9
Niclas Overby <noverby@prozum.dk>
2020-10-15 20:53:55 +0200

    librealsense: port to github

--- recipes/librealsense/all/conanfile.py
+++ recipes/librealsense/all/conanfile.py
@@ -22,6 +22,12 @@ class LibRealsenseConan(ConanFile):
         tools.patch(patch_file="libusb-fix.patch", base_path=f"{self.name}-{self.version}")
 
     def build(self):
+        env = {
+            # "CMAKE_PROGRAM_PATH": self.deps_cpp_info["python"].rootpath,
+            # "PythonInterp_ROOT": self.deps_cpp_info["python"].rootpath,
+            "PATH": os.environ["PATH"].replace("/usr/bin:", "")
+        }
+        # with tools.environment_append(env):
         cmake = CMake(self)
         # cmake.definitions["BUILD_WITH_CUDA"] = self.options.cuda
         cmake.definitions["BUILD_PYTHON_BINDINGS"] = True
@@ -30,6 +36,11 @@ class LibRealsenseConan(ConanFile):
         cmake.definitions["BUILD_PCL_EXAMPLES"] = False
         cmake.definitions["BUILD_NODEJS_BINDINGS"] = False
         cmake.definitions["BUILD_UNIT_TESTS"] = False
+        cmake.definitions["PYTHON_INCLUDE_DIRS"] = os.path.join(self.deps_cpp_info["python"].rootpath, "include", "python3.8")
+        cmake.definitions["PYTHON_LIBRARIES"] = os.path.join(self.deps_cpp_info["python"].rootpath, "lib", "libpython.a")
+        cmake.definitions["PYTHONLIBS_FOUND"] = True
+        cmake.definitions["PYTHON_MODULE_EXTENSION"] = "a"
+
         cmake.configure(source_folder=f"{self.name}-{self.version}")
         cmake.build()
         cmake.install()
