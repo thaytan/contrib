a8562deed080c8f62807f8c78e35af2b49667fa4
Niclas Overby <noverby@prozum.dk>
2020-10-15 17:38:05 +0200

    cmake: add ninja requirement

--- recipes/cmake/all/conanfile.py
+++ recipes/cmake/all/conanfile.py
@@ -7,10 +7,10 @@ class CMakeConan(ConanFile):
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
         "bootstrap-cmake/[^3.18.0]",
-        "bootstrap-ninja/[^1.10.0]",
         "clang/[^10.0.1]",
         "pkgconf/[^1.7.3]",
     )
+    requires = ("ninja/[^1.10.0]",)
 
     def source(self):
         tools.get(f"https://github.com/Kitware/CMake/releases/download/v{self.version}/cmake-{self.version}.tar.gz")
