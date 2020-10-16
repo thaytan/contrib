f198c943628f493e868e8da4218afd62dc22733c
Niclas Overby <noverby@prozum.dk>
2020-10-16 17:04:27 +0200

    bootstrap-glibc: rename package

--- recipes/bootstrap-glibc/all/conanfile.py
+++ recipes/bootstrap-glibc/all/conanfile.py
@@ -3,8 +3,8 @@ import os
 from conans import *
 
 
-class BootstrapGlibcHeadersConan(ConanFile):
-    description = "glibc bootstrap headers"
+class BootstrapGlibcConan(ConanFile):
+    description = "glibc bootstrap"
     license = "GPL"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     requires = "bootstrap-linux-headers/[^5.4.50]"
@@ -19,3 +19,20 @@ class BootstrapGlibcHeadersConan(ConanFile):
 
         # install-headers does not create include/gnu/stubs.h
         pathlib.Path(os.path.join(self.package_folder, "include", "gnu", "stubs.h")).touch()
+
+        # Use system libgcc_s
+        arch = {"x86_64": "x86_64", "armv8": "aarch64"}[str(self.settings.arch_build)]
+        with tools.chdir(os.path.join(self.package_folder, "lib")):
+            os.symlink(f"/lib/{arch}-linux-gnu/libgcc_s.so.1", "libgcc_s.so")
+
+        # Copy system libs from glibc-dev
+        libs = [
+            "libc.a",
+            "libm.a",
+            "libpthread.a",
+            "crt1.o",
+            "crti.o",
+            "crtn.o",
+        ]
+        for lib in libs:
+            os.symlink(f"/lib/{arch}-linux-gnu/{lib}", lib)
