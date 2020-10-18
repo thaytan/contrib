cea8074ce59679abb673eff3ce331f40c137beaa
Niclas Overby <noverby@prozum.dk>
2020-10-18 19:22:09 +0200

    xcb-proto: port to github

--- recipes/xcb-proto/all/conanfile.py
+++ recipes/xcb-proto/all/conanfile.py
@@ -5,13 +5,15 @@ class XcbProtoConan(ConanFile):
     description = "XML-XCB protocol descriptions"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("pkgconf/[^1.6.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://xcb.freedesktop.org/dist/xcb-proto-{self.version}.tar.bz2")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools.configure()
-            autotools.install()
+        autotools.configure(f"xcb-proto-{self.version}")
+        autotools.install()
