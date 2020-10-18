969ecf88ec97cf4e55ace7e0ee6e0308dba3e4fd
Niclas Overby <noverby@prozum.dk>
2020-10-18 19:41:28 +0200

    rapidjson: port to github

--- recipes/rapidjson/all/conanfile.py
+++ recipes/rapidjson/all/conanfile.py
@@ -1,5 +1,3 @@
-import os
-
 from conans import *
 
 
@@ -7,16 +5,19 @@ class RapidJsonConan(ConanFile):
     description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("cmake/[^3.15.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "cmake/[^3.18.4]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/Tencent/rapidjson/archive/{self.version}.tar.gz")
 
     def build(self):
-        cmake = CMake(self, generator="Ninja")
+        cmake = CMake(self)
         cmake.definitions["RAPIDJSON_HAS_STDSTRING"] = True
         cmake.definitions["RAPIDJSON_BUILD_CXX11"] = True
         cmake.definitions["RAPIDJSON_ENABLE_INSTRUMENTATION_OPT"] = True
-        cmake.configure(source_folder=f"{self.name}-{self.version}")
+        cmake.configure(source_folder=f"rapidjson-{self.version}")
         cmake.build()
         cmake.install()
