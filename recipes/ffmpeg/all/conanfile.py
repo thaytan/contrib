5bee9cb22e92c43441aef64ebc7c417f45ff50d7
Niclas Overby <noverby@prozum.dk>
2020-10-18 20:19:51 +0200

    ffmpeg: port to github

--- recipes/ffmpeg/all/conanfile.py
+++ recipes/ffmpeg/all/conanfile.py
@@ -5,24 +5,23 @@ class FFMpegConan(ConanFile):
     description = "A complete, cross-platform solution to record, convert and stream audio and video"
     license = "GPL3"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("yasm/[^1.3.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+        "yasm/[^1.3.0]",
+    )
 
     def source(self):
         tools.get(f"http://ffmpeg.org/releases/ffmpeg-{self.version}.tar.bz2")
 
     def build(self):
         args = [
-            "--disable-static",
-            "--enable-shared",
+            "--disable-stared",
+            "--enable-static",
             "--disable-doc",
             "--disable-programs",
         ]
-        if self.settings.build_type == "Debug":
-            args.extend(
-                ["--disable-optimizations", "--disable-mmx", "--disable-stripping", "--enable-debug",]
-            )
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.make()
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"ffmpeg-{self.version}", args)
+        autotools.make()
+        autotools.install()
