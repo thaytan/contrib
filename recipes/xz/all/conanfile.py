e3fda1341f83b81e31b934253649dd517f5cfe9f
Niclas Overby <noverby@prozum.dk>
2020-10-18 19:04:47 +0200

    xz: port to github

--- recipes/xz/all/conanfile.py
+++ recipes/xz/all/conanfile.py
@@ -5,14 +5,17 @@ from conans import *
 
 class XzConan(ConanFile):
     description = "Library and command line tools for XZ and LZMA compressed files"
-    license = "custom", "GPL", "LGPL"
+    license = "GPL"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "cc/1.0.0",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://tukaani.org/xz/xz-{self.version}.tar.gz")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools.configure()
-            autotools.install()
+        autotools.configure(f"xz-{self.version}")
+        autotools.install()
