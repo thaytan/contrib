62406f6c1d7c63a444d6db67ac260b25422ce6cd
Niclas Overby <noverby@prozum.dk>
2020-10-18 13:32:58 +0200

    gstreamer: port to github

--- recipes/gstreamer/all/conanfile.py
+++ recipes/gstreamer/all/conanfile.py
@@ -1,45 +1,32 @@
 import os
-
 from conans import *
 
 
 class GStreamerConan(ConanFile):
-    name = "gstreamer"
     description = "A framework for streaming media"
     license = "LGPL"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    options = {
-        "check": [True, False],
-        "tools": [True, False],
-    }
-    default_options = (
-        "check=True",
-        "tools=True",
-    )
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
     build_requires = (
-        "meson/[^0.51.2]",
-        "bison/[^3.3]",
+        "clang/[^10.0.1]",
+        "meson/[^0.55.3]",
+        "bison/[^3.7.2]",
         "flex/[^2.6.4]",
         "gobject-introspection/[^1.59.3]",
     )
-    requires = (
-        "glib/[^2.62.0]",
-    )
+    requires = ("glib/[^2.62.0]",)
 
     def source(self):
-        git = tools.Git(folder=f"{self.name}-{self.version}")
-        git.clone(
-            url="https://gitlab.freedesktop.org/gstreamer/gstreamer.git", branch=self.version, shallow=True,
-        )
+        tools.get(f"https://github.com/GStreamer/gstreamer/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = ["--auto-features=disabled"]
-        args.append("-Dcheck=" + ("enabled" if self.options.check else "disabled"))
-        args.append("-Dtools=" + ("enabled" if self.options.tools else "disabled"))
+        args = [
+            "--auto-features=disabled",
+            "-Dcheck=enabled",
+            "-Dtools=enabled",
+        ]
         meson = Meson(self)
-        meson.configure(source_folder=f"{self.name, self.version), args=args}-{pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"}")
+        meson.configure(args, source_folder=f"gstreamer-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
 
     def package_info(self):
         self.env_info.GST_PLUGIN_SCANNER = os.path.join(self.package_folder, "bin", "gstreamer-1.0", "gst-plugin-scanner")
-        self.env_info.GI_TYPELIB_PATH.append(os.path.join(self.package_folder, "lib", "girepository-1.0"))
