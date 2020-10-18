ba7f15d76dc9cac6afc2351738abdb65f46f96e3
Niclas Overby <noverby@prozum.dk>
2020-10-18 19:32:35 +0200

    sccache: port to github

--- recipes/sccache/all/conanfile.py
+++ recipes/sccache/all/conanfile.py
@@ -1,5 +1,3 @@
-import os
-
 from conans import *
 
 
@@ -7,19 +5,16 @@ class SccacheConan(ConanFile):
     description = "Development and debugging tools for GStreamer"
     license = "Apache2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "rust/[^1.47.0]",
+        "openssl1/[^1.1.1h]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/mozilla/sccache/archive/{self.version}.tar.gz")
 
-    build_requires = (
-        "base/[^1.0.0]",
-        "rust/[^1.3.8]",
-        "openssl/[^1.1.1b]",
-    )
-
     def build(self):
-        with tools.chdir(f"{self.name}-{self.version}"):
-            self.run("cargo build --release")
+        self.run("cargo build --release", cwd=f"sccache-{self.version}")
 
     def package(self):
         self.copy(pattern="*/sccache", dst="bin", keep_path=False)
