52a0f1b2f4dd6851728f5b12f1c8dd97bf4a3071
Niclas Overby <noverby@prozum.dk>
2020-10-18 20:52:16 +0200

    libsrtp: port to github

--- recipes/libsrtp/all/conanfile.py
+++ recipes/libsrtp/all/conanfile.py
@@ -1,17 +1,20 @@
 from conans import *
 
 
-class LibSrtpConan(ConanFile):
+class LibsrtpConan(ConanFile):
     description = "Library for SRTP (Secure Realtime Transport Protocol)"
     license = "BSD"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/cisco/libsrtp/archive/v{self.version}.tar.gz")
 
     def build(self):
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure()
-            autotools.make(args=["shared_library"])
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"libsrtp-{self.version}")
+        autotools.make()
+        autotools.install()
