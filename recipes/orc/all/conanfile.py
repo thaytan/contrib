5073deac5920fde9c6d1b111dbdfc04903ef6d07
Niclas Overby <noverby@prozum.dk>
2020-10-18 13:49:24 +0200

    orc: port to github

--- recipes/orc/all/conanfile.py
+++ recipes/orc/all/conanfile.py
@@ -1,20 +1,26 @@
+import os
 from conans import *
 
 
 class OrcConan(ConanFile):
-    name = "orc"
     description = "Optimized Inner Loop Runtime Compiler"
     license = "LGPL-2.1"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
-    build_requires = ("meson/[^0.51.2]",)
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/GStreamer/orc/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = ["-Dgtk_doc=disabled"]
-        args.append("-Dbenchmarks=disabled")
-        args.append("-Dexamples=disabled")
+        args = [
+            "--auto-features=disabled",
+            "-Dgtk_doc=disabled",
+            "-Dbenchmarks=disabled",
+            "-Dexamples=disabled",
+        ]
         meson = Meson(self)
-        meson.configure(source_folder="orc-" + self.version, args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.configure(args, source_folder=f"orc-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
