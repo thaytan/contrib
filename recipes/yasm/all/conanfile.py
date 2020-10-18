d32739d9f2b3fad7d944231eab4a768ea3e981bd
Niclas Overby <noverby@prozum.dk>
2020-10-18 19:02:10 +0200

    yasm: port to github

--- recipes/yasm/all/conanfile.py
+++ recipes/yasm/all/conanfile.py
@@ -5,13 +5,15 @@ class YasmConan(ConanFile):
     description = "Yasm is a complete rewrite of the NASM assembler under the “new” BSD License"
     license = "BSD"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("gcc/7.4.0",)
+    build_requires = (
+        "cc/1.0.0",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         tools.get(f"http://www.tortall.net/projects/yasm/releases/yasm-{self.version}.tar.gz")
 
     def build(self):
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure()
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"yasm-{self.version}")
+        autotools.install()
