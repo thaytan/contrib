593d669d5d1fb7df7e5cec2a201587062fd067d0
Niclas Overby <noverby@prozum.dk>
2020-10-18 19:39:10 +0200

    renderproto: port to github

--- recipes/renderproto/all/conanfile.py
+++ recipes/renderproto/all/conanfile.py
@@ -7,10 +7,14 @@ class RenderprotoConan(ConanFile):
     description = "X11 Render extension wire protocol"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://xorg.freedesktop.org/releases/individual/proto/renderproto-{self.version}.tar.gz")
-        with tools.chdir(f"{self.name}-{self.version}"):
+        with tools.chdir(f"renderproto-{self.version}"):
             os.remove("config.guess")
             os.remove("config.sub")
             tools.download("http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD", "config.guess")
@@ -18,6 +22,5 @@ class RenderprotoConan(ConanFile):
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools.configure()
-            autotools.install()
+        autotools.configure(f"renderproto-{self.version}")
+        autotools.install()
