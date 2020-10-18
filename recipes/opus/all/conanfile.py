3239c1f265678da603a8c41699d72d30b4106fe6
Niclas Overby <noverby@prozum.dk>
2020-10-18 17:00:17 +0200

    opus: port to github

--- recipes/opus/all/conanfile.py
+++ recipes/opus/all/conanfile.py
@@ -2,17 +2,21 @@ from conans import *
 
 
 class OpusConan(ConanFile):
-    name = "opus"
     description = "Modern audio compression for the internet"
     license = "BSD"
-    settings = {"os_build": ["Linux"], "arch_build": ["x86_64", "armv8"]}
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "make/[^4.3]",
+    )
 
     def source(self):
         tools.get(f"https://archive.mozilla.org/pub/opus/opus-{self.version}.tar.gz")
 
     def build(self):
-        args = ["--disable-static"]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        args = [
+            "--disable-shared",
+        ]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"opus-{self.version}", args)
+        autotools.install()
