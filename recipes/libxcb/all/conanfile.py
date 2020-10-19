f89a4068e1479720438f54b4575163e36c1257c5
Niclas Overby <noverby@prozum.dk>
2020-10-19 18:46:43 +0200

    libxcb: port to github

--- recipes/libxcb/all/conanfile.py
+++ recipes/libxcb/all/conanfile.py
@@ -6,19 +6,19 @@ class LibxcbConan(ConanFile):
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "pkgconf/[^1.6.3]",
+        "cc/[^1.0.0]",
+        "make/[^4.3]",
+        "pkgconf/[^1.7.3]",
         "xcb-proto/[^1.13]",
-    )
-    requires = (
-        "libxau/[^1.0.9]",
         "libpthread-stubs/[^0.4]",
     )
+    requires = ("libxau/[^1.0.9]",)
 
     def source(self):
-        tools.get(f"https://xcb.freedesktop.org/dist/libxcb-{self.version}.tar.bz2")
+        tools.get(f"https://xcb.freedesktop.org/dist/libxcb-{self.version}.tar.xz")
 
     def build(self):
-        args = ["--disable-static"]
+        args = ["--disable-shared"]
         autotools = AutoToolsBuildEnvironment(self)
         autotools.configure(f"libxcb-{self.version}", args)
         autotools.install()
