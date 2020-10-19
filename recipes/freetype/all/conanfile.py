17d9eef9e63c8f144a0c34916b054b5c33c0be79
Niclas Overby <noverby@prozum.dk>
2020-10-19 20:21:19 +0200

    freetype: port to github

--- recipes/freetype/all/conanfile.py
+++ recipes/freetype/all/conanfile.py
@@ -1,6 +1,3 @@
-from glob import glob
-from os import path, remove
-
 from conans import *
 
 
@@ -8,19 +5,17 @@ class FreetypeConan(ConanFile):
     description = "FreeType is a software library to render fonts"
     license = "GPL2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("autotools/[^1.0.0]",)
-    requires = (
-        "base/[^1.0.0]",
-        "harfbuzz/2.6.1",
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+        "harfbuzz/[^2.7.2]",
     )
 
     def source(self):
-        tools.get(f"https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-{self.version}.tar.gz".replace(".", "-"))
+        tools.get(f"https://download-mirror.savannah.gnu.org/releases/freetype/freetype-{self.version}.tar.xz")
 
     def build(self):
-        args = ["--disable-static"]
-        with tools.chdir("freetype2-VER-" + self.version.replace(".", "-")):
-            self.run("sh autogen.sh")
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        args = ["--disable-shared"]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"freetype-{self.version}", args)
+        autotools.install()
