d1d5d221e2376afb06ef2820f504f4e64af66a39
Niclas Overby <noverby@prozum.dk>
2020-10-19 18:40:22 +0200

    libpthread-stubs: port to github

--- recipes/libpthread-stubs/all/conanfile.py
+++ recipes/libpthread-stubs/all/conanfile.py
@@ -5,12 +5,15 @@ class LibpthreadStubsConan(ConanFile):
     description = "X11 client-side library"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://xcb.freedesktop.org/dist/libpthread-stubs-{self.version}.tar.bz2")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir(f"{self.name}-{self.version}"):
-            autotools.configure()
-            autotools.install()
+        autotools.configure(f"libpthread-stubs-{self.version}")
+        autotools.install()
