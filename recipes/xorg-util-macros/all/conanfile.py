33e82550ac92bc8a83ff4117228f8fc52b96964a
Niclas Overby <noverby@prozum.dk>
2020-10-19 16:05:35 +0200

    xorg-util-macros: port to github

--- recipes/xorg-util-macros/all/conanfile.py
+++ recipes/xorg-util-macros/all/conanfile.py
@@ -1,21 +1,16 @@
-import os
-
 from conans import *
 
 
-class LibXorgUtilMacrosConan(ConanFile):
+class XorgUtilMacrosConan(ConanFile):
     description = "X.Org Autotools macros"
     license = "custom"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = ("autotools/[^1.0.0]",)
 
     def source(self):
         tools.get(f"https://xorg.freedesktop.org/releases/individual/util/util-macros-{self.version}.tar.gz")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir(f"util-macros-{self.version}"):
-            autotools.configure()
-            autotools.install()
-
-    def package_info(self):
-        self.env_info.ACLOCAL_PATH.append(os.path.join(self.package_folder, "share", "aclocal"))
+        autotools.configure(f"util-macros-{self.version}")
+        autotools.install()
