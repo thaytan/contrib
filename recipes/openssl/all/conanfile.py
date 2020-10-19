2b713ff1bb662c9fa794a5ffe1f65485ecab76f1
Niclas Overby <noverby@prozum.dk>
2020-10-19 09:16:06 +0200

    openssl: bump bootstrap-llvm req version

--- recipes/openssl/all/conanfile.py
+++ recipes/openssl/all/conanfile.py
@@ -8,7 +8,7 @@ class OpensslConan(ConanFile):
     license = "BSD"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "bootstrap-llvm/[^10.0.1]",
+        "bootstrap-llvm/[^11.0.0]",
         "make/[^4.3]",
         "perl/[^5.30.0]",
     )
