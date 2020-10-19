447e82fa5c039ab07673121085cee6ac8aabdeac
Niclas Overby <noverby@prozum.dk>
2020-10-19 18:34:30 +0200

    libxau: port to github

--- recipes/libxau/all/conanfile.py
+++ recipes/libxau/all/conanfile.py
@@ -7,19 +7,16 @@ class LibxauConan(ConanFile):
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
         "cc/[^1.0.0]",
-        "pkgconf/[^1.6.3]",
+        "make/[^4.3]",
+        "pkgconf/[^1.7.3]",
     )
-    requires = (
-        "base/[^1.0.0]",
-        "xorgproto/[^2019.1]",
-    )
+    requires = ("xorgproto/[^2020.1]",)
 
     def source(self):
         tools.get(f"https://xorg.freedesktop.org/releases/individual/lib/libXau-{self.version}.tar.gz")
 
     def build(self):
-        args = ["--disable-static"]
+        args = ["--disable-shared"]
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir("libXau-" + self.version):
-            autotools.configure(args=args)
-            autotools.install()
+        autotools.configure(f"libXau-{self.version}", args)
+        autotools.install()
