1b82730d65eaa8c3dd3566c6d470c4e924996064
Niclas Overby <noverby@prozum.dk>
2020-10-19 20:13:19 +0200

    harfbuzz: port to github

--- recipes/harfbuzz/all/conanfile.py
+++ recipes/harfbuzz/all/conanfile.py
@@ -1,29 +1,27 @@
-from glob import glob
-from os import path, remove
-
+import os
 from conans import *
 
 
 class HarfbuzzConan(ConanFile):
     description = "HarfBuzz text shaping engine"
-    license = "Old MIT"
+    license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "autotools/[^1.0.0]",
-        "freetype-no-harfbuzz/[^2.10.1]",
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+        "freetype-no-harfbuzz/[^2.10.3]",
     )
-    requires = (
-        "base/[^1.0.0]",
-        "glib/[^2.62.0]",
-    )
+    requires = ("glib/[^2.66.1]",)
+    options = {"shared": [True, False]}
+    default_options = ("shared=False",)
 
     def source(self):
         tools.get(f"https://github.com/harfbuzz/harfbuzz/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = ["--disable-static"]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            self.run("sh autogen.sh")
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        args = [
+            "--auto-features=disabled",
+        ]
+        meson = Meson(self)
+        meson.configure(args, source_folder=f"harfbuzz-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.install()
