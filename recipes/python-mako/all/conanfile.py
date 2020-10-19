c975cb4dcbcda2b23fa3228fa8834d7ad7962b43
Niclas Overby <noverby@prozum.dk>
2020-10-19 15:38:39 +0200

    python-mako: port to github

--- recipes/python-mako/all/conanfile.py
+++ recipes/python-mako/all/conanfile.py
@@ -1,5 +1,3 @@
-import os
-
 from conans import *
 
 
@@ -7,16 +5,11 @@ class PythonMakoConan(ConanFile):
     description = "A super-fast templating language that borrows the best ideas from the existing templating languages"
     license = "Apache"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = ("python-setuptools/[^50.3.0]",)
+    requires = ("python/[^3.8.5]",)
 
     def source(self):
-        tools.get(f"https://github.com/sqlalchemy/mako/archive/rel_{self.version}.tar.gz".replace(".", "_"))
-
-    build_requires = ("python-setuptools/[^41.2.0]",)
-    requires = (
-        "base/[^1.0.0]",
-        "python/[^3.7.4]",
-    )
+        tools.get(f"https://github.com/sqlalchemy/mako/archive/rel_{self.version.replace('.', '_')}.tar.gz")
 
     def build(self):
-        with tools.chdir("mako-rel_" + self.version.replace(".", "_")):
-            self.run('python setup.py install --optimize=1 --prefix= --root="%s"' % self.package_folder)
+        self.run(f'python setup.py install --optimize=1 --prefix= --root="{self.package_folder}"', cwd=f"mako-rel_{self.version.replace('.', '_')}")
