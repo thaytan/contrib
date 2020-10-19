473f9f4dcea3f00605e175f8ac3083165df91083
Niclas Overby <noverby@prozum.dk>
2020-10-19 20:11:08 +0200

    freetype-no-harfbuzz: port to github

--- recipes/freetype-no-harfbuzz/all/conanfile.py
+++ recipes/freetype-no-harfbuzz/all/conanfile.py
@@ -1,6 +1,3 @@
-from glob import glob
-from os import path, remove
-
 from conans import *
 
 
@@ -8,15 +5,16 @@ class FreetypeNoHarfbuzzConan(ConanFile):
     description = "FreeType is a software library to render fonts"
     license = "GPL2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("autotools/[^1.0.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
-        tools.get(f"https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-{self.version}.tar.gz".replace(".", "-"))
+        tools.get(f"https://download-mirror.savannah.gnu.org/releases/freetype/freetype-{self.version}.tar.xz")
 
     def build(self):
-        args = ["--disable-static"]
-        with tools.chdir("freetype2-VER-" + self.version.replace(".", "-")):
-            self.run("sh autogen.sh")
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        args = ["--disable-shared"]
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"freetype-{self.version}", args)
+        autotools.install()
