e702de36197299a4c5d77080a0bea53dc4328377
Niclas Overby <noverby@prozum.dk>
2020-10-20 21:33:22 +0200

    libglvnd: port to github

--- recipes/libglvnd/all/conanfile.py
+++ recipes/libglvnd/all/conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conans import *
 
 
@@ -5,34 +6,30 @@ class LibglvndConan(ConanFile):
     description = "The GL Vendor-Neutral Dispatch library"
     license = "custom"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
     options = {
         "x11": [True, False],
     }
     default_options = ("x11=True",)
-    exports = "ignore-warnings.patch"
 
     def build_requirements(self):
-        self.build_requirements("base/[^1.0.0]")
-        self.build_requirements("autotools/[^1.0.0]")
         if self.options.x11:
-            self.build_requires("xorgproto/[^2019.1]")
-
-    def requirements(self):
-        if self.options.x11:
-            self.requires("libx11/[^1.6.8]")
-            self.requires("libxext/[^1.3.4]")
+            self.build_requires("xorgproto/[^2020.1]")
 
     def source(self):
-        tools.get(f"https://github.com/NVIDIA/libglvnd/archive/v{self.version}.tar.gz")
-        tools.patch(patch_file="ignore-warnings.patch", base_path=f"{self.name}-{self.version}")
+        tools.get(f"https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v{self.version}/libglvnd-v{self.version}.tar.gz")
 
     def build(self):
-        args = ["--disable-static"]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            self.run("sh autogen.sh")
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        args = [
+            "--auto-features=disabled",
+        ]
+
+        meson = Meson(self)
+        meson.configure(args, source_folder=f"libglvnd-v{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.install()
 
     def package_info(self):
         self.env_info.__EGL_VENDOR_LIBRARY_DIRS.append("/usr/share/glvnd/egl_vendor.d")
