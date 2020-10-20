658bbbf9d1bbb5b4d173c52e32b02e697cd1068a
Niclas Overby <noverby@prozum.dk>
2020-10-20 20:25:45 +0200

    libxfixes: port to github

--- recipes/libxfixes/all/conanfile.py
+++ recipes/libxfixes/all/conanfile.py
@@ -6,13 +6,11 @@ class LibxfixesConan(ConanFile):
     license = "custom"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "pkgconf/[^1.6.3]",
-        "xorg-util-macros/[^1.19.1]",
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+        "xorg-util-macros/[^1.19.2]",
     )
-    requires = (
-        "base/[^1.0.0]",
-        "libx11/[^1.6.8]",
-    )
+    requires = ("libx11/[^1.6.12]",)
 
     def source(self):
         tools.get(f"https://xorg.freedesktop.org/releases/individual/lib/libXfixes-{self.version}.tar.gz")
@@ -20,6 +18,5 @@ class LibxfixesConan(ConanFile):
     def build(self):
         args = ["--disable-static"]
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir("libXfixes-" + self.version):
-            autotools.configure(args=args)
-            autotools.install()
+        autotools.configure(f"libXfixes-{self.version}", args)
+        autotools.install()
