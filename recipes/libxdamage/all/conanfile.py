8b2ab68d8ff13a597f191faf98ec7159df530926
Niclas Overby <noverby@prozum.dk>
2020-10-20 20:33:39 +0200

    libxdamage: port to github

--- recipes/libxdamage/all/conanfile.py
+++ recipes/libxdamage/all/conanfile.py
@@ -5,17 +5,16 @@ class LibxdamageConan(ConanFile):
     description = "X11 damaged region extension library"
     license = "custom"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("pkgconf/[^1.6.3]",)
-    requires = (
-        "base/[^1.0.0]",
-        "libxfixes/[^5.0.3]",
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
     )
+    requires = ("libxfixes/[^5.0.3]",)
 
     def source(self):
         tools.get(f"https://xorg.freedesktop.org/releases/individual/lib/libXdamage-{self.version}.tar.gz")
 
     def build(self):
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir("libXdamage-" + self.version):
-            autotools.configure()
-            autotools.install()
+        autotools.configure(f"libXdamage-{self.version}")
+        autotools.install()
