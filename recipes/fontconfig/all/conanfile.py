c2060ac35568e904867d77767035c8a68542ef92
Niclas Overby <noverby@prozum.dk>
2020-10-20 15:34:10 +0200

    fontconfig: port to github

--- recipes/fontconfig/all/conanfile.py
+++ recipes/fontconfig/all/conanfile.py
@@ -1,45 +1,39 @@
 import os
-from glob import glob
-
 from conans import *
 
 
 class FontconfigConan(ConanFile):
     description = "A library for configuring and customizing font access"
-    license = "Old MIT"
+    license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
+        "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
         "gperf/[^3.1]",
-    )
-    requires = (
-        "base/[^1.0.0]",
-        "freetype/[^2.10.1]",
         "libuuid/[^1.0.3]",
         "expat/[^2.2.7]",
     )
+    requires = ("freetype/[^2.10.3]",)
 
     def source(self):
-        tools.get(f"https://gitlab.freedesktop.org/fontconfig/fontconfig/-/archive/{self.version}/fontconfig-{self.version}.tar.gz")
+        tools.get(f"https://www.freedesktop.org/software/fontconfig/release/fontconfig-{self.version}.tar.xz")
 
     def build(self):
         args = ["--disable-static"]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            self.run("sh autogen.sh")
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"fontconfig-{self.version}", args)
+        autotools.install()
 
     def package(self):
-        with tools.chdir(self.package_folder + "/etc/fonts/conf.d"):
-            for root, dirs, files in os.walk("."):
+        with tools.chdir(f"{self.package_folder}/etc/fonts/conf.d"):
+            for _, _, files in os.walk("."):
                 for filename in files:
                     os.remove(filename)
 
-        with tools.chdir(self.package_folder + "/etc/fonts/conf.d"):
-            for root, dirs, files in os.walk(self.package_folder + "/share/fontconfig/conf.avail"):
+        with tools.chdir(f"{self.package_folder}/etc/fonts/conf.d"):
+            for _, _, files in os.walk(f"{self.package_folder}/share/fontconfig/conf.avail"):
                 for filename in files:
-                    os.symlink("../../../share/fontconfig/conf.avail/" + filename, filename)
+                    os.symlink(f"../../../share/fontconfig/conf.avail/{filename}", filename)
 
     def package_info(self):
         self.env_info.FONTCONFIG_PATH.append(os.path.join(self.package_folder, "etc", "fonts"))
