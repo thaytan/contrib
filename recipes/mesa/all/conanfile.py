dd6ee0cfd3c59a315000a6c115c5d52b5feec2d5
Niclas Overby <noverby@prozum.dk>
2020-10-20 22:04:26 +0200

    mesa: port to github

--- recipes/mesa/all/conanfile.py
+++ recipes/mesa/all/conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import *
 
 
@@ -10,25 +9,25 @@ class MesaConan(ConanFile):
     options = {"x11": [True, False]}
     default_options = ("x11=True",)
     build_requires = (
-        "meson/[^0.51.2]",
-        "gettext/[^0.20.1]",
+        "bootstrap-llvm/[^11.0.0]",
+        "meson/[^0.55.3]",
+        "gettext/[^0.21]",
         "bison/[^3.3]",
         "flex/[^2.6.4]",
         "python-mako/[^1.1.0]",
         "zlib/[^1.2.11]",
         "expat/[^2.2.7]",
         "libdrm/[^2.4.99]",
+    )
+    requires = ("libglvnd/[^1.3.2]",)
+
+    def build_requirements(self):
         if self.options.x11:
             self.build_requires("libx11/[^1.6.8]")
-            self.build_requires("libxext/[^1.3.4]")
+            self.build_requires("libxrandr/[^1.5.2]")
             self.build_requires("libxdamage/[^1.1.5]")
             self.build_requires("libxshmfence/[^1.3]")
             self.build_requires("libxxf86vm/[^1.1.4]")
-    )
-    requires = (
-        "base/[^1.0.0]",
-        "libglvnd/[^1.2.0]",
-    )
 
     def source(self):
         tools.get(f"https://mesa.freedesktop.org/archive/mesa-{self.version}.tar.xz")
@@ -40,19 +39,19 @@ class MesaConan(ConanFile):
             "-Dglvnd=true",
             "-Dglx=dri",
             "-Degl=true",
-            "-Dgles1=false",
-            "-Dgles2=true",
-            "-Dgles3=true",
-            "-Dplatforms=x11",
-            "-Dvulkan-drivers=",
-            "-Dgallium-drivers=",
+            "-Dgles2=enabled",
+            "-Dgles3=enabled",
         ]
+        if self.options.x11:
+            args.append("-Dplatforms=x11")
         if self.settings.arch_build == "x86_64":
             args.append("-Ddri-drivers=i915,i965")
+            args.append("-Dgallium-drivers=iris")
+            args.append("-Dvulkan-drivers=intel")
         if self.settings.arch_build == "armv8":
             args.append("-Dgallium-drivers=nouveau,tegra")
         meson = Meson(self)
-        meson.configure(source_folder=f"{self.name, self.version), args=args}-{pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"}")
+        meson.configure(args, source_folder=f"mesa-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
 
     def package_info(self):
