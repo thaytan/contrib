7c7ce846d06eb1f92698b12050d8072646f76c5e
Niclas Overby <noverby@prozum.dk>
2020-10-21 22:42:39 +0200

    libwebp: port to github

--- recipes/libwebp/all/conanfile.py
+++ recipes/libwebp/all/conanfile.py
@@ -5,14 +5,19 @@ class ConanLibwebp(ConanFile):
     description = "library to encode and decode images in WebP format"
     license = "BSD"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("cmake/[^3.15.3]",)
+    options = {"shared": [True, False]}
+    default_options = {"shared": True}
+    build_requires = (
+        "cc/[^1.0.0]",
+        "cmake/[^3.18.4]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/webmproject/libwebp/archive/v{self.version}.tar.gz")
 
     def build(self):
-        cmake = CMake(self, generator="Ninja")
-        cmake.definitions["BUILD_SHARED_LIBS"] = True
-        cmake.configure(source_folder=f"{self.name}-{self.version}")
+        cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIB"] = True
+        cmake.configure(source_folder=f"libwebp-{self.version}")
         cmake.build()
         cmake.install()
