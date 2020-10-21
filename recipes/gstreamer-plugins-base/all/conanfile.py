8fdc104a35f12ed8623bec0aaeca5979e04d4d83
Niclas Overby <noverby@prozum.dk>
2020-10-21 19:58:33 +0200

    gstreamer-plugins-base: port to github

--- recipes/gstreamer-plugins-base/all/conanfile.py
+++ recipes/gstreamer-plugins-base/all/conanfile.py
@@ -6,21 +6,23 @@ class GStreamerPluginsBaseConan(ConanFile):
     description = "A well-groomed and well-maintained collection of GStreamer plugins and elements"
     license = "LGPL"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
-
     options = {
         "audioresample": [True, False],
     }
     default_options = ("audioresample=False",)
     build_requires = (
-        "meson/[^0.51.2]",
-        "mesa/[^19.2.0]",
+        "cc/[^1.0.0]",
+        "meson/[^0.55.2]",
+        "mesa/[^20.2.1]",
         "gobject-introspection/[^1.59.3]",
         "orc/[^0.4.29]",
         "opus/[^1.3.1]",
-        "pango/[^1.43.0, include_prerelease=True]",
+        "pango/[^1.43.0]",
         "libx11/[^1.6.8]",
     )
-    requires = ("gstreamer/[~1.16]",)
+
+    def requirements(self):
+        self.requires(f"gstreamer/[~{self.settings.gstreamer}]")
 
     def source(self):
         tools.get(f"https://github.com/GStreamer/gst-plugins-base/archive/{self.version}.tar.gz")
@@ -44,7 +46,7 @@ class GStreamerPluginsBaseConan(ConanFile):
             "-Dvideoscale=enabled",
             "-Daudioconvert=enabled",
         ]
-        args.append("-Daudioresample=" + ("enabled" if self.options.audiotestsrc else "disabled"))
+        args.append("-Daudioresample=" + ("enabled" if self.options.audioresample else "disabled"))
         meson = Meson(self)
         meson.configure(args, source_folder=f"gst-plugins-base-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
