273ac858fe6e8fc62167680859ec7592125522f3
Niclas Overby <noverby@prozum.dk>
2020-10-21 19:27:46 +0200

    pango: port to github

--- recipes/pango/all/conanfile.py
+++ recipes/pango/all/conanfile.py
@@ -9,12 +9,12 @@ class PangoConan(ConanFile):
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
         "cc/[^1.0.0]",
-        "meson/[^0.51.2]",
+        "meson/[^0.55.3]",
         "gobject-introspection/[^1.59.3]",
     )
     requires = (
         "fribidi/[^1.0.5]",
-        "cairo/[^1.16.0]",
+        "cairo/[^1.17.2]",
         "harfbuzz/[^2.6.1]",
     )
 
@@ -22,9 +22,12 @@ class PangoConan(ConanFile):
         tools.get(f"https://github.com/GNOME/pango/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = ["--auto-features=disabled"]
-        args.append("-Dgtk_doc=false")
-        args.append("-Dinstall-tests=false")
+        args = [
+            "--auto-features=disabled",
+            "-Dfontconfig=enabled",
+            "-Dgtk_doc=false",
+            "-Dinstall-tests=false",
+        ]
         meson = Meson(self)
         meson.configure(args, source_folder=f"pango-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
