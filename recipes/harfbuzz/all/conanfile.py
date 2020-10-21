68266b87c92a9060b3ff536defaa8612201e90eb
Niclas Overby <noverby@prozum.dk>
2020-10-21 19:25:38 +0200

    harfbuzz: enable introspection

--- recipes/harfbuzz/all/conanfile.py
+++ recipes/harfbuzz/all/conanfile.py
@@ -10,10 +10,9 @@ class HarfbuzzConan(ConanFile):
         "cc/[^1.0.0]",
         "meson/[^0.55.3]",
         "freetype-no-harfbuzz/[^2.10.3]",
+        "gobject-introspection/[^1.66.1]",
     )
     requires = ("glib/[^2.66.1]",)
-    options = {"shared": [True, False]}
-    default_options = ("shared=False",)
 
     def source(self):
         tools.get(f"https://github.com/harfbuzz/harfbuzz/archive/{self.version}.tar.gz")
@@ -22,6 +21,8 @@ class HarfbuzzConan(ConanFile):
         args = [
             "--auto-features=disabled",
             "-Dfreetype=enabled",
+            "-Dgobject=enabled",
+            "-Dintrospection=enabled",
         ]
         meson = Meson(self)
         meson.configure(args, source_folder=f"harfbuzz-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
