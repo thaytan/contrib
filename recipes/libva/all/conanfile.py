baa0b05adf0720a8cc0529f60d2d48ba5a04b407
Niclas Overby <noverby@prozum.dk>
2020-10-22 14:15:18 +0200

    libva: port to github

--- recipes/libva/all/conanfile.py
+++ recipes/libva/all/conanfile.py
@@ -1,19 +1,19 @@
 import os
-
 from conans import *
 
 
-class LibVaConan(ConanFile):
-    description = "Libva is an implementation for VA-API (VIdeo Acceleration API)"
+class LibvaConan(ConanFile):
+    description = "Libva is an implementation for VA-API (Video Acceleration API)"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     options = {"x11": [True, False], "wayland": [True, False]}
     default_options = ("x11=True", "wayland=False")
-    build_requires = ("meson/[^0.51.2]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
     requires = (
-        "base/[^1.0.0]",
-        "libdrm/[^2.4.96]",
-        "libx11/[^1.6.8]",
+        "libdrm/[^2.4.102]",
         "libxext/[^1.3.4]",
         "libxfixes/[^5.0.3]",
     )
@@ -26,7 +26,5 @@ class LibVaConan(ConanFile):
         args = ["--auto-features=disabled"]
         args.append("-Dwith_x11=" + ("yes" if self.options.x11 else "no"))
         args.append("-Dwith_wayland=" + ("yes" if self.options.wayland else "no"))
-        meson.configure(
-            source_folder=f"{self.name}-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"),
-        )
+        meson.configure(args, source_folder=f"libva-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
