736719581e7294e6ec9a1217ff32d25a7bdd3a7a
Niclas Overby <noverby@prozum.dk>
2020-10-22 19:42:41 +0200

    libxml2: fix python support

--- recipes/libxml2/all/conanfile.py
+++ recipes/libxml2/all/conanfile.py
@@ -6,25 +6,30 @@ from conans import *
 class Libxml2Conan(ConanFile):
     description = "XML parsing library, version 2"
     license = "MIT"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
     build_requires = (
         "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
         "zlib/[^1.2.11]",
-        "python/[^3.8.5]",
     )
+
+    def build_requirements(self):
+        self.build_requires(f"python/[~{self.settings.python}]")
 
     def source(self):
         tools.get(f"https://gitlab.gnome.org/GNOME/libxml2/-/archive/v{self.version}/libxml2-v{self.version}.tar.bz2")
 
     def build(self):
         args = [
-            "--disable-shared",
+            "--disable-static",
         ]
-        env = {"with_python_install_dir": os.path.join(self.package_folder, "lib", "python3.8", "site-packages")}
-        with tools.chdir(f"{self.name}-v{self.version}"), tools.environment_append(env):
-            self.run("sh autogen.sh")
+        env = {
+            "with_python_install_dir": os.path.join(self.package_folder, "lib", f"python{self.settings.python}", "site-packages"),
+            "NOCONFIGURE": "1",
+        }
+        with tools.environment_append(env):
+            self.run("sh autogen.sh", cwd=f"libxml2-v{self.version}")
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
+            autotools.configure(f"libxml2-v{self.version}", args=args)
             autotools.make()
             autotools.install()
