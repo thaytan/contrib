202786d5e433970c7a36b772697c77a707b4211c
Niclas Overby <noverby@prozum.dk>
2020-10-22 16:08:25 +0200

    python: move openssl to reqs

--- recipes/python/all/conanfile.py
+++ recipes/python/all/conanfile.py
@@ -6,18 +6,18 @@ from conans import *
 class PythonConan(ConanFile):
     description = "Next generation of the python high-level scripting language"
     license = "MIT"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
     build_requires = (
         "bootstrap-llvm/[^10.0.1]",
         "make/[^4.3]",
         "expat/[^2.2.7]",
-        "openssl/[^3.0.0-alpha6]",
         "libffi/[^3.3]",
         "zlib/[^1.2.11]",
         "bzip2/[^1.0.8]",
         "sqlite/[^3.30.1]",
         "readline/[^8.0]",
     )
+    requires = ("openssl/[^3.0.0-alpha6]",)
 
     def source(self):
         tools.get(f"https://www.python.org/ftp/python/{self.version}/Python-{self.version}.tar.xz")
@@ -43,12 +43,11 @@ class PythonConan(ConanFile):
         autotools.configure(f"Python-{self.version}", args, vars=env)
         autotools.make()
         autotools.install()
-        os.symlink("python3.8", os.path.join(self.package_folder, "bin", "python"))
+        os.symlink(f"python{self.settings.python}", os.path.join(self.package_folder, "bin", "python"))
 
     def package_info(self):
         self.env_info.PYTHON = os.path.join(self.package_folder, "bin", "python")
         self.env_info.PYTHONHOME = self.package_folder
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "python3.8"))
         if "CC" in os.environ:
             ldshared = os.environ["CC"] + " -pthread -shared "
             if self.settings.arch_build == "x86_64":
