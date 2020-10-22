f879131a594cb10489ed4a09695df9cd2b253f0c
Niclas Overby <noverby@prozum.dk>
2020-10-22 17:40:16 +0200

    linux-headers: use only bootstrap package

--- recipes/linux-headers/all/conanfile.py
+++ recipes/linux-headers/all/conanfile.py
@@ -6,10 +6,6 @@ class LinuxHeadersConan(ConanFile):
     description = "Linux headers"
     license = "GPL2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = (
-        "bootstrap-llvm/[^10.0.1]",
-        "make/[^4.3]",
-    )
 
     def source(self):
         tools.get(f"https://cdn.kernel.org/pub/linux/kernel/v{self.version.split('.')[0]}.x/linux-{self.version}.tar.xz")
@@ -18,7 +14,7 @@ class LinuxHeadersConan(ConanFile):
         arch = {"x86_64": "x86_64", "armv8": "arm64"}[str(self.settings.arch_build)]
         with tools.chdir(f"linux-{self.version}"):
             autotools = AutoToolsBuildEnvironment(self)
-            autotools.make(target="headers_install", args=[f"ARCH={arch}", "HOSTCC=cc", f'INSTALL_HDR_PATH="{self.package_folder}"'])
+            autotools.make(target="headers_install", args=[f"ARCH={arch}", f'INSTALL_HDR_PATH="{self.package_folder}"'])
 
     def package_info(self):
         self.env_info.CPATH.append(os.path.join(self.package_folder, "include"))
