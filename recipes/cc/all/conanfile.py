10e9749ac2ab35266a40552176332bdea5387c9f
Niclas Overby <noverby@prozum.dk>
2020-10-23 17:01:50 +0200

    cc: require llvm as compiler toolchain

--- recipes/cc/all/conanfile.py
+++ recipes/cc/all/conanfile.py
@@ -5,4 +5,4 @@ class CCConan(ConanFile):
     description = "Virtual c/c++ compiler package"
     license = "GPL"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    requires = ("clang/[^11.0.0]",)
+    requires = ("llvm/[^11.0.0]",)
