b3f13710d992150d66ca1d6e283d1d8c0f04bfe8
Niclas Overby <noverby@prozum.dk>
2020-10-23 11:46:48 +0200

    cmake: use only bootstrap package

--- recipes/cmake/all/conanfile.py
+++ recipes/cmake/all/conanfile.py
@@ -5,14 +5,17 @@ class CMakeConan(ConanFile):
     description = "A cross-platform open-source make system"
     license = "custom"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = (
-        "bootstrap-cmake/[^3.18.0]",
-        "cc/[^1.0.0]",
-    )
-    requires = (
-        "ninja/[^1.10.0]",
-        "pkgconf/[^1.7.3]",
-    )
+    options = {"bootstrap": [True, False]}
+    default_options = ("bootstrap=False",)
+
+    def build_requirements(self):
+        if not self.options.bootstrap:
+            self.build_requires("llvm/[^11.0.0]")
+
+    def requirements(self):
+        if not self.options.bootstrap:
+            self.requires("ninja/[^1.10.0]")
+            self.requires("pkgconf/[^1.7.3]")
 
     def source(self):
         tools.get(f"https://github.com/Kitware/CMake/releases/download/v{self.version}/cmake-{self.version}.tar.gz")
