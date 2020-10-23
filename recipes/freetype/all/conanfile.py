293e9b821bdccd6974b2dfe62aeb98cc067cb4d5
Niclas Overby <noverby@prozum.dk>
2020-10-23 09:20:50 +0200

    freetype: add harfbuzz option

--- recipes/freetype/all/conanfile.py
+++ recipes/freetype/all/conanfile.py
@@ -5,12 +5,17 @@ class FreetypeConan(ConanFile):
     description = "FreeType is a software library to render fonts"
     license = "GPL2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
+    options = {"harfbuzz": [True, False]}
+    default_options = ("harfbuzz=True",)
     build_requires = (
         "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
-        "harfbuzz/[^2.7.2]",
     )
     requires = ("zlib/[^1.2.11]",)
+
+    def requirements(self):
+        if self.options.harfbuzz:
+            self.requires("harfbuzz/[^2.7.2]")
 
     def source(self):
         tools.get(f"https://download-mirror.savannah.gnu.org/releases/freetype/freetype-{self.version}.tar.xz")
