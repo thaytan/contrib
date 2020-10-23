004c285b417a8e98babb88c38c581a1dbd9eae5a
Niclas Overby <noverby@prozum.dk>
2020-10-23 11:37:55 +0200

    ninja: use only bootstrap package

--- recipes/ninja/all/conanfile.py
+++ recipes/ninja/all/conanfile.py
@@ -5,11 +5,12 @@ class NinjaConan(ConanFile):
     description = "Small build system with a focus on speed"
     license = "Apache"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = (
-        "bootstrap-cmake/[^3.18.0]",
-        "bootstrap-ninja/[^1.10.0]",
-        "cc/[^1.0.0]",
-    )
+    options = {"bootstrap": [True, False]}
+    default_options = ("bootstrap=False",)
+
+    def build_requirements(self):
+        if not self.options.bootstrap:
+            self.build_requires("llvm/[^11.0.0]")
 
     def source(self):
         tools.get(f"https://github.com/ninja-build/ninja/archive/v{self.version}.tar.gz")
