b2d8759d8e66f94f8329db87b367de88e718b0a4
Niclas Overby <noverby@prozum.dk>
2020-10-23 17:03:41 +0200

    make: require cc

--- recipes/make/all/conanfile.py
+++ recipes/make/all/conanfile.py
@@ -6,7 +6,7 @@ class MakeConan(ConanFile):
     description = "GNU make utility to maintain groups of programs"
     license = "GPL3"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = "llvm/[^11.0.0]"
+    build_requires = "cc/[^1.0.0]"
 
     def source(self):
         tools.get(f"https://ftp.gnu.org/gnu/make/make-{self.version}.tar.gz")
