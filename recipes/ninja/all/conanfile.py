07bbcb9bb03191bf04809f045fc5cb14916ff8f5
Niclas Overby <noverby@prozum.dk>
2020-10-23 17:05:37 +0200

    ninja: require cc

--- recipes/ninja/all/conanfile.py
+++ recipes/ninja/all/conanfile.py
@@ -11,7 +11,7 @@ class NinjaConan(ConanFile):
 
     def build_requirements(self):
         if not self.options.bootstrap:
-            self.build_requires("llvm/[^11.0.0]")
+            self.build_requires("cc/[^1.0.0]")
             self.build_requires("make/[^4.3]")
 
     def configure(self):
