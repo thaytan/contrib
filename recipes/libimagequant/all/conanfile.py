3ed303af82742b8d0c5ad2165420c540bf8a77b5
Niclas Overby <noverby@prozum.dk>
2020-10-23 22:35:15 +0200

    libimagequant: port to github

--- recipes/libimagequant/all/conanfile.py
+++ recipes/libimagequant/all/conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conans import *
 
 
@@ -5,20 +6,24 @@ class LibimagequantConan(ConanFile):
     description = "Library for high-quality conversion of RGBA images to 8-bit indexed-color (palette) images"
     license = "BSD"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = ("autotools/1.0.0",)
-    requires = ("base/[^1.0.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/1.0.0",
+        "openmp/[^11.0.0]",
+    )
 
     def source(self):
         tools.get(f"https://github.com/ImageOptim/libimagequant/archive/{self.version}/libimagequant-{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "--with-openmp",
-        ]
         env = {
             "DESTDIR": self.package_folder,
         }
-        with tools.chdir(f"{self.name}-{self.version}"), tools.environment_append(env):
+        args = [
+            "--with-openmp",
+        ]
+        with tools.environment_append(env), tools.chdir(f"libimagequant-{self.version}"):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.configure(args=args)
             autotools.install()
+        os.remove(os.path.join(self.package_folder, "lib", "libimagequant.a"))
