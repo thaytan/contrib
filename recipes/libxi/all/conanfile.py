47fb7f4d77606d92a946c51f16349f7bb8bfcf72
Niclas Overby <noverby@prozum.dk>
2020-10-25 20:37:07 +0100

    libxi: port to github

--- recipes/libxi/all/conanfile.py
+++ recipes/libxi/all/conanfile.py
@@ -10,8 +10,6 @@ class LibxiConan(ConanFile):
         "xorg-util-macros/[^1.19.1]",
     )
     requires = (
-        "base/[^1.0.0]",
-        "libx11/[^1.6.8]",
         "libxext/[^1.3.4]",
         "libxfixes/[^5.0.3]",
     )
@@ -22,6 +20,5 @@ class LibxiConan(ConanFile):
     def build(self):
         args = ["--disable-static"]
         autotools = AutoToolsBuildEnvironment(self)
-        with tools.chdir("libXi-" + self.version):
-            autotools.configure(args=args)
-            autotools.install()
+        autotools.configure(f"libXi-{self.version}", args)
+        autotools.install()
