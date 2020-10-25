1daf6419be753e7e8aadecf6a47e2452ce5e8d67
Niclas Overby <noverby@prozum.dk>
2020-10-25 20:26:19 +0100

    at-spi2-atk: port to github

--- recipes/at-spi2-atk/all/conanfile.py
+++ recipes/at-spi2-atk/all/conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import *
 
 
@@ -7,24 +6,19 @@ class AtSpi2AtkConan(ConanFile):
     description = "A GTK+ module that bridges ATK to D-Bus at-spi"
     license = "LGPL"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = (
-        "meson/[^0.51.2]",
-    )
+    build_requires = ("meson/[^0.55.3]",)
     requires = (
-        "base/[^1.0.0]",
-        "atk/[^2.35.1]",
-        "at-spi2-core/[^2.34.0]",
+        "atk/[^2.36.0]",
+        "at-spi2-core/[^2.38.0]",
         "libxml2/[^2.9.9]",
     )
 
     def source(self):
-        tools.get(f"https://gitlab.gnome.org/GNOME/at-spi2-atk/-/archive/AT_SPI2_ATK_{self.version}/at-spi2-atk-AT_SPI2_ATK_{self.version.replace(".", "_")}.tar.bz2")
+        version = self.version.replace(".", "_")
+        tools.get(f"https://gitlab.gnome.org/GNOME/at-spi2-atk/-/archive/AT_SPI2_ATK_{version}/at-spi2-atk-AT_SPI2_ATK_{version}.tar.bz2")
 
     def build(self):
         args = ["--auto-features=disabled", "--wrap-mode=nofallback"]
         meson = Meson(self)
-        meson.configure(source_folder="at-spi2-atk-AT_SPI2_ATK_" + self.version.replace(".", "_"), args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.configure(args, source_folder=f"at-spi2-atk-AT_SPI2_ATK_{self.version.replace('.', '_')}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
-
-    def package_info(self):
-        self.env_info.GI_TYPELIB_PATH.append(os.path.join(self.package_folder, "lib", "girepository-1.0"))
