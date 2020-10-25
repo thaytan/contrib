31be19527f3604841f477127f9b9e081eceb5624
Niclas Overby <noverby@prozum.dk>
2020-10-25 17:26:18 +0100

    libepoxy: port to github

--- recipes/libepoxy/all/conanfile.py
+++ recipes/libepoxy/all/conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import *
 
 
@@ -7,20 +6,27 @@ class LibepoxyConan(ConanFile):
     description = "Library handling OpenGL function pointer management"
     license = "MIT"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = (
-        "meson/[^0.51.2]",
-    )
-    requires = (
-        "base/[^1.0.0]",
-        "libx11/[^1.6.8]",
-        "mesa/[^19.2.0]",
-    )
+    options = {
+        "x11": [True, False],
+    }
+    default_options = ("x11=True",)
+    build_requires = ("meson/[^0.55.3]",)
+    requires = ("mesa/[^20.2.1]",)
+
+    def requirements(self):
+        if self.options.x11:
+            self.requires("libx11/[^1.6.8]")
 
     def source(self):
         tools.get(f"https://github.com/anholt/libepoxy/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = ["--auto-features=disabled", "-Dglx=yes", "-Dx11=true", "-Dtests=false"]
+        args = [
+            "--auto-features=disabled",
+            "-Dglx=yes",
+            "-Dtests=false",
+            f"-Dx11={self.options.x11}",
+        ]
         meson = Meson(self)
-        meson.configure(source_folder=f"{self.name, self.version), args=args}-{pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"}")
+        meson.configure(args, source_folder=f"libepoxy-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
