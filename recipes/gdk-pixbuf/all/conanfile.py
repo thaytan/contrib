b22c11fe99e76ef75ae7abc1bca51681b8fcb101
Niclas Overby <noverby@prozum.dk>
2020-10-25 16:43:34 +0100

    imagemagick: port to github

--- recipes/gdk-pixbuf/all/conanfile.py
+++ recipes/gdk-pixbuf/all/conanfile.py
@@ -1,6 +1,4 @@
 import os
-from os import environ, path, pathsep
-
 from conans import *
 
 
@@ -9,13 +7,12 @@ class GdkPixbufConan(ConanFile):
     license = "LGPL-2.1"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = (
-        "meson/[^0.51.2]",
-        "gobject-introspection/[^1.59.3]",
-        "gettext/[^0.20.1]",
-        "imagemagick/7.0.9.25",
+        "meson/[^0.55.3]",
+        "gobject-introspection/[^1.66.1]",
+        "gettext/[^0.21]",
     )
     requires = (
-        "glib/[^2.62.0]",
+        "glib/[^2.66.1]",
         "libx11/[^1.6.8]",
         "libpng/[^1.6.37]",
         "shared-mime-info/[^1.14]",
@@ -31,11 +28,6 @@ class GdkPixbufConan(ConanFile):
             "-Dinstalled_tests=false",
             "-Drelocatable=true",
         ]
-        self.run(f'convert gdk-pixbuf-{self.version}/tests/icc-profile.png +profile "*" gdk-pixbuf-{self.version}/tests/icc-profile.png')
-        with tools.environment_append({"PATH": environ["PATH"] + pathsep + path.join(self.build_folder, "gdk-pixbuf")}):
-            meson = Meson(self)
-            meson.configure(source_folder=f"{self.name, self.version), args=args}-{pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"}")
-            meson.install()
-
-    def package_info(self):
-        self.env_info.GI_TYPELIB_PATH.append(os.path.join(self.package_folder, "lib", "girepository-1.0"))
+        meson = Meson(self)
+        meson.configure(args, source_folder=f"gdk-pixbuf-{self.version}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.install()
--- recipes/imagemagick/all/conanfile.py
+++ recipes/imagemagick/all/conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import *
 
 
@@ -8,22 +7,18 @@ class ImagemagickConan(ConanFile):
     license = "GPL2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
     build_requires = ("autotools/[^1.0.0]",)
-    requires = (
-        "base/[^1.0.0]",
-        "libpng/[^1.6.37]",
-    )
+    requires = ("libpng/[^1.6.37]",)
 
     def source(self):
-        tar_version = "-".join([self.version[: self.version.rfind(".")], self.version[self.version.rfind(".") + 1 :]])
-        tools.get(f"https://github.com/ImageMagick/ImageMagick/archive/{tar_version}.tar.gz")
+        self.tar_version = "-".join([self.version[: self.version.rfind(".")], self.version[self.version.rfind(".") + 1 :]])
+        tools.get(f"https://github.com/ImageMagick/ImageMagick/archive/{self.tar_version}.tar.gz")
 
     def build(self):
         args = ["--disable-static", "--disable-dependency-tracking"]
-        with tools.chdir(f"ImageMagick-{self.tar_version}"):
-            autotools = AutoToolsBuildEnvironment(self)
-            autotools.configure(args=args)
-            autotools.make()
-            autotools.install()
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(f"ImageMagick-{self.tar_version}", args)
+        autotools.make()
+        autotools.install()
 
     def package_info(self):
         self.env_info.MAGICK_CONFIGURE_PATH = os.path.join(self.package_folder, "etc", "ImageMagick-" + self.version.split(".")[0])
