4696bdcf3505f14789a85db785eb62f57f51a50f
Niclas Overby <noverby@prozum.dk>
2020-10-25 20:23:01 +0100

    at-spi2-core: port to github

--- recipes/at-spi2-core/all/conanfile.py
+++ recipes/at-spi2-core/all/conanfile.py
@@ -1,5 +1,4 @@
 import os
-
 from conans import *
 
 
@@ -7,19 +6,18 @@ class AtSpi2CoreConan(ConanFile):
     description = "Protocol definitions and daemon for D-Bus at-spi"
     license = "GPL2"
     settings = "build_type", "compiler", "arch_build", "os_build", "libc_build"
-    build_requires = (
-        "meson/[^0.51.2]",
-    )
+    build_requires = ("meson/[^0.55.3]",)
     requires = (
-        "glib/[^2.62.0]",
+        "glib/[^2.66.1]",
         "dbus/[^1.12.16]",
     )
 
     def source(self):
-        tools.get(f"https://gitlab.gnome.org/GNOME/at-spi2-core/-/archive/AT_SPI2_CORE_{self.version}/at-spi2-core-AT_SPI2_CORE_{self.version.replace(".", "_")}.tar.gz")
+        version = self.version.replace(".", "_")
+        tools.get(f"https://gitlab.gnome.org/GNOME/at-spi2-core/-/archive/AT_SPI2_CORE_{version}/at-spi2-core-AT_SPI2_CORE_{version}.tar.gz")
 
     def build(self):
         args = ["--auto-features=disabled", "--wrap-mode=nofallback"]
         meson = Meson(self)
-        meson.configure(source_folder="at-spi2-core-AT_SPI2_CORE_" + self.version.replace(".", "_"), args=args, pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
+        meson.configure(args, source_folder=f"at-spi2-core-AT_SPI2_CORE_{self.version.replace('.', '_')}", pkg_config_paths=os.environ["PKG_CONFIG_PATH"].split(":"))
         meson.install()
