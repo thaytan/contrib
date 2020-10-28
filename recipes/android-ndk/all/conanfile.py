2f1f9811cfbb8e5e2374fb28dee2a10a61d1c4d9
Niclas Overby <noverby@prozum.dk>
2020-10-28 21:02:34 +0100

    android-ndk: add package

---
+++ recipes/android-ndk/all/conanfile.py
@@ -0,0 +1,28 @@
+from build import *
+
+
+class AndroidNdkRecipe(Recipe):
+    description = "Android NDK"
+    license = "Apache-2.0"
+    settings = "build_type", "compiler", "arch", "os", "libc"
+    options = {}
+    default_options = {}
+
+    def source(self):
+        major, minor = self.version.split(".")
+
+        minor = chr(ord("a") + int(minor))
+        tools.get(f"https://dl.google.com/android/repository/android-ndk-r{major}{minor}-linux-x86_64.zip")
+
+    def build(self):
+        pass
+
+    def package(self):
+        arch = {"x86_64": "x86_64", "armv8": "arm64"}[str(self.settings.arch)]
+        self.copy(pattern="*", src=f"android-ndk-r{self.ndk_version}", symlinks=True)
+
+        # Fix permissions
+        exe_dir = os.path.join(self.package_folder, "toolchains", "llvm", "prebuilt", "linux-x86_64", "bin")
+        for exe in os.listdir(exe_dir):
+            exe_path = os.path.join(exe_dir, exe)
+            os.chmod(exe_path, 0o775)
