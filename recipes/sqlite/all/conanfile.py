aee919937578dec4a0b1d6c6f13e3134cf168422
Niclas Overby <noverby@prozum.dk>
2020-10-29 13:57:13 +0100

    sqlite: add cc req and handle shared opt

--- recipes/sqlite/all/conanfile.py
+++ recipes/sqlite/all/conanfile.py
@@ -10,8 +10,11 @@ class SqliteRecipe(Recipe):
     description = "A C library that implements an SQL database engine"
     license = "custom"
     build_requires = (
+        "cc/[^1.0.0]",
         "make/[^4.3]",
         "tcl/[^8.6.10]",
+    )
+    requires = (
         "zlib/[^1.2.11]",
         "readline/[^8.0]",
     )
@@ -20,6 +23,6 @@ class SqliteRecipe(Recipe):
         self.get(f"https://www.sqlite.org/2019/sqlite-src-{conv_version(self.version)}.zip")
 
     def build(self):
-        self.run("chmod +x configure", cwd=f"{self.name}-{self.version}")
+        self.exe("chmod +x configure")
 
         self.autotools()
