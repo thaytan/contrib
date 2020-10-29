924f9a93aade8df213b6c7b0a9445d4eb659eb3e
Niclas Overby <noverby@prozum.dk>
2020-10-29 18:58:19 +0100

    rust: add rust setting and shared opt support

--- recipes/rust/all/conanfile.py
+++ recipes/rust/all/conanfile.py
@@ -4,27 +4,30 @@ from build import *
 class RustRecipe(Recipe):
     description = "Systems programming language focused on safety, speed and concurrency"
     license = "MIT", "Apache"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
+    settings = Recipe.settings + ("python", "rust")
     build_requires = (
-        "cmake/[^3.18.0]",
-        "ninja/[^1.10.0]",
+        "cmake/[^3.18.4]",
         "curl/[^7.72.0]",
         "pkgconf/[^1.7.3]",
-        "zlib/[^1.2.11]",
-        "git/[2.28.0]",
+        "git/[^2.29.1]",
     )
-    requires = ("openssl1/[^1.1.1h]",)
+    requires = (
+        "zlib/[^1.2.11]",
+        "openssl1/[^1.1.1h]",
+    )
 
     def build_requirements(self):
-        self.build_requires(f"llvm/[^{self.settings.clang.version}]")
         self.build_requires(f"python/[~{self.settings.python}]")
+
+    def requirements(self):
+        self.requires(f"llvm/[^{self.settings.compiler.version}]")
 
     def source(self):
         self.get(f"https://static.rust-lang.org/dist/rustc-{self.version}-src.tar.gz")
 
     def build(self):
         os.environ["RUSTFLAGS"] = "-Clinker-plugin-lto -Copt-level=2"
-        arch = {"x86_64": "x86_64", "armv8": "aarch64"}[str(self.settings.arch_build)]
+        arch = {"x86_64": "x86_64", "armv8": "aarch64"}[str(self.settings.arch)]
         triple = f"{arch}-unknown-linux-gnu"
         args = [
             f"--host={triple}",
@@ -35,15 +38,14 @@ class RustRecipe(Recipe):
             "--enable-llvm-link-shared",
             "--enable-locked-deps",
             "--enable-extended",
-            "--tools='cargo'",
+            "--tools=cargo",
             "--disable-docs",
             "--enable-vendor",
             "--release-channel=stable",
             "--set=llvm.thin-lto=true",
         ]
-        with tools.chdir(f"{self.name}-{self.version}"):
-            self.run(f"./configure {' '.join(args)}")
-            self.run("python x.py install")
+        self.exe("./configure", args)
+        self.exe("python x.py install")
 
     def package_info(self):
         self.env_info.RUSTFLAGS = "-Clinker-plugin-lto -Copt-level=2"
