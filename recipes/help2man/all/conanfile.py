2ecb8af7bcdb59c9e44c883d99174fa76cc3d58f
Niclas Overby <noverby@prozum.dk>
2020-10-29 11:58:57 +0100

    help2man: add cc req

--- recipes/help2man/all/conanfile.py
+++ recipes/help2man/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class Help2ManRecipe(Recipe):
     description = "Conversion tool to create man files"
     license = "GPL"
-    build_requires = ("make/[^4.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "make/[^4.3]",
+    )
 
     def source(self):
         self.get(f"https://ftp.gnu.org/gnu/help2man/help2man-{self.version}.tar.xz")
