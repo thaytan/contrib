7f05aa434bba33badc37616f325f660611eaa5a2
Niclas Overby <noverby@prozum.dk>
2020-10-29 12:26:08 +0100

    libffi: add cc req

--- recipes/libffi/all/conanfile.py
+++ recipes/libffi/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class LibffiRecipe(Recipe):
     description = "Portable foreign function interface library"
     license = "MIT"
-    build_requires = ("make/[^4.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "make/[^4.3]",
+    )
 
     def source(self):
         self.get(f"https://github.com/libffi/libffi/releases/download/v{self.version}/libffi-{self.version}.tar.gz")
