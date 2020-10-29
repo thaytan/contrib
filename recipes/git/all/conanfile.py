98105820b0f92083b1047864f7c587f3690f601c
Niclas Overby <noverby@prozum.dk>
2020-10-29 17:57:14 +0100

    git: bump version and add shared opt support

--- recipes/git/all/conanfile.py
+++ recipes/git/all/conanfile.py
@@ -10,19 +10,20 @@ class GitRecipe(Recipe):
     description = "The fast distributed version control system"
     license = "GPL2"
     build_requires = (
+        "cc/[^1.0.0]",
         "make/[^4.3]",
         "gettext/[^0.21]",
-        "zlib/[^1.2.11]",
-        "curl/[^7.66.0]",
+    )
+    requires = (
+        "curl/[^7.73.0]",
         "expat/[^2.2.7]",
     )
-    requires = ("openssl/[^3.0.0-alpha6]",)
 
     def source(self):
         self.get(f"https://www.kernel.org/pub/software/scm/git/git-{self.version}.tar.xz")
 
     def build(self):
-        with open(os.path.join(f"{self.name}-{self.version}", "config.mak"), "w") as cfg:
+        with open(os.path.join(self.src, "config.mak"), "w") as cfg:
             cfg.write(CONFIG_MAK.format(f"-I{self.source_folder} {os.environ['CFLAGS']}"))
         args = [
             f"prefix={self.package_folder}",
