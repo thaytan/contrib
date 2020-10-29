f9960d6355fbec323b1046141ce661e45c8f7e72
Niclas Overby <noverby@prozum.dk>
2020-10-29 18:18:22 +0100

    libssl: add package

---
+++ recipes/libssl/all/conanfile.py
@@ -0,0 +1,15 @@
+from build import *
+
+
+class LibsslRecipe(Recipe):
+    description = "Virtual libssl"
+    license = "MIT"
+    options = {}
+    default_options = {}
+    requires = (
+        "openssl/[^3.0.0-alpha7]",
+        "ca-certificates/[^20191127]",
+    )
+
+    def package_info(self):
+        self.env_info.SSL_CERT_DIR = os.path.join(self.deps_cpp_info["ca-certificates"].rootpath, "etc", "ssl", "certs")
