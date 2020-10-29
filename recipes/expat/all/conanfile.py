294a02a285c520c8234ad9124cde1527f8575a6a
Niclas Overby <noverby@prozum.dk>
2020-10-29 12:23:39 +0100

    expat: add cc req

--- recipes/expat/all/conanfile.py
+++ recipes/expat/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class ExpatRecipe(Recipe):
     description = "An XML parser library"
     license = "MIT"
-    build_requires = ("make/[^4.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "cmake/[^3.18.4]",
+    )
 
     def source(self):
         self.get(f"https://github.com/libexpat/libexpat/releases/download/R_{self.version.replace('.', '_')}/expat-{self.version}.tar.bz2")
