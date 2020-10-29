4aafa8edf8236a56d479675f251f6afc65b084fd
Niclas Overby <noverby@prozum.dk>
2020-10-29 13:23:03 +0100

    tcl: fix build

--- recipes/tcl/all/conanfile.py
+++ recipes/tcl/all/conanfile.py
@@ -5,6 +5,7 @@ class TclRecipe(Recipe):
     description = "The Tcl scripting language"
     license = "custom"
     build_requires = (
+        "cc/[^1.0.0]",
         "make/[^4.3]",
         "zlib/[^1.2.11]",
     )
@@ -13,9 +14,9 @@ class TclRecipe(Recipe):
         self.get(f"https://downloads.sourceforge.net/sourceforge/tcl/tcl{self.version}-src.tar.gz")
 
     def build(self):
+        os.environ["CFLAGS"] += " -lm"
         args = [
             "--enable-threads",
             "--enable-64bit",
         ]
-        os.environ["CFLAGS"] += " -lm"
-        self.autotools(args)
+        self.autotools(args, os.path.join(self.src, "unix"))
