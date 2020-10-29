b6e9166cdd5db032a840eb9e85e4e3792b038375
Niclas Overby <noverby@prozum.dk>
2020-10-29 21:43:50 +0100

    libssl: add SSL_CERT_FILE env var

--- recipes/libssl/all/conanfile.py
+++ recipes/libssl/all/conanfile.py
@@ -13,3 +13,4 @@ class LibsslRecipe(Recipe):
 
     def package_info(self):
         self.env_info.SSL_CERT_DIR = os.path.join(self.deps_cpp_info["ca-certificates"].rootpath, "etc", "ssl", "certs")
+        self.env_info.SSL_CERT_FILE = os.path.join(self.deps_cpp_info["ca-certificates"].rootpath, "etc", "ssl", "certs", "ca-certificates.crt")
