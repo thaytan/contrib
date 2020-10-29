b05101d9c016d4033d6f1c9516a0debe70cb1d11
Niclas Overby <noverby@prozum.dk>
2020-10-29 15:52:16 +0100

    bzip2: add cc req and handle shared opt

--- recipes/bzip2/all/conanfile.py
+++ recipes/bzip2/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class Bzip2Recipe(Recipe):
     description = "A high-quality data compression program"
     license = "custom"
-    build_requires = ("make/[^4.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "make/[^4.3]",
+    )
 
     def source(self):
         self.get(f"https://sourceware.org/pub/bzip2/bzip2-{self.version}.tar.gz")
@@ -14,7 +17,7 @@ class Bzip2Recipe(Recipe):
             f"PREFIX={self.package_folder}",
             "CC=cc",
         ]
-        self.autotools(args)
+        self.make(args)
 
     def package(self):
         with tools.chdir(os.path.join(self.package_folder, "bin")):
