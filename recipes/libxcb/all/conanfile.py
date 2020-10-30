8d2a3d4ef1d8182f6d91a94038c420d577f0b5f3
Niclas Overby <noverby@prozum.dk>
2020-10-30 17:20:33 +0100

    libxcb: move reqs to build reqs

--- recipes/libxcb/all/conanfile.py
+++ recipes/libxcb/all/conanfile.py
@@ -5,12 +5,15 @@ class LibxcbRecipe(Recipe):
     description = "X11 client-side library"
     license = "MIT"
     build_requires = (
+        "cc/[^1.0.0]",
         "make/[^4.3]",
         "pkgconf/[^1.7.3]",
-        "xcb-proto/[^1.13]",
-        "libpthread-stubs/[^0.4]",
     )
-    requires = ("libxau/[^1.0.9]",)
+    requires = (
+        ("xcb-proto/[^1.13]", "private"),
+        ("libpthread-stubs/[^0.4]", "private"),
+        "libxau/[^1.0.9]",
+    )
 
     def source(self):
         self.get(f"https://xcb.freedesktop.org/dist/libxcb-{self.version}.tar.xz")
