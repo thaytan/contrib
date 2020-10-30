435c927020eec712b209425ac8fe475a44e17673
Niclas Overby <noverby@prozum.dk>
2020-10-30 16:45:55 +0100

    libglvnd: make x11 private

--- recipes/libglvnd/all/conanfile.py
+++ recipes/libglvnd/all/conanfile.py
@@ -4,15 +4,18 @@ from build import *
 class LibglvndRecipe(Recipe):
     description = "The GL Vendor-Neutral Dispatch library"
     license = "custom"
-    build_requires = ("meson/[^0.55.3]",)
     options = {
         "x11": [True, False],
     }
     default_options = ("x11=True",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
 
-    def build_requirements(self):
+    def requirements(self):
         if self.options.x11:
-            self.build_requires("xorgproto/[^2020.1]")
+            self.requires("xorgproto/[^2020.1]", "private")
 
     def source(self):
         self.get(f"https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v{self.version}/libglvnd-v{self.version}.tar.gz")
