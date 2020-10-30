da91dbd34fc6afd00f6a4e8f7b0678bc68114e36
Niclas Overby <noverby@prozum.dk>
2020-10-30 13:53:56 +0100

    : add cc req

--- recipes/gobject-introspection/all/conanfile.py
+++ recipes/gobject-introspection/all/conanfile.py
@@ -1,23 +1,21 @@
 from build import *
 
 
-class GObjectIntrospectionRecipe(Recipe):
+class GObjectIntrospectionRecipe(PythonRecipe):
     description = "Middleware layer between C libraries (using GObject) and language bindings"
     license = "LGPL"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
     build_requires = (
+        "cc/[^1.0.0]",
         "meson/[^0.55.3]",
         "bison/[^3.7.2]",
         "flex/[^2.6.4]",
         "git/[^2.28.0]",
+        "python/[~3.8.5]",
     )
     requires = ("glib/[^2.66.1]",)
-
-    def requirements(self):
-        self.requires(f"python/[~{self.settings.python}]")
 
     def source(self):
         self.get(f"https://github.com/GNOME/gobject-introspection/archive/{self.version}.tar.gz")
 
     def package_info(self):
-        self.env_info.PYTHONPATH = os.path.join(self.package_folder, "lib", "gobject-introspection")
+        self.env_info.PYTHONPATH += os.path.join(self.package_folder, "lib", "gobject-introspection")
