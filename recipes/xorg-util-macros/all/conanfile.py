a34dda28e238e10ddbd61cf49d3b3b94183e3f6a
Niclas Overby <noverby@prozum.dk>
2020-10-30 16:38:14 +0100

    xorg-util-macros: cleanup

--- recipes/xorg-util-macros/all/conanfile.py
+++ recipes/xorg-util-macros/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class XorgUtilMacrosRecipe(Recipe):
     description = "X.Org Autotools macros"
     license = "custom"
-    build_requires = ("autotools/[^1.0.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         self.get(f"https://xorg.freedesktop.org/releases/individual/util/util-macros-{self.version}.tar.gz")
