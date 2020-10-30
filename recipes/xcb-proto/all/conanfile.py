98c06bb238dc7ce8aaa5e92f4142e124f88aeca1
Niclas Overby <noverby@prozum.dk>
2020-10-30 17:14:17 +0100

    xcb-proto: cleanup

--- recipes/xcb-proto/all/conanfile.py
+++ recipes/xcb-proto/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class XcbProtoRecipe(Recipe):
     description = "XML-XCB protocol descriptions"
     license = "MIT"
-    build_requires = ("autotools/[^1.0.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         self.get(f"https://xcb.freedesktop.org/dist/xcb-proto-{self.version}.tar.xz")
