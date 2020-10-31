72fbb0065a0d478b204f1f81e584c66c28912976
Niclas Overby <noverby@prozum.dk>
2020-10-31 22:58:43 +0100

    freetype: cleanup

--- recipes/freetype/all/conanfile.py
+++ recipes/freetype/all/conanfile.py
@@ -6,7 +6,10 @@ class FreetypeRecipe(Recipe):
     license = "GPL2"
     options = {"harfbuzz": [True, False]}
     default_options = ("harfbuzz=True",)
-    build_requires = ("autotools/[^1.0.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def requirements(self):
         if self.options.harfbuzz:
