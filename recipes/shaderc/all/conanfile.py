3b0ed30dd999bc563fd34675b2feca388d96a65f
Niclas Overby <noverby@prozum.dk>
2020-10-31 11:17:08 +0100

    shaderc: use cmake helper

--- recipes/shaderc/all/conanfile.py
+++ recipes/shaderc/all/conanfile.py
@@ -1,18 +1,19 @@
 from build import *
 
 
-class LibShadercRecipe(Recipe):
+class ShadercRecipe(Recipe):
     description = "A collection of tools, libraries, and tests for Vulkan shader compilation."
     license = "Apache"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
-    build_requires = ("cmake/[^3.18.4]",)
-
-    def build_requirements(self):
-        self.build_requires(f"python/[~{self.settings.python}]")
+    build_requires = (
+        "cc/[^1.0.0]",
+        "cmake/[^3.18.4]",
+        "python/[^3.8.5]",
+        "git/[^2.29.1]",
+    )
 
     def source(self):
         self.get(f"https://github.com/google/shaderc/archive/v{self.version}.tar.gz")
-        self.run(f"sh utils/git-sync-deps", cwd=f"{self.name}-{self.version}")
+        self.exe("./utils/git-sync-deps")
 
     def package_info(self):
         self.env_info.SHADERC_LIB_DIR.append(os.path.join(self.package_folder, "lib"))
