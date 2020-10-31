6a7010848750182b1a936e41016b1989642698b5
Niclas Overby <noverby@prozum.dk>
2020-10-31 22:30:50 +0100

    fribidi: use meson helper

--- recipes/fribidi/all/conanfile.py
+++ recipes/fribidi/all/conanfile.py
@@ -4,13 +4,16 @@ from build import *
 class FribidiRecipe(Recipe):
     description = "The Free Implementation of the Unicode Bidirectional Algorithm"
     license = "LGPL"
-    build_requires = ("meson/[^0.55.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
 
     def source(self):
         self.get(f"https://github.com/fribidi/fribidi/archive/v{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Ddocs=false",
-        ]
-        self.meson(args)
+        opts = {
+            "docs": False,
+        }
+        self.meson(opts)
