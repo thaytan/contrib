9b0037ed5ef4ddf07c1fd1331542fc3b15c2619f
Niclas Overby <noverby@prozum.dk>
2020-10-31 22:55:18 +0100

    libuuid: cleanup

--- recipes/libuuid/all/conanfile.py
+++ recipes/libuuid/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class LibuuidRecipe(Recipe):
     description = "Portable uuid C library"
     license = "BSD-3-Clause"
-    build_requires = ("autotools/[^1.0.0]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         self.get(f"https://netix.dl.sourceforge.net/project/libuuid/libuuid-{self.version}.tar.gz")
