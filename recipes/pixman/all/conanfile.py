52b49418be2b147c58311cf027972d5082b0b3ec
Niclas Overby <noverby@prozum.dk>
2020-10-31 22:50:13 +0100

    pixman: cleanup

--- recipes/pixman/all/conanfile.py
+++ recipes/pixman/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class PixmanRecipe(Recipe):
     description = "Image processing and manipulation library"
     license = "custom"
-    build_requires = ("meson/[^0.55.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
 
     def source(self):
         self.get(f"https://xorg.freedesktop.org/releases/individual/lib/pixman-{self.version}.tar.gz")
