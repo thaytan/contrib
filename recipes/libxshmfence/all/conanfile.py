acc2aa9cae7e40de1639f1a377de827e1a267718
Niclas Overby <noverby@prozum.dk>
2020-11-01 11:36:18 +0100

    libxshmfence: add cc req

--- recipes/libxshmfence/all/conanfile.py
+++ recipes/libxshmfence/all/conanfile.py
@@ -5,6 +5,7 @@ class LibxshmfenceRecipe(Recipe):
     description = "Library that exposes a event API on top of Linux futexes"
     license = "custom"
     build_requires = (
+        "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
         "xorg-util-macros/[^1.19.2]",
     )
