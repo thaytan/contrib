ab7f07181a3d12b5ef5b3864c7c890d65bd0d513
Niclas Overby <noverby@prozum.dk>
2020-11-01 14:03:01 +0100

    librealsense: remove python support

--- recipes/librealsense/all/conanfile.py
+++ recipes/librealsense/all/conanfile.py
@@ -1,7 +1,7 @@
 from build import *
 
 
-class LibRealsenseRecipe(PythonRecipe):
+class LibRealsenseRecipe(Recipe):
     description = "Intel RealSense SDK"
     license = "Apache"
     exports = "libusb-fix.patch", "pkgconfig-fix.patch"
@@ -10,10 +10,7 @@ class LibRealsenseRecipe(PythonRecipe):
         "cmake/[^3.18.4]",
         # "cuda/[^10.1.243]",
     )
-    requres = (
-        "python/[^3.8.4]",
-        "libusb/[^1.0.23]",
-    )
+    requires = ("libusb/[^1.0.23]",)
 
     def source(self):
         self.get(f"https://github.com/IntelRealSense/librealsense/archive/v{self.version}.tar.gz")
@@ -22,18 +19,12 @@ class LibRealsenseRecipe(PythonRecipe):
 
     def build(self):
         defs = {
-            "PYTHON_LIBRARIES": os.path.join(self.deps_cpp_info["python"].rootpath, "lib", "libpython.so"),
-            "PYTHON_INCLUDE_DIRS": os.path.join(self.deps_cpp_info["python"].rootpath, "include", f"python{self.settings.python}"),
             # "BUILD_WITH_CUDA": self.options.cuda,
-            "BUILD_PYTHON_BINDINGS": True,
+            "BUILD_PYTHON_BINDINGS": False,
             "BUILD_EXAMPLES": False,
             "BUILD_GRAPHICAL_EXAMPLES": False,
             "BUILD_PCL_EXAMPLES": False,
             "BUILD_NODEJS_BINDINGS": False,
             "BUILD_UNIT_TESTS": False,
-            "PYTHONLIBS_FOUND": True,
         }
         self.cmake(defs)
-
-    def package_info(self):
-        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib"))
