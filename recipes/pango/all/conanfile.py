ad465089d6f2699c9ce70d890508e7b252abb150
Niclas Overby <noverby@prozum.dk>
2020-11-01 12:11:52 +0100

    pango: use meson helper

--- recipes/pango/all/conanfile.py
+++ recipes/pango/all/conanfile.py
@@ -5,6 +5,7 @@ class PangoRecipe(Recipe):
     description = "A library for layout and rendering of text"
     license = "GPL"
     build_requires = (
+        "cc/[^1.0.0]",
         "meson/[^0.55.3]",
         "gobject-introspection/[^1.59.3]",
     )
@@ -17,9 +18,9 @@ class PangoRecipe(Recipe):
         self.get(f"https://github.com/GNOME/pango/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Dfontconfig=enabled",
-            "-Dfreetype=enabled",
-            "-Dcairo=enabled",
-        ]
-        self.meson(args)
+        opts = {
+            "fontconfig": True,
+            "freetype": True,
+            "cairo": True,
+        }
+        self.meson(opts)
