571ce76468fed867c66e4ae06242c73c74520007
Niclas Overby <noverby@prozum.dk>
2020-11-01 13:20:14 +0100

    eudev: add cc req

--- recipes/eudev/all/conanfile.py
+++ recipes/eudev/all/conanfile.py
@@ -5,6 +5,7 @@ class EudevRecipe(Recipe):
     description = "OpenRC compatible fork of systemd-udev"
     license = "GPL2"
     build_requires = (
+        "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
         "gperf/[^3.1]",
         "gobject-introspection/[^1.59.3]",
