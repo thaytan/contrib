aef1c09ef8c8355abfa01e485fc5386155fa5f02
Niclas Overby <noverby@prozum.dk>
2020-11-01 12:10:02 +0100

    linpng: use cmake

--- recipes/libpng/all/conanfile.py
+++ recipes/libpng/all/conanfile.py
@@ -6,9 +6,16 @@ class LibpngRecipe(Recipe):
     license = "custom"
     build_requires = (
         "cc/[^1.0.0]",
-        "autotools/1.0.0",
+        "cmake/3.18.4",
     )
     requires = ("zlib/[^1.2.11]",)
 
     def source(self):
         self.get(f"https://downloads.sourceforge.net/sourceforge/libpng/libpng-{self.version}.tar.xz")
+
+    def build(self):
+        defs = {
+            "PNG_SHARED": self.options.shared,
+            "PNG_STATIC": not self.options.shared,
+        }
+        self.cmake(defs)
