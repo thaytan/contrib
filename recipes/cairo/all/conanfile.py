febb6652f6f559fa25b912b4e6f9964d8b7de6aa
Niclas Overby <noverby@prozum.dk>
2020-11-01 11:58:19 +0100

    cairo: use autotools helper

--- recipes/cairo/all/conanfile.py
+++ recipes/cairo/all/conanfile.py
@@ -4,20 +4,35 @@ from build import *
 class CairoRecipe(Recipe):
     description = "2D graphics library with support for multiple output devices"
     license = "LGPL"
+    options = {
+        "shared": [True, False],
+        "introspection": [True, False],
+        "x11": [True, False],
+    }
+    default_options = (
+        "shared=True",
+        "introspection=True",
+        "x11=True",
+    )
     build_requires = (
+        "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
         "gobject-introspection/[^1.66.1]",
-        "mesa/[^20.2.1]",
-        "zlib/[^1.2.11]",
     )
     requires = (
-        "glib/[^2.62.0]",
+        "glib/[^2.66.1]",
         "pixman/[^0.40.0]",
         "fontconfig/[^2.13.92]",
         "libpng/[^1.6.37]",
-        "libxrender/[^0.9.10]",
-        "libxext/[^1.3.4]",
     )
+
+    def build_requirements(self):
+        if self.options.introspection:
+            self.build_requires("gobject-introspection/[^1.66.1]")
+
+    def requirements(self):
+        self.requires("libxrender/[^0.9.10]")
+        self.requires("libxext/[^1.3.4]")
 
     def source(self):
         self.get(f"https://gitlab.freedesktop.org/cairo/cairo/-/archive/{self.version}/cairo-{self.version}.tar.gz")
