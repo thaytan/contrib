0430add126bcd9ab58e1b55216ccdb6f2534a9e8
Niclas Overby <noverby@prozum.dk>
2020-11-01 11:27:10 +0100

    libpng: add cc req

--- recipes/libpng/all/conanfile.py
+++ recipes/libpng/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class LibpngRecipe(Recipe):
     description = "A collection of routines used to create PNG format graphics files"
     license = "custom"
-    build_requires = ("autotools/1.0.0",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/1.0.0",
+    )
     requires = ("zlib/[^1.2.11]",)
 
     def source(self):
