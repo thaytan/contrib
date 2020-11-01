a97faa5228bb02a8f2fe37904cc5d9638d2ff3d1
Niclas Overby <noverby@prozum.dk>
2020-11-01 13:02:56 +0100

    gstreamer-plugins-base: use meson helper

--- recipes/gstreamer-plugins-base/all/conanfile.py
+++ recipes/gstreamer-plugins-base/all/conanfile.py
@@ -1,49 +1,57 @@
 from build import *
 
 
-class GStreamerPluginsBaseRecipe(Recipe):
+class GStreamerPluginsBaseRecipe(GstreamerRecipe):
     description = "A well-groomed and well-maintained collection of GStreamer plugins and elements"
     license = "LGPL"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
     options = {
+        "shared": [True, False],
+        "introspection": [True, False],
+        "x11": [True, False],
         "audioresample": [True, False],
     }
-    default_options = ("audioresample=False",)
+    default_options = (
+        "shared=True",
+        "introspection=True",
+        "x11=True",
+        "audioresample=False",
+    )
     build_requires = (
+        "cc/[^1.0.0]",
         "meson/[^0.55.2]",
-        "gobject-introspection/[^1.59.3]",
-        "opus/[^1.3.1]",
-        "pango/[^1.43.0]",
     )
     requires = (
+        "gstreamer/[^1.18.1]",
+        "opus/[^1.3.1]",
+        "pango/[^1.43.0]",
         "orc/[^0.4.29]",
         "mesa/[^20.2.1]",
-        "libx11/[^1.6.8]",
     )
 
-    def requirements(self):
-        self.requires(f"gstreamer/[~{self.settings.gstreamer}]")
+    def build_requirements(self):
+        if self.options.introspection:
+            self.build_requires("gobject-introspection/[^1.66.1]")
 
     def source(self):
         self.get(f"https://github.com/GStreamer/gst-plugins-base/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Dgl_platform=egl",
-            "-Dintrospection=enabled",
-            "-Dgl=enabled",
-            "-Dx11=enabled",
-            "-Dvideotestsrc=enabled",
-            "-Daudiotestsrc=enabled",
-            "-Dvideoconvert=enabled",
-            "-Dapp=enabled",
-            "-Dplayback=enabled",
-            "-Dtypefind=enabled",
-            "-Dorc=enabled",
-            "-Dopus=enabled",
-            "-Dpango=enabled",
-            "-Dvideoscale=enabled",
-            "-Daudioconvert=enabled",
-        ]
-        args.append("-Daudioresample=" + ("enabled" if self.options.audioresample else "disabled"))
-        self.meson(args)
+        opts = {
+            "gl_platform": "egl",
+            "introspection": self.options.introspection,
+            "x11": self.options.x11,
+            "audioresample": self.options.audioresample,
+            "gl": True,
+            "videotestsrc": True,
+            "audiotestsrc": True,
+            "videoconvert": True,
+            "app": True,
+            "playback": True,
+            "typefind": True,
+            "orc": True,
+            "opus": True,
+            "pango": True,
+            "videoscale": True,
+            "audioconvert": True,
+        }
+        self.meson(opts)
