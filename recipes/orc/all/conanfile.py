51a9d174159e5f2984da3b32a8d7320861ccb387
Niclas Overby <noverby@prozum.dk>
2020-11-01 11:30:53 +0100

    orc: use meson helper

--- recipes/orc/all/conanfile.py
+++ recipes/orc/all/conanfile.py
@@ -4,14 +4,16 @@ from build import *
 class OrcRecipe(Recipe):
     description = "Optimized Inner Loop Runtime Compiler"
     license = "LGPL-2.1"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
-    build_requires = ("meson/[^0.55.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
 
     def source(self):
         self.get(f"https://github.com/GStreamer/orc/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Dtools=enabled",
-        ]
-        self.meson(args)
+        opts = {
+            "tools": True,
+        }
+        self.meson(opts)
