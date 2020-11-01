e2b4889494edb73c54c15bbd9d76be5353ab9ff9
Niclas Overby <noverby@prozum.dk>
2020-11-01 08:58:14 +0100

    gperf: cleanup

--- recipes/gperf/all/conanfile.py
+++ recipes/gperf/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class GperfRecipe(Recipe):
     description = "A portable, high level programming interface to various calling conventions"
     license = "GPL3"
-    build_requires = ("meson/[^0.55.3]",)
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
 
     def source(self):
-        self.get(f"https://gitlab.com/aivero/public/gperf/-/archive/meson/gperf-meson.tar.gz")
+        self.get("https://gitlab.com/aivero/public/gperf/-/archive/meson/gperf-meson.tar.gz")
