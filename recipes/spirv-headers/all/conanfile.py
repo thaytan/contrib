e863d6d972b1278edc4b1349b6f078895da86c2a
Niclas Overby <noverby@prozum.dk>
2020-11-02 21:43:28 +0100

    spirv-headers: add package

---
+++ recipes/spirv-headers/all/conanfile.py
@@ -0,0 +1,15 @@
+from build import *
+
+
+class SpirvToolsRecipe(Recipe):
+    description = "API and commands for processing SPIR-V modules"
+    license = "custom"
+    build_requires = (
+        "cmake/[^3.18.4]",
+        "python/[^3.8.5]",
+        "git/[^2.29.1]",
+        "ninja/[^1.10.0]",
+    )
+
+    def source(self):
+        self.get(f"https://github.com/KhronosGroup/SPIRV-Headers/archive/{self.version}.tar.gz")
