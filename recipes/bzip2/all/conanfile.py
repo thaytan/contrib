a6b88cdbca0d3fa146dbcba9ec3376d29e690def
Niclas Overby <noverby@prozum.dk>
2020-11-07 21:00:52 +0100

    bzip2: add support for shared

--- recipes/bzip2/all/conanfile.py
+++ recipes/bzip2/all/conanfile.py
@@ -15,11 +15,18 @@ class Bzip2Recipe(Recipe):
     def build(self):
         args = [
             f"PREFIX={self.package_folder}",
-            "CC=cc",
+            f"CC=cc {os.environ['CFLAGS']}",
         ]
         self.make(args)
+        if self.options.shared:
+            args.extend(["-f", "Makefile-libbz2_so"])
+        self.make(args, target="all")
 
     def package(self):
+        if self.options.shared:
+            self.copy("*.so*", dst="lib", keep_path=False, links=True)
+            os.symlink("libbz2.so.1.0", os.path.join(self.package_folder, "lib", "libbz2.so"))
+            os.remove(os.path.join(self.package_folder, "lib", "libbz2.a"))
         with tools.chdir(os.path.join(self.package_folder, "bin")):
             for source, link in (
                 ("bzdiff", "bzcmp"),
