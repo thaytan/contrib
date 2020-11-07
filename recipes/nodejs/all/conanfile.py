206076e3049a633944189561320a3eec6bd35a4e
Niclas Overby <noverby@prozum.dk>
2020-11-07 21:23:31 +0100

    nodejs: port to github

--- recipes/nodejs/all/conanfile.py
+++ recipes/nodejs/all/conanfile.py
@@ -4,15 +4,15 @@ from build import *
 class NodejsRecipe(Recipe):
     description = "Evented I/O for V8 javascript"
     license = "MIT"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
-    build_requires = ("cc/[^1.0.0]", "autotools/[^1.0.0]")
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+        "python/[^3]",
+    )
     requires = (
-        "openssl/[^1.1.1b]",
+        "openssl1/[^1.1.1h]",
         "zlib/[^1.2.11]",
     )
-
-    def build_requirements(self):
-        self.build_requires(f"python/[~{self.settings.python}]")
 
     def source(self):
         self.get(f"https://github.com/nodejs/node/archive/v{self.version}.tar.gz")
