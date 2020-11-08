3ba48f7528ad62e913c64a6e5843739b855940d6
Niclas Overby <noverby@prozum.dk>
2020-11-08 07:54:04 +0100

    yasm: port to cmake

--- recipes/yasm/all/conanfile.py
+++ recipes/yasm/all/conanfile.py
@@ -6,7 +6,7 @@ class YasmRecipe(Recipe):
     license = "BSD"
     build_requires = (
         "cc/[^1.0.0]",
-        "autotools/[^1.0.0]",
+        "cmake/[^3.18.4]",
     )
 
     def source(self):
