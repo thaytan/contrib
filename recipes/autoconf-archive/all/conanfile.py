fe6989115775e94b8e12c06e50e29cee9282ae0b
Niclas Overby <noverby@prozum.dk>
2020-11-08 10:02:02 +0100

    autoconf-archive: cleanup

--- recipes/autoconf-archive/all/conanfile.py
+++ recipes/autoconf-archive/all/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class AutoconfArchiveRecipe(Recipe):
     description = "A collection of freely re-usable Autoconf macros"
     license = "GPL3"
-    build_requires = ("cc/[^1.0.0]", "autoconf/[^2.69]")
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autoconf/[^2.69]",
+    )
 
     def source(self):
         self.get(f"https://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-{self.version}.tar.xz")
