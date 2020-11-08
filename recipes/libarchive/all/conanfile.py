9949e5c38792545299825dba0e182e87d7a30457
Niclas Overby <noverby@prozum.dk>
2020-11-08 08:54:01 +0100

    libarchive: properly handle shared option

--- recipes/libarchive/all/conanfile.py
+++ recipes/libarchive/all/conanfile.py
@@ -6,13 +6,22 @@ class LibarchiveRecipe(Recipe):
     license = "BSD"
     build_requires = (
         "cc/[^1.0.0]",
-        "autotools/[^1.0.0]",
-        "expat/[^2.2.7]",
-        "zlib/[^1.2.11]",
-        "xz/[^5.2.5]",
-        "bzip2/[^1.0.8]",
+        "cmake/[^3.18.4]",
     )
-    requires = ("openssl1/[^1.1.1h]",)
+    requires = (
+        "zlib/[^1.2.11]",
+        "bzip2/[^1.0.8]",
+        "xz/[^5.2.5]",
+        "expat/[^2.2.7]",
+        "openssl1/[^1.1.1h]",
+    )
 
     def source(self):
         self.get(f"https://github.com/libarchive/libarchive/releases/download/v{self.version}/libarchive-{self.version}.tar.xz")
+
+    def build(self):
+        self.cmake()
+        if self.options.shared:
+            os.remove(os.path.join(self.package_folder, "lib", "libarchive.a"))
+        else:
+            os.remove(os.path.join(self.package_folder, "lib", "libarchive.so"))
