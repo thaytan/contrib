c2119268abbf0ff2c641eb5838a5f3ed879c7fe3
Niclas Overby <noverby@prozum.dk>
2020-11-08 07:46:13 +0100

    yasm: cleanup

--- recipes/yasm/all/conanfile.py
+++ recipes/yasm/all/conanfile.py
@@ -4,10 +4,10 @@ from build import *
 class YasmRecipe(Recipe):
     description = "Yasm is a complete rewrite of the NASM assembler under the new BSD License"
     license = "BSD"
-    build_requires = ("cc/[^1.0.0]", "autotools/[^1.0.0]")
+    build_requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         self.get(f"http://www.tortall.net/projects/yasm/releases/yasm-{self.version}.tar.gz")
-
-    def build(self):
-        self.autotools()
