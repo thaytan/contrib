92d701ecc50fe618ac85ca5ac67c59062436d03d
Niclas Overby <noverby@prozum.dk>
2020-12-22 14:24:14 +0100

    libsrtp: cleanup

--- recipes/libsrtp/conanfile.py
+++ recipes/libsrtp/conanfile.py
@@ -4,7 +4,10 @@ from build import *
 class LibsrtpRecipe(Recipe):
     description = "Library for SRTP (Secure Realtime Transport Protocol)"
     license = "BSD"
-    build_requires = ("cc/[^1.0.0]", "autotools/[^1.0.0]")
+    build_requires = (
+        "cc/[^1.0.0]", 
+        "autotools/[^1.0.0]",
+    )
 
     def source(self):
         self.get(f"https://github.com/cisco/libsrtp/archive/v{self.version}.tar.gz")
