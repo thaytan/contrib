ad3b521950d42b853f94074ce7babf559063d420
Niclas Overby <noverby@prozum.dk>
2020-12-22 15:26:28 +0100

    libsrtp: manually copy crypto_types.h

--- recipes/libsrtp/conanfile.py
+++ recipes/libsrtp/conanfile.py
@@ -8,6 +8,9 @@ class LibsrtpRecipe(Recipe):
         "cc/[^1.0.0]", 
         "cmake/[^3.18.4]",
     )
+    requires = (
+        "libssl/[^1.0.0]",
+    )
     exports_sources = (
         "libsrtp2.pc",
     )
@@ -15,5 +18,12 @@ class LibsrtpRecipe(Recipe):
     def source(self):
         self.get(f"https://github.com/cisco/libsrtp/archive/v{self.version}.tar.gz")
 
+    def build(self):
+        defs = {
+            "ENABLE_OPENSSL": True,
+        }
+        self.cmake(defs)
+
     def package(self):
         self.copy("libsrtp2.pc", dst="lib/pkgconfig")
+        self.copy("*crypto_types.h", dst="include/srtp2", keep_path=False)
