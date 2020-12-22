8300b2638107d91449657cb986f48df317a98a0b
Niclas Overby <noverby@prozum.dk>
2020-12-22 14:33:46 +0100

    libsrtp: port to cmake

--- recipes/libsrtp/conanfile.py
+++ recipes/libsrtp/conanfile.py
@@ -6,7 +6,7 @@ class LibsrtpRecipe(Recipe):
     license = "BSD"
     build_requires = (
         "cc/[^1.0.0]", 
-        "autotools/[^1.0.0]",
+        "cmake/[^3.18.4]",
     )
 
     def source(self):
