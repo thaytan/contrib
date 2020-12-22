ad1b77eb39726f52c091233775be38f7a9e839ad
Niclas Overby <noverby@prozum.dk>
2020-12-22 13:08:18 +0100

    libnice: use new meson helper

--- recipes/libnice/conanfile.py
+++ recipes/libnice/conanfile.py
@@ -4,21 +4,20 @@ from build import *
 class LibNiceRecipe(GstreamerRecipe):
     description = "An implementation of the IETF's Interactive Connectivity Establishment (ICE) standard"
     license = "LGPL"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "gstreamer"
     build_requires = (
         "cc/[^1.0.0]", 
         "meson/[^0.55.3]",
     )
     requires = (
         "openssl1/[^1.1.1h]",
-        "gst-plugins-base/[^1.18.1],
+        "gst-plugins-base/[^1.18.1]",
     )
 
     def source(self):
         self.get(f"https://github.com/libnice/libnice/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Dgstreamer=enabled",
-        ]
-        self.meson(args)
+        opts = {
+            "gstreamer": True,
+        }
+        self.meson(opts)
