ff37eb12a8d36d1c75ccf6a44d057230a2a149f2
Niclas Overby <noverby@prozum.dk>
2020-12-22 14:35:35 +0100

    libwebp: cleanup

--- recipes/libwebp/conanfile.py
+++ recipes/libwebp/conanfile.py
@@ -1,16 +1,14 @@
 from build import *
 
 
-class RecipeLibwebp(Recipe):
+class LibwebpRecipe(Recipe):
     description = "library to encode and decode images in WebP format"
     license = "BSD"
-    build_requires = ("cc/[^1.0.0]", "cmake/[^3.18.4]")
+    build_requires = (
+        "cc/[^1.0.0]", 
+        "cmake/[^3.18.4]",
+    )
 
     def source(self):
         self.get(f"https://github.com/webmproject/libwebp/archive/v{self.version}.tar.gz")
 
-    def build(self):
-        defs = {
-            "BUILD_SHARED_LIBS": self.options.shared,
-        }
-        self.cmake(defs)
