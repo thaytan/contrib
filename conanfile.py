6fcae3239837355215795512eed744407b967c7e
Niclas Overby <noverby@prozum.dk>
2021-01-23 17:14:21 +0100

    Port to Github

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from conans import ConanFile, CMake, tools
+from build import *
 
-class DepthMetaConan(ConanFile):
-    name = "gstreamer-depth-meta"
-    license = "Apache 2.0"
+class GstDepthMeta(GstProject):
+    license = "Apache"
     description = "Library to stream depth video"
-    url = "https://aivero.com"
-    settings = "os", "arch", "compiler", "build_type", "gstreamer"
-    version = tools.get_env("GIT_TAG", "master")
-    exports_sources = ["CMakeLists.txt", "src/*"]
-
-    def build_requirements(self):
-        self.build_requires("cmake/[>=3.15.3]@%s/stable" % (self.user))
-
-    def requirements(self):
-        self.requires("gstreamer-plugins-base/[~%s]@%s/stable" % (self.settings.gstreamer, self.user))
-
-    def build(self):
-        env = {
-            "GIT_PKG_VER": "%s" % self.version,
-        }
-        with tools.environment_append(env):
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.install()
+    exports_sources = (
+        "CMakeLists.txt", 
+        "src/*"
+    )
+    build_requires = (
+        "cc/[^1.0.0]",
+        "cmake/[^3.18.4]"
+    )
+    requires = (
+        "gst-plugins-base/[^1.18.1]"
+    )
