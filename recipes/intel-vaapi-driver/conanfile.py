b53a76e57a2e162afdcb36845676128af5e75065
Niclas Overby <noverby@prozum.dk>
2021-01-25 13:26:35 +0100

    intel-vaapi-driver: fix build

--- recipes/intel-vaapi-driver/conanfile.py
+++ recipes/intel-vaapi-driver/conanfile.py
@@ -1,15 +1,16 @@
 from build import *
 
 
-class IntelVaapiDriverRecipe(Recipe):
+class IntelVaapiDriver(Recipe):
     description = "VA-API user mode driver for Intel GEN Graphics family"
     license = "MIT"
     options = {"x11": [True, False], "wayland": [True, False]}
     default_options = ("x11=True", "wayland=False")
-    build_requires = ("cc/[^1.0.0]", "meson/[^0.51.2]")
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[>=0.51.2]",
+    )
     requires = (
-        "base/[^1.0.0]",
-        "libdrm/[^2.4.96]",
         "libva/[^2.3.0]",
     )
 
@@ -17,11 +18,11 @@ class IntelVaapiDriverRecipe(Recipe):
         self.get(f"https://github.com/intel/intel-vaapi-driver/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = []
-        args.append("-Dwith_x11=" + ("yes" if self.options.x11 else "no"))
-        args.append("-Dwith_wayland=" + ("yes" if self.options.wayland else "no"))
-        args.append("-Ddriverdir=" + os.path.join(self.package_folder, "lib", "dri"))
-        self.meson(args)
+        opts = {}
+        opts["with_x11"] = self.options.x11
+        opts["with_wayland"] = self.options.wayland
+        opts["driverdir"] = os.path.join(self.package_folder, "lib", "dri")
+        self.meson(opts)
 
     def package_info(self):
         self.env_info.LIBVA_DRIVERS_PATH.append(os.path.join(self.package_folder, "lib", "dri"))
