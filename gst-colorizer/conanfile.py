cbd4eba6f336b551c2699991f3c37dd7458d6576
Niclas Overby <noverby@prozum.dk>
2021-02-01 23:11:50 +0100

    gst-colorizer: cleanup

--- gst-colorizer/conanfile.py
+++ gst-colorizer/conanfile.py
@@ -1,16 +1,9 @@
 from build import *
 
+
 class GstColorizer(GstProject):
     license = "Apache"
     description = "Library to stream depth video"
-    exports_sources = (
-        "CMakeLists.txt", 
-        "src/*"
-    )
-    build_requires = (
-        "cc/[^1.0.0]",
-        "cmake/[^3.18.4]"
-    )
-    requires = (
-        "gst-plugins-base/[^1.18.1]"
-    )
+    exports_sources = ("CMakeLists.txt", "src/*")
+    build_requires = ("cc/[^1.0.0]", "cmake/[^3.18.4]")
+    requires = "gst-plugins-base/[^1.18]"
