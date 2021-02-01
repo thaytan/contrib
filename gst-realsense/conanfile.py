82f333b0f2d5b8fafb28d93f6aa30a6cd45aa76c
Niclas Overby <noverby@prozum.dk>
2021-02-01 23:09:33 +0100

    gst-realsense: use branch for reqs

--- gst-realsense/conanfile.py
+++ gst-realsense/conanfile.py
@@ -1,12 +1,11 @@
 from build import *
 
+
 class GstRealsense(GstRustProject):
     description = "GStreamer plugin containing `video/rgbd` source for a RealSense device"
     license = "LGPL"
-    build_requires = (
-        "rust/[^1.0.0]",
-    )
+    build_requires = ("rust/[^1.0.0]",)
     requires = (
         "librealsense/[^2.39.0]",
-        "rgbd-timestamps/master",
+        f"rgbd-timestamps/{branch()}",
     )
