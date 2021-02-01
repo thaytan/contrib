dadcb3cfcca9e8807e845e3a99e66216ca6b07f8
Niclas Overby <noverby@prozum.dk>
2021-02-01 22:21:49 +0100

    gst-rgbd: use branch for reqs

--- gst-rgbd/conanfile.py
+++ gst-rgbd/conanfile.py
@@ -1,12 +1,11 @@
 from build import *
 
+
 class Rgbd(GstRustProject):
     description = "GStreamer plugin for demuxing and muxing `video/rgbd` streams"
     license = "LGPL"
-    build_requires = (
-        "rust/[^1.0.0]",
-    )
+    build_requires = ("rust/[^1.0.0]",)
     requires = (
-        "gst-depth-meta/master",
+        f"gst-depth-meta/{branch()}",
         "capnproto/[^0.8.0]",
     )
