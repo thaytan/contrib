75f7fd26e60072dcff851cc3bd9168fcebc44d86
Niclas Overby <noverby@prozum.dk>
2021-02-02 14:58:12 +0100

    freetype: don't use options

--- recipes/freetype/conanfile.py
+++ recipes/freetype/conanfile.py
@@ -4,28 +4,20 @@ from build import *
 class FreetypeRecipe(Recipe):
     description = "FreeType is a software library to render fonts"
     license = "GPL"
-    options = {"harfbuzz": [True, False]}
-    default_options = ("harfbuzz=True",)
     build_requires = (
         "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
     )
 
-    def configure(self):
-        # Avoid circular requirement
-        self.options["freetype-no-harfbuzz"].harfbuzz = False
-
     def requirements(self):
-        if self.options.harfbuzz:
+        if self.name == "harfbuzz":
             self.requires("harfbuzz/[^2.7.2]")
-            # Using harfbuzz requires freetype
-            self.requires(f"freetype-no-harfbuzz/[^{self.version}]", "private")
-            self.requires("zlib/[^1.2.11]")
         else:
-            self.requires("zlib/[^1.2.11]", "private")
+            self.requires("libpng/[^1.6.37]")
 
     def source(self):
         self.get(f"https://download-mirror.savannah.gnu.org/releases/freetype/freetype-{self.version}.tar.xz")
 
     def build(self):
+        # Force use autotools
         self.autotools()
