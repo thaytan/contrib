09c62c784f5848d338d1476d9a4b3e1fd26a13d1
Niclas Overby <noverby@prozum.dk>
2021-02-03 13:47:22 +0100

    scripts: in upload_aliases set fetch_repo

--- scripts/upload_aliases.py
+++ scripts/upload_aliases.py
@@ -3,10 +3,12 @@ import os
 
 branch = os.environ["GITHUB_REF"].split("/")[2]
 old_branch = "master"
+fetch_repo = os.environ["CONAN_REPO_ALL"]
 upload_repo = os.environ["CONAN_REPO_UPLOAD"]
 
 util.create_aliases(
     branch,
     old_branch,
+    fetch_repo,
     upload_repo,
 )
