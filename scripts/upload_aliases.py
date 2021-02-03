abd0056877b3cfefcc8e43fe273edec3fb120118
Niclas Overby <noverby@prozum.dk>
2021-02-03 16:52:15 +0100

    cicd: print debug info

--- scripts/upload_aliases.py
+++ scripts/upload_aliases.py
@@ -2,9 +2,12 @@ import util
 import os
 
 branch = os.environ["GITHUB_REF"].split("/")[2]
+print(f"Branch: {branch}")
 old_branch = "master"
 fetch_repo = os.environ["CONAN_REPO_ALL"]
+print(f"Fetching from: {fetch_repo}")
 upload_repo = os.environ["CONAN_REPO_UPLOAD"]
+print(f"Uploading to: {upload_repo}")
 
 util.create_aliases(
     branch,
