606dc19f0e7d320994e3ac0128b97a21e260c60e
Niclas Overby <noverby@prozum.dk>
2021-02-03 13:37:44 +0100

    scripts: properly fetch branch

--- scripts/upload_aliases.py
+++ scripts/upload_aliases.py
@@ -1,7 +1,7 @@
 import util
 import os
 
-branch = os.environ["GITHUB_REF"].split()[2]
+branch = os.environ["GITHUB_REF"].split("/")[2]
 old_branch = "master"
 upload_repo = os.environ["CONAN_REPO_UPLOAD"]
 
