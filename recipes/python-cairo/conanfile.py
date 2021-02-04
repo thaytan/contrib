09d94225bb6c2d4d4617b423b7b23cb13876227e
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2021-02-04 13:38:23 +0100

    python-cairo: Fix build

--- recipes/python-cairo/conanfile.py
+++ recipes/python-cairo/conanfile.py
@@ -4,11 +4,8 @@ from build import *
 class PythonCairoRecipe(Recipe):
     description = "Python bindings for the cairo graphics library"
     license = "LGPL"
-    settings = "build_type", "compiler", "arch_build", "os_build", "libc_build", "python"
-    build_requires = (
-        "cc/[^1.0.0]",
-        "pkgconf/[^1.6.3]",
-    )
+    settings = Recipe.settings + ("python",)
+    build_requires = ("cc/[^1.0.0]", "meson/[^0.55.3]")
     requires = ("cairo/[^1.16.0]",)
 
     def requirements(self):
