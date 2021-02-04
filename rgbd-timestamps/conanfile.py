e4eef8d049264d8d0632d393458fe8d91f71b25a
Niclas Overby <noverby@prozum.dk>
2021-02-04 21:59:28 +0100

    rgbd-timestamps: add rust-libstd req

--- rgbd-timestamps/conanfile.py
+++ rgbd-timestamps/conanfile.py
@@ -1,12 +1,12 @@
 from build import *
 
+
 class RgbdTimestamps(GstRustProject):
     description = "Definition of RGB-D enums for custom source elements that use video/rgbd CAPS"
     license = "LGPL"
-    build_requires = (
-        "rust/[^1.0.0]",
-    )
+    build_requires = ("rust/[^1.0.0]",)
     requires = (
         "capnproto/[^0.8.0]",
         f"gst-depth-meta/{branch()}",
+        "rust-libstd/[^1.0.0]",
     )
