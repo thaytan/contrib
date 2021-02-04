eadc368ef3b80e6adc6f2b7ff9b57903bcd67dc1
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2021-02-04 09:40:40 +0100

    nv-jetson-*: Build only for l4t profiles (TX2/TX1)

--- recipes/nv-jetson-compiled-source/conanfile.py
+++ recipes/nv-jetson-compiled-source/conanfile.py
@@ -10,12 +10,12 @@ from pathlib import Path
 download_tx2_url_sources = {
     "32.2.1": "https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/TX2-AGX/sources/public_sources.tbz2",
     "32.3.1": "https://developer.nvidia.com/embedded/dlc/r32-3-1_Release_v1.0/Sources/T186/public_sources.tbz2",
-    "32.4.3": "https://developer.nvidia.com/embedded/L4T/r32_Release_v4.3/sources/T186/public_sources.tbz2"
+    "32.4.3": "https://developer.nvidia.com/embedded/L4T/r32_Release_v4.3/sources/T186/public_sources.tbz2",
 }
 download_tx1_url_sources = {
     "32.2.1": "https://developer.nvidia.com/embedded/dlc/r32-2-1_Release_v1.0/Nano-TX1/sources/public_sources.tbz2",
     "32.3.1": "https://developer.nvidia.com/embedded/dlc/r32-3-1_Release_v1.0/Sources/T210/public_sources.tbz2",
-    "32.4.3": "https://developer.nvidia.com/embedded/L4T/r32_Release_v4.3/sources/T210/public_sources.tbz2"
+    "32.4.3": "https://developer.nvidia.com/embedded/L4T/r32_Release_v4.3/sources/T210/public_sources.tbz2",
 }
 
 
@@ -31,21 +31,14 @@ class NvJetsonCompiledSource(GstProject):
     version = tools.get_env("GIT_TAG", "32.4.3")
     license = "LGPL"
     description = "Compiled elements from NVIDIAs Jetson sources"
-    settings = {"os": None, "compiler": None, "build_type": None, "arch": "armv8",
-                "hardware": {"l4t": {"board", "version"}}, "gstreamer": None}
+    settings = {"os": None, "compiler": None, "build_type": None, "arch": "armv8", "hardware": {"l4t": {"board", "version"}}, "gstreamer": None}
     exports = ["*.patch"]
 
-    build_requires = (
-        "autotools/[^1.0.0]",
-    )
-    requires = (
-        "gst-plugins-base/[^1.18]",
-    )
-
+    build_requires = ("autotools/[^1.0.0]",)
+    requires = ("gst-plugins-base/[^1.18]",)
 
     def requirements(self):
         self.requires(f"nv-jetson-drivers/{self.settings.hardware.version}")
-        
 
     def source(self):
         if self.settings.hardware.board == "t186":
@@ -57,15 +50,12 @@ class NvJetsonCompiledSource(GstProject):
             raise KeyError("Unknown option board type")
 
         # Unpack the relevant sources
-        tools.untargz(
-            "Linux_for_Tegra/source/public/gstomx1_src.tbz2", self.source_folder)
+        tools.untargz("Linux_for_Tegra/source/public/gstomx1_src.tbz2", self.source_folder)
         tools.patch(patch_file="default-config.patch", base_path="gstomx1_src")
         tools.patch(patch_file="configure.ac.patch", base_path="gstomx1_src")
 
-        tools.untargz(
-            "Linux_for_Tegra/source/public/gstegl_src.tbz2", self.source_folder)
-        tools.untargz(
-            "Linux_for_Tegra/source/public/gst-nvvideo4linux2_src.tbz2", self.source_folder)
+        tools.untargz("Linux_for_Tegra/source/public/gstegl_src.tbz2", self.source_folder)
+        tools.untargz("Linux_for_Tegra/source/public/gst-nvvideo4linux2_src.tbz2", self.source_folder)
         tools.rmdir("Linux_for_Tegra")
 
     def build(self):
@@ -78,12 +68,9 @@ class NvJetsonCompiledSource(GstProject):
             self.autotools()
         pc_path_base = os.path.join(self.package_folder, "lib", "pkgconfig")
         pc_path_egl = os.path.join(pc_path_base, "gstreamer-egl-1.0.pc")
-        self.run(
-            'sed -i "s/Requires: .*/Requires: gstreamer-1.0 libglvnd x11/" %s' % pc_path_egl)
-        self.run(
-            'sed -i "s/Libs: .*/Libs: -L\${libdir} -lgstegl-1.0/" %s' % pc_path_egl)
-        self.run(
-            'sed -i "s/Cflags: .*/Cflags: -I\${includedir}/" %s' % pc_path_egl)
+        self.run('sed -i "s/Requires: .*/Requires: gstreamer-1.0 libglvnd x11/" %s' % pc_path_egl)
+        self.run('sed -i "s/Libs: .*/Libs: -L\${libdir} -lgstegl-1.0/" %s' % pc_path_egl)
+        self.run('sed -i "s/Cflags: .*/Cflags: -I\${includedir}/" %s' % pc_path_egl)
 
         env = {
             "NOCONFIGURE": "true",
@@ -91,7 +78,7 @@ class NvJetsonCompiledSource(GstProject):
             "PKG_CONFIG_PATH": os.environ["PKG_CONFIG_PATH"] + ":" + pc_path_base,
             "LIBRARY_PATH": os.environ["LIBRARY_PATH"] + ":" + os.path.join(self.package_folder, "lib") + ":" + os.path.join(self.build_folder, "usr/lib/aarch64-linux-gnu/tegra"),
             "CFLAGS": f" -I{self.build_folder} -Wno-error",
-            "ERROR_CFLAGS": ""
+            "ERROR_CFLAGS": "",
         }
         args = ["--with-omx-target=tegra"]
 
@@ -109,15 +96,12 @@ class NvJetsonCompiledSource(GstProject):
             # autotools.make()
             # autotools.install()
             self.autotools()
-            
+
     def package(self):
         lib_folder = os.path.join(self.package_folder, "lib")
 
-        self.copy("*.so*", src="usr", dst="lib", keep_path=False,
-                  symlinks=False, excludes=("*libgst*.so*"))
-        self.copy("*libgst*.so*", dst="lib/gstreamer-1.0",
-                  keep_path=False, symlinks=False, excludes=("*libgstomx.*"))
+        self.copy("*.so*", src="usr", dst="lib", keep_path=False, symlinks=False, excludes=("*libgst*.so*"))
+        self.copy("*libgst*.so*", dst="lib/gstreamer-1.0", keep_path=False, symlinks=False, excludes=("*libgstomx.*"))
 
     def package_info(self):
-        self.env_info.JETSON_DRIVER_PATH = os.path.join(
-            self.package_folder, "lib")
+        self.env_info.JETSON_DRIVER_PATH = os.path.join(self.package_folder, "lib")
