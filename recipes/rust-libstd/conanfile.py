182ad75e4692b281c2978b6a54229224b4ab5ffd
Niclas Overby <noverby@prozum.dk>
2021-02-04 21:57:44 +0100

    rust-libstd: add package

---
+++ recipes/rust-libstd/conanfile.py
@@ -0,0 +1,14 @@
+from build import *
+
+
+class RustLibstd(RustRecipe):
+    description = "Rust libstd"
+    license = "MIT"
+    keep_imports = True
+
+    def requirements(self):
+        # Rust is not compatible between major or minor releases
+        self.requires(f"rustc/[~{self.settings.rust}]", "private")
+
+    def imports(self):
+        self.copy("*libstd-*.so", excludes="*rustlib*")
