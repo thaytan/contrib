37281f21b23f30c066d9b0441d8603e0657650a5
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2021-02-25 14:05:03 +0100

    recipes/gst-devtools: Fix conanfile, requires port to 1.18

--- recipes/gst-devtools/conanfile.py
+++ recipes/gst-devtools/conanfile.py
@@ -1,7 +1,7 @@
 from build import *
 
 
-class GstDevtoolsRecipe(Recipe):
+class GstDevtoolsRecipe(GstRecipe):
     description = "Development and debugging tools for GStreamer"
     license = "LGPL"
     options = {
@@ -17,17 +17,18 @@ class GstDevtoolsRecipe(Recipe):
         "nls=False",
     )
     build_requires = (
-        "cc/[^1.0.0]",
-        "meson/[^0.51.2]",
+        "cc/[>=1.0.0]",
+        "meson/[>=0.51.2]",
+        "pkgconf/[^1.7.3]",
     )
     requires = (
-        "gst-plugins-base/[^1.16]",
-        "json-glib/[~1.4.4]",
+        "gst-plugins-base/[>=1.18]",
+        "json-glib/[>=1.6.2]",
     )
 
     def source(self):
-        git = tools.Git(folder="gst-devtools")
-        git.clone("https://gitlab.com/aivero/public/gstreamer/gst-devtools-mirror.git", "rebased-aivero_mse_compare_changes")
-
-    def build(self):
-        self.meson(folder="gst-devtools")
+        # if "1.18" in self.settings.gstreamer:
+        #     self.get(f"https://gitlab.freedesktop.org/gstreamer/gst-devtools/-/archive/{self.version}/gst-devtools-{self.version}.tar.bz2")
+        # else:
+        #     self.get(f"https://gitlab.com/aivero/public/gstreamer/gst-devtools-mirror/-/archive/{self.version}/gst-devtools-mirror-{self.version}.tar.gz")
+        self.get(f"https://gitlab.com/aivero/public/gstreamer/gst-devtools-mirror/-/archive/{self.version}/gst-devtools-mirror-{self.version}.tar.gz")
