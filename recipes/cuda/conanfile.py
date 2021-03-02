60de7cbbc166cf00e33ba651c020ad1606ec93c5
Raphael Dürscheid <1826220-raffael_santi@users.noreply.gitlab.com>
2021-03-02 15:09:03 +0100

    recipes/cuda: fix conan, add 11.2.1

--- recipes/cuda/conanfile.py
+++ recipes/cuda/conanfile.py
@@ -3,20 +3,21 @@ from build import *
 driver_map = {
     "10.1.243": "418.87.00",
     "11.1.0": "455.23",
+    "11.2.1": "460.32.03",
 }
 
 
 class CudaRecipe(Recipe):
     description = "NVIDIA's GPU programming toolkit"
     license = "Proprietary"
-    exports_sources = ("cuda-10.1.pc", "cudart-10.1.pc")
+    exports_sources = ("*.pc")
     build_requires = (
         "cc/[^1.0.0]",
-        "libxml2/[^2.9.10]",
+        "libxml2/[>=2.9.10]",
     )
 
     def source(self):
-        tools.download(f"http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_{self.version}_{driver_map[self.version]}_linux.run", filename=f"cuda_{self.version}_linux.run")
+        tools.download(f"https://developer.download.nvidia.com/compute/cuda/{self.version}/local_installers/cuda_{self.version}_{driver_map[self.version]}_linux.run", filename=f"cuda_{self.version}_linux.run")
 
     def build(self):
         self.run(f'sh cuda_{self.version}_linux.run  --silent --override --override-driver-check --extract="{self.build_folder}"')
@@ -24,8 +25,8 @@ class CudaRecipe(Recipe):
         os.remove(f"cuda_{self.version}_linux.run")
         self.run(f"sh NVIDIA-Linux-x86_64-{driver_map[self.version]}.run --extract-only")
         os.remove(f"NVIDIA-Linux-x86_64-{driver_map[self.version]}.run")
-        tools.rmdir("cublas")
-        tools.rmdir("cuba-samples")
+        self.run("rm -rf libcublas")
+        self.run("rm -rf cuda-samples")
 
     def package(self):
         for toolkit in ("cuda-toolkit", "cuda-toolkit/nvvm"):
@@ -36,6 +37,6 @@ class CudaRecipe(Recipe):
         self.copy("*libcuda.so*", dst="lib", keep_path=False, symlinks=True)
         self.copy("*libnvcuvid.so*", dst="lib", keep_path=False, symlinks=True)
         with tools.chdir(os.path.join(self.package_folder, "lib")):
-            os.symlink("libnvcuvid.so.418.87.00", "libnvcuvid.so.1")
+            os.symlink(f"libnvcuvid.so.{driver_map[self.version]}", "libnvcuvid.so.1")
             os.symlink("libnvcuvid.so.1", "libnvcuvid.so")
         self.copy(pattern="*.pc", dst="lib/pkgconfig")
