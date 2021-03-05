6da3ec277771ef436aba1361535e458a46e315a9
Niclas Overby <noverby@prozum.dk>
2021-03-05 07:48:03 +0100

    librealsense: enable cuda and python support

--- recipes/librealsense/conanfile.py
+++ recipes/librealsense/conanfile.py
@@ -1,16 +1,23 @@
 from build import *
 
 
-class LibRealsenseRecipe(Recipe):
+class LibRealsenseRecipe(PythonRecipe):
     description = "Intel RealSense SDK"
     license = "Apache"
     exports = "libusb-fix.patch", "pkgconfig-fix.patch"
+    options = {"cuda": [True, False], "python": [True, False]}
+    default_options = ("cuda=True", "python=True")
     build_requires = (
         "cc/[^1.0.0]",
         "cmake/[^3.18.4]",
-        # "cuda/[^10.1.243]",
     )
     requires = ("libusb/[^1.0.23]",)
+
+    def requirements(self):
+        if self.options.cuda:
+            self.requires("cuda/[^11.2.1]")
+        if self.options.python:
+            self.requires(f"python/[^3.8]")
 
     def source(self):
         self.get(f"https://github.com/IntelRealSense/librealsense/archive/v{self.version}.tar.gz")
@@ -19,7 +26,8 @@ class LibRealsenseRecipe(Recipe):
 
     def build(self):
         defs = {
-            # "BUILD_WITH_CUDA": self.options.cuda,
+            "BUILD_WITH_CUDA": self.options.cuda,
+            "BUILD_WITH_PYTHON": self.options.python,
             "BUILD_PYTHON_BINDINGS": False,
             "BUILD_EXAMPLES": False,
             "BUILD_GRAPHICAL_EXAMPLES": False,
