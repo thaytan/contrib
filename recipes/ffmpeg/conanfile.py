cbb6c0186f61aa9ba62bf1006304ea18affbeeea
Kunal Singh <kunal.singh@aivero.com>
2021-03-17 17:17:22 +0530

    ffmpeg: update conanfile.py

--- recipes/ffmpeg/conanfile.py
+++ recipes/ffmpeg/conanfile.py
@@ -4,6 +4,16 @@ from build import *
 class FFMpegRecipe(Recipe):
     description = "A complete, cross-platform solution to record, convert and stream audio and video"
     license = "LGPL"
+    settings = Recipe.settings + ("hardware",)
+
+    def configure(self):
+        if self.settings.hardware == "rpi":
+            if self.settings.arch == "armv8":
+                # we don't define OS version for rpi 64-bit OS
+                del self.settings.hardware.version
+                # enable ffmpeg
+                self.options.ffmpeg = True
+
     build_requires = (
         "cc/[^1.0.0]",
         "autotools/[^1.0.0]",
@@ -22,12 +32,13 @@ class FFMpegRecipe(Recipe):
         ]
 
         # intended for rpi 64-bit build, may need alternate implementation
-        if self.settings.arch == "armv8":
-            args += ["--enable-gpl", "--enable-nonfree", "--arch=aarch64"]
-            #args += ["--enable-libfreetype", "--enable-libopus"]
-            #args += ["--enable-libwebp", "--enable-libdrm"]
-            #args += ["--enable-libaom", "--enable-libass", "--enable-libxml2"]
-            #args += ["--enable-libfdk-aac", "--enable-libmp3lame", "--enable-libvorbis"]
-            #args += ["--enable-libx264", "--enable-libx265", "--enable-libvpx"]
+        if self.settings.hardware == "rpi":
+            if self.settings.arch == "armv8":
+                args += ["--enable-gpl", "--enable-nonfree", "--arch=aarch64"]
+                #args += ["--enable-libfreetype", "--enable-libopus"]
+                #args += ["--enable-libx264", "--enable-libx265", "--enable-libvpx"]
+                #args += ["--enable-libwebp", "--enable-libdrm"]
+                #args += ["--enable-libaom", "--enable-libass", "--enable-libxml2"]
+                #args += ["--enable-libfdk-aac", "--enable-libmp3lame", "--enable-libvorbis"]
 
         self.autotools(args)
