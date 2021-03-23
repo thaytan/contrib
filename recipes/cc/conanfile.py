8810f41b8f5ba76ab731c3c0699a7884c183304e
Niclas Overby <noverby@prozum.dk>
2021-03-23 20:27:27 +0100

    cc: include llvm headers

--- recipes/cc/conanfile.py
+++ recipes/cc/conanfile.py
@@ -14,10 +14,11 @@ class CCRecipe(Recipe):
         if self.settings.libc == "musl":
             static_flags = "-static"
         libc_inc = self.env["LIBC_INCLUDE_PATH"]
-        clang_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "lib", "clang", self.deps_cpp_info["llvm"].version, "include")
+        libclang_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "lib", "clang", self.deps_cpp_info["llvm"].version, "include")
+        llvm_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "include")
         libcxx_inc = os.path.join(self.deps_cpp_info["llvm"].rootpath, "include", "c++", "v1")
         # -Wno-unused-command-line-argument is needed for some sanity tests in cmake
-        cflags = f" -nostdinc -idirafter {clang_inc} -idirafter {libc_inc} {static_flags} -fPIC -flto=thin -Wno-unused-command-line-argument "
+        cflags = f" -nostdinc -idirafter {libclang_inc} -idirafter {libc_inc} -idirafter {llvm_inc} {static_flags} -fPIC -flto=thin -Wno-unused-command-line-argument "
         cxxflags = f" -nostdinc++ -idirafter {libcxx_inc} {cflags} "
 
         self.env_info.CFLAGS = cflags
