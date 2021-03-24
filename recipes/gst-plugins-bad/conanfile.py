0fcf0623badd555abea4ce1c9d861de08eb3f09f
Raphael Dürscheid <compiaffe@users.noreply.github.com>
2021-03-24 15:57:46 +0100

    gst-plugins-bad: Checkout newest commit on master, revert meson deps on 1.19 and failing builds

--- recipes/gst-plugins-bad/conanfile.py
+++ recipes/gst-plugins-bad/conanfile.py
@@ -88,17 +88,20 @@ class GstPluginsBadRecipe(GstRecipe):
 
         elif "1.18" in self.settings.gstreamer:
             git = tools.Git(folder=f"{self.name}-{self.version}.src")
-            git.clone("https://gitlab.freedesktop.org/GStreamer/gst-plugins-bad.git", self.version)
-            # Apply: webrtc: expose transport property on sender and receiver
-            # Not required for 1.20 onward
-            # https://gitlab.freedesktop.org/meh/gst-plugins-bad/-/commit/f89d48377091a844d995209eaac03c97b17d2651
-            git.run('-c user.email="cicd@civero.com" -c user.name="Chlorine Cadmium" am -3 ../f89d48377091a844d995209eaac03c97b17d2651.patch')
+            git.clone("https://gitlab.freedesktop.org/GStreamer/gst-plugins-bad.git", "master")
+            
+            # Pick a random cutoff date close to HEAD on origin/master and try to build
+            git.run("checkout 316ddddc160de4f1e5546ef1d70d21bce5459fea")
 
-            # Apply: webrtcbin: Accept end-of-candidate pass it to libnice
-            # Probably not required from 1.20 onward
-            # https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad/-/commit/825a79f01f58bdae0ff68d11bda22499a7d8ad6c?merge_request_iid=1139
-            git.run('-c user.email="cicd@civero.com" -c user.name="Chlorine Cadmium" am -3 ../825a79f01f58bdae0ff68d11bda22499a7d8ad6c.patch')
+            # Build it for 1.18 by undoing some 1.19 specifics:
+            # 9b082e7 undoes the requirement on gst 1.19
+            # 6adf7df Fails to build - webp: allow per feature registration 
+            # 4f16edf Fails to build - srtp: allow per feature registration 
+            # a216a1f Fails to build - dtls: allow per feature registration 
+            git.run('-c user.email="cicd@civero.com" -c user.name="Chlorine Cadmium" revert 9b082e7467797a6e1c5626a67f7ffc5d0248eccd 4f16edf0d07e5fd42221d5e3727c6d5aa548cdb7 6adf7dff71b2808e8b5fbef7bf45f1ae50ae1b34 a216a1f2cf84b66601524be347ac4b45a995b044 --no-edit ')
 
+        elif "1.20" in self.settings.gstreamer:
+            self.get(f"https://github.com/GStreamer/gst-plugins-bad/archive/{self.version}.tar.gz")
 
     def build(self):
         opts = {
@@ -121,4 +124,6 @@ class GstPluginsBadRecipe(GstRecipe):
         if self.settings.arch == "x86_64":
             opts["msdk"] = self.options.msdk
             opts["nvcodec"] = self.options.nvcodec
+
+
         self.meson(opts)
