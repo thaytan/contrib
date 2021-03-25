dbbe5560a84f3b2e06a7f5cd7141a38cfe6ae31f
Niclas Overby <noverby@prozum.dk>
2021-03-25 20:50:51 +0100

    file: set MAGIC env var

--- recipes/file/conanfile.py
+++ recipes/file/conanfile.py
@@ -7,3 +7,6 @@ class FileRecipe(Recipe):
 
     def source(self):
         self.get(f"http://astron.com/pub/file/file-{self.version}.tar.gz")
+
+    def package_info(self):
+        self.env_info.MAGIC = os.path.join(self.package_folder, "share", "misc", "magic.mgc")
