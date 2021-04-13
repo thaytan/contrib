3e68918c0ec409b2bfff3fea23c51f03efdad539
Niclas Overby <niclas@overby.me>
2021-04-13 14:46:51 +0200

    nasm: fix build

--- recipes/nasm/conanfile.py
+++ recipes/nasm/conanfile.py
@@ -1,15 +1,13 @@
 from build import *
 
 
-class (Recipe):
-    description = ""
-    license = ""
+class Nasm(Recipe):
+    description = "80x86 assembler designed for portability and modularity"
+    license = "BSD"
     requires = (
+        "cc/[^1.0.0]",
+        "autotools/[^1.0.0]"
     )
 
     def source(self):
-        self.get(f"")
-
-    def build(self):
-        args = []
-        self.make(args)
+        self.get(f"https://www.nasm.us/pub/nasm/releasebuilds/{self.version}/nasm-{self.version}.tar.xz")
