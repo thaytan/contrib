ab842fe8543b5daef04cd940523535beb6bc2866
Niclas Overby <niclas@overby.me>
2021-04-13 14:40:00 +0200

    nasm: add package

---
+++ recipes/nasm/conanfile.py
@@ -0,0 +1,15 @@
+from build import *
+
+
+class (Recipe):
+    description = ""
+    license = ""
+    requires = (
+    )
+
+    def source(self):
+        self.get(f"")
+
+    def build(self):
+        args = []
+        self.make(args)
