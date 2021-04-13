3b0fdd57e364e750e0b7070912d3d8d7c631a3a2
Niclas Overby <niclas@overby.me>
2021-04-13 16:11:13 +0200

    libelf: fetch newest config.guess

--- recipes/libelf/conanfile.py
+++ recipes/libelf/conanfile.py
@@ -11,3 +11,4 @@ class Libelf(Recipe):
 
     def source(self):
         self.get(f"https://fossies.org/linux/misc/old/libelf-{self.version}.tar.gz")
+        self.download(f"https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD", "config.guess")
