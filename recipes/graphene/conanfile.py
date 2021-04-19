ef8a0ccdd855aebde6ea7a5e84d6cc68db13d114
Niclas Overby <niclas@overby.me>
2021-04-19 14:42:57 +0200

    graphene: add package

---
+++ recipes/graphene/conanfile.py
@@ -0,0 +1,15 @@
+from build import *
+
+
+class Graphene(Recipe):
+    description = "A thin layer of graphic data types"
+    license = "MIT"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+        "gobject-introspection/[^1.66.1]",
+    )
+    requires = ("glib/[^2.66.1]",)
+
+    def source(self):
+        self.get(f"https://github.com/ebassi/graphene/archive/refs/tags/{self.version}.tar.gz")
