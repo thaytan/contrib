b46cc1bee1a23f0aeb02a8502870b61acf2a2eae
Niclas Overby <niclas@overby.me>
2021-04-19 15:08:24 +0200

    gulkan: add package

---
+++ recipes/gulkan/conanfile.py
@@ -0,0 +1,28 @@
+from build import *
+
+
+class Gulkan(Recipe):
+    description = "A GLib library for Vulkan abstraction"
+    license = "MIT"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[>=0.55.3]",
+    )
+    requires = (
+        "vulkan-icd-loader/[^1.2.172]",
+        "glslang/[^11.2.0]",
+        "gdk-pixbuf2/[^2.40.0]",
+        "graphene/[^1.10.6]",
+        "cairo/[^1.17.2]",
+    )
+
+    def source(self):
+        self.get(f"https://gitlab.freedesktop.org/xrdesktop/gulkan/-/archive/{self.version}/gulkan-{self.version}.tar.gz")
+
+    def build(self):
+        os.environ["CFLAGS"] += f" -I{os.path.join(self.deps_cpp_info['vulkan-headers'].rootpath, 'include')}"
+
+        opts = {
+            "examples": False,
+        }
+        self.meson(opts)
