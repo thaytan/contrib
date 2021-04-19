afd837daedc39cfbc1283ecb74d61c8e67aeddc4
Niclas Overby <niclas@overby.me>
2021-04-19 15:04:58 +0200

    libepoxy: fix build

--- recipes/libepoxy/conanfile.py
+++ recipes/libepoxy/conanfile.py
@@ -8,20 +8,19 @@ class LibepoxyRecipe(Recipe):
         "x11": [True, False],
     }
     default_options = ("x11=True",)
-    build_requires = ("cc/[^1.0.0]", "meson/[^0.55.3]")
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[^0.55.3]",
+    )
     requires = ("mesa/[^20.2.1]",)
-
-    def requirements(self):
-        if self.options.x11:
-            self.requires("libx11/[^1.6.8]")
 
     def source(self):
         self.get(f"https://github.com/anholt/libepoxy/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Dglx=yes",
-            "-Dtests=false",
-            f"-Dx11={self.options.x11}",
-        ]
-        self.meson(args)
+        opts = {
+            "glx": True,
+            "tests": False,
+            "x11": self.options.x11,
+        }
+        self.meson(opts)
