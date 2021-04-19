8dbcc4b3945d747ff864a68b964db69b0e5a1613
Niclas Overby <niclas@overby.me>
2021-04-19 14:26:42 +0200

    gtk-doc: add package

---
+++ recipes/gtk-doc/conanfile.py
@@ -0,0 +1,18 @@
+from build import *
+
+
+class GtkDoc(Recipe):
+    description = "Generate API documentation from comments added to C code,typically used to document the public API of GTK+ and GNOME libraries."
+    license = "GPL"
+    build_requires = (
+        "cc/[^1.0.0]",
+        "meson/[>=0.55.0]",
+        "itstool/[^2.0.6]",
+    )
+    requires = (
+        "python/[^3.8]",
+        "glib/[^2.66.1]",
+    )
+
+    def source(self):
+        self.get(f"https://github.com/GNOME/gtk-doc/archive/refs/tags/{self.version}.tar.gz")
