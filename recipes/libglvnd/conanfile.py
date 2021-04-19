1437e6113f8f8b77ff8e92ab8b8d3d615d7219ab
Niclas Overby <niclas@overby.me>
2021-04-19 14:21:46 +0200

    libglvnd: enable glx

--- recipes/libglvnd/conanfile.py
+++ recipes/libglvnd/conanfile.py
@@ -15,10 +15,19 @@ class LibglvndRecipe(Recipe):
 
     def requirements(self):
         if self.options.x11:
+            self.requires("libxext/[^1.3.4]", "private")
+            self.requires("libx11/[^1.6.12]", "private")
             self.requires("xorgproto/[^2020.1]", "private")
 
     def source(self):
         self.get(f"https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/v{self.version}/libglvnd-v{self.version}.tar.gz")
+
+    def build(self):
+        opts = {
+            "x11": self.options.x11,
+            "glx": self.options.x11,
+        }
+        self.meson(opts)
 
     def package_info(self):
         self.env_info.__EGL_VENDOR_LIBRARY_DIRS.append("/usr/share/glvnd/egl_vendor.d")
