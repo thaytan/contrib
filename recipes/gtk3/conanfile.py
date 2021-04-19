0f3b657ab0bf5b047f7afaa7afa033c34667c223
Niclas Overby <niclas@overby.me>
2021-04-19 15:07:40 +0200

    gtk3: fix build

--- recipes/gtk3/conanfile.py
+++ recipes/gtk3/conanfile.py
@@ -5,40 +5,48 @@ class Gtk3Recipe(Recipe):
     description = "GObject-based multi-platform GUI toolkit"
     license = "LGPL"
     options = {
+        "shared": [True, False],
         "introspection": [True, False],
         "x11": [True, False],
     }
     default_options = (
+        "shared=True",
         "introspection=True",
         "x11=True",
     )
     build_requires = (
         "cc/[^1.0.0]",
-        "meson/[^0.55.3]",
+        "meson/[>=0.55.3]",
         "gettext/[^0.21]",
     )
     requires = (
         "libepoxy/[^1.5.4]",
         "at-spi2-atk/[^2.38.0]",
-        "gdk-pixbuf/[^2.40.0]",
-        "pango/[^1.43.0]",
+        "gdk-pixbuf2/[^2.40.0]",
     )
 
     def build_requirements(self):
         if self.options.introspection:
-            self.build_requires("gobject-introspection/[^1.59.3]",)
+            self.build_requires(
+                "gobject-introspection/[^1.59.3]",
+            )
 
     def requirements(self):
         if self.options.x11:
             self.requires("libxkbcommon/[^1.0.1]")
-            self.requires("libxrandr/[^1.5.2]")
             self.requires("libxi/[^1.7.1]")
 
     def source(self):
         self.get(f"https://github.com/GNOME/gtk/archive/{self.version}.tar.gz")
 
     def build(self):
-        args = [
-            "-Dwayland_backend=false",
-        ]
-        self.meson(args)
+        opts = {
+            "wayland_backend": False,
+            "tracker3": False,
+            "color": True,
+            "gtk_doc": False,
+            "man": False,
+        }
+
+        # Meson cannot properly check options for unknown reasons
+        self.meson(opts, opt_check=False)
