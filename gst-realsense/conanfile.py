d4ea65cc2b883a6b6925a72e13e814ca9b0ffeec
Raphael Dürscheid <compiaffe@users.noreply.github.com>
2021-05-18 12:46:40 +0200

    gst-realsense: fix glib dependency

--- gst-realsense/conanfile.py
+++ gst-realsense/conanfile.py
@@ -4,9 +4,10 @@ from build import *
 class GstRealsense(GstRustProject):
     description = "GStreamer plugin containing `video/rgbd` source for a RealSense device"
     license = "LGPL"
-    build_requires = ("rust/[^1.0.0]","capnproto/[^0.8]")
+    build_requires = ("rust/[^1.0.0]", "capnproto/[^0.8]")
     requires = (
-        "librealsense/[^2.39.0]"
+        "librealsense/[^2.39.0]",
+        f"gst-depth-meta/{branch()}",
     )
 
     def build(self):
