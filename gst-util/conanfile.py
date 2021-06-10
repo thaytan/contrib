922341bf39fd93d0562c0050e3f516674981df4a
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-06-10 09:49:42 +0200

    gst-util requires rust to build

--- gst-util/conanfile.py
+++ gst-util/conanfile.py
@@ -4,5 +4,5 @@ from build import *
 class GstUtil(GstRustProject):
     description = "Utility library for making it a bit easier to work with gstreamer from rust"
     license = "LGPL"
-    build_requires = ()
+    build_requires = ("rust/[^1.0.0]")
     requires = ()
