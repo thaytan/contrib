47618af5c26c2951cc754a0c0e384f6e7c875b27
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-07-16 16:55:27 +0000

    xorgproto: Change meson version

--- recipes/xorgproto/conanfile.py
+++ recipes/xorgproto/conanfile.py
@@ -5,9 +5,11 @@ class XorgProtoRecipe(Recipe):
     description = "combined X.Org X11 Protocol headers"
     license = "custom"
     build_requires = (
-        "meson/[^0.55.3]",
+        "meson/[>=0.55.3]",
         "xorg-util-macros/[^1.19.1]",
     )
 
     def source(self):
-        self.get(f"https://xorg.freedesktop.org/archive/individual/proto/xorgproto-{self.version}.tar.bz2")
+        self.get(
+            f"https://xorg.freedesktop.org/archive/individual/proto/xorgproto-{self.version}.tar.bz2"
+        )
