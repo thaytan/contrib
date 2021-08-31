1d202bcff220ae975b34939da8036f22b5b7d178
Niclas Overby <niclas@overby.me>
2021-08-31 13:55:00 +0200

    python-pip: bump version

--- recipes/python-pip/conanfile.py
+++ recipes/python-pip/conanfile.py
@@ -1,14 +1,13 @@
 from build import *
 
 
-class PythonPipRecipe(Recipe):
+class PythonPipRecipe(PythonRecipe):
     description = "High productivity build system"
     license = "MIT"
-    settings = "build_type", "compiler",    "python"
     build_requires = ("python-setuptools/[>=41.2.0]",)
 
     def requirements(self):
-        self.requires(f"python/[^3]")
+        self.requires(f"python/[~{self.settings.python}]")
 
     def source(self):
         self.get(f"https://github.com/pypa/pip/archive/{self.version}.tar.gz")
