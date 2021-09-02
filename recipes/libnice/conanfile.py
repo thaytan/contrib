512db68bf3243449681df9a70ae6265cce176e02
Raphael Dürscheid <compiaffe@users.noreply.github.com>
2021-09-02 13:45:19 +0200

    libnice: Use gitlab source

--- recipes/libnice/conanfile.py
+++ recipes/libnice/conanfile.py
@@ -8,18 +8,21 @@ class LibNiceRecipe(GstRecipe):
         "cc/[^1.0.0]",
         "meson/[>=0.55.3]",
     )
-    requires = (
-        "openssl1/[>=1.1.1h]",
-        "gst-plugins-base/[^1.18]",
-    )
+    requires = ("openssl1/[>=1.1.1h]",)
+
+    def requirements(self):
+        # This will SemVer match PATH changes, but not MINOR or MAJOR changes
+        # That way we can still build for a lower gst minor release (i.e. 1.18), despite a newer one being in your conan (i.e. 1.19)
+        # [^1.18] will match any `1.` version - not what we need
+        self.requires(f"gst-plugins-base/[~{self.settings.gstreamer}]")
+
     exports_sources = "nice.pc"
 
     def source(self):
-        self.get(
-            f"https://gitlab.freedesktop.org/libnice/libnice/-/archive/{self.version}/libnice-{self.version}.tar.gz"
-        )
+        self.get(f"https://gitlab.freedesktop.org/libnice/libnice/-/archive/{self.version}/libnice-{self.version}.tar.gz")
 
     def package(self):
+        tools.replace_prefix_in_pc_file("nice.pc", self.package_folder)
         self.copy("nice.pc", dst="lib/pkgconfig")
 
     def build(self):
