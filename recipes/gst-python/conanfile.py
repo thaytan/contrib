dddd1d0d8b101d42d21f20f358b5f970ec387767
Raphael Dürscheid <compiaffe@users.noreply.github.com>
2021-09-02 18:39:06 +0200

    gst-python: Fix gst deps

--- recipes/gst-python/conanfile.py
+++ recipes/gst-python/conanfile.py
@@ -1,14 +1,14 @@
 from build import *
 
 
-class GstPythonRecipe(Recipe):
+class GstPythonRecipe(GstRecipe):
     description = "Gstreamer Python bindings"
     license = "LGPL"
     requires = (
-        "gstreamer/[~1.16]",
-        "gobject-introspection/1.66.1",
-        "python-gobject/3.33.1",
+        "gobject-introspection/^1.66.1",
+        "python-gobject/^3.33.1",
     )
-
+    def requirements(self):
+        self.requires(f"gstreamer/[~{self.settings.gstreamer}]")
     def source(self):
         self.get(f"https://github.com/GStreamer/gst-python/archive/{self.version}.tar.gz")
