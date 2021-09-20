3042925f1d073bf078e6cce52b81fa30ed2573a2
Niclas Overby <niclas@overby.me>
2021-09-20 16:07:45 +0200

    python-virtualenv: fix build

--- recipes/python-virtualenv/conanfile.py
+++ recipes/python-virtualenv/conanfile.py
@@ -5,7 +5,6 @@ class PythonVirtualenvRecipe(PythonRecipe):
     description = "Virtual Python Environment builder"
     license = "MIT"
     requires = (
-        "python-setuptools/[>=41.2.0]",
         "python-appdirs/[^1.4.4]",
         "python-distlib/[^0.3.0]",
         "python-filelock/[^3.0.12]",
