27fc718a24360b910156c2b98d4dae78bb436939
Niclas Overby <niclas@overby.me>
2021-09-21 07:05:28 +0200

    python-zipp: add python-setuptools req

--- recipes/python-zipp/conanfile.py
+++ recipes/python-zipp/conanfile.py
@@ -4,6 +4,7 @@ from build import *
 class PythonZippRecipe(PythonRecipe):
     description = "Pathlib-compatible object wrapper for zip files"
     license = "MIT"
+    requires = ("python-setuptools/[^50.3.2]",)
 
     def source(self):
         self.get(f"https://github.com/jaraco/zipp/archive/v{self.version}.tar.gz")
