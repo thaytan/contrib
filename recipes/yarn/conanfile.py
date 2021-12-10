4556c502c7a8ae21b0c8af1719514d45d803f117
Niclas Overby <niclas@overby.me>
2021-12-10 16:14:52 +0100

    nodejs: use correct version range

--- recipes/yarn/conanfile.py
+++ recipes/yarn/conanfile.py
@@ -4,8 +4,8 @@ from build import *
 class YarnRecipe(Recipe):
     description = "Fast, reliable, and secure dependency management"
     license = "BSD"
-    build_requires = ("npm/[^7.0.5]",)
-    requires = ("nodejs/[^15.1.0]",)
+    build_requires = ("npm/[>=7.0.5]",)
+    requires = ("nodejs/[>=16.6.1]",)
 
     def source(self):
         self.get(f"https://github.com/yarnpkg/yarn/releases/download/v{self.version}/yarn-v{self.version}.tar.gz")
