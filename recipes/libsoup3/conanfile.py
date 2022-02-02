6203b38f0bc27f0fdac5917cc892730d685a5ef3
Niclas Overby <niclas@overby.me>
2022-02-02 20:25:11 +0100

    libsoup3: add recipe

--- recipes/libsoup3/conanfile.py
+++ recipes/libsoup3/conanfile.py
@@ -6,29 +6,13 @@ class Libsoup3(Recipe):
     license = "LGPL"
     build_requires = (
         "cc/[^1.0.0]",
-        "gobject-introspection/[^1.69.0]",
-        #"python/[^3.10.2]",
-        #"vala/[^0.54.6]",
-        #"git/[^2.35.0]",
-        #"gtk-doc/[^1.33.2]",
+        "gobject-introspection/[^1.70.0]",
         "meson/[>=0.57.2]",
-        #"samba/[^4.15.4]",
-        #"python-quart/[^0.16.2]",
+        "vala/[^0.55.1]"
     )
     requires = (
-        "glib/[^2.68.4]",
-        #"glib-networking/[^2.70.1]",
-        #"sqlite/[^3.37.2]",
-        #"krb5/[^1.19.2]",
-        #"libpsl/[^0.21.1]",
-        #"brotli/[^1.0.9]",
-        #"libnghttp2/[^1.46.0]",
+        "glib/[^2.70.3]",
     )
 
     def source(self):
         self.get(f"https://gitlab.gnome.org/GNOME/libsoup/-/archive/{self.version}/libsoup-{self.version}.tar.gz")
-
-    
-    def build(self):
-        self.run("meson introspect --buildoptions libsoup3-3.0.4.src/meson.build")
-        #self.meson()
