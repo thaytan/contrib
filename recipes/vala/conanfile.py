d77f5f103fa5bf8405817814e78dcb99e0a6a072
Niclas Overby <niclas@overby.me>
2022-02-02 17:22:49 +0100

    vala: add recipe

--- recipes/vala/conanfile.py
+++ recipes/vala/conanfile.py
@@ -6,12 +6,22 @@ class Vala(Recipe):
     license = "LGPL"
     build_requires = (
         "libxslt/[^1.1.34]",
+        "git/[^2.34.1]",
     )
     requires = (
         "autotools/[^1.0.0]",
+        "autoconf-archive/[^2019.01.06]",
+        "gobject-introspection/[^1.66.1]",
         "cc/[^1.0.0]",
-        "glib/[^2.68.4]",
+        "graphviz/[^2.42.1]",
     )
 
     def source(self):
-        self.get(f"https://gitlab.gnome.org/GNOME/vala/-/archive/{self.version}/vala-{self.version}.zip")
+        git = tools.Git(folder=self.src)
+        git.clone("https://gitlab.gnome.org/GNOME/vala.git", self.version)
+
+    def build(self):
+        os.environ["CPATH"] += ":" + self.deps_cpp_info["graphviz"].include_paths[0]
+        self.exe(f"sh autogen.sh --prefix={self.package_folder}")
+        self.exe("make")
+        self.exe("make install")
