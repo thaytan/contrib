30fd8819c483c62c8f1a8311ec4d2354223ea3b7
Niclas Overby <niclas@overby.me>
2022-02-02 19:40:01 +0100

    gobject-introspection: bump and fix build with new meson

--- recipes/gobject-introspection/conanfile.py
+++ recipes/gobject-introspection/conanfile.py
@@ -1,7 +1,7 @@
 from build import *
 
 
-class GObjectIntrospectionRecipe(PythonRecipe):
+class GObjectIntrospection(PythonRecipe):
     description = "Middleware layer between C libraries (using GObject) and language bindings"
     license = "LGPL"
     build_requires = (
@@ -10,13 +10,16 @@ class GObjectIntrospectionRecipe(PythonRecipe):
         "bison/[^3.7.2]",
         "flex/[^2.6.4]",
     )
-    requires = ("glib/[^2.66.1]",)
+    requires = ("glib/[^2.70.3]",)
 
     def requirements(self):
         self.requires(f"python/[~{self.settings.python}]")
 
     def source(self):
-        self.get(f"https://github.com/GNOME/gobject-introspection/archive/{self.version}.tar.gz")
+        # Workaround: https://gitlab.gnome.org/GNOME/gobject-introspection/-/issues/414
+        # Upgrade to 1.71.0 when available
+        version = "effb1e09dee263cdac4ec593e8caf316e6f01fe2"
+        self.get(f"https://github.com/GNOME/gobject-introspection/archive/{version}.tar.gz")
 
     def package_info(self):
         self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "lib", "gobject-introspection"))
