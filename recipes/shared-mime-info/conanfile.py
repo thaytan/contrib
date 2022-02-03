de76573c7fab33dd8e2b54da74ac7d674fb48b2d
Niclas Overby <niclas@overby.me>
2022-02-03 07:47:11 +0100

    shared-mime-info: fix build with new meson

--- recipes/shared-mime-info/conanfile.py
+++ recipes/shared-mime-info/conanfile.py
@@ -18,7 +18,10 @@ class SharedMimeInfo(Recipe):
     )
 
     def source(self):
-        self.get(f"https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/{self.version}/shared-mime-info-{self.version}.tar.gz")
+        # Fixes build with new meson
+        # TODO: upgrade to new stable release with fix
+        version = "3c064c850488ede673cc87dfd5da29d36384b8f4"
+        self.get(f"https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/{version}/shared-mime-info-{version}.tar.gz")
         tools.replace_in_file(os.path.join(self.src, "data", "meson.build"), "build_by_default: true", "build_by_default: false")
 
     def build(self):
